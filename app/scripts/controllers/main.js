b9bd114c408848b80cb686899b2eeaddbb2db1f6
Christian Idylle <cidylle@cisco.com>
2014-02-12 17:24:00 -0800

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('wx2AdminWebClientApp')
+  .controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  }]);
