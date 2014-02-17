93ae8bca3fd5d1234fe273a4198a3c2ee0294bcd
Christian Idylle <cidylle@cisco.com>
2014-02-17 12:50:14 -0800

    Small fix for login view.

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,13 +1,13 @@
 'use strict';
 
 angular.module('wx2AdminWebClientApp')
-  .controller('LoginCtrl', ['$scope', '$location', '$http', 'Storage',
-    function($scope, $location, $http, Storage) {
+  .controller('LoginCtrl', ['$scope', '$location', '$http', 'Storage', 'Config',
+    function($scope, $location, $http, Storage, Config) {
 
       var token = Storage.get('accessToken');
       $scope.token = token ? true : false;
 
-      var authorizeUrl = 'http://localhost:8080/atlas-server/atlas/api/v1/authorize';
+      var authorizeUrl = Config.adminUrl + 'authorize';
       $scope.oauth2LoginUrl = 'https://idbrokerbeta.webex.com/idb/oauth2/v1/authorize?response_type=token&client_id=C96d389d632c96d038d8f404c35904b5108988bd6d601d4b47f4eec88a569d5db&scope=webexsquare%3Aget_conversation&redirect_uri=http%3A%2F%2F127.0.0.1%3A8000&realm=1eb65fdf-9643-417f-9974-ad72cae0e10f&state=random-string';
 
       if (token) {
