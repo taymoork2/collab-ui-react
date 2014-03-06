3686df0634974b1bffa02397c9ccb29f302c9e73
Christian Idylle <cidylle@cisco.com>
2014-03-06 15:21:28 -0800

    Login-flow integration test.

--- test/e2e-protractor/login_spec.js
+++ test/e2e-protractor/login_spec.js
@@ -1,50 +1,44 @@
-// 'use strict';
+'use strict';
 
-// /* global describe */
-// /* global it */
-// /* global browser */
-// /* global by */
-// /* global element */
-// /* global expect */
+/* global describe */
+/* global it */
+/* global browser */
+/* global by */
+/* global expect */
 
-// var testuser = {
-//   username: 'adminTestUser@wx2.example.com',
-//   password: 'C1sc0123!'
-// };
+var testuser = {
+  username: 'adminTestUser@wx2.example.com',
+  password: 'C1sc0123!'
+};
 
-// describe('Login flow', function() {
 
-//   it('should redirect to CI global login page.', function() {
-//     browser.get('#/login');
-//     browser.waitForAngular();
-//     browser.driver.wait(function() {
-//       return browser.driver.isElementPresent(by.css('#IDToken1'));
-//     }).then(function() {
-//       expect(browser.driver.getCurrentUrl()).toContain('idbrokerbeta.webex.com');
-//     });
-//   });
 
-//   it('should log in with valid credentials and display users page', function() {
-//     browser.get('#/login');
-//     browser.waitForAngular();
+describe('Login flow', function() {
 
-//     browser.driver.wait(function() {
-//       return browser.driver.isElementPresent(by.css('#IDToken1'));
-//     }).then(function() {
-//       browser.driver.findElement(by.css('#IDToken1')).sendKeys(testuser.username);
-//       browser.driver.findElement(by.css('#IDButton2')).click();
-//     });
+  beforeEach(function() {
+    //Setup before each test is run.
+  });
 
-//     browser.driver.wait(function() {
-//       return browser.driver.isElementPresent(by.css('#IDToken2'));
-//     }).then(function() {
-//       browser.driver.findElement(by.css('#IDToken2')).sendKeys(testuser.password);
-//       browser.driver.findElement(by.css('#Button1')).click();
-//     });
+  afterEach(function() {
+    //Cleanup after each test is run.
+  });
 
-//     browser.waitForAngular();
-//     expect(browser.driver.getCurrentUrl()).toContain('/users');
-//     expect(browser.driver.findElement(by.css('h2')).getText()).toContain('User Accounts');
-//   });
+  it('should redirect to CI global login page.', function() {
+    browser.get('#/login');
+    browser.sleep(5000);
+    expect(browser.driver.getCurrentUrl()).toContain('idbrokerbeta.webex.com');
+  });
 
-// });
+  it('should log in with valid credentials and display users page', function() {
+    browser.get('#/login');
+    browser.driver.findElement(by.css('#IDToken1')).sendKeys(testuser.username);
+    browser.driver.findElement(by.css('#IDButton2')).click();
+    browser.sleep(2000);
+    browser.driver.findElement(by.css('#IDToken2')).sendKeys(testuser.password);
+    browser.driver.findElement(by.css('#Button1')).click();
+    browser.sleep(2000);
+    expect(browser.driver.getCurrentUrl()).toContain('/users');
+    expect(browser.driver.findElement(by.css('h2')).getText()).toContain('User Accounts');
+  });
+
+});
