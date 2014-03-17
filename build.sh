0680c2b2a836b48bec52642488bbe5210ae19bc8
Christian Idylle <cidylle@cisco.com>
2014-03-17 13:45:03 -0700

    Temporary fix for css dependency linking issue.

--- build.sh
+++ build.sh
@@ -15,9 +15,6 @@ else
     BUILD_NUMBER=0
 fi
 
-# Temporary fix of the linking
-sed -i '' 's/#fixtokenfield/bower_components\/bootstrap-tokenfield\/dist\/css\/bootstrap-tokenfield.css/g' app/index.html
-
 # Package
 rm -f wx2-admin-web-client.*.tar.gz
 tar -zcvf wx2-admin-web-client.$BUILD_NUMBER.tar.gz dist/*
