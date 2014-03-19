b3ffaaa40ea87bd2f19cf7bccdf8eef5bdb770fa
sandralo <sandralo@cisco.com>
2014-03-19 01:51:23 -0700

    entitle user integration tests

--- test/e2e-protractor/entitle_user_spec.js
+++ test/e2e-protractor/entitle_user_spec.js
@@ -22,10 +22,10 @@ function randomId() {
 // - State is conversed between each describe and it blocks.
 // - When a page is being loaded, use wait() to check if elements are there before asserting.
 
-describe('App flow', function() {
+describe('Entitle flow', function() {
 
   // Logging in. Write your tests after the login flow is complete.
-  describe('Login flow', function() {
+  describe('Login as sso admin user', function() {
 
     it('should redirect to CI global login page.', function() {
       browser.get('#/login');
@@ -36,21 +36,9 @@ describe('App flow', function() {
       });
     });
 
-    it('should not log in with invalid credentials', function() {
-      browser.driver.findElement(by.css('#IDToken1')).sendKeys(testuser.username);
+    it('should log in with valid sso admin user and display users page', function() {
+      browser.driver.findElement(by.css('#IDToken1')).sendKeys(testuser.ssousername);
       browser.driver.findElement(by.css('#IDButton2')).click();
-      browser.driver.wait(function() {
-        return browser.driver.isElementPresent(by.css('#IDToken2'));
-      }).then(function() {
-        browser.driver.findElement(by.css('#IDToken2')).sendKeys('fakePassword');
-        browser.driver.findElement(by.css('#Button1')).click();
-      });
-      expect(browser.driver.findElement(by.css('.generic-error')).getText()).toBe('You\'ve entered an incorrect username or password.');
-      expect(browser.driver.getCurrentUrl()).toContain('idbrokerbeta.webex.com');
-    });
-
-    it('should log in with valid credentials and display users page', function() {
-
       browser.driver.wait(function() {
         return browser.driver.isElementPresent(by.css('#IDToken2'));
       }).then(function() {
@@ -70,57 +58,57 @@ describe('App flow', function() {
       expect(browser.driver.isElementPresent(by.css('#logout-btn'))).toBe(true);
     });
 
-    it('should display the username and the orgname', function() {
-      expect(element(by.binding('username')).getText()).toContain(testuser.username);
+    it('should display the sso username and the orgname', function() {
+      expect(element(by.binding('username')).getText()).toContain(testuser.ssousername);
       expect(element(by.binding('orgname')).getText()).toContain(testuser.orgname);
     });
 
   });
 
-  // Add User Flows: state is in the users page
-  describe('Add User Flows', function() {
+  // Entitle User Flows: state is in the users page
+  describe('Entitle User Flows', function() {
 
     describe('Page initialization', function() {
 
-      it('should initialize users page for adding users', function() {
-        expect(element(by.id('subTitleAdd')).isDisplayed()).toBe(true);
-        expect(element(by.id('subTitleEnable')).isDisplayed()).toBe(false);
-        expect(element(by.id('subTitleAdd')).getText()).toBe('Add new users');
-        expect(element(by.id('btnAdd')).isDisplayed()).toBe(true);
-        expect(element(by.id('btnEntitle')).isDisplayed()).toBe(false);
+      it('should initialize users page for entitling users', function() {
+        expect(element(by.id('subTitleAdd')).isDisplayed()).toBe(false);
+        expect(element(by.id('subTitleEnable')).isDisplayed()).toBe(true);
+        expect(element(by.id('subTitleEnable')).getText()).toBe('Entitle users');
+        expect(element(by.id('btnAdd')).isDisplayed()).toBe(false);
+        expect(element(by.id('btnEntitle')).isDisplayed()).toBe(true);
       });
 
       it('should display error if no user is entered', function() {
-        element(by.id('btnAdd')).click();
+        element(by.id('btnEntitle')).click();
         var result = element(by.repeater('userResult in results.resultList').row(0));
         expect(result.getText()).toBe('Please enter valid user email(s).');
       });
 
     });
 
-    describe('Add an existing user', function() {
-      it('should display input user email in results with already exists message', function() {
+    describe('Entitle an existing user', function() {
+      it('should display input user email in results with success message', function() {
         element(by.id('usersfield')).clear();
         element(by.id('usersfield')).sendKeys(testuser.username);
-        element(by.id('btnAdd')).click();
+        element(by.id('btnEntitle')).click();
         element.all(by.repeater('userResult in results.resultList')).then(function(rows) {
           expect(rows.length).toBe(1);
           expect(rows[0].getText()).toContain(testuser.username);
-          expect(rows[0].getText()).toContain('already exists');
+          expect(rows[0].getText()).toContain('entitled successfully');
         });
       });
     });
 
-    describe('Add a new user', function() {
-      it('should display input user email in results with success message', function() {
+    describe('Entitle non-existing user', function() {
+      it('should display input user email in results with error message', function() {
         var inputEmail = randomId() + '@example.com';
         element(by.id('usersfield')).clear();
         element(by.id('usersfield')).sendKeys(inputEmail);
-        element(by.id('btnAdd')).click();
+        element(by.id('btnEntitle')).click();
         element.all(by.repeater('userResult in results.resultList')).then(function(rows) {
           expect(rows.length).toBe(1);
           expect(rows[0].getText()).toContain(inputEmail);
-          expect(rows[0].getText()).toContain('added successfully');
+          expect(rows[0].getText()).toContain('does not exist');
         });
       });
     });
