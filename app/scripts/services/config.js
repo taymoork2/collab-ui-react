de4ed4fbecdeb677ea64815e934ca9173eb8ea0c
Raji Kunjal <rkunjal@cisco.com>
2014-04-04 16:55:58 -0700

    Pointing the admin portal UI to CI PROD environment @rev sandralo

--- app/scripts/services/config.js
+++ app/scripts/services/config.js
@@ -8,13 +8,13 @@ angular.module('wx2AdminWebClientApp')
       //adminServiceUrl: 'https://atlas-integration.wbx2.com/admin/api/v1/',
       //adminServiceUrl: 'http://localhost:8080/atlas-server/admin/api/v1/',
       defaultOrgId: '584cf4cd-eea7-4c8c-83ee-67d88fc6eab5',
-      oauth2LoginUrlDev: 'https://idbrokerbeta.webex.com/idb/oauth2/v1/authorize?response_type=token&client_id=C80fb9c7096bd8474627317ee1d7a817eff372ca9c9cee3ce43c3ea3e8d1511ec&scope=webexsquare%3Aadmin%20Identity%3ASCIM%20Identity%3AConfig%20Identity%3AOrganization&redirect_uri=http%3A%2F%2F127.0.0.1%3A8000&state=random-string&service=webex-squared',
-      oauth2LoginUrlProd: 'https://idbrokerbeta.webex.com/idb/oauth2/v1/authorize?response_type=token&client_id=C80fb9c7096bd8474627317ee1d7a817eff372ca9c9cee3ce43c3ea3e8d1511ec&scope=webexsquare%3Aadmin%20Identity%3ASCIM%20Identity%3AConfig%20Identity%3AOrganization&redirect_uri=https%3A%2F%2Fadmin.wbx2.com%2F&state=random-string&service=webex-squared',
-      //logoutUrl: 'https://idbrokerbeta.webex.com/idb/UI/Logout?goto=',
-      logoutUrl: 'https://idbrokerbeta.webex.com/idb/saml2/jsp/doSSO.jsp?type=logout&service=webex-squared&goto=',
+      oauth2LoginUrlDev: 'https://idbroker.webex.com/idb/oauth2/v1/authorize?response_type=token&client_id=C80fb9c7096bd8474627317ee1d7a817eff372ca9c9cee3ce43c3ea3e8d1511ec&scope=webexsquare%3Aadmin%20Identity%3ASCIM%20Identity%3AConfig%20Identity%3AOrganization&redirect_uri=http%3A%2F%2F127.0.0.1%3A8000&state=random-string&service=webex-squared',
+      oauth2LoginUrlProd: 'https://idbroker.webex.com/idb/oauth2/v1/authorize?response_type=token&client_id=C80fb9c7096bd8474627317ee1d7a817eff372ca9c9cee3ce43c3ea3e8d1511ec&scope=webexsquare%3Aadmin%20Identity%3ASCIM%20Identity%3AConfig%20Identity%3AOrganization&redirect_uri=https%3A%2F%2Fadmin.wbx2.com%2F&state=random-string&service=webex-squared',
+      //logoutUrl: 'https://idbroker.webex.com/idb/UI/Logout?goto=',
+      logoutUrl: 'https://idbroker.webex.com/idb/saml2/jsp/doSSO.jsp?type=logout&service=webex-squared&goto=',
       conversationUrl: 'https://conv-a.wbx2.com/conversation/api/v1',
       locusUrl: 'https://locus-a.wbx2.com/locus/api/v1',
-      scimUrl: 'https://idcsbeta.webex.com/identity/scim/%s/v1/Users',
+      scimUrl: 'https://identity.webex.com/identity/scim/%s/v1/Users',
       refreshTokenInterval: 30 * 1000, //in seconds. Be sure (refreshTokenInterval < expires_in)
 
       supportEmailAddress: 'wx2support@external.cisco.com',
