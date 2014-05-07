458a67e73b1f290d9ccbff2ef40fb4c3c2b488a0
nkamboh <nkamboh@NKAMBOH-M-603U.CISCO.COM>
2014-05-07 16:11:40 -0700

    US3388 - stub for user profile

---
+++ app/scripts/controllers/userprofile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('wx2AdminWebClientApp')
+	.controller('UserProfileCtrl', ['$scope', '$location', 'Log', 'Utils', '$filter',
+		function($scope, $location, Log, Utils, $filter) {
+
+
+		}
+	]);
