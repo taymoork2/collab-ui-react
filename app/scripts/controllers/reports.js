48df5350d5d571aae52cb7d8572b6d2c8ccfe513
Rajashree Kamath <rajashka@cisco.com>
2014-07-03 12:37:07 -0700

    US4455: Remove 1:1 call stats in report page @sandralo

--- app/scripts/controllers/reports.js
+++ app/scripts/controllers/reports.js
@@ -5,48 +5,22 @@ angular.module('wx2AdminWebClientApp')
 	.controller('ReportsCtrl', ['$scope', '$parse', 'ReportsService', 'Log', 'Auth',
 		function($scope, $parse, ReportsService, Log, Auth) {
 
-			var searchData;
-			$('#refresh').html('<i class=\'fa fa-refresh fa-spin fa-2x\'></i>');
 			$('#avg-entitlements-refresh').html('<i class=\'fa fa-refresh fa-spin fa-2x\'></i>');
 			$('#avg-calls-refresh').html('<i class=\'fa fa-refresh fa-spin fa-2x\'></i>');
 			$('#avg-conversations-refresh').html('<i class=\'fa fa-refresh fa-spin fa-2x\'></i>');
 			$('#active-users-refresh').html('<i class=\'fa fa-refresh fa-spin fa-2x\'></i>');
 
-			$('#chartdiv').addClass('chart-border');
 			$('#avgEntitlementsdiv').addClass('chart-border');
 			$('#avgCallsdiv').addClass('chart-border');
 			$('#avgConversationsdiv').addClass('chart-border');
 			$('#activeUsersdiv').addClass('chart-border');
 
-			$scope.showRefresh = true;
 			$scope.showAvgEntitlementsRefresh = true;
 			$scope.showAvgCallsRefresh = true;
 			$scope.showAvgConversationsRefresh = true;
 			$scope.showActiveUsersRefresh = true;
 
 			var chartVals = [];
-
-			var getMetrics = function() {
-				var params = {
-					'intervalCount': 1,
-					'intervalType': 'month'
-				};
-				ReportsService.getUsageMetrics('callUsage', params, function(data, status) {
-					if (data.success) {
-						searchData = data.data;
-						makeChart(searchData);
-						if (searchData.length !== 0) {
-							$scope.showRefresh = false;
-							$('#chartdiv').removeClass('chart-border');
-						} else {
-							$('#refresh').html('<h3>No results available.</h3>');
-						}
-					} else {
-						Log.debug('Query existing users failed. Status: ' + status);
-						$('#refresh').html('<h3>Error processing request</h3>');
-					}
-				});
-			};
 
 			var getMetricData = function(dataList, metric) {
 				var count = 0;
@@ -96,31 +70,6 @@ angular.module('wx2AdminWebClientApp')
 				});
 			};
 
-			var makeChart = function(sdata) {
-				var piChart = AmCharts.makeChart('chartdiv', {
-					'type': 'pie',
-					'theme': 'none',
-					'legend': {
-						'markerType': 'circle',
-						'position': 'right',
-						'marginRight': 80,
-						'autoMargins': false
-					},
-					'dataProvider': sdata,
-					'valueField': 'count',
-					'titleField': 'eventName',
-					'balloonText': '[[title]]<br><span style=\"font-size:14px\"><b>[[value]]</b> ([[percents]]%)</span>',
-					'exportConfig': {
-						'menuTop': '0px',
-						'menuItems': [{
-							'icon': '/lib/3/images/export.png',
-							'format': 'png'
-						}]
-					}
-				});
-				return piChart;
-			};
-
 			var makeTimeChart = function(sdata, divName, metricName, title, color) {
 				var homeChart = AmCharts.makeChart(divName, {
 					'type': 'serial',
@@ -165,11 +114,7 @@ angular.module('wx2AdminWebClientApp')
 				});
 			};
 
-			var chart = makeChart(searchData);
-			chart.colors = ['#FFCCFF', '#B8DBFF', '#FFFF99', '#C2FFC2', '#3399FF', '#FF6666', '#FFB870', '#FF6600', '#FF9E01', '#FCD202', '#F8FF01', '#B0DE09', '#04D215', '#0D8ECF', '#0D52D1', '#2A0CD0', '#8A0CCF', '#CD0D74', '#754DEB', '#DDDDDD', '#999999', '#333333', '#000000', '#57032A', '#CA9726', '#990000', '#4B0C25'];
-
 			if (Auth.isAuthorized($scope)) {
-				getMetrics();
 				getTimeCharts('entitlements', 1, 'month', 1, 'week', 'avgEntitlementsdiv', 'avg-entitlements-refresh', 'showAvgEntitlementsRefresh', 'Entitlements', '#B8DBFF');
 				getTimeCharts('avgCalls', 1, 'month', 1, 'week', 'avgCallsdiv', 'avg-calls-refresh', 'showAvgCallsRefresh', 'Calls', '#FFFF99');
 				getTimeCharts('avgConversations', 1, 'month', 1, 'week', 'avgConversationsdiv', 'avg-conversations-refresh', 'showAvgConversationsRefresh', 'Conversations', '#C2FFC2');
@@ -177,7 +122,6 @@ angular.module('wx2AdminWebClientApp')
 			}
 
 			$scope.$on('AuthinfoUpdated', function() {
-				getMetrics();
 				getTimeCharts('entitlements', 1, 'month', 1, 'week', 'avgEntitlementsdiv', 'avg-entitlements-refresh', 'showAvgEntitlementsRefresh', 'Entitlements', '#B8DBFF');
 				getTimeCharts('avgCalls', 1, 'month', 1, 'week', 'avgCallsdiv', 'avg-calls-refresh', 'showAvgCallsRefresh', 'Calls', '#FFFF99');
 				getTimeCharts('avgConversations', 1, 'month', 1, 'week', 'avgConversationsdiv', 'avg-conversations-refresh', 'showAvgConversationsRefresh', 'Conversations', '#C2FFC2');
