5acf4887cc2e28d7cdae11dd54c2999c5238c1f3
Rajashree Kamath <rajashka@cisco.com>
2014-08-21 12:42:52 -0700

    US4934 - Users list Page - Detail Panel after Merge @rev 194656

--- test/e2e-protractor/integration_spec.js
+++ test/e2e-protractor/integration_spec.js
@@ -401,7 +401,7 @@ describe('App flow', function() {
       it('should exit the preview panel when clicking the x', function() {
           browser.sleep(1000);
           element(by.id('exitPreviewButton')).click().then(function(){
-            browser.sleep(1000);
+            browser.sleep(2000);
             expect(element(by.id('entitlementCol')).isDisplayed()).toEqual(true);
             expect(element(by.id('details-panel')).isDisplayed()).toEqual(false);
           });
