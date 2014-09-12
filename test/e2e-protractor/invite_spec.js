8559d90529219445802a352c134e8fb59f4e2e8f
sandralo <sandralo@cisco.com>
2014-09-12 00:19:09 -0700

    new download and app links @rev 198497

--- test/e2e-protractor/invite_spec.js
+++ test/e2e-protractor/invite_spec.js
@@ -5,12 +5,13 @@
 /* global by */
 /* global browser */
 /* global expect */
+/* global element */
 
 //var config = require('./testconfig.js');
 
 // encrypted param from invitation email link for "Test User <phtest77+int1@gmail.com>"
 //var encryptedQueryParam = 'bnlA6k3ZerWIccYYY2RndVEeMjFu914UOsnFVyYNQoMrkJ7Hye+VFJ20BW2ghuv/7auSaumsYWmMkAlT+HIMqMKyK7AmUY3QhKY8fFXx34AQbKMkqy1ogx8uJUp1QL0E';
-var encryptedQueryParam = 'bnlA6k3ZerWIccYYY2RndVEeMjFu914UOsnFVyYNQoOtS2WBXdmPiQau5G/ErBDSiG5JxjtV9Dk6HIGGAAAkBQmkfHv5S9E8Ub+8rIeosI0QXZbR+/9ZN0m7BEtQIvRLfDFBFqh+L0B7vKsyzLGY/hy+SZ6sLAV22vzHZzWsIMg6OIP5gV/zkw8MLEFAyPNQHHkQQ5t7WB5QhUExd05+XQ=='
+var encryptedQueryParam = 'bnlA6k3ZerWIccYYY2RndVEeMjFu914UOsnFVyYNQoOtS2WBXdmPiQau5G/ErBDSiG5JxjtV9Dk6HIGGAAAkBQmkfHv5S9E8Ub+8rIeosI0QXZbR+/9ZN0m7BEtQIvRLfDFBFqh+L0B7vKsyzLGY/hy+SZ6sLAV22vzHZzWsIMg6OIP5gV/zkw8MLEFAyPNQHHkQQ5t7WB5QhUExd05+XQ==';
 // Notes:
 // - State is conserved between each describe and it blocks.
 // - When a page is being loaded, use wait() to check if elements are there before asserting.
@@ -24,7 +25,7 @@ describe('invite page with user param', function() {
     browser.driver.wait(function() {
       return browser.driver.isElementPresent(by.id('btn-login'));
     }).then(function() {
-      expect(browser.driver.getCurrentUrl()).toContain('wx2-web.wbx2.com');
+      expect(browser.driver.getCurrentUrl()).toContain('web.wbx2.com');
     });
 
   });
@@ -35,7 +36,7 @@ describe('invite page with user param', function() {
     browser.driver.wait(function() {
       return browser.driver.isElementPresent(by.id('btn-login'));
     }).then(function() {
-      expect(browser.driver.getCurrentUrl()).toContain('wx2-web.wbx2.com');
+      expect(browser.driver.getCurrentUrl()).toContain('web.wbx2.com');
     });
 
   });
@@ -48,7 +49,7 @@ describe('invite page with user param', function() {
   });
 
 
-  });
+});
 
   // it('should create cookie', function() {
   //   browser.sleep(2000);
