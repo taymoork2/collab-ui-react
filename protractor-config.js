a82130b4ceb64a595d034b1b4beb92bb4581aa72
Nabeel Kamboh <nkamboh@cisco.com>
2014-09-23 15:35:38 -0700

    US5419 - fix test

--- protractor-config.js
+++ protractor-config.js
@@ -3,7 +3,7 @@
 exports.config = {
   // Spec patterns are relative to the location of this config.
   specs: [
-    'test/e2e-protractor/shared*_spec.js'
+    'test/e2e-protractor/*_spec.js'
   ],
   exclude: [
     'test/e2e-protractor/telephony_info_spec.js'  // excluding until proper user with proper entitlement is added to integration
