469b63fbc33105a2920f77972afdf0502da00caa
Stig Murberg <stig.murberg@gmail.com>
2014-12-12 10:54:57 +0100

    Run tests in fullscreen. Workaround for non responsive menu. @rev tvasset

--- protractor-config.js
+++ protractor-config.js
@@ -6,7 +6,7 @@ exports.config = {
   capabilities: {
     'browserName': 'chrome',
     'chromeOptions': {
-      'args': ['--disable-extensions', '--start-maximized']
+      'args': ['--disable-extensions', '--start-fullscreen']
     }
   },
 
