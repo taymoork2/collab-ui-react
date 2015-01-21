74fe4166669dba4cb59b663dccf079b2a397064c
disr <disr@cisco.com>
2015-01-21 11:48:11 +0530

    WC

--- app/modules/mediafusion/meetings/meetingList/meetinglistservice.js
+++ app/modules/mediafusion/meetings/meetingList/meetinglistservice.js
@@ -1,16 +1,22 @@
 'use strict';
 
+//Defining a MeetingListService.
 angular.module('Core')
   .service('MeetingListService', ['$http', '$rootScope', 'Config', 'Authinfo', 'Log', 'Utils', 'Auth',
     function ($http, $rootScope, Config, Authinfo, Log, Utils, Auth) {
 
+      //Fetching the Base url form config.js file.
       var meetingUrl = Config.getMeetingServiceUrl();
       var meetinglistservice = {
+
+        //listMeetings will actually perfomr a rest call and fetches the data from the server and return back to controller.
 
         listMeetings: function (callback) {
 
           var listUrl = Utils.sprintf(meetingUrl + '/meeting/getallminmeeting', [Authinfo.getOrgId()]);
           $http.defaults.headers.common.Authorization = 'Bearer ' + $rootScope.token;
+
+          //Actual rest call to get meeting info from server and also error case is handeled.
           $http.get(listUrl)
             .success(function (data, status) {
               data.success = true;
