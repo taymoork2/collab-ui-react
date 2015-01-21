7654ad208596354c03765edf6a8e52b279fbbf1a
napal <napal@cisco.com>
2015-01-21 16:00:33 +0530

    e2e test meeting page

--- test/e2e-protractor/pages/meetings.page.js
+++ test/e2e-protractor/pages/meetings.page.js
@@ -1,18 +1,6 @@
 'use strict'
 
 var MeetingsPage = function() {
-  this.searchButton = element(by.css('.header-search-toggle'));
-  this.searchField = element(by.css('.search-form input'));
-  this.settingsBar = element(by.id('setting-bar'));
-  this.logoutButton = element(by.id('logout-btn'));
-  this.iconSearch = element(by.id('icon-search'));
-  this.statusField = element(by.id('statusField'));
-  this.subjectField = element(by.id('subjectField'));
-  this.dateField = element(by.id('dateField'));
-  this.startTimeField = element(by.id('startTimeField'));
-  this.resourceField = element(by.id('resourceField'));
-  this.webexSiteField = element(by.id('webexSiteField'));
-  
   
   this.assertPage = function (page) {
     expect(this.currentPage.getText()).toBe(page);
