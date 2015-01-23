bec0a400e3b4031f4729e2ea4d46c703537fdbfd
Tom Vasset <tvasset@cisco.com>
2015-01-23 09:19:49 +0100

    Fixing inconsistent naming

--- app/modules/hercules/services/service-descriptor.js
+++ app/modules/hercules/services/service-descriptor.js
@@ -12,14 +12,14 @@ angular.module('Hercules')
           icon: 'fa fa-tachometer',
           type: 'c_mgmt'
         }, {
-          name: 'Telephony service (UCM)',
+          name: 'UCM Service',
           icon: 'fa fa-phone',
           type: 'c_ucmc',
           descs: [
             'Zero touch meetings, move calls between desk phones and soft clients, and reuse your enterprise phone number.'
           ]
         }, {
-          name: 'Calendar service (Exchange)',
+          name: 'Calendar Service',
           icon: 'fa fa-calendar',
           type: 'c_cal',
           descs: [
