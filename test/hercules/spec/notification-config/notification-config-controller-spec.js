ae55aa73e7671f37ffe3c5d84135a80a431d44e9
Stig Murberg <stig.murberg@gmail.com>
2015-02-04 09:33:16 +0100

    Show loading / saving status in notfication popup

--- test/hercules/spec/notification-config/notification-config-controller-spec.js
+++ test/hercules/spec/notification-config/notification-config-controller-spec.js
@@ -22,14 +22,18 @@ describe('NotificationConfigController', function() {
   }));
 
   it('reads data and updates scope', function() {
+    expect($scope.loading).toBe(true);
     expect(service.read.callCount).toBe(1);
     service.read.callArgWith(0, null, { foo: 'bar' });
+    expect($scope.loading).toBe(false);
     expect($scope.config.foo).toBe('bar');
   });
 
   it('notifies when read fails', function() {
+    expect($scope.loading).toBe(true);
     expect(service.read.callCount).toBe(1);
     service.read.callArgWith(0, ['fails'], null);
+    expect($scope.loading).toBe(false);
     expect(notification.notify.callCount).toBe(1);
     expect(notification.notify.args[0][0].length).toBe(1);
     expect(notification.notify.args[0][0][0]).toBe('fails');
@@ -47,12 +51,15 @@ describe('NotificationConfigController', function() {
     $scope.config = { foo: 'bar' };
     $scope.$parent = { modal: { close: sinon.stub() } };
     expect(service.write.callCount).toBe(0);
+    expect($scope.saving).toBe(false);
 
     $scope.writeConfig();
+    expect($scope.saving).toBe(true);
     expect(service.write.callCount).toBe(1);
     expect(service.write.args[0][0].foo).toBe('bar');
 
     service.write.callArgWith(1, null);
+    expect($scope.saving).toBe(false);
     expect(notification.notify.callCount).toBe(0);
     expect($scope.$parent.modal.close.callCount).toBe(1);
   });
@@ -62,9 +69,11 @@ describe('NotificationConfigController', function() {
     $scope.writeConfig();
     expect(service.write.callCount).toBe(1);
     expect(service.write.args[0][0].foo).toBe('bar');
+    expect($scope.saving).toBe(true);
 
     service.write.callArgWith(1, ['err']);
     expect(notification.notify.callCount).toBe(1);
+    expect($scope.saving).toBe(false);
   });
 
 });
--- test/hercules/spec/notification-config/notification-config-service-spec.js
+++ test/hercules/spec/notification-config/notification-config-service-spec.js
@@ -33,6 +33,20 @@ describe('NotificationConfigService', function () {
     expect(callback.args[0][1]).toBe('notification_config_yay');
   });
 
+  it('should callback when read fails', function() {
+    $httpBackend
+      .when('GET', 'https://hercules.hitest.huron-dev.com/v1/notification_config')
+      .respond(500, 'notification_config_yay');
+
+    var callback = sinon.stub();
+    Service.read(callback);
+    $httpBackend.flush();
+
+    expect(callback.callCount).toBe(1);
+    expect(callback.args[0][0]).toBeTruthy();
+    expect(callback.args[0][1]).toBeFalsy();
+  });
+
   it('should post all the data', function() {
     var data = {username: 'foo', password: 'bar', wx2users: 'yo,lo'}
     $httpBackend
@@ -47,5 +61,21 @@ describe('NotificationConfigService', function () {
     expect(callback.args[0][0]).toBe(null);
     expect(callback.args[0][1]).toBe('nc');
   });
+
+  it('should callback when post fails', function() {
+    var data = {username: 'foo', password: 'bar', wx2users: 'yo,lo'}
+    $httpBackend
+      .when('POST', 'https://hercules.hitest.huron-dev.com/v1/notification_config', data)
+      .respond(500);
+
+    var callback = sinon.stub();
+    Service.write(data, callback);
+    $httpBackend.flush();
+
+    expect(callback.callCount).toBe(1);
+    expect(callback.args[0][0]).toBeTruthy();
+    expect(callback.args[0][1]).toBe(null);
+  });
+
 
 });
