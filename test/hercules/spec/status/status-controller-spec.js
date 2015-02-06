ab9d5a6fcd37d8588628cab5ccda630db0697449
Stig Murberg <stig.murberg@gmail.com>
2015-02-06 15:11:36 +0100

    Only show the fusion status nugget when user is authorized

--- test/hercules/spec/status/status-controller-spec.js
+++ test/hercules/spec/status/status-controller-spec.js
@@ -1,53 +1,90 @@
 describe('StatusController', function() {
   beforeEach(module('wx2AdminWebClientApp'));
 
-  var $scope, service;
+  describe('is enabled', function() {
 
-  beforeEach(inject(function(_$controller_){
-    $scope = {
-      $watch: sinon.stub()
-    };
-    service = {
-      fetch: sinon.stub()
-    }
-    controller = _$controller_('StatusController', {
-      $scope: $scope,
-      ConnectorService: service
+    var $scope, service, auth;
+
+    beforeEach(inject(function(_$controller_){
+      $scope = {
+        $watch: sinon.stub()
+      };
+      service = {
+        fetch: sinon.stub()
+      }
+      auth = {
+        isFusion: function() { return true; }
+      }
+      _$controller_('StatusController', {
+        $scope: $scope,
+        ConnectorService: service,
+        Authinfo: auth
+      });
+    }));
+
+    it('defaults some scope vars', function() {
+      expect($scope.isEnabled).toEqual(true);
+      expect($scope.color).toEqual('gray');
+      expect($scope.className).toEqual('fa fa-gear fa-spin');
     });
-  }));
 
-  it('defaults some scope vars', function() {
-    expect($scope.color).toEqual('gray');
-    expect($scope.className).toEqual('fa fa-gear fa-spin');
+    it('fetches on load', function() {
+      expect(service.fetch.callCount).toEqual(1);
+    });
+
+    it('calls fetch with squelchErrors flag set', function() {
+      expect(service.fetch.callCount).toEqual(1);
+      expect(service.fetch.args[0][1].squelchErrors).toEqual(true);
+    });
+
+    it('sets appropriate values when xhr fails', function() {
+      service.fetch.callArgWith(0, {}, {});
+      expect($scope.color).toEqual('red');
+      expect($scope.className).toEqual('fa fa-circle');
+    });
+
+    it('sets appropriate values when there are clusters with errors', function() {
+      service.fetch.callArgWith(0, null, [{needs_attention: true}, {needs_attention: true}, {needs_attention: false}]);
+      expect($scope.color).toEqual('red');
+      expect($scope.className).toEqual('fa fa-circle');
+      expect($scope.needs_attention).toEqual(2);
+    });
+
+    it('sets appropriate values when there are no clusters with errors', function() {
+      service.fetch.callArgWith(0, null, [{needs_attention: false}, {needs_attention: false}, {needs_attention: false}]);
+      expect($scope.color).toEqual('green');
+      expect($scope.className).toEqual('fa fa-circle');
+      expect($scope.needs_attention).toEqual(0);
+    });
+
   });
 
-  it('fetches on load', function() {
-    expect(service.fetch.callCount).toEqual(1);
-  });
+  describe('is disabled', function() {
 
-  it('calls fetch with squelchErrors flag set', function() {
-    expect(service.fetch.callCount).toEqual(1);
-    expect(service.fetch.args[0][1].squelchErrors).toEqual(true);
-  });
+    var $scope, service, auth;
 
-  it('sets appropriate values when xhr fails', function() {
-    service.fetch.callArgWith(0, {}, {});
-    expect($scope.color).toEqual('red');
-    expect($scope.className).toEqual('fa fa-circle');
-  });
+    beforeEach(inject(function(_$controller_){
+      $scope = {
+        $watch: sinon.stub()
+      };
+      service = {
+        fetch: sinon.stub()
+      }
+      auth = {
+        isFusion: function() { return false; }
+      }
+      _$controller_('StatusController', {
+        $scope: $scope,
+        ConnectorService: service,
+        Authinfo: auth
+      });
+    }));
 
-  it('sets appropriate values when there are clusters with errors', function() {
-    service.fetch.callArgWith(0, null, [{needs_attention: true}, {needs_attention: true}, {needs_attention: false}]);
-    expect($scope.color).toEqual('red');
-    expect($scope.className).toEqual('fa fa-circle');
-    expect($scope.needs_attention).toEqual(2);
-  });
+    it('defaults some scope vars', function() {
+      expect($scope.isEnabled).toEqual(false);
+      expect(service.fetch.callCount).toEqual(0);
+    });
 
-  it('sets appropriate values when there are no clusters with errors', function() {
-    service.fetch.callArgWith(0, null, [{needs_attention: false}, {needs_attention: false}, {needs_attention: false}]);
-    expect($scope.color).toEqual('green');
-    expect($scope.className).toEqual('fa fa-circle');
-    expect($scope.needs_attention).toEqual(0);
   });
 
 });
