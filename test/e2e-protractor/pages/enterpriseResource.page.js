b0e65d1b641831b490e6eaedb2b75604c8869929
krbalakr <krbalakr@cisco.com>
2015-02-13 05:01:50 +0530

    Adding test cases for enterprise resource

---
+++ test/e2e-protractor/pages/enterpriseResource.page.js
@@ -0,0 +1,35 @@
+'use strict'
+var EnterpriseResourcePage = function() {
+  //details-panel
+	this.enterpriseResourceLink = element(by.id('entire-slide'));
+
+	this.assertResultsLength = function (results) {
+  	element.all(by.repeater('col in renderedColumns')).then(function (rows) {
+		if (results === 20) {
+    		expect(rows.length).toBeLessThanOrEqualTo(results);
+  		} else if (results === 0) {
+    		return expect(rows.length).toBeGreaterThan(results);
+  		} else {
+    		expect(rows.length).toBe(results);
+  		}
+  	});
+	};
+
+	this.clickOnResource = function(){
+		element.all(by.repeater('col in renderedColumns')).get(2).click();
+	};
+
+  this.clickOnFilter = function(){
+    element(by.css('.ngHeaderButtonArrow')).click();
+  };
+
+  this.provideFilterValues = function(valueToFilter) {
+    element(by.model('filterText')).sendKeys(valueToFilter);
+  };
+
+  this.clearFilterValues = function() {
+    element(by.model('filterText')).clear();
+  };
+
+};
+module.exports = EnterpriseResourcePage;
