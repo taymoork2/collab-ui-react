2a7996c458075769c25245711f9636e304064fac
napal <napal@cisco.com>
2015-02-13 11:50:19 +0530

    url change
    url change

--- app/modules/core/scripts/services/config.js
+++ app/modules/core/scripts/services/config.js
@@ -31,15 +31,15 @@ angular.module('Core')
         meetingServiceUrl: {
           //dev: 'http://multimediafusion-krishna.mb-lab.huron.uno/admin/api/v1',
           dev: 'http://multimediafusion-dummy.mb-lab.huron.uno/admin/api/v1',
-          integration: 'https://multimediafusion-dummy.mb-lab.huron.uno/admin/api/v1',
-          prod: 'https://multimediafusion-krishna.mb-lab.huron.uno/admin/api/v1'
+          integration: 'http://multimediafusion-dummy.mb-lab.huron.uno/admin/api/v1',
+          prod: 'http://multimediafusion-krishna.mb-lab.huron.uno/admin/api/v1'
         },
 
         MeetinginfoserviceUrl: {
           //dev: 'http://multimediafusion-krishna.mb-lab.huron.uno/admin/api/v1',
           dev: 'http://multimediafusion-dummy.mb-lab.huron.uno/admin/api/v1',
-          integration: 'https://multimediafusion-dummy.mb-lab.huron.uno/admin/api/v1',
-          prod: 'https://multimediafusion-krishna.mb-lab.huron.uno/admin/api/v1'
+          integration: 'http://multimediafusion-dummy.mb-lab.huron.uno/admin/api/v1',
+          prod: 'http://multimediafusion-krishna.mb-lab.huron.uno/admin/api/v1'
         },
 
         oauthClientRegistration: {
