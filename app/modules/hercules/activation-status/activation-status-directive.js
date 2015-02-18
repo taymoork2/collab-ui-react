30d6b755e5fea80cff11c4f15085840f3b68e6ed
Bjørn Nordlund <bnordlun@cisco.com>
2015-02-18 14:02:08 +0100

    Hide fusion activation status if user is not entitled to fusion.

--- app/modules/hercules/activation-status/activation-status-directive.js
+++ app/modules/hercules/activation-status/activation-status-directive.js
@@ -2,7 +2,12 @@
   'use strict';
   angular
     .module('Hercules')
-    .controller('ActivationStatusController', ['$scope', 'USSService', function ($scope, ussService) {
+    .controller('ActivationStatusController', ['$scope', 'USSService', 'Authinfo', function ($scope, ussService, Authinfo) {
+      if (!Authinfo.isFusion()) {
+        $scope.isEnabled = false;
+        return;
+      };
+      $scope.isEnabled = true;
       $scope.$watch('currentUser', function (newUser, oldUser) {
         if (!newUser || !newUser.id) return $scope.activationStatus = null;
         ussService.getStatusesForUser(newUser.id, function (err, data) {
