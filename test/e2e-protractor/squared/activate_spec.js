039b68d3424d6cbd0756e91de4f561b1da08f421
Stig Murberg <stig.murberg@gmail.com>
2015-03-05 16:54:34 -0800

    Refactored activate and download spec.

--- test/e2e-protractor/squared/activate_spec.js
+++ test/e2e-protractor/squared/activate_spec.js
@@ -63,88 +63,84 @@ function verifyEmail(obj) {
   });
 }
 
-function setup(obj) {
+function setup(deviceUA) {
+  var obj = {
+    body: getTestBody(),
+    deviceUA: deviceUA
+  };
   var flow = protractor.promise.controlFlow();
   flow.execute(getToken.bind(null, obj));
   flow.execute(verifyEmail.bind(null, obj));
+  return obj;
 }
 
 describe('Self Registration Activation Page', function() {
 
-  describe('iOS', function() {
-    var obj = {
-      body: getTestBody(),
-      deviceUA: config.deviceUserAgent.iPhone
-    };
-    setup(obj);
+  var iosData = setup(config.deviceUserAgent.iPhone);
+  var androidData = setup(config.deviceUserAgent.android);
 
-    describe('Desktop activation for iOS device', function() {
-      it('should display without admin controls on navigation bar', function() {
-        expect(obj.encryptedQueryParam).not.toBe(null);
-        browser.get('#/activate?eqp=' + encodeURIComponent(obj.encryptedQueryParam));
-        navigation.expectAdminSettingsNotDisplayed();
-      });
+  describe('Desktop activation for iOS device', function() {
+    it('should display without admin controls on navigation bar', function() {
+      expect(iosData.encryptedQueryParam).not.toBe(null);
+      browser.get('#/activate?eqp=' + encodeURIComponent(iosData.encryptedQueryParam));
+      navigation.expectAdminSettingsNotDisplayed();
+    });
 
-      it('should activate user and display success info', function() {
-        utils.expectIsDisplayed(activate.provisionSuccess);
-        utils.expectIsNotDisplayed(activate.codeExpired);
-        utils.expectIsNotDisplayed(activate.resendSuccess);
-      });
+    it('should activate user and display success info', function() {
+      utils.expectIsDisplayed(activate.provisionSuccess);
+      utils.expectIsNotDisplayed(activate.codeExpired);
+      utils.expectIsNotDisplayed(activate.resendSuccess);
+    });
 
-      it('should delete added user', function() {
-        deleteUtils.deleteUser(obj.body.email);
+  });
+
+  describe('Desktop activation after code is invalidated', function() {
+
+    it('should display without admin controls on navigation bar', function() {
+      browser.get('#/activate?eqp=' + encodeURIComponent(iosData.encryptedQueryParam));
+      navigation.expectAdminSettingsNotDisplayed();
+    });
+
+    it('should display code expired with user email', function() {
+      utils.expectIsNotDisplayed(activate.provisionSuccess);
+      utils.expectIsDisplayed(activate.codeExpired);
+      utils.expectIsNotDisplayed(activate.resendSuccess);
+      expect(activate.userEmail.getText()).toContain(iosData.body.email);
+    });
+
+    it('should request new code when link is clicked', function() {
+      utils.click(activate.sendCodeLink);
+      utils.expectIsNotDisplayed(activate.provisionSuccess);
+      utils.expectIsNotDisplayed(activate.codeExpired);
+      utils.expectIsDisplayed(activate.resendSuccess);
+
+      activate.testData.getAttribute('eqp').then(function(eqp) {
+        expect(eqp).not.toBe(null);
       });
+    });
+
+  });
+
+  describe('Desktop activation for android device', function() {
+    it('should display without admin controls on navigation bar', function() {
+      browser.get('#/activate?eqp=' + encodeURIComponent(androidData.encryptedQueryParam));
+      navigation.expectAdminSettingsNotDisplayed();
+    });
+
+    it('should activate user and display success info', function() {
+      utils.expectIsDisplayed(activate.provisionSuccess);
+      utils.expectIsNotDisplayed(activate.codeExpired);
+      utils.expectIsNotDisplayed(activate.resendSuccess);
     });
   });
 
-  describe('android', function() {
-    var obj = {
-      body: getTestBody(),
-      deviceUA: config.deviceUserAgent.android
-    };
-    setup(obj);
-
-    describe('Desktop activation for android device', function() {
-      it('should display without admin controls on navigation bar', function() {
-        browser.get('#/activate?eqp=' + encodeURIComponent(obj.encryptedQueryParam));
-        navigation.expectAdminSettingsNotDisplayed();
-      });
-
-      it('should activate user and display success info', function() {
-        utils.expectIsDisplayed(activate.provisionSuccess);
-        utils.expectIsNotDisplayed(activate.codeExpired);
-        utils.expectIsNotDisplayed(activate.resendSuccess);
-      });
+  describe('deletes the accounts', function() {
+    it('should delete ios user', function() {
+      deleteUtils.deleteUser(iosData.body.email);
     });
-
-    describe('Desktop activation after code is invalidated', function() {
-
-      it('should display without admin controls on navigation bar', function() {
-        browser.get('#/activate?eqp=' + encodeURIComponent(obj.encryptedQueryParam));
-        navigation.expectAdminSettingsNotDisplayed();
-      });
-
-      it('should display code expired with user email', function() {
-        utils.expectIsNotDisplayed(activate.provisionSuccess);
-        utils.expectIsDisplayed(activate.codeExpired);
-        utils.expectIsNotDisplayed(activate.resendSuccess);
-        expect(activate.userEmail.getText()).toContain(obj.body.email);
-      });
-
-      it('should request new code when link is clicked', function() {
-        utils.click(activate.sendCodeLink);
-        utils.expectIsNotDisplayed(activate.provisionSuccess);
-        utils.expectIsNotDisplayed(activate.codeExpired);
-        utils.expectIsDisplayed(activate.resendSuccess);
-
-        activate.testData.getAttribute('eqp').then(function(eqp) {
-          expect(eqp).not.toBe(null);
-        });
-      });
-
-      it('should delete added user', function() {
-        deleteUtils.deleteUser(obj.body.email);
-      });
+    it('should delete android user', function() {
+      deleteUtils.deleteUser(androidData.body.email);
     });
   });
+
 });
