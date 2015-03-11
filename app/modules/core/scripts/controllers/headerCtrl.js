e667741acfcaed4bbc32494c9c37843faf43b881
Christian Idylle <cidylle@cisco.com>
2015-03-11 12:00:58 -0700

    Making header branding conditional.

--- app/modules/core/scripts/controllers/headerCtrl.js
+++ app/modules/core/scripts/controllers/headerCtrl.js
@@ -1,10 +1,16 @@
 'use strict';
 
 angular.module('Core')
-  .controller('HeaderCtrl', ['$scope',
-    function ($scope) {
-      $scope.headerTitle = 'Spark Management';
+  .controller('HeaderCtrl', ['$scope', '$filter', 'Config',
+    function ($scope, $filter, Config) {
+      //remove after spark branding is done
+      if (Config.getEnv() === 'sparkprod' || Config.getEnv() === 'sparkint') {
+        $scope.icon = '/images/sparkSm.png';
+      } else {
+        $scope.icon = '/images/cisco_logo.png';
+        angular.element('.app-icon').css('width', '47px').css('margin-top', '-4px').css('margin-left', '-4px');
+      }
+      $scope.headerTitle = $filter('translate')('loginPage.title');
       $scope.navStyle = 'admin';
-      $scope.icon = '/images/sparkSm.png';
     }
   ]);
