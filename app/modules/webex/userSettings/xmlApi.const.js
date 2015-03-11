7480d975ff9d9a5c20a9dad7dfb3eb32ef357f71
lhsieh <lhsieh@cisco.com>
2015-03-10 18:13:32 -0700

    US42118: Parse XML API return data in Atlas front end

--- app/modules/webex/userSettings/xmlApi.const.js
+++ app/modules/webex/userSettings/xmlApi.const.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('WebExUserSettings')
-  .service('WebExUserSettingsConstants', [
-    function WebExUserSettingsConstants() {
+  .service('XmlApiConstants', [
+    function XmlApiConstants() {
       return {
         siteInfoRequest: "<serv:message xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">" +
           "    <header>" +
@@ -10,7 +10,6 @@ angular.module('WebExUserSettings')
           "            <webExID>{{webexAdminID}}</webExID>" +
           "            <password>{{webexAdminPswd}}</password>" +
           "            <siteID>{{siteID}}</siteID>" +
-          "            <partnerID>{{partnerID}}</partnerID>" +
           "        </securityContext>" +
           "    </header>" +
           "    <body>" +
@@ -24,7 +23,6 @@ angular.module('WebExUserSettings')
           "            <webExID>{{webexAdminID}}</webExID>" +
           "            <password>{{webexAdminPswd}}</password>" +
           "            <siteID>{{siteID}}</siteID>" +
-          "            <partnerID>{{partnerID}}</partnerID>" +
           "        </securityContext>" +
           "    </header>" +
           "    <body>" +
@@ -40,12 +38,41 @@ angular.module('WebExUserSettings')
           "            <webExID>{{webexAdminID}}</webExID>" +
           "            <password>{{webexAdminPswd}}</password>" +
           "            <siteID>{{siteID}}</siteID>" +
-          "            <partnerID>{{partnerID}}</partnerID>" +
           "        </securityContext>" +
           "    </header>" +
           "    <body>" +
           "        <bodyContent xsi:type=\"java:com.webex.service.binding.meetingtype.LstMeetingType\" />" +
           "    </body>" +
+          "</serv:message>",
+
+        updateUserPrivileges1: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+          "<serv:message xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"" +
+          "    xmlns:serv=\"http://www.webex.com/schemas/2002/06/service\">" +
+          "    <header>" +
+          "        <securityContext>" +
+          "            <siteID>{{siteID}}</siteID>" +
+          "            <webExID>{{webexAdminID}}</webExID>" +
+          "            <password>{{webexAdminPswd}}</password>" +
+          "        </securityContext>" +
+          "    </header>",
+
+        updateUserPrivileges2: "<body>" +
+          "    <bodyContent xsi:type=\"java:com.webex.service.binding.user.SetUser\">" +
+          "        <webExId>{{webexUserId}}</webExId>",
+
+        updateUserPrivileges3_1: "            <use:meetingTypes>",
+        updateUserPrivileges3_2: "                <use:meetingType>{{meetingType}}</use:meetingType>",
+        updateUserPrivileges3_3: "            </use:meetingTypes>",
+
+        updateUserPrivileges4: "            <use:supportedServices>" +
+          "                <use:meetingCenter>{{meetingCenter}}</use:meetingCenter>" +
+          "                <use:trainingCenter>{{trainingCenter}}</use:trainingCenter>" +
+          "                <use:supportCenter>{{supportCenter}}</use:supportCenter>" +
+          "                <use:eventCenter>{{eventCenter}}</use:eventCenter>" +
+          "                <use:salesCenter>{{salesCenter}}</use:salesCenter>" +
+          "            </use:supportedServices>" +
+          "    </bodyContent>" +
+          "</body>" +
           "</serv:message>",
 
         sessionTicketRequest: "<serv:message xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">" +
