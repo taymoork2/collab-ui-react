7c521707a257b234d34add1ae4d0fab78d8dc709
Stig Murberg <stig.murberg@gmail.com>
2015-04-08 16:58:03 +0200

    Added cluster proxy service

---
+++ app/modules/hercules/services/cluster-proxy.js
@@ -0,0 +1,77 @@
+'use strict';
+
+angular.module('Hercules')
+  .service('ClusterProxy', ['$interval', 'ConnectorService',
+    function ClusterProxy($interval, connectorService) {
+      var pollPromise,
+        clusters = [],
+        pollCount = 0,
+        pollDelay = 1000,
+        pollInFlight = false;
+
+      var start = function () {
+        pollCount++;
+        togglePolling();
+      };
+
+      var stop = function () {
+        if (pollCount > 0) pollCount--;
+        togglePolling();
+      };
+
+      var deleteHost = function (clusterId, serial, callback) {
+        connectorService.deleteHost(clusterId, serial, function () {
+          $interval.flush(pollDelay);
+          callback.apply(arguments);
+        });
+      };
+
+      var getClusters = function () {
+        return clusters;
+      };
+
+      var upgradeSoftware = function (clusterId, serviceType, callback, opts) {
+        connectorService.upgradeSoftware(clusterId, serviceType, function () {
+          $interval.flush(pollDelay);
+          callback.apply(arguments);
+        }, opts);
+      };
+
+      var isPolling = function () {
+        return !!pollCount;
+      };
+
+      var togglePolling = function () {
+        if (pollCount <= 0) {
+          $interval.cancel(pollPromise);
+          pollInFlight = false;
+          return;
+        }
+        if (pollInFlight) {
+          return;
+        }
+        pollInFlight = true;
+        pollPromise = $interval(reload, pollDelay, 1);
+      };
+
+      var reload = function () {
+        connectorService.fetch(function (err, _clusters) {
+          clusters = _clusters || [];
+          pollInFlight = false;
+          togglePolling();
+        }, {
+          squelchErrors: true
+        });
+      };
+
+      return {
+        stopPolling: stop,
+        startPolling: start,
+        deleteHost: deleteHost,
+        getClusters: getClusters,
+        upgradeSoftware: upgradeSoftware,
+        /* for testing */
+        _isPolling: isPolling
+      };
+    }
+  ]);
---
+++ test/hercules/spec/services/cluster-proxy-spec.js
@@ -0,0 +1,91 @@
+'use strict';
+
+describe('ClusterProxy', function () {
+  beforeEach(module('wx2AdminWebClientApp'));
+
+  var clusterProxy, connectorService, $interval;
+
+  beforeEach(function () {
+    module(function ($provide) {
+      connectorService = {
+        fetch: sinon.stub(),
+        deleteHost: sinon.stub(),
+        upgradeSoftware: sinon.stub()
+      };
+      $provide.value('ConnectorService', connectorService);
+    });
+  });
+
+  beforeEach(inject(function ($injector, _$interval_, _ClusterProxy_) {
+    $interval = _$interval_;
+    clusterProxy = _ClusterProxy_;
+    $injector.get('$httpBackend').when('GET', 'l10n/en_US.json').respond({});
+  }));
+
+  it('should start and stop polling', function () {
+    expect(clusterProxy._isPolling()).toBeFalsy();
+
+    clusterProxy.startPolling();
+    expect(clusterProxy._isPolling()).toBeTruthy();
+
+    clusterProxy.startPolling();
+    expect(clusterProxy._isPolling()).toBeTruthy();
+
+    clusterProxy.stopPolling();
+    expect(clusterProxy._isPolling()).toBeTruthy();
+
+    clusterProxy.stopPolling();
+    expect(clusterProxy._isPolling()).toBeFalsy();
+
+    clusterProxy.stopPolling();
+    expect(clusterProxy._isPolling()).toBeFalsy();
+  });
+
+  it('should return data after service is started', function () {
+    expect(clusterProxy.getClusters().length).toBe(0);
+
+    clusterProxy.startPolling();
+    $interval.flush(10000);
+    expect(connectorService.fetch.callCount).toBe(1);
+    connectorService.fetch.callArgWith(0, null, [{
+      id: 'foo'
+    }]);
+
+    expect(clusterProxy.getClusters().length).toBe(1);
+    expect(clusterProxy.getClusters()[0].id).toBe('foo');
+  });
+
+  it('should poll', function () {
+    $interval.flush(1000);
+    expect(connectorService.fetch.callCount).toBe(0);
+
+    clusterProxy.startPolling();
+    $interval.flush(1000);
+    expect(connectorService.fetch.callCount).toBe(1);
+    connectorService.fetch.callArgWith(0, null, [{
+      id: 'foo'
+    }]);
+
+    $interval.flush(1000);
+    expect(connectorService.fetch.callCount).toBe(2);
+  });
+
+  it('should upgrade software and flush', function () {
+    clusterProxy.startPolling();
+    var cb = sinon.stub();
+    clusterProxy.upgradeSoftware('clusterId', 'serviceType', cb);
+    connectorService.upgradeSoftware.callArg(2);
+    expect(connectorService.fetch.callCount).toBe(1);
+    expect(cb.callCount).toBe(1);
+  });
+
+  it('should delete host and flush', function () {
+    clusterProxy.startPolling();
+    var cb = sinon.stub();
+    clusterProxy.deleteHost('clusterId', 'serial', cb);
+    connectorService.deleteHost.callArg(2);
+    expect(connectorService.fetch.callCount).toBe(1);
+    expect(cb.callCount).toBe(1);
+  });
+
+});
