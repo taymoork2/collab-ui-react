997903825940ad1268440d3c4821aec9a45fabc2
Amarkanth Ranganamayna <amrangan@cisco.com>
2015-05-05 12:51:48 -0700

    Ability to download call logs and callflow charts for all participants in a call (fix regression)

--- app/modules/squared/scripts/services/reportsservice.js
+++ app/modules/squared/scripts/services/reportsservice.js
@@ -202,7 +202,7 @@ angular.module('Squared')
         },
 
         getLogInfo: function (locusId, startTime, callback) {
-          var logInfoUrl = logInfoBaseUrl + locusId + '?locusCallStartTime=' + startTime;
+          var logInfoUrl = apiUrl + logInfoBaseUrl + locusId + '?locusCallStartTime=' + startTime;
 
           $http.get(logInfoUrl)
             .success(function (data, status) {
@@ -218,7 +218,7 @@ angular.module('Squared')
         },
 
         getCallSummary: function (locusId, startTime, callback) {
-          var callSummaryUrl = logInfoBaseUrl + locusId + '/callSummaryEvents' + '?locusCallStartTime=' + startTime;
+          var callSummaryUrl = apiUrl + logInfoBaseUrl + locusId + '/callSummaryEvents' + '?locusCallStartTime=' + startTime;
 
           $http.get(callSummaryUrl)
             .success(function (data, status) {
