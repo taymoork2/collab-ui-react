f77d4335569b0dfcf176cb8ec425d03e27ba81e0
Wouter Lammers <woulamme@cisco.com>
2015-06-09 13:08:49 +0200

    Splitting off the url injection into its own file and adding tests for it

---
+++ app/modules/squared/devicesRedux/CsdmConfigService.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('Squared')
+  .service('CsdmConfigService', ['$window', 'Config',
+    function ConfigService($window, Config) {
+
+      var getOverriddenUrl = function (queryParam) {
+        var regex = new RegExp(queryParam + "=([^&]*)");
+        var match = $window.location.search.match(regex);
+        if (match && match.length == 2) {
+          return decodeURIComponent(match[1]);
+        }
+      };
+
+      var getUrl = function () {
+        var overriddenUrl = getOverriddenUrl('csdm-url');
+        if (overriddenUrl) {
+          return overriddenUrl;
+        } else {
+          return Config.getCsdmServiceUrl();
+        }
+      };
+
+      return {
+        getUrl: getUrl
+      };
+    }
+  ]);
--- app/modules/squared/devicesRedux/CsdmService.js
+++ app/modules/squared/devicesRedux/CsdmService.js
@@ -3,26 +3,10 @@
 angular.module('Squared').service('CsdmService',
 
   /* @ngInject  */
-  function ($window, $rootScope, $http, Authinfo, Config) {
+  function ($window, $rootScope, $http, Authinfo, Config, CsdmConfigService) {
 
-    var getUrl = function () {
-      var getOverriddenUrl = function (queryParam) {
-        var regex = new RegExp(queryParam + "=([^&]*)");
-        var match = $window.location.search.match(regex);
-        if (match && match.length == 2) {
-          return decodeURIComponent(match[1]);
-        }
-      };
-      var overriddenUrl = getOverriddenUrl('csdm-url');
-      if (overriddenUrl) {
-        return overriddenUrl;
-      } else {
-        return Config.getCsdmServiceUrl();
-      }
-    };
-
-    var codesUrl = getUrl() + '/organization/' + Authinfo.getOrgId() + '/codes';
-    var devicesUrl = getUrl() + '/organization/' + Authinfo.getOrgId() + '/devices';
+    var codesUrl = CsdmConfigService.getUrl() + '/organization/' + Authinfo.getOrgId() + '/codes';
+    var devicesUrl = CsdmConfigService.getUrl() + '/organization/' + Authinfo.getOrgId() + '/devices';
 
     var codesAndDevicesCache = {};
 
