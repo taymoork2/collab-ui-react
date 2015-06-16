ab21a449027b34c3c7523dc5b5f91cbc4882d94a
Tor Hagemann (tohagema) <tohagema@cisco.com>
2015-06-15 17:51:18 -0700

    testing/core: add spec skeleton for subscriptions

---
+++ test/e2e-protractor/squared/subscriptions_spec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* global describe:false */
+
+describe('Leaderboard', function () {
+  // TODO (tohagema): write cases for aggregate license counts w.r.t:
+  // * proper licenseType "bucket"
+  // * multiple subscriptions
+  // * disregarding CMRs
+});
+
+describe('Onboard Users', function () {
+  // TODO (tohagema): write cases for radio-buttons (multiple subscriptions)
+});
