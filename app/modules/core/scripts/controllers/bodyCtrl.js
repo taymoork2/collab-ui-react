dc4e2bc635e3c73da2a073203414a2fea77202c4
Zack Zatkin-Gold <zzatking@cisco.com>
2015-07-09 14:16:27 -0700

    Adjustments based on feedback

--- app/modules/core/scripts/controllers/bodyCtrl.js
+++ app/modules/core/scripts/controllers/bodyCtrl.js
@@ -1,11 +1,16 @@
-'use strict';
+(function () {
+  'use strict';
 
-angular.module('Core')
-  .controller('BodyCtrl', function ($scope) {
+  angular.module('Core')
+    .controller('BodyCtrl', BodyCtrl);
+
+  /* @ngInject */
+  function BodyCtrl($scope) {
     var vm = this;
     vm.partner = false;
 
     $scope.$on('InvertNavigation', function () {
       vm.partner = true;
     });
-  });
+  }
+})();
