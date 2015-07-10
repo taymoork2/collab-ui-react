e9f3ae1ac6125e0dc8295f837cb7de99f7c37bb0
Admas Molla <admolla@cisco.com>
2015-07-10 10:26:12 -0700

    DE1839: reloading current entitlements every time view gets loaded

--- app/modules/squared/scripts/directives/userentitlements.js
+++ app/modules/squared/scripts/directives/userentitlements.js
@@ -4,6 +4,7 @@ angular.module('Squared')
   .controller('UserEntitlementsCtrl', ['$scope', '$timeout', '$location', '$window', 'Userservice', 'UserListService', 'Log', '$log', 'Config', 'Pagination', '$rootScope', 'Notification', '$filter', 'Utils', 'Authinfo', 'HttpUtils',
     function ($scope, $timeout, $location, $window, Userservice, UserListService, Log, $log, Config, Pagination, $rootScope, Notification, $filter, Utils, Authinfo, HttpUtils) {
       $scope.hasAccount = Authinfo.hasAccount();
+      $scope.entitlements = Utils.getSqEntitlements($scope.currentUser);
 
       //TODO: In the hasAccount case, come up with a better UX for a license category
       //that has no configurable entitlements, intead of an empty pane with a disabled
