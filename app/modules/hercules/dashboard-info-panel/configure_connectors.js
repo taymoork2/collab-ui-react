b00677fc5215797cb518615789ff78fbf12b764f
Stig Murberg <stimurbe@cisco.com>
2015-07-14 12:21:45 +0200

    JSB

--- app/modules/hercules/dashboard-info-panel/configure_connectors.js
+++ app/modules/hercules/dashboard-info-panel/configure_connectors.js
@@ -6,7 +6,7 @@
       var configureConnectors = this;
       var clusters = ClusterService.getClusters();
 
-      configureConnectors.showConfigureConnectors = function() {
+      configureConnectors.showConfigureConnectors = function () {
         return _.some(clusters, function (cluster) {
           return cluster.cluster_type == 'c_mgmt' && (!cluster.any_service_connectors_enabled || cluster.any_service_connectors_not_configured);
         });
