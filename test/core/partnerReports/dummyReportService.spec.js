073886d99b8520c48fdf5a56fe1b8308574e6cd2
khempel <khempel@cisco.com>
2015-08-09 16:22:27 -0500

    Fix for dummy report date mismatches

--- test/core/partnerReports/dummyReportService.spec.js
+++ test/core/partnerReports/dummyReportService.spec.js
@@ -14,6 +14,27 @@ describe('Controller: Dummy Reports', function () {
     label: "Test Org One"
   };
   var dummyData = getJSONFixture('core/json/partnerReports/dummyReportData.json');
+  var activeUser = angular.copy(dummyData.activeUser);
+  var mediaQuality = angular.copy(dummyData.mediaQuality);
+
+  var updateDates = function (data, time) {
+    var dayFormat = "MMM DD";
+    var monthFormat = "MMMM";
+    if (time.value === 0) {
+      for (var i = 6; i >= 0; i--) {
+        data[i].modifiedDate = moment().subtract(6 - i, 'day').format(dayFormat);
+      }
+    } else if (time.value === 1) {
+      for (var x = 0; x <= 3; x++) {
+        data[x].modifiedDate = moment().subtract((3 - x) * 7, 'day').startOf('week').format(dayFormat);
+      }
+    } else {
+      for (var y = 0; y <= 2; y++) {
+        data[y].modifiedDate = moment().subtract((2 - y), 'month').format(monthFormat);
+      }
+    }
+    return data;
+  };
 
   beforeEach(module('Core'));
 
@@ -23,9 +44,13 @@ describe('Controller: Dummy Reports', function () {
     }));
 
     it('dummyActiveUserData should return the expected responses', function () {
-      expect(DummyReportService.dummyActiveUserData(timeFilter[0])).toEqual(angular.copy(dummyData.activeUser.one));
-      expect(DummyReportService.dummyActiveUserData(timeFilter[1])).toEqual(angular.copy(dummyData.activeUser.two));
-      expect(DummyReportService.dummyActiveUserData(timeFilter[2])).toEqual(angular.copy(dummyData.activeUser.three));
+      activeUser.one = updateDates(activeUser.one, timeFilter[0]);
+      activeUser.two = updateDates(activeUser.two, timeFilter[1]);
+      activeUser.three = updateDates(activeUser.three, timeFilter[2]);
+
+      expect(DummyReportService.dummyActiveUserData(timeFilter[0])).toEqual(activeUser.one);
+      expect(DummyReportService.dummyActiveUserData(timeFilter[1])).toEqual(activeUser.two);
+      expect(DummyReportService.dummyActiveUserData(timeFilter[2])).toEqual(activeUser.three);
     });
 
     it('dummyActivePopulationData should return the expected response', function () {
@@ -33,9 +58,13 @@ describe('Controller: Dummy Reports', function () {
     });
 
     it('dummyMediaQualityData should return the expected responses', function () {
-      expect(DummyReportService.dummyMediaQualityData(timeFilter[0])).toEqual(angular.copy(dummyData.mediaQuality.one));
-      expect(DummyReportService.dummyMediaQualityData(timeFilter[1])).toEqual(angular.copy(dummyData.mediaQuality.two));
-      expect(DummyReportService.dummyMediaQualityData(timeFilter[2])).toEqual(angular.copy(dummyData.mediaQuality.three));
+      mediaQuality.one = updateDates(mediaQuality.one, timeFilter[0]);
+      mediaQuality.two = updateDates(mediaQuality.two, timeFilter[1]);
+      mediaQuality.three = updateDates(mediaQuality.three, timeFilter[2]);
+
+      expect(DummyReportService.dummyMediaQualityData(timeFilter[0])).toEqual(mediaQuality.one);
+      expect(DummyReportService.dummyMediaQualityData(timeFilter[1])).toEqual(mediaQuality.two);
+      expect(DummyReportService.dummyMediaQualityData(timeFilter[2])).toEqual(mediaQuality.three);
     });
 
     it('dummyCallMetricsData should return the expected response', function () {
