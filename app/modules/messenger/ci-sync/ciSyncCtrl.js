68e9edf362928104a5348a70c5f6e0b13dce18f4
Nader Matinnia <nmatinni@cisco.com>
2015-09-01 16:12:36 -0600

    Messenger: Adding Messenger sub-tab of Services tab

---
+++ app/modules/messenger/ci-sync/ciSyncCtrl.js
@@ -0,0 +1,105 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('Messenger')
+    .controller('CiSyncCtrl', CiSyncCtrl);
+
+  /** @ngInject */
+  function CiSyncCtrl($window, CiService) {
+    // Interface ---------------------------------------------------------------
+    var vm = this;
+
+    vm.statusOptions = Object.freeze({
+      on: {
+        badgeClass: 'badge-primary',
+        label: 'On'
+      },
+      off: {
+        badgeClass: 'badge-default',
+        label: 'Off'
+      }
+    });
+
+    vm.adminTypes = Object.freeze({
+      org: {
+        label: 'ORG Admin'
+      },
+      ops: {
+        label: 'OPS Admin'
+      }
+    });
+
+    vm.data = {
+      messengerOrgName: CiService.orgName,
+      linkDate: new Date('08/20/2015'),
+      isAuthRedirect: false,
+      isSyncing: false
+    };
+
+    vm.fields = [{
+      key: 'messengerOrgName',
+      type: 'input',
+      templateOptions: {
+        type: '',
+        label: 'Messenger Organization Name',
+        required: false,
+        disabled: true,
+        placeholder: ''
+      }
+    }, {
+      key: 'linkDate',
+      type: 'input',
+      templateOptions: {
+        type: 'date',
+        label: 'CI Link Date',
+        required: false,
+        disabled: true,
+        placeholder: ''
+      }
+    }];
+
+    vm.init = init;
+    vm.windowReady = windowReady;
+
+    // Event handlers
+    vm.setOrgAdmin = setOrgAdmin;
+    vm.setOpsAdmin = setOpsAdmin;
+
+    var domReady = false;
+
+    vm.init();
+
+    ////////////////////////////////////////////////////////////////////////////
+
+    // Implementation ----------------------------------------------------------
+
+    // CI Calls Inside
+    function init() {
+      vm.status = vm.statusOptions.on;
+    }
+
+    function windowReady() {
+      domReady = true;
+      setOrgAdmin();
+    }
+
+    function setOrgAdmin() {
+      $window.console.log('Setting to Org Admin');
+      vm.adminType = vm.adminTypes.org;
+
+      // Disable inputs
+      //$('#toggleIsSyncing').attr('is-disabled', true);
+      //$('#toggleAuthRedirect').attr('disabled', true);
+    }
+
+    function setOpsAdmin() {
+      $window.console.log('Setting to Ops Admin');
+      vm.adminType = vm.adminTypes.ops;
+
+      // Enable inputs
+      //$('#toggleIsSyncing').removeAttr('is-disabled');
+      //$('#toggleAuthRedirect').removeAttr('disabled');
+    }
+  }
+})();
