a6c00b0b4bcc0360081312f9c800f442d44cb244
Brad DeGrazia <degrazia@cisco.com>
2015-09-01 14:12:24 -0700

    DE1962 - escapeParameters

--- app/scripts/appconfig.js
+++ app/scripts/appconfig.js
@@ -74,7 +74,7 @@ angular
       $httpProvider.interceptors.push('ResponseInterceptor');
 
       // See ... http://angular-translate.github.io/docs/#/guide/19_security
-      $translateProvider.useSanitizeValueStrategy(null);
+      $translateProvider.useSanitizeValueStrategy('escapeParameters');
 
       $translateProvider.useStaticFilesLoader({
         prefix: 'l10n/',
