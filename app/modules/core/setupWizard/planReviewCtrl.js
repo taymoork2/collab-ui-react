052fbeaefcb2103442602185ad232494b2a77c60
Stig Murberg <stimurbe@cisco.com>
2015-09-07 11:00:41 +0200

    Sort by displayName, pageSize of 10, JSB

--- app/modules/core/setupWizard/planReviewCtrl.js
+++ app/modules/core/setupWizard/planReviewCtrl.js
@@ -48,106 +48,106 @@
     });
 
     function activate() {
-      vm.messagingServices.services = Authinfo.getMessageServices();
-      if (vm.messagingServices.services) {
-        angular.forEach(vm.messagingServices.services, function (service) {
-          if (service.license.isTrial) {
-            vm.trialExists = true;
-            vm.trialId = service.license.trialId;
+        vm.messagingServices.services = Authinfo.getMessageServices();
+        if (vm.messagingServices.services) {
+          angular.forEach(vm.messagingServices.services, function (service) {
+            if (service.license.isTrial) {
+              vm.trialExists = true;
+              vm.trialId = service.license.trialId;
 
-            if (service.license.status === 'PENDING') {
-              vm.messagingServices.isNewTrial = true;
+              if (service.license.status === 'PENDING') {
+                vm.messagingServices.isNewTrial = true;
+              }
             }
-          }
-        });
-      }
-
-      vm.commServices.services = Authinfo.getCommunicationServices();
-      if (vm.commServices.services) {
-        angular.forEach(vm.commServices.services, function (service) {
-          if (service.license.isTrial) {
-            vm.trialExists = true;
-            vm.trialId = service.license.trialId;
-
-            if (service.license.status === 'PENDING') {
-              vm.commServices.isNewTrial = true;
-            }
-          }
-        });
-      }
-
-      // AFAIK webex conferencing will never have trials
-      // so no need to check if it's a new trial.
-      vm.confServices.services = Authinfo.getConferenceServices();
-      if (vm.confServices.services) {
-        angular.forEach(vm.confServices.services, function (service) {
-          if (vm.gsxFeature && service.label.indexOf('Meeting Center') != -1) {
-            service.label = 'Meeting Center';
-          }
-          if (service.license.isTrial) {
-            vm.trialExists = true;
-            vm.trialId = service.license.trialId;
-            if (service.license.status === 'PENDING') {
-              vm.confServices.isNewTrial = true;
-            }
-          }
-        });
-      }
-      //check if the trial exists
-      if (vm.trialExists) {
-        vm.processing = true;
-        TrialService.getTrial(vm.trialId).then(function (trial) {
-          populateTrialData(trial);
-        }).finally(function () {
-          vm.processing = false;
-        });
-      }
-
-      vm.cmrServices.services = Authinfo.getCmrServices();
-
-      vm.sites = {};
-      var lastservice = {};
-      angular.forEach(vm.confServices.services, function (service) {
-        if (service.license) {
-          if (service.license.siteUrl) {
-            if (!vm.sites[service.license.siteUrl]) {
-              vm.sites[service.license.siteUrl] = [];
-            }
-            vm.sites[service.license.siteUrl].push(service);
-          }
+          });
         }
-      });
-      if (Object.prototype.toString.call(vm.cmrServices.services) == '[object Array]') {
-        angular.forEach(vm.cmrServices.services, function (service) {
+
+        vm.commServices.services = Authinfo.getCommunicationServices();
+        if (vm.commServices.services) {
+          angular.forEach(vm.commServices.services, function (service) {
+            if (service.license.isTrial) {
+              vm.trialExists = true;
+              vm.trialId = service.license.trialId;
+
+              if (service.license.status === 'PENDING') {
+                vm.commServices.isNewTrial = true;
+              }
+            }
+          });
+        }
+
+        // AFAIK webex conferencing will never have trials
+        // so no need to check if it's a new trial.
+        vm.confServices.services = Authinfo.getConferenceServices();
+        if (vm.confServices.services) {
+          angular.forEach(vm.confServices.services, function (service) {
+            if (vm.gsxFeature && service.label.indexOf('Meeting Center') != -1) {
+              service.label = 'Meeting Center';
+            }
+            if (service.license.isTrial) {
+              vm.trialExists = true;
+              vm.trialId = service.license.trialId;
+              if (service.license.status === 'PENDING') {
+                vm.confServices.isNewTrial = true;
+              }
+            }
+          });
+        }
+        //check if the trial exists
+        if (vm.trialExists) {
+          vm.processing = true;
+          TrialService.getTrial(vm.trialId).then(function (trial) {
+            populateTrialData(trial);
+          }).finally(function () {
+            vm.processing = false;
+          });
+        }
+
+        vm.cmrServices.services = Authinfo.getCmrServices();
+
+        vm.sites = {};
+        var lastservice = {};
+        angular.forEach(vm.confServices.services, function (service) {
           if (service.license) {
             if (service.license.siteUrl) {
               if (!vm.sites[service.license.siteUrl]) {
                 vm.sites[service.license.siteUrl] = [];
               }
-              service.label = $translate.instant('onboardModal.cmr');
               vm.sites[service.license.siteUrl].push(service);
             }
           }
         });
-      } else {
-        var cmrService = vm.cmrServices.services;
-        if (cmrService && cmrService.license) {
-          if (!vm.sites[cmrService.license.siteUrl]) {
-            vm.sites[cmrService.license.siteUrl] = [];
+        if (Object.prototype.toString.call(vm.cmrServices.services) == '[object Array]') {
+          angular.forEach(vm.cmrServices.services, function (service) {
+            if (service.license) {
+              if (service.license.siteUrl) {
+                if (!vm.sites[service.license.siteUrl]) {
+                  vm.sites[service.license.siteUrl] = [];
+                }
+                service.label = $translate.instant('onboardModal.cmr');
+                vm.sites[service.license.siteUrl].push(service);
+              }
+            }
+          });
+        } else {
+          var cmrService = vm.cmrServices.services;
+          if (cmrService && cmrService.license) {
+            if (!vm.sites[cmrService.license.siteUrl]) {
+              vm.sites[cmrService.license.siteUrl] = [];
+            }
+            cmrService.label = $translate.instant('onboardModal.cmr');
+            vm.sites[cmrService.license.siteUrl].push(cmrService);
           }
-          cmrService.label = $translate.instant('onboardModal.cmr');
-          vm.sites[cmrService.license.siteUrl].push(cmrService);
         }
-      }
 
-      //set the parent property for showdoitlater button based on trial states
-      if (!vm.messagingServices.isNewTrial && vm.commServices.isNewTrial && !vm.confServices.isNewTrial) {
-        if (angular.isDefined($scope.wizard)) {
-          $scope.wizard.showDoItLater = true;
+        //set the parent property for showdoitlater button based on trial states
+        if (!vm.messagingServices.isNewTrial && vm.commServices.isNewTrial && !vm.confServices.isNewTrial) {
+          if (angular.isDefined($scope.wizard)) {
+            $scope.wizard.showDoItLater = true;
+          }
         }
       }
-    }
-    /////////////////
+      /////////////////
 
     function populateTrialData(trial) {
       vm.trial = trial;
