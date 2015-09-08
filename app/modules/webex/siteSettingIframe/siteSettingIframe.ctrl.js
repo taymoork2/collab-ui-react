deebcb24c99afa01486cb4e1adee43508fc3a3bb
lhsieh <lhsieh@cisco.com>
2015-09-08 12:30:08 -0700

    Code changes for iFrame functionality.

--- app/modules/webex/siteSettingIframe/siteSettingIframe.ctrl.js
+++ app/modules/webex/siteSettingIframe/siteSettingIframe.ctrl.js
@@ -9,6 +9,8 @@
     '$filter',
     '$state',
     '$stateParams',
+    '$sce',
+    '$timeout',
     'Authinfo',
     'WebExSiteSettingIframeFact',
     'Notification',
@@ -20,6 +22,8 @@
       $filter,
       $state,
       $stateParams,
+      $sce,
+      $timeout,
       Authinfo,
       WebExSiteSettingIframeFact,
       Notification
@@ -41,10 +45,30 @@
       $scope.iframeUrl = _this.iframeUrl;
       $scope.adminEmail = _this.adminEmail;
       $scope.locale = _this.locale;
+      $scope.trustIframeUrl = $sce.trustAsResourceUrl(_this.iframeUrl);
+
+      _this.logMsg = _this.funcName + ": " + "\n" +
+        "siteUrl=" + $scope.siteUrl + "\n" +
+        "settingPageIframeUrl=" + $scope.settingPageIframeUrl + "\n" +
+        "iframeUrl=" + $scope.iframeUrl + "\n" +
+        "adminEmail=" + $scope.adminEmail + "\n" +
+        "locale=" + $scope.locale + "\n" +
+        "trustIframeUrl=" + $scope.trustIframeUrl;
+      $log.log(_this.logMsg);
+
+      // var secretIframeBtn = document.getElementById("secretIframeBtn");
+      // secretIframeBtn.click();
+      $timeout(
+        function () {
+          var secretIframeBtn = document.getElementById("secretIframeBtn");
+          secretIframeBtn.click();
+        },
+
+        0
+      );
 
       // _this.siteSettingIframeObj = WebExSiteSettingIframeFact.getSiteSettingIframeObj();
       // _this.siteSettingIframeObj = WebExSiteSettingIframeFact.initSiteSettingIframeObj();
-      // WebExSiteSettingIframeFact.loadIframe();
     } // function()
   ]); // angular.module().controller()
 })(); // function()
