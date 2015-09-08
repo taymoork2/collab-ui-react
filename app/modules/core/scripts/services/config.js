6f740f5b8d02439239d28ecc85d368168fe0fd36
Brian Spence <brspence@cisco.com>
2015-09-08 14:09:37 -0500

    Change onboarding batch size to 10 due to backend restriction

--- app/modules/core/scripts/services/config.js
+++ app/modules/core/scripts/services/config.js
@@ -443,7 +443,7 @@ angular.module('Core')
 
         defaultEntitlements: ['webex-squared', 'squared-call-initiation'],
 
-        batchSize: 20,
+        batchSize: 10,
 
         isDev: function () {
           var currentHostname = getCurrentHostname();
