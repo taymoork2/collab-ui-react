9253e95f7f8688367381e4878ad1c58586020ada
lhsieh <lhsieh@cisco.com>
2015-09-10 13:29:16 -0700

    Add Site Admin cross-launch link in the Settings Index page.

--- app/modules/webex/siteSettings/siteSettings.ctrl.js
+++ app/modules/webex/siteSettings/siteSettings.ctrl.js
@@ -36,14 +36,6 @@
       $scope.webexAdvancedUrl = Config.getWebexAdvancedHomeUrl(this.siteSettingsObj.siteUrl);
       $scope.adminEmailParam = Authinfo.getPrimaryEmail();
       $scope.localeParam = $translate.use();
-      $scope.iframeSref =
-    	"site-setting-iframe({" +
-    	"  siteUrl:'" + $stateParams.siteUrl + "'," +
-    	"  settingPageId:'pageId_xxxx'," +
-    	"  settingPageIframeUrl:'/dispatcher/AtlasIntegration.do?cmd=GoToSiteAdminEditUserPage'" +
-        "})";
-        
-      $log.log("iframeSref=" + $scope.iframeSref);
 
       $scope.trustSrc = function (src) {
         return $sce.trustAsResourceUrl(src);
