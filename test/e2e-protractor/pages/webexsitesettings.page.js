bed35dcca3e90d60c36e0949b24f06a89689e4e4
Jacob Pallapati <jpallapa@cisco.com>
2015-09-15 23:01:24 -0700

    US48565: Implement the bread crumb for the Index page

--- test/e2e-protractor/pages/webexsitesettings.page.js
+++ test/e2e-protractor/pages/webexsitesettings.page.js
@@ -12,12 +12,20 @@ var SiteSettigsPage = function () {
   this.conferencing = element(by.css('a[href="#site-list"]'));
   this.configureSite = element(by.css('a[href="#/webexSiteSettings"]'));
   this.webexSiteSettingsPanel = element(by.css('#webexSiteSettings'));
+  this.siteConfigBreadCrumbs = element(by.id('siteListCrumb'));
+  this.siteConfigFirstCrumb = element(by.id('siteListCrumb'));
 
   this.isWebexSiteSettingsPanelPresent = function () {
     return browser.driver.isElementPresent(by.css('#webexSiteSettings'));
   };
   this.waitForWebexSiteSettingsPanel = function () {
     browser.driver.wait(this.isWebexSiteSettingsPanelPresent, TIMEOUT);
+  };
+  this.isSiteConfigBreadCrumbsPanelPresent = function () {
+    return browser.driver.isElementPresent(by.id('siteListCrumb'));
+  };
+  this.waitForSiteConfigBreadCrumbsPanel = function () {
+    browser.driver.wait(this.isSiteConfigBreadCrumbsPanelPresent, TIMEOUT);
   };
 };
 
