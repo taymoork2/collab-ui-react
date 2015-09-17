f46ae34b5332c91f1262e339e14cd2a0d011ba4c
Dharmendra <desai.dharmendra@gmail.com>
2015-09-17 11:41:02 -0700

    add dependencies to siteReports

--- app/modules/webex/siteReports/siteReports.svc.js
+++ app/modules/webex/siteReports/siteReports.svc.js
@@ -1,34 +1,51 @@
 'use strict';
 
-angular.module('WebExReports').service('reportService', function () {
+angular.module('WebExReports').service('reportService',
 
-  var ReportsSection = function (sectionName, siteUrl, reportLinks) {
-    this.section_name = sectionName;
-    this.site_url = siteUrl;
-    this.report_links = reportLinks;
-  };
-  var Reports = function () {
-    this.sections = [];
-    this.setSections = function (secs) {
-      this.sections = secs;
-    };
-    this.getSections = function () {
-      return this.sections;
-    };
-  };
+  [
+    '$q',
+    '$log',
+    '$stateParams',
+    '$translate',
+    '$filter',
+    'Authinfo',
+    'WebExUtilsFact',
+    'WebExXmlApiFact',
+    'WebExXmlApiInfoSvc',
+    'Notification',
 
-  this.getReports = function (siteUrl) {
+    function () {
 
-    //get the reports list, for now hard code.
+      var ReportsSection = function (sectionName, siteUrl, reportLinks) {
+        this.section_name = sectionName;
+        this.site_url = siteUrl;
+        this.report_links = reportLinks;
+      };
+      var Reports = function () {
+        this.sections = [];
+        this.setSections = function (secs) {
+          this.sections = secs;
+        };
+        this.getSections = function () {
+          return this.sections;
+        };
+      };
 
-    var sec1 = new ReportsSection("Section1", siteUrl, ["/x/y/z", "/u/io/p"]);
-    var sec2 = new ReportsSection("Section2", siteUrl, ["/u/y/z", "www.yahoo.com"]);
+      this.getReports = function (siteUrl) {
 
-    var repts = new Reports();
-    repts.setSections([sec1, sec2]);
+        //get the reports list, for now hard code.
 
-    return repts;
+        var sec1 = new ReportsSection("Section1", siteUrl, ["/x/y/z", "/u/io/p"]);
+        var sec2 = new ReportsSection("Section2", siteUrl, ["/u/y/z", "www.yahoo.com"]);
 
-  };
+        var repts = new Reports();
+        repts.setSections([sec1, sec2]);
 
-});
+        return repts;
+
+      };
+
+    } //end function 
+  ]
+
+);
