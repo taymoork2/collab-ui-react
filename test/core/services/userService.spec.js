3f3062dc4c48e8332decfef544ccdc92cd6b8c08
Nishant <nchitham@cisco.com>
2015-09-28 17:05:40 -0700

    BLUE-1512: Users converted from consumer org to partner org doesn't get a line assigned

--- test/core/services/userService.spec.js
+++ test/core/services/userService.spec.js
@@ -4,19 +4,23 @@ describe('User Service', function () {
   beforeEach(module('Core'));
   beforeEach(module('Huron'));
 
-  var $httpBackend, Config, Userservice;
+  var $httpBackend, Config, Userservice, HuronConfig, rootScope;
 
   var Authinfo = {
     getOrgId: jasmine.createSpy('getOrgId').and.returnValue('1')
   };
+
   beforeEach(module(function ($provide) {
     $provide.value("Authinfo", Authinfo);
   }));
 
-  beforeEach(inject(function (_$httpBackend_, _Userservice_, _Config_) {
+  beforeEach(inject(function (_$httpBackend_, $injector, _Config_, _HuronConfig_, _$rootScope_) {
     $httpBackend = _$httpBackend_;
-    Userservice = _Userservice_;
+    Userservice = $injector.get("Userservice");
+    rootScope = _$rootScope_;
+    spyOn(rootScope, '$broadcast').and.returnValue({});
     Config = _Config_;
+    HuronConfig = _HuronConfig_;
   }));
 
   afterEach(function () {
@@ -31,6 +35,47 @@ describe('User Service', function () {
     $httpBackend.expectDELETE(Config.getAdminServiceUrl() + 'organization/' + Authinfo.getOrgId() + '/user?email=' + user.email).respond(204);
     Userservice.deactivateUser(user);
     $httpBackend.flush();
+  });
+
+  it('updateUsers should send POST request to create user', function () {
+    var users = [{
+      address: 'dntodid@gmail.com',
+      assignedDn: {
+        uuid: "testUUID",
+        pattern: "5000"
+      },
+      externalNumber: {
+        uuid: "testUUID",
+        pattern: "+123423423445"
+      }
+    }, {
+      address: 'dntodid1@gmail.com',
+      assignedDn: {
+        uuid: "testUUID",
+        pattern: "5001"
+      },
+      externalNumber: {
+        uuid: "testUUID",
+        pattern: "+133423423445"
+      }
+    }];
+    $httpBackend.expectPATCH(Config.getAdminServiceUrl() + 'organization/' + Authinfo.getOrgId() + '/users').respond(201, {
+      status: 200,
+      userResponse: [{
+        email: "dntodid@gmail.com",
+        entitled: ["ciscouc"]
+      }, {
+        email: "dntodid1@gmail.com",
+        entitled: ["ciscouc"]
+      }]
+    });
+    $httpBackend.when('POST', HuronConfig.getCmiV2Url() + '/customers/' + Authinfo.getOrgId() + '/users').respond(201);
+    $httpBackend.when('GET', HuronConfig.getCmiUrl() + '/voice/customers/' + Authinfo.getOrgId() + '/users/directorynumbers').respond(200);
+    $httpBackend.when('POST', HuronConfig.getCmiUrl() + '/identity/users/otp').respond(201);
+    $httpBackend.when('POST', HuronConfig.getEmailUrl() + '/email/userwelcome').respond(201);
+    Userservice.updateUsers(users);
+    $httpBackend.flush();
+    expect(rootScope.$broadcast).toHaveBeenCalledWith('Userservice::updateUsers');
   });
 
 });
