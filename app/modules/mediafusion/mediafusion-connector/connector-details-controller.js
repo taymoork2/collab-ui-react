84c29992972d9e696f3d9db3f97b613c60065b69
napal <napal@localhost.localdomain>
2015-10-08 10:26:43 +0530

    @rev 258557 Assign and list existing groups for media fusion device - using new fms properties api

--- app/modules/mediafusion/mediafusion-connector/connector-details-controller.js
+++ app/modules/mediafusion/mediafusion-connector/connector-details-controller.js
@@ -4,7 +4,7 @@ angular.module('Mediafusion')
   .controller('ConnectorDetailsController',
 
     /* @ngInject */
-    function ($scope, $state, $stateParams, MediafusionProxy, MediafusionClusterService) {
+    function ($scope, $state, $stateParams, MediafusionProxy, MediafusionClusterService, $log) {
       $scope.visibleAlarm = {};
       $scope.clusters = [];
       $scope.selectedCluster = '';
@@ -20,8 +20,32 @@ angular.module('Mediafusion')
 
       $scope.groups = MediafusionClusterService.getGroups();
 
-      $scope.selectGroup = function (group) {
-        $scope.displayName = group.name;
+
+      $scope.onSelect = function ($item, $model, $label) {
+        $log.log("$scope.groups :", $scope.groups);
+        //var groupList = $scope.groups.value;
+        //$log.log("$groupList :", groupList);
+        $scope.currentPropertySetId = _.chain(scope.groups)
+          .filter(function (group) {
+            $log.log('group :', group);
+            $log.log('group value :', group.value);
+            $log.log('group name :', group.value["name"]); 
+            return group.value.name == $scope.displayName;
+          })
+          .pluck('id')
+          .compact()
+          .first()
+          .value();
+
+        $scope.displayName = $item.name;
+        $log.log('selected group name :', $item.name);
+        $log.log('selected group id :', $item.id);
+        $log.log('selected properties :', $item.properties);
+        $log.log('selected properties value:', $item.properties["mf.group.displayName"]);
+        $log.log("currentPropertySetId: ", $scope.currentPropertySetId);
+        MediafusionClusterService.updateGroupAssignment($stateParams.connectorId, $item.id);
+
       };
+
     }
   );
