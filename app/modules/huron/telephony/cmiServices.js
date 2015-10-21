694b1f205bb7020dfebbb11778718cac10a9593a
Sundar Rajan Muthuraj <sumuthur@cisco.com>
2015-10-21 11:59:19 -0500

    Added MockHg endpoints

--- app/modules/huron/telephony/cmiServices.js
+++ app/modules/huron/telephony/cmiServices.js
@@ -98,20 +98,20 @@
   })
 
   .factory('NumberSearchServiceV2', function ($resource, HuronConfig) {
-    //var baseUrl = HuronConfig.getMockHgUrl();
-    var baseUrl = HuronConfig.getCmiV2Url();
+    var baseUrl = HuronConfig.getMockHgUrl();
+    //var baseUrl = HuronConfig.getCmiV2Url();
     return $resource(baseUrl + '/customers/:customerId/numbers', {
-      //secret: 'sunlight',
+      secret: 'sunlight', // TODO: Remove this parameter when Mock is replaced with CmiV2.
       customerId: '@customerId',
       number: '@number'
     });
   })
 
   .factory('HuntGroupServiceV2', function ($resource, HuronConfig) {
-    //var baseUrl = HuronConfig.getMockHgUrl();
-    var baseUrl = HuronConfig.getCmiV2Url();
+    var baseUrl = HuronConfig.getMockHgUrl();
+    //var baseUrl = HuronConfig.getCmiV2Url();
     return $resource(baseUrl + '/customers/:customerId/features/huntgroups/:huntGroupId', {
-      //secret: 'sunlight',
+      secret: 'sunlight', // TODO: Remove this parameter when Mock is replaced with CmiV2.
       customerId: '@customerId',
       huntGroupId: '@huntGroupId'
     }, {});
