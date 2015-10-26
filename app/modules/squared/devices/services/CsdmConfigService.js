d80e372b2616baf80c9445c628065c08d9c0abff
Wouter Lammers <woulamme@cisco.com>
2015-10-26 09:05:00 +0100

    Bug 217929 - Security issue that allows an attacker to hijack OAuth tokens
    Removing CSDM-URL override in URL parameter

--- app/modules/squared/devices/services/CsdmConfigService.js
+++ app/modules/squared/devices/services/CsdmConfigService.js
@@ -1,24 +1,11 @@
 'use strict';
 
 angular.module('Squared')
-  .service('CsdmConfigService', ['$window', 'Config',
-    function ConfigService($window, Config) {
-
-      var getOverriddenUrl = function (queryParam) {
-        var regex = new RegExp(queryParam + "=([^&]*)");
-        var match = $window.location.search.match(regex);
-        if (match && match.length == 2) {
-          return decodeURIComponent(match[1]);
-        }
-      };
+  .service('CsdmConfigService', ['Config',
+    function ConfigService(Config) {
 
       var getUrl = function () {
-        var overriddenUrl = getOverriddenUrl('csdm-url');
-        if (overriddenUrl) {
-          return overriddenUrl;
-        } else {
-          return Config.getCsdmServiceUrl();
-        }
+        return Config.getCsdmServiceUrl();
       };
 
       var getEnrollmentServiceUrl = function () {
