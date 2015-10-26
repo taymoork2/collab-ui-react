dbd5c6ee9d52035bbac56a09282488122e39137a
Helge Langehaug <hellange@cisco.com>
2015-10-26 15:02:33 +0100

    Hybrid services: Removed unused email validation and alarm functionality.

--- app/modules/hercules/expressway-service/expressway-service-controller.js
+++ app/modules/hercules/expressway-service/expressway-service-controller.js
@@ -44,49 +44,6 @@
     }
   };
 
-  //TODO: Rewrite or use some existing stuff !!!!!!!!!!!!
-  var enableEmailValidation = function () {
-    //TODO: Someone rewrite code below - it's just a placeholder
-    //No real functionality here, just needed something so I could style the input
-
-    $(document).on('keyup', '#add-mails', function (e) {
-      //console.log(e.keyCode);
-      switch (e.keyCode) {
-      case 13:
-        addMail();
-        break;
-
-      case 186:
-        addMail();
-        break;
-
-      case 188:
-        addMail();
-        break;
-      }
-
-      function addMail() {
-        var mail = $("#add-mails").val();
-        var pattern =
-          /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
-        if (mail.indexOf(",") > -1 || mail.indexOf(";") > -1) {
-          //console.log("has , or ;");
-          mail = mail.slice(0, -1);
-        }
-
-        if (mail !== "") {
-          if (!pattern.test(mail)) {
-            $("#existing-mails").append("<p class='token-label alert'>" + mail + "<span class='del icon icon-close'></span></p>");
-          } else {
-            $("#existing-mails").append("<p class='token-label'>" + mail + "<span class='del icon icon-close'></span></p>");
-          }
-        }
-
-        $("#add-mails").val("");
-      }
-    });
-  };
-
   /* @ngInject */
   function ExpresswayServiceController(XhrNotificationService, NotificationService, ServiceStateChecker, ServiceDescriptor, $stateParams, $state,
     $modal,
@@ -272,16 +229,6 @@
       });
     };
 
-    vm.showAlarms = function () {
-      $modal.open({
-        templateUrl: "modules/hercules/expressway-service/alarms.html",
-        controller: AlarmsController,
-        controllerAs: "alarmsDialog"
-      }).result.then(function () {
-        //console.log("Starting alarms dialog...");
-      });
-    };
-
     vm.deleteHost = function (host) {
       //console.log("Delete host ",host)
       return ClusterService.deleteHost(vm.clusterId, host.serial).then(function () {
@@ -301,29 +248,6 @@
         $modalInstance.dismiss();
       };
       modalVm.clusterName = vm.cluster.name;
-    }
-
-    /* @ngInject */
-    function AlarmsController($modalInstance) {
-      var alarmsVm = this;
-      alarmsVm.connectors = vm.selectedService.connectors;
-
-      alarmsVm.colorFromSeverity = function (alarm) {
-        if (alarm.severity === "error") {
-          return "red";
-        } else if (alarm.severity === "critical") {
-          return "orange";
-        } else {
-          return "black";
-        }
-      };
-
-      alarmsVm.ok = function () {
-        $modalInstance.close();
-      };
-      alarmsVm.cancel = function () {
-        $modalInstance.dismiss();
-      };
     }
   }
 
@@ -349,8 +273,6 @@
     vm.wx2users = "";
     vm.serviceType = $stateParams.serviceType;
     vm.serviceId = serviceType2ServiceId(vm.serviceType);
-
-    enableEmailValidation();
 
     var readCerts = function () {
       CertService.getCerts(Authinfo.getOrgId()).then(function (res) {
