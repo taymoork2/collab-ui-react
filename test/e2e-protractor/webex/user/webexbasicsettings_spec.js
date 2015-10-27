9e33cd00f32a6fc4c9817569a997991dd98a28ca
lhsieh <lhsieh@cisco.com>
2015-10-27 00:02:36 -0700

    Temporarily turn off Webex User Setting e2e protractor tests.

---
+++ test/e2e-protractor/webex/user/webexbasicsettings_spec.js
@@ -0,0 +1,114 @@
+'use strict';
+
+/* global describe, xit, expect, login */
+
+describe('WebEx user settings', function () {
+
+  xit('should allow login as admin user', function () {
+    login.loginThroughGui(usersettings.testAdmin.username, usersettings.testAdmin.password);
+  });
+
+  xit('click on users tab', function () {
+    navigation.clickUsers();
+  });
+
+  xit('should allow search and click on user', function () {
+    utils.search(usersettings.testUser.username);
+    users.clickOnUser();
+  });
+
+  xit('should allow click on conferencing', function () {
+    utils.wait(users.conferencingService);
+    expect(users.conferencingService.isPresent()).toBeTruthy();
+    utils.click(users.conferencingService);
+  });
+
+  xit('should allow click on site name', function () {
+    utils.wait(usersettings.testSiteElement);
+    expect(usersettings.testSiteElement.isPresent()).toBeTruthy();
+    utils.click(usersettings.testSiteElement);
+  });
+
+  xit('should display basic WebEx settigns page', function () {
+    utils.wait(usersettings.userSettingsPanel);
+    expect(usersettings.userSettingsPanel.isPresent()).toBeTruthy();
+    expect(usersettings.userSettingsPanel.isDisplayed()).toBeTruthy();
+  });
+
+  xit('should not display WebEx error page', function () {
+    expect(usersettings.errorPanel.isPresent()).toBeFalsy();
+  });
+
+  xit('should allow navigation to the 4th panel', function () {
+    utils.click(usersettings.userPrivilegesLink);
+    utils.wait(usersettings.userPrivilegesPanel);
+    expect(usersettings.userPrivilegesPanel.isPresent()).toBeTruthy();
+    expect(usersettings.userPrivilegesPanel.isDisplayed()).toBeTruthy();
+  });
+
+  xit('should allow navigation back to the 3rd panel', function () {
+    utils.clickLastBreadcrumb();
+    expect(usersettings.userSettingsPanel.isPresent()).toBeTruthy();
+    expect(usersettings.userSettingsPanel.isDisplayed()).toBeTruthy();
+  });
+
+  xit('should not show save button without any changes', function () {
+    expect(usersettings.saveButton.isPresent()).toBeFalsy();
+  });
+
+  xit('should allow edit in 3rd panel', function () {
+    expect(usersettings.mcAuoCheckbox.isPresent());
+    usersettings.mcAuo.click();
+    expect(usersettings.saveButton.isPresent()).toBeTruthy();
+  });
+
+  xit('should allow save in 3rd panel', function () {
+    usersettings.save();
+    //    expect(usersettings.alertSuccess.isDisplayed()).toBeTruthy();
+  });
+
+  /**  
+    xit('should allow edit in 4th panel', function () {
+      utils.click(usersettings.userPrivilegesLink);
+      utils.wait(usersettings.userPrivilegesPanel);
+      expect(usersettings.userPrivilegesPanel.isPresent()).toBeTruthy();
+      expect(usersettings.userPrivilegesPanel.isDisplayed()).toBeTruthy();
+      if (usersettings.userPrivilegesPanel.telephonyPrivileges.isPresent()) {
+        expect(usersettings.callInTeleconfCheckbox.isPresent());
+      }
+
+    });
+
+    xit('should not allow save of mc AUO without mc PRO', function () {
+      if (usersettings.mc.isPresent() && usersettings.mc.isDisplayed()) {
+        usersettings.unSelectAllMcSessionTypeByPrefix('PRO');
+        usersettings.selectAllMcSessionTypeByPrefix('AUO');
+        expect(usersettings.saveButton.isDisplayed()).toBeTruthy();
+        usersettings.save();
+        expect(usersettings.alertError.isDisplayed()).toBeTruthy();
+        usersettings.alertError.click();
+      }
+    });
+
+    xit('should allow save of mc AUO with mc PRO', function () {
+      if (usersettings.mc.isPresent() && usersettings.mc.isDisplayed()) {
+        usersettings.selectAllMcSessionTypeByPrefix('PRO');
+        usersettings.selectAllMcSessionTypeByPrefix('AUO');
+        expect(usersettings.saveButton.isDisplayed()).toBeTruthy();
+        usersettings.save();
+        expect(usersettings.alertSuccess.isDisplayed()).toBeTruthy();
+        usersettings.alertSuccess.click();
+      }
+    });
+  **/
+
+  /**  
+    xit('should pause', function () {
+      browser.pause();
+    });
+  **/
+
+  xit('should allow log out', function () {
+    navigation.logout();
+  });
+});
