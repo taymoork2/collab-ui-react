7c6932cfd3bdf633e93e583a72bb907969149ab0
lhsieh <lhsieh@cisco.com>
2015-10-27 18:06:37 -0700

    US50842: Automated e2e tests for reports pages

--- test/e2e-protractor/pages/webexsitereports.page.js
+++ test/e2e-protractor/pages/webexsitereports.page.js
@@ -12,13 +12,16 @@ var SiteReportsPage = function () {
 
   this.conferencing = element(by.css('a[href="#site-list"]'));
   this.siteReports = element(by.css('#webex-reports-list-iframe'));
-  this.siteReportsUrl = '/webexreports';
+  this.siteReportsUrl = '/reports';
   this.reportSJSITE14 = element(by.id("sjsite14.webex.com_webex-site-reports"));
+  this.webexSiteReportsPanel = element(by.id('webexSiteReports'));
   this.webexReportCrumb = element(by.id('webexReportCrumb'))
   this.webexReportBreadCrumbs = element(by.id('webexReportBreadCrumbs'));
-  this.meetingUsageReportLink = element(by.id('meeting_usage'));
   this.webexReportIFrameBreadCrumbs = element(by.id('webexReportIFrameBreadCrumbs'));
   this.webexReportIFrameCrumb2 = element(by.id('webexReportIFrameCrumb2'));
+  this.webexCommonReportsCardId = element(by.id('common_reports'));
+  this.meetingUsageReportLink = element(by.id('meeting_usage'));
+
   this.siteAdminReportsUrl = '/webexreports';
 };
 
