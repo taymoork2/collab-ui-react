e15c27fede00ab9940cda6eed85097f20512074a
lhsieh <lhsieh@cisco.com>
2015-10-27 22:44:03 -0700

    US50842: Automated e2e tests for reports pages

--- gulp.config.js
+++ gulp.config.js
@@ -100,7 +100,8 @@ module.exports = function() {
         huron: e2e + '/huron/**/*_spec.js',
         mediafusion: e2e + '/mediafusion/**/*_spec.js',
         squared: e2e + '/squared/**/*_spec.js',
-        webexsite: e2e + '/webex/site/**/*_spec.js',
+        webexsitesettings: e2e + '/webex/siteSettings/**/*_spec.js',
+        webexsitereports: e2e + '/webex/siteReports/**/*_spec.js',
         webexuser: e2e + '/webex/user/**/*_spec.js',
         sunlight: e2e + '/sunlight/**/*_spec.js'
       }
