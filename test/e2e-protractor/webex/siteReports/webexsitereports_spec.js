9f3fabf92ebc7d56f2318dfbce56be47d6c71184
lhsieh <lhsieh@cisco.com>
2015-10-28 12:50:01 -0700

    US50842: Automated e2e tests for reports pages

--- test/e2e-protractor/webex/siteReports/webexsitereports_spec.js
+++ test/e2e-protractor/webex/siteReports/webexsitereports_spec.js
@@ -14,47 +14,47 @@ describe('WebEx site reports', function () {
     utils.click(sitereports.webexReportsLink);
     utils.wait(sitereports.webexSiteReportsPanel);
   });
-  
+
   it('click on common reports meetings in progress link', function () {
     utils.wait(sitereports.webexCommonMeetingsInProgressLink);
     utils.click(sitereports.webexCommonMeetingsInProgressLink);
     utils.wait(sitereports.webexCommonMeetingsInProgressId);
   });
-  
+
   it('Click on site reports index bread crumb link', function () {
-	utils.wait(sitereports.webexReportCrumb2);
+    utils.wait(sitereports.webexReportCrumb2);
     utils.click(sitereports.webexReportCrumb2);
     utils.wait(sitereports.webexSiteReportsPanel);
   });
 
   it('click on common reports meeting usage link', function () {
-	    utils.wait(sitereports.webexCommonMeetingUsageLink);
-	    utils.click(sitereports.webexCommonMeetingUsageLink);
-	    utils.wait(sitereports.webexCommonMeetingUsageId);
+    utils.wait(sitereports.webexCommonMeetingUsageLink);
+    utils.click(sitereports.webexCommonMeetingUsageLink);
+    utils.wait(sitereports.webexCommonMeetingUsageId);
   });
-  
+
   it('Click on site reports index bread crumb link', function () {
-		utils.wait(sitereports.webexReportCrumb2);
-	    utils.click(sitereports.webexReportCrumb2);
-	    utils.wait(sitereports.webexSiteReportsPanel);
+    utils.wait(sitereports.webexReportCrumb2);
+    utils.click(sitereports.webexReportCrumb2);
+    utils.wait(sitereports.webexSiteReportsPanel);
   });
 
   it('click on common reports recording usage link', function () {
-	    utils.wait(sitereports.webexCommonRecordingUsageLink);
-	    utils.click(sitereports.webexCommonRecordingUsageLink);
-	    utils.wait(sitereports.webexCommonRecordingUsageId);
+    utils.wait(sitereports.webexCommonRecordingUsageLink);
+    utils.click(sitereports.webexCommonRecordingUsageLink);
+    utils.wait(sitereports.webexCommonRecordingUsageId);
   });
 
   it('Click on site reports index bread crumb link', function () {
-		utils.wait(sitereports.webexReportCrumb2);
-	    utils.click(sitereports.webexReportCrumb2);
-	    utils.wait(sitereports.webexSiteReportsPanel);
+    utils.wait(sitereports.webexReportCrumb2);
+    utils.click(sitereports.webexReportCrumb2);
+    utils.wait(sitereports.webexSiteReportsPanel);
   });
 
   it('click on common reports storage usage link', function () {
-	    utils.wait(sitereports.webexCommonStorageUsageLink);
-	    utils.click(sitereports.webexCommonStorageUsageLink);
-	    utils.wait(sitereports.webexCommonStorageUsageId);
+    utils.wait(sitereports.webexCommonStorageUsageLink);
+    utils.click(sitereports.webexCommonStorageUsageLink);
+    utils.wait(sitereports.webexCommonStorageUsageId);
   });
 
   /**  
