e6bda1dea95425bda788979985229fe221fe438e
sjalipar <sjalipar@cisco.com>
2015-10-29 13:31:39 -0500

    flushed the httpBackend after unit test cases

--- test/huron/features/huntGroup/huntGroupService.spec.js
+++ test/huron/features/huntGroup/huntGroupService.spec.js
@@ -17,50 +17,57 @@ describe('Hunt Group Service', function () {
     huntGroupService = _HuntGroupService_;
     customerId = '123';
     huntGroupId = '456';
-    getHuntListUrl = HuronConfig.getCmiV2Url() + "/customers/123/features/huntgroups";
-    deleteHGUrl = HuronConfig.getCmiV2Url() + '/customers/123/features/huntgroups/456';
+    getHuntListUrl = HuronConfig.getMockHgUrl() + '/customers/' + customerId + '/features/huntgroups?secret=sunlight';
+    deleteHGUrl = HuronConfig.getMockHgUrl() + '/customers/' + customerId + '/features/huntgroups/' + huntGroupId + '?secret=sunlight';
     huntList = {
       "url": getHuntListUrl,
       "items": [{
         "uuid": "abcd1234-abcd-abcd-abcddef123456",
         "name": "Technical Support",
-        "numbers": ["5076", "(414) 555-1244"],
+        "numbers": ["5076", "4145551244"],
         "memberCount": 2
       }, {
         "uuid": "bbcd1234-abcd-abcd-abcddef123456",
         "name": "Marketing",
-        "numbers": ["5076", "(124) 456-7890", "(414) 555-1244", "(414) 555-1245"],
+        "numbers": ["5076", "1244567890", "4145551244", "4145551245"],
         "memberCount": 16
       }]
     };
   }));
 
+  afterEach(function () {
+    $httpBackend.flush();
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
   it('should be able to get list of huntGroups for a given customerId', function () {
     $httpBackend.whenGET(getHuntListUrl).respond(200, huntList);
     huntGroupService.getListOfHuntGroups(customerId).then(function (response) {
-      expect(response.data.url).toBe(getHuntListUrl);
-      expect(response.data.items).toBe(huntList.items);
+      expect(response.url).toEqual(getHuntListUrl);
+      expect(response.items).toEqual(huntList.items);
     });
   });
 
   it('should fail to get list of huntGroups when server gives an error', function () {
     $httpBackend.whenGET(getHuntListUrl).respond(500);
     huntGroupService.getListOfHuntGroups(customerId).then(function (response) {}, function (response) {
-      expect(response.status).toBe(500);
+      expect(response.status).toEqual(500);
     });
   });
 
-  it('should be able to delete a given huntGroup for a given customerId', function () {
-    $httpBackend.whenDELETE(deleteHGUrl).respond(200);
-    huntGroupService.deleteHuntGroup(customerId, huntGroupId).then(function (response) {
-      expect(response.status).toBe(200);
+  it('should be able to delete a given huntGroup for a given customerId and huntGroupId', function () {
+    $httpBackend.expectDELETE(deleteHGUrl).respond('OK');
+    huntGroupService.deleteHuronHuntGroup(customerId, huntGroupId).then(function (resp) {
+      expect(resp[0]).toEqual('O');
+      expect(resp[1]).toEqual('K');
     });
   });
 
   it('should fail to delete a given huntGroup when server gives an error', function () {
-    $httpBackend.whenDELETE(deleteHGUrl).respond(500);
-    huntGroupService.deleteHuntGroup(customerId, huntGroupId).then(function (response) {}, function (response) {
-      expect(response.status).toBe(500);
+    $httpBackend.expectDELETE(deleteHGUrl).respond(500);
+    huntGroupService.deleteHuronHuntGroup(customerId, huntGroupId).then(function (response) {}, function (response) {
+      expect(response.status).toEqual(500);
     });
   });
 });
