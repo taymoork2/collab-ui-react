5e190f00832f90e2a5c8f7b691b48ac45271c76a
Chris Laughbon <claughbo@cisco.com>
2015-11-03 09:36:43 -0800

    US203704

--- app/modules/huron/features/autoAttendant/builder/aaBuilderNumbersCtrl.js
+++ app/modules/huron/features/autoAttendant/builder/aaBuilderNumbersCtrl.js
@@ -6,14 +6,14 @@
     .controller('AABuilderNumbersCtrl', AABuilderNumbersCtrl); /* was AutoAttendantGeneralCtrl */
 
   /* @ngInject */
-  function AABuilderNumbersCtrl($q, $stateParams, AAUiModelService, AutoAttendantCeInfoModelService, AutoAttendantCeMenuModelService, Authinfo, AutoAttendantCeService,
-    AAModelService, Notification) {
+  function AABuilderNumbersCtrl($scope, $q, $stateParams, AAUiModelService, AutoAttendantCeInfoModelService,
+    AAModelService, ExternalNumberPoolService, Authinfo, Notification, $translate) {
     var vm = this;
-
-    vm.aaModel = {};
-    vm.ui = {};
-    vm.ui.ceInfo = {};
-    vm.resourceInfos = {};
+    vm.addOne = addOne;
+    vm.deleteAAResource = deleteAAResource;
+    vm.filter = filter;
+    vm.getDupeNumberAnyAA = getDupeNumberAnyAA;
+    vm.getExternalNumbers = getExternalNumbers;
 
     vm.inputNumber = {
       id: '',
@@ -22,99 +22,81 @@
       trigger: 'incomingCall'
     };
 
+    // vm.selectedPhoneNums = [];
+
+    vm.externalPhoneNums = [];
+    vm.availablePhoneNums = [];
     vm.addAAResourceFromInputNumber = addAAResourceFromInputNumber;
     vm.deleteAAResource = deleteAAResource;
 
+    vm.selectPlaceHolder = $translate.instant('autoAttendant.selectPlaceHolder');
+
+    vm.inputPlaceHolder = $translate.instant('autoAttendant.inputPlaceHolder');
+
+    // needed for cs-select's model, aaBuilderNumbers.tpl.html
+    vm.selected = {
+      "label": "",
+      "value": ""
+    };
+
     /////////////////////
 
+    function addOne(value) {
+      var index = vm.availablePhoneNums.indexOf(value);
+
+      if (index >= 0) {
+
+        vm.inputNumber.number = value.replace(/\D/g, '');
+        addAAResourceFromInputNumber();
+
+        vm.availablePhoneNums.splice(index, 1);
+
+      }
+
+    }
+
+    function getDupeNumberAnyAA(testNum) {
+
+      var isNumberInUseOtherAA = vm.aaModel.aaRecords.some(function (record) {
+        return record.assignedResources.some(function (resource) {
+          return resource.number === testNum;
+        });
+      });
+      return isNumberInUseOtherAA;
+
+    }
+
     function addAAResourceFromInputNumber() {
+
       if (angular.isUndefined(vm.inputNumber) || vm.inputNumber.number === '') {
         return;
       }
 
+      // the server POST schema specifies an id and number field but the number is actually not used; the id is used as the number
+      // So just set them both to the number
+      vm.inputNumber.id = vm.inputNumber.number;
+
+      // add new resource for this number
+      var resource = AutoAttendantCeInfoModelService.newResource();
+      resource.setNumber(vm.inputNumber.number);
+      resource.setId(vm.inputNumber.id);
+      resource.setType(vm.inputNumber.type);
+      resource.setTrigger(vm.inputNumber.trigger);
+
       var resources = vm.ui.ceInfo.getResources();
+      resources.push(resource);
 
-      // already added to this AA
-      var isNumberInUse = vm.ui.ceInfo.getResources().some(function (resource) {
-        return resource.getNumber() === vm.inputNumber.number;
-      });
-      if (isNumberInUse) {
-        Notification.error('autoAttendant.errorAddNumberInUse', {
-          number: vm.inputNumber.number
-        });
-        return;
-      }
-
-      // already on resource list for another AA
-      var otherAAName = '';
-      var isNumberInUseOtherAA = vm.aaModel.aaRecords.some(function (record) {
-        otherAAName = record.callExperienceName;
-        if (otherAAName !== vm.ui.ceInfo.getName()) {
-          return record.assignedResources.some(function (resource) {
-            return resource.number === vm.inputNumber.number;
-          });
-        }
-      });
-      if (isNumberInUseOtherAA) {
-        Notification.error('autoAttendant.errorAddNumberInUseOtherAA', {
-          name: otherAAName,
-          number: vm.inputNumber.number
-        });
-        return;
-      }
-
-      // check if it exists in system and get the id
-      $q.all([setInputNumberId(vm.inputNumber.number)])
-        .then(function (result) {
-          if (vm.inputNumber.id === '') {
-            Notification.error('autoAttendant.errorAddNumberNotFound', {
-              number: vm.inputNumber.number
-            });
-            return;
-          } else {
-            // add new resource for this number
-            var resource = AutoAttendantCeInfoModelService.newResource();
-            resource.setNumber(vm.inputNumber.number);
-            resource.setId(vm.inputNumber.id);
-            resource.setType(vm.inputNumber.type);
-            resource.setTrigger(vm.inputNumber.trigger);
-
-            resources.push(resource);
-
-            AutoAttendantCeInfoModelService.setCeInfo(vm.aaModel.aaRecord, vm.ui.ceInfo);
-            resetInputNumber();
-          }
-        });
-    }
-
-    function setInputNumberId(number) {
-      vm.inputNumber.id = number;
-      /*  workaround for Tropo-AA integration
-      return DirectoryNumberService.query({
-          customerId: Authinfo.getOrgId(),
-          directoryNumberId: '',
-          pattern: number
-        }).$promise
-        .then(function (dirNumbers) {
-          if (angular.isArray(dirNumbers) && dirNumbers.length > 0) {
-            vm.inputNumber.id = dirNumbers[0].uuid;
-          } else {
-            vm.inputNumber.id = '';
-          }
-        });
-        */
-    }
-
-    function resetInputNumber() {
-      vm.inputNumber.number = '';
-      vm.inputNumber.id = '';
     }
 
     function deleteAAResource(number) {
       var i = 0;
+
       if (angular.isUndefined(number)) {
         return;
       }
+      vm.availablePhoneNums.push(filter(number));
+
+      vm.availablePhoneNums.sort();
 
       var resources = vm.ui.ceInfo.getResources();
       for (i = 0; i < resources.length; i++) {
@@ -123,62 +105,60 @@
         }
       }
 
-      AutoAttendantCeInfoModelService.setCeInfo(vm.aaModel.aaRecord, vm.ui.ceInfo);
     }
 
-    function selectAA(aaName) {
-      vm.aaModel.aaName = aaName;
-      // angular.isUndefine(vm.aaModel.aaRecord);
-      if (angular.isUndefined(vm.aaModel.aaRecord)) {
-        if (aaName === '') {
-          vm.aaModel.aaRecord = AAModelService.newAARecord();
-        } else {
-          for (var i = 0; i < vm.aaModel.aaRecords.length; i++) {
-            if (vm.aaModel.aaRecords[i].callExperienceName === aaName) {
-              // vm.aaModel.aaRecord = angular.copy(vm.aaModel.aaRecords[i]);
-              AutoAttendantCeService.readCe(vm.aaModel.aaRecords[i].callExperienceURL).then(
-                function (data) {
-                  vm.aaModel.aaRecord = data;
-
-                  // Workaround for reading the dn number: by copying it from aaRecords[i], until
-                  // dn number is officialy stored in ceDefintion.
-                  vm.aaModel.aaRecord.assignedResources = angular.copy(vm.aaModel.aaRecords[i].assignedResources);
-                  //
-
-                  vm.ui.ceInfo = AutoAttendantCeInfoModelService.getCeInfo(vm.aaModel.aaRecord);
-                },
-                function (response) {
-                  Notification.error('autoAttendant.errorReadCe', {
-                    name: aaName,
-                    statusText: response.statusText,
-                    status: response.status
-                  });
-                }
-              );
-              return;
-            }
-          }
-        }
+    function filter(number) {
+      if (!number) {
+        return '';
       }
-      vm.ui.ceInfo = AutoAttendantCeInfoModelService.getCeInfo(vm.aaModel.aaRecord);
+
+      var countryTelephoneNumberRegex = /(\+?\d{1})(\d{3})(\d{3})(\d{4})/;
+      var basicTelephoneNumberRegex = /(\d{3})(\d{3})(\d{4})/;
+
+      if (countryTelephoneNumberRegex.test(number)) {
+        return number.replace(countryTelephoneNumberRegex, "$1 ($2) $3-$4");
+      } else if (basicTelephoneNumberRegex.test(number)) {
+        return number.replace(basicTelephoneNumberRegex, "($1) $2-$3");
+      } else {
+        return number;
+      }
+    }
+
+    function getExternalNumbers() {
+
+      ExternalNumberPoolService.query({
+          customerId: Authinfo.getOrgId(),
+          order: 'pattern'
+        }).$promise
+        .then(function (extPool) {
+          for (var i = 0; i < extPool.length; i++) {
+
+            var dn = {
+              id: extPool[i].uuid,
+              number: extPool[i].pattern.replace(/\D/g, ''),
+              type: '', //tbd
+              trigger: '' //tbd
+            };
+
+            if (!getDupeNumberAnyAA(dn.number)) {
+              vm.availablePhoneNums.push(filter(dn.number));
+            }
+
+          }
+        });
     }
 
     function activate() {
-      var aaName = $stateParams.aaName;
-
-      if (aaName === '') {
-        vm.aaNameDisabled = false;
-      }
 
       vm.aaModel = AAModelService.getAAModel();
+
       vm.ui = AAUiModelService.getUiModel();
-      AutoAttendantCeInfoModelService.getCeInfosList().then(function (data) {
-        selectAA(aaName);
-      }, function (data) {
-        selectAA(aaName);
-      });
+
+      getExternalNumbers();
+
     }
 
     activate();
+
   }
 })();
