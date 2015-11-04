3819b7bc87baabf37286591146a39643cbed800f
sjalipar <sjalipar@cisco.com>
2015-11-04 12:10:58 -0600

    using xdescribe instead of xit

--- test/e2e-protractor/huron/create-hunt-group_spec.js
+++ test/e2e-protractor/huron/create-hunt-group_spec.js
@@ -3,29 +3,29 @@
  */
 'use strict';
 
-describe('Create Hunt Group', function () {
+xdescribe('Create Hunt Group', function () {
 
   beforeAll(function () {
     login.login('huron-int1', '#/hurondetails/features');
   }, 120000);
 
-  xit('should open features modal pop up', function () {
+  it('should open features modal pop up', function () {
     utils.expectIsDisplayed(huronFeatures.newFeatureBtn);
     utils.click(huronFeatures.newFeatureBtn);
   });
 
-  xit('should go to huntgroup create page', function() {
+  it('should go to huntgroup create page', function() {
     utils.expectIsDisplayed(huntGroup.huntGroupBtn);
     utils.click(huntGroup.huntGroupBtn);
     utils.expectTextToBeSet(huntGroup.huntGroupCreateTitle, 'Create Hunt Group');
   });
 
-  xit('should set the name of the hunt group', function() {
+  it('should set the name of the hunt group', function() {
     expect(huntGroup.typeAheadInput.getAttribute('placeholder')).toContain('Name the Hunt Group');
     utils.sendKeys(huntGroup.typeAheadInput, huntGroup.randomHGName);
   });
 
-  xit('should go to the next section', function() {
+  it('should go to the next section', function() {
     utils.click(huntGroup.rightBtn);
     utils.expectTextToBeSet(huntGroup.description, 'When this number is called, this hunt group is activated');
     utils.sendKeys(huntGroup.pilotTypeAheadInput, huntGroup.pilotNumber);
@@ -33,35 +33,35 @@ describe('Create Hunt Group', function () {
     utils.click(huntGroup.dropdownItem);
   });
 
-  xit('should go to the hunting method section', function() {
+  it('should go to the hunting method section', function() {
     utils.click(huntGroup.rightBtn);
     utils.expectTextToBeSet(huntGroup.hgmethodsTitle, 'Select Hunting Method');
     utils.click(huntGroup.huntMethodLI);
   });
 
-  xit('should go to the hunt members section', function() {
+  it('should go to the hunt members section', function() {
     utils.expectTextToBeSet(huntGroup.description, 'These are the users who pickup the hunt group calls');
     utils.sendKeys(huntGroup.typeAheadInput, huntGroup.memberSearch);
     utils.expectTextToBeSet(huntGroup.memberDropdownItemName, huntGroup.memberSearch);
     utils.click(huntGroup.memberDropdownItem);
   });
 
-  xit('should go to the fallback destination section', function(){
+  it('should go to the fallback destination section', function(){
     utils.click(huntGroup.rightBtn);
     utils.expectTextToBeSet(huntGroup.description, 'This is where the call goes if it was not answered by this hunt group');
   });
 
-  xit('the form should be disabled', function() {
+  it('the form should be disabled', function() {
     navigation.hasClass(huntGroup.submitBtn, 'disabled');
   });
 
-  xit('enable the button to submit', function() {
+  it('enable the button to submit', function() {
     utils.sendKeys(huntGroup.typeAheadInput, huntGroup.memberSearch);
     utils.expectTextToBeSet(huntGroup.memberDropdownItemName, huntGroup.memberSearch);
     utils.click(huntGroup.memberDropdownItem);
   });
 
-  xit('the form should be enabled', function() {
+  it('the form should be enabled', function() {
     navigation.hasClass(huntGroup.submitBtn, 'success');
   });
 
--- test/e2e-protractor/huron/features-list_spec.js
+++ test/e2e-protractor/huron/features-list_spec.js
@@ -7,43 +7,43 @@
 /* global describe */
 /* global it */
 
-describe('As an admin i should be able to', function () {
+xdescribe('As an admin i should be able to', function () {
 
   afterEach(function () {
     utils.dumpConsoleErrors();
   });
 
 
-  xit('login as an account admin and goto Huron Features Page', function () {
+  it('login as an account admin and goto Huron Features Page', function () {
     login.login('huron-int1', '#/hurondetails/features');
   });
 
-  xit('see Create New Feature pop up when clicked on New button', function(){
+  it('see Create New Feature pop up when clicked on New button', function(){
     utils.expectIsDisplayed(huronFeatures.featuresList);
     utils.click(huronFeatures.newFeatureBtn);
     utils.expectIsPresent(huronFeatures.newfeatureModal);
     utils.expectIsPresent(huronFeatures.closeBtnOnModal);
   });
 
-  xit('close the create new feature pop up and stay on huron features page', function(){
+  it('close the create new feature pop up and stay on huron features page', function(){
     utils.expectIsPresent(huronFeatures.closeBtnOnModal);
     utils.click(huronFeatures.closeBtnOnModal);
     utils.expectIsDisplayed(huronFeatures.featuresList);
   });
 
-  xit('see only auto attendants when auto attendant filter is selected', function(){
+  it('see only auto attendants when auto attendant filter is selected', function(){
     utils.click(huronFeatures.autoAttendantFilter);
     utils.expectIsDisplayed(huronFeatures.autoAttendants);
     utils.expectIsNotDisplayed(huronFeatures.huntGroups);
   });
 
-  xit('see only hunt groups when Hunt Group filter is selected', function(){
+  it('see only hunt groups when Hunt Group filter is selected', function(){
     utils.click(huronFeatures.huntGroupFilter);
     utils.expectIsDisplayed(huronFeatures.huntGroups);
     expect(huronFeatures.autoAttendants.count()).toBe(0);
   });
 
-  xit('see the searched hunt group', function(){
+  it('see the searched hunt group', function(){
     utils.click(huronFeatures.allFilter);
     utils.click(utils.searchbox);
     utils.clear(utils.searchField);
@@ -52,7 +52,7 @@ describe('As an admin i should be able to', function () {
     utils.expectCountToBeGreater(huronFeatures.selectedHuntGroups , 0);
   });
 
-  xit('see the edit and delete button when clicked on menu button of a selected hunt group', function(){
+  it('see the edit and delete button when clicked on menu button of a selected hunt group', function(){
     utils.expectIsDisplayed(huronFeatures.huntGroupMenu);
     utils.click(huronFeatures.huntGroupMenu);
     utils.expectIsDisplayed(huronFeatures.huntGroupEditBtn);
@@ -62,18 +62,18 @@ describe('As an admin i should be able to', function () {
   });
 
 
-  xit('see the delete pop up when clicked on delete button of a selected hunt group', function(){
+  it('see the delete pop up when clicked on delete button of a selected hunt group', function(){
     utils.expectIsDisplayed(huronFeatures.huntGroupDeleteBtn);
     utils.click(huronFeatures.huntGroupDeleteBtn);
     utils.click(huronFeatures.closeBtnOnModal);
   });
 
-  xit('click on close button of delete hunt group pop up', function(){
+  it('click on close button of delete hunt group pop up', function(){
     utils.expectIsDisplayed(huronFeatures.closeBtnOnModal);
     utils.click(huronFeatures.closeBtnOnModal);
   });
 
-  xit('click on cancel button of delete hunt group pop up', function(){
+  it('click on cancel button of delete hunt group pop up', function(){
     utils.expectIsDisplayed(huronFeatures.huntGroupMenu);
     utils.click(huronFeatures.huntGroupMenu);
     utils.expectIsDisplayed(huronFeatures.huntGroupDeleteBtn);
@@ -81,7 +81,7 @@ describe('As an admin i should be able to', function () {
     utils.click(huronFeatures.popUpCancelBtn);
   });
 
-  xit('click on delete button of delete hunt group pop up', function(){
+  it('click on delete button of delete hunt group pop up', function(){
     utils.expectIsDisplayed(huronFeatures.huntGroupMenu);
     utils.click(huronFeatures.huntGroupMenu);
     utils.expectIsDisplayed(huronFeatures.huntGroupDeleteBtn);
@@ -91,7 +91,7 @@ describe('As an admin i should be able to', function () {
     notifications.assertSuccess(huntGroup.randomHGName + ' hunt group has been deleted successfully');
   });
 
-  xit('verify the deleted hunt group is not shown on features list page', function(){
+  it('verify the deleted hunt group is not shown on features list page', function(){
     utils.click(utils.searchbox);
     utils.clear(utils.searchField);
     utils.sendKeys(utils.searchField, huntGroup.randomHGName);
