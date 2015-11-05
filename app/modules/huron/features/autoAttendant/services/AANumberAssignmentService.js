22e29487ba5b3d2fd4354e80be40bb421e512648
Glen Frison <gfrison@cisco.com>
2015-11-05 09:03:50 -0800

    US211158 UI: Integrate with CMI to enable number routing

---
+++ app/modules/huron/features/autoAttendant/services/AANumberAssignmentService.js
@@ -0,0 +1,102 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('uc.autoattendant')
+    .service('AANumberAssignmentService', AANumberAssignmentService);
+
+  /* @ngInject */
+
+  function AANumberAssignmentService($q, AssignAutoAttendantService, Authinfo, $http) {
+
+    /* jshint validthis: true */
+
+    var feature;
+    var customerId = Authinfo.getOrgId();
+    var service = {
+      addAANumberAssignment: addAANumberAssignment,
+      getListOfAANumberAssignments: getListOfAANumberAssignments,
+      deleteAANumberAssignments: deleteAANumberAssignments,
+      updateAANumberAssignments: updateAANumberAssignments
+    };
+
+    return service;
+
+    function getListOfAANumberAssignments(customerId, cesId) {
+
+      return AssignAutoAttendantService.query({
+        customerId: customerId,
+        cesId: cesId
+      }).$promise.then(
+        function (response) {
+          // success
+          return response[0].numbers;
+        },
+        function (response) {
+          // failure
+          return response;
+        }
+      );
+
+    }
+
+    function addAANumberAssignment(customerId, cesId, resources) {
+      var numObjList = [];
+
+      for (var i = 0; i < resources.length; i++) {
+
+        var numType = "NUMBER_FORMAT_DIRECT_LINE";
+        if (resources[i].getType() === "directoryNumber") {
+          numType = "NUMBER_FORMAT_EXTENSION";
+        } else if (resources[i].getType() === "externalNumber") {
+          numType = "NUMBER_FORMAT_DIRECT_LINE";
+        }
+
+        var numObj = {
+          number: resources[i].getNumber(),
+          type: numType
+        };
+
+        numObjList.push(numObj);
+
+      }
+
+      var data = {
+        numbers: numObjList
+      };
+      return AssignAutoAttendantService.update({
+        customerId: customerId,
+        cesId: cesId
+      }, data).$promise.then(
+        function (response) {
+          // success
+          return response;
+        },
+        function (response) {
+          // failure
+          return response;
+        }
+      );
+
+    }
+
+    function updateAANumberAssignments(customerId, cesId) {
+      return AssignAutoAttendantService.update({
+        customerId: customerId,
+        cesId: cesId
+      }).$promise;
+
+    }
+
+    function deleteAANumberAssignments(customerId, cesId) {
+
+      return AssignAutoAttendantService.delete({
+        customerId: customerId,
+        cesId: cesId
+      }).$promise;
+
+    }
+
+  }
+
+})();
