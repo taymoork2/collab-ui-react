aa7c617c72360b33b758b401e5985ddf33257d61
Magnus Holst <maholst@cisco.com>
2015-11-05 08:34:01 +0100

    patch from SMU: updated overview prototype.

--- app/modules/core/overview/overview.ctrl.js
+++ app/modules/core/overview/overview.ctrl.js
@@ -9,6 +9,15 @@
   function OverviewCtrl($log, $translate) {
     var vm = this;
     vm.pageTitle = $translate.instant('overview.pageTitle');
-    $log.log('hello world');
+    vm.cards = [
+      {name: 'Room Systems',    num: n(), icon: 'icon-circle-message',        level: 'info'},
+      {name: 'Message',         num: n(), icon: 'icon-circle-calendar',       level: 'warn'},
+      {name: 'Meeting',         num: n(), icon: 'icon-circle-comp-pos',       level: 'error'},
+      {name: 'Call',            num: n(), icon: 'icon-circle-contact-centre', level: 'info'},
+      {name: 'Hybrid Services', num: n(), icon: 'icon-circle-localize',       level: 'info'}
+    ];
+    function n() {
+      return Math.floor(Math.random() * 99999);
+    }
   }
 })();
