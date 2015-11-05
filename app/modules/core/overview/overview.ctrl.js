2f103e122c7f68a841af4b68884ad23b0fc3aa37
Magnus Holst <maholst@cisco.com>
2015-11-05 09:12:44 +0100

    added level text to the overview cards

--- app/modules/core/overview/overview.ctrl.js
+++ app/modules/core/overview/overview.ctrl.js
@@ -10,11 +10,11 @@
     var vm = this;
     vm.pageTitle = $translate.instant('overview.pageTitle');
     vm.cards = [
-      {name: 'Room Systems',    num: n(), icon: 'icon-circle-message',        level: 'info'},
-      {name: 'Message',         num: n(), icon: 'icon-circle-calendar',       level: 'warn'},
-      {name: 'Meeting',         num: n(), icon: 'icon-circle-comp-pos',       level: 'error'},
-      {name: 'Call',            num: n(), icon: 'icon-circle-contact-centre', level: 'info'},
-      {name: 'Hybrid Services', num: n(), icon: 'icon-circle-localize',       level: 'info'}
+      {name: 'Room Systems',    num: n(), icon: 'icon-circle-message',        level: 'info',  levelText: 'Excellent'},
+      {name: 'Message',         num: n(), icon: 'icon-circle-calendar',       level: 'warn',  levelText: 'Warning'},
+      {name: 'Meeting',         num: n(), icon: 'icon-circle-comp-pos',       level: 'error', levelText: 'Error'},
+      {name: 'Call',            num: n(), icon: 'icon-circle-contact-centre', level: 'info',  levelText: 'Check status'},
+      {name: 'Hybrid Services', num: n(), icon: 'icon-circle-localize',       level: 'info',  levelText: 'Excellent'}
     ];
     function n() {
       return Math.floor(Math.random() * 99999);
