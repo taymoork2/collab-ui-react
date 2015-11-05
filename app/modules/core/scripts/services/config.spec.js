50de5d7add2a68d84682a471bacb6feb92b991c2
mikecan <mikecan@cisco.com>
2015-11-04 20:09:14 -0800

    Merge config specs

--- app/modules/core/scripts/services/config.spec.js
+++ app/modules/core/scripts/services/config.spec.js
@@ -48,6 +48,42 @@ describe('Config', function () {
     };
   };
 
+  it('should exist', function () {
+    expect(Config).toBeDefined();
+  });
+
+  it('should have tabs', function () {
+    expect(Config.tabs).toBeDefined();
+  });
+
+  it('should have roleStates', function () {
+    expect(Config.roleStates).toBeDefined();
+  });
+
+  it('should not have development states assigned to Full_Admin role', function () {
+    function getDevelopmentStates() {
+      var devStates = [];
+      for (var i = 0; i < Config.tabs.length; i++) {
+        var tab = Config.tabs[i];
+        if (tab && tab.tab === 'developmentTab') {
+          var subPages = tab.subPages;
+          for (var j = 0; j < subPages.length; j++) {
+            var devTab = subPages[j];
+            if (devTab && devTab.state) {
+              devStates.push(devTab.state);
+            }
+          }
+        }
+      }
+      return devStates;
+    }
+    var adminStates = Config.roleStates.Full_Admin || [];
+    var developmentStates = getDevelopmentStates();
+    for (var i = 0; i < adminStates.length; i++) {
+      expect(developmentStates).not.toContain(adminStates[i]);
+    }
+  });
+
   it('should detect dev environment', function () {
     $location.host.and.returnValue('wbx2.com/bla');
     expect(Config.isDev()).toBe(false);
@@ -327,10 +363,6 @@ describe('Config', function () {
     });
   });
 
-  it('spark-device-mgmt should contain devices state', function () {
-    expect(Config.serviceStates['spark-device-mgmt'][0]).toBe('devices');
-  });
-
   it('should return correct web client url', function () {
     whenCalling('getWebClientUrl').expectUrlToBe({
       dev: 'https://web.ciscospark.com/',
@@ -391,6 +423,18 @@ describe('Config', function () {
       cfe: 'https://config.integration-tx1.thunderhead.io/config/v1',
       integration: 'https://config.integration-tx1.thunderhead.io/config/v1',
       prod: 'https://config.integration-tx1.thunderhead.io/config/v1'
+    });
+  });
+
+  describe('service states', function () {
+
+    it('squared-fusion-mgmt should contain fusion states', function () {
+      expect(Config.serviceStates['squared-fusion-mgmt'][0]).toBe('fusion');
+      expect(Config.serviceStates['squared-fusion-mgmt'][1]).toBe('cluster-details');
+    });
+
+    it('spark-device-mgmt should contain devices state', function () {
+      expect(Config.serviceStates['spark-device-mgmt'][0]).toBe('devices');
     });
   });
 
