292b55c52d14f216fdef35e718438623d3c36030
Shivangi Sharma <fshivang@cisco.com>
2015-11-06 14:28:31 -0600

    US215005 ASSIGN NUMBERS FOR AUSTRALIAN DIAL PLAN

--- app/modules/huron/dialPlans/dialPlanService.js
+++ app/modules/huron/dialPlans/dialPlanService.js
@@ -12,7 +12,7 @@
       getVoiceClusters: getVoiceClusters,
       getCustomerVoice: getCustomerVoice,
       getDialPlans: getDialPlans,
-      getCustomerDialPlanDetails: getCustomerDialPlanDetails,
+      getCustomerDialPlanDetails: getCustomerDialPlanDetails
     };
 
     return service;
@@ -59,7 +59,7 @@
             'name': 'NANP'
           });
           customerVoice.dialPlan = {
-            uuid: northAmericanDialPlan.uuid
+            uuid: "f193ed3e-9591-43e9-a631-3bcf9f1f6c40"
           };
         }
 
