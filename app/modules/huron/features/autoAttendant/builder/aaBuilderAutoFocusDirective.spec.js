0a4152269c2ec506e5d23e2625eebf947d5c2bfe
Chris Laughbon <claughbo@cisco.com>
2015-11-17 12:25:31 -0800

    Indigo 147

---
+++ app/modules/huron/features/autoAttendant/builder/aaBuilderAutoFocusDirective.spec.js
@@ -0,0 +1,26 @@
+'use strict';
+
+describe('Directive: aaBuilderAutofocus', function () {
+  var $compile, $rootScope;
+  var $timeout, element;
+
+  beforeEach(module('Huron'));
+
+  beforeEach(inject(function ($injector, _$compile_, _$rootScope_, _$timeout_) {
+    $compile = _$compile_;
+    $rootScope = _$rootScope_;
+    $timeout = _$timeout_;
+    element = $compile("<input type='text' aa-builder-focus>")($rootScope);
+    $rootScope.$digest();
+  }));
+
+  it('should focus on the name entry', function () {
+
+    element = $compile("<input type='text' aa-builder-autofocus>")($rootScope);
+    spyOn(element[0], 'focus');
+    $timeout.flush();
+
+    expect(element[0].focus).toHaveBeenCalled();
+
+  });
+});
