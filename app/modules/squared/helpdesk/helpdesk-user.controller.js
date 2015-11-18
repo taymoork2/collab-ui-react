96bd1bcd7a167dc932d71c8b19ae9afa96131520
Helge Langehaug <hellange@cisco.com>
2015-11-18 18:43:37 +0100

    Added check of entitlements to decide if card should be shown or not. Not ready.

--- app/modules/squared/helpdesk/helpdesk-user.controller.js
+++ app/modules/squared/helpdesk/helpdesk-user.controller.js
@@ -4,20 +4,45 @@
   /* @ngInject */
   function HelpdeskUserController($stateParams, HelpdeskService, XhrNotificationService) {
     var vm = this;
-    vm.user = $stateParams.user;
-    vm.org = {};
+    console.log($stateParams)
+    var userId = $stateParams.user.id;
+    var orgId = $stateParams.user.organization.id;
 
-    HelpdeskService.getUser(vm.user.organization.id, vm.user.id).then(function (res) {
+    vm.user = null;
+    vm.showCard = showCard;
+
+    HelpdeskService.getUser(orgId, userId).then(function (res) {
       vm.user = res;
     }, function (err) {
       XhrNotificationService.notify(err);
     });
 
-    HelpdeskService.getOrg(vm.user.organization.id).then(function (res) {
-      vm.org = res;
-    }, function (err) {
-      XhrNotificationService.notify(err);
-    });
+    //HelpdeskService.getOrg(vm.user.organization.id).then(function (res) {
+    //  vm.org = res;
+    //}, function (err) {
+    //  XhrNotificationService.notify(err);
+    //});
+
+    function showCard(type) {
+      var entitlements = vm.user.entitlements;
+      console.log("User has the following entitlemnts:",entitlements)
+      switch(type){
+        // cloudmeetings
+        case 'message':
+          return _.includes(entitlements, "webex-squared");
+        case 'meeting':
+          return _.includes(entitlements, "webex-squared");
+        case 'call':
+          return _.includes(entitlements, "squared-call-initiation");
+        case 'hybrid':
+          return _.includes(entitlements, "squared-fusion-cal")
+            || _.includes(entitlements, "squared-fusion-uc");
+
+      }
+      //TODO: Include test of user entitlemnts
+      return true;
+    }
+
   }
 
   angular
