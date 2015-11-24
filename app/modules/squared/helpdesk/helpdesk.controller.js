5fbf523fd21467f9f6d3b1b56ce3fb0700b057ff
Sarah Van Neyghem <svanneyg@cisco.com>
2015-11-24 14:55:31 +0100

    Navigate by using keyboard
    * Go left, right, up, down in search results with arrows on keyboard
    * Go to the next card by using tab
    * Escape in input field clears input field
    * Escape outside input field selects input
    * Enter in input field selects first item
    * Enter in search result opens the card
    * CSS

--- app/modules/squared/helpdesk/helpdesk.controller.js
+++ app/modules/squared/helpdesk/helpdesk.controller.js
@@ -99,26 +99,41 @@
       var newTabIndex = activeCard;
       switch(event.keyCode.toString()){
         case "37":
-        //left
-        newTabIndex = parseInt(activeCard) - 1;
+          newTabIndex = parseInt(activeCard) - 1;
         break;
 
         case "38":
-        //up
-        newTabIndex = parseInt(activeCard) - 10;
+          newTabIndex = parseInt(activeCard) - 10;
         break;
 
         case "39":
-        //right
-        newTabIndex = parseInt(activeCard) + 1;
+          newTabIndex = parseInt(activeCard) + 1;
         break;
 
         case "40":
-        //down
-        newTabIndex = parseInt(activeCard) + 10;
+          newTabIndex = parseInt(activeCard) + 10;
+        break;
+
+        case "27":
+          if(angular.element(document.activeElement)[0]["id"] == "searchInput"){
+            angular.element('#searchInput').val("");
+          }else{
+            angular.element('#searchInput').focus().select();
+            newTabIndex = "-1";
+          }
+        break;
+
+        case "13":
+          if(angular.element(document.activeElement)[0]["id"] == "searchInput"){
+            newTabIndex = 1;
+          }else{
+            angular.element(document.activeElement).click();
+          }
         break;
       }
-      $('[tabindex='+newTabIndex+']').focus();
+      if(newTabIndex!="-1"){
+        $('[tabindex='+newTabIndex+']').focus();
+      }
     }
   }
 
