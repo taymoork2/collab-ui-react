749a1e00944790d619bbb7b5eb3cfdc091a60b8d
Tom Vasset <tvasset@cisco.com>
2015-11-26 11:50:33 +0100

    Org controller refactoring

--- app/modules/squared/helpdesk/helpdesk-org.controller.js
+++ app/modules/squared/helpdesk/helpdesk-org.controller.js
@@ -2,16 +2,19 @@
   'use strict';
 
   /* @ngInject */
-  function HelpdeskOrgController(Config, $stateParams, HelpdeskService, XhrNotificationService, Authinfo, ReportsService) {
+  function HelpdeskOrgController($stateParams, HelpdeskService, XhrNotificationService, HelpdeskCardsService, ReportsService) {
     var vm = this;
-    var orgId = null;
     if ($stateParams.org) {
       vm.org = $stateParams.org;
-      orgId = vm.org.id;
+      vm.orgId = vm.org.id;
     } else {
-      orgId = $stateParams.id;
+      vm.orgId = $stateParams.id;
     }
-    vm.showCard = showCard;
+    vm.messageCard = {};
+    vm.meetingCard = {};
+    vm.callCard = {};
+    vm.hybridServicesCard = {};
+    vm.roomSystemsCard = {};
 
     var componentMapping = {
       message: ['Mobile Clients', 'Rooms', 'Web and Desktop Clients'],
@@ -86,60 +89,16 @@
       vm.healthStatuses.hybrid = deduceHealthStatus(result.hybrid);
     }
 
-    HelpdeskService.getOrg(orgId).then(function (res) {
-      vm.org = res;
-      findPartners(vm.org);
-    }, function (err) {
-      XhrNotificationService.notify(err);
-    });
+    HelpdeskService.getOrg(vm.orgId).then(initOrgView, XhrNotificationService.notify);
 
-    if (hasEntitlement(Config.entitlements.fusion_mgmt)) {
-      HelpdeskService.getHybridServices(orgId).then(function (services) {
-        var enabledHybridServices = _.filter(services, {
-          enabled: true
-        });
-        if (enabledHybridServices.length === 1 && enabledHybridServices[0].id === "squared-fusion-mgmt") {
-          enabledHybridServices = []; // Don't show the management service if none of the others are enabled.
-        }
-        vm.enabledHybridServices = enabledHybridServices;
-        vm.availableHybridServices = _.filter(services, {
-          enabled: false
-        });
-      }, function (err) {
-        XhrNotificationService.notify(err);
-      });
-    }
-
-    /*
-      message : entitlement = "webex-squared" ?
-      meeting (webex) : entitlement = "webex-messenger" ?
-      call(huron) : authinfo.issquareduc()
-      hybrid: Authinfo.isFusion()
-      room (cloudberry): Authinfo.isDeviceManagement()
-     */
-    // TODO: Move and and reuse between user and org ?
-    function showCard(type) {
-      switch (type) {
-        //TODO: Check for the CORRECT entitlements !!!
-      case 'message':
-        return hasEntitlement(Config.entitlements.squared); //???
-      case 'meeting':
-        return hasEntitlement("webex-messenger"); // ???
-      case 'call':
-        return hasEntitlement(Config.entitlements.huron);
-      case 'hybrid':
-        return hasEntitlement(Config.entitlements.fusion_mgmt);
-      case 'room':
-        return hasEntitlement(Config.entitlements.device_mgmt);
-      }
-      return true;
-    }
-
-    function hasEntitlement(entitlement) {
-      if (vm.org && vm.org.services) {
-        return _.includes(vm.org.services, entitlement);
-      }
-      return false;
+    function initOrgView(org) {
+      vm.org = org;
+      vm.messageCard = HelpdeskCardsService.getMessageCardForOrg(org);
+      vm.meetingCard = HelpdeskCardsService.getMeetingCardForOrg(org);
+      vm.callCard = HelpdeskCardsService.getCallCardForOrg(org);
+      vm.hybridServicesCard = HelpdeskCardsService.getHybridServicesCardForOrg(org);
+      vm.roomSystemsCard = HelpdeskCardsService.getRoomSystemsCardForOrg(org);
+      findPartners(org);
     }
 
     function findPartners(org) {
@@ -148,7 +107,7 @@
         _.each(org.managedBy, function (parnterOrg) {
           HelpdeskService.getOrg(parnterOrg.orgId).then(function (res) {
             org.partners.push(res);
-          }, function (err) {});
+          }, angular.noop);
         });
       }
     }
