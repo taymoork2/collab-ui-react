216077e4005a8c1aa5d13d4953a5800f64cff44a
Tom Vasset <tvasset@cisco.com>
2015-11-26 10:22:18 +0100

    Added help desk cards service and moved a lot of controller logic there. Now tests :)...

--- app/modules/squared/helpdesk/helpdesk-user.controller.js
+++ app/modules/squared/helpdesk/helpdesk-user.controller.js
@@ -2,145 +2,59 @@
   'use strict';
 
   /* @ngInject */
-  function HelpdeskUserController($stateParams, HelpdeskService, XhrNotificationService, $translate, USSService2) {
+  function HelpdeskUserController($stateParams, HelpdeskService, XhrNotificationService, USSService2, HelpdeskCardsService) {
     var vm = this;
-    var userId = null;
-    var orgId = null;
     if ($stateParams.user) {
-      userId = $stateParams.user.id;
-      orgId = $stateParams.user.organization.id;
+      vm.userId = $stateParams.user.id;
+      vm.orgId = $stateParams.user.organization.id;
     } else {
-      userId = $stateParams.id;
-      orgId = $stateParams.orgId;
+      vm.userId = $stateParams.id;
+      vm.orgId = $stateParams.orgId;
     }
-
     vm.resendInviteEmail = resendInviteEmail;
     vm.user = $stateParams.user;
     vm.resendInviteEnabled = false;
-    vm.messaging = {
-      entitled: false,
-      entitlements: []
-    };
-    vm.meeting = {
-      entitled: false,
-      entitlements: []
-    };
-    vm.call = {
-      entitled: false,
-      entitlements: []
-    };
-    vm.hybrid = {
-      entitled: false,
-      cal: {
-        entitled: false
-      },
-      uc: {
-        entitled: false
-      },
-      ec: {
-        entitled: false
-      }
-    };
+    vm.messageCard = {};
+    vm.meetingCard = {};
+    vm.callCard = {};
+    vm.hybridServicesCard = {};
 
-    HelpdeskService.getUser(orgId, userId).then(function (res) {
-      vm.user = res;
-      reviewEntitlementsAndLicenses();
-      vm.resendInviteEnabled = _.includes(vm.user.statuses, 'helpdesk.userStatuses.pending');
-    }, function (err) {
-      XhrNotificationService.notify(err);
-    });
+    HelpdeskService.getUser(vm.orgId, vm.userId).then(initUserView, XhrNotificationService.notify);
 
-    HelpdeskService.getOrg(orgId).then(function (res) {
+    HelpdeskService.getOrg(vm.orgId).then(function (res) {
       vm.org = res;
-    }, function (err) {
-      XhrNotificationService.notify(err);
-    });
+    }, XhrNotificationService.notify);
 
     function resendInviteEmail() {
-      HelpdeskService.resendInviteEmail(vm.user.displayName, vm.user.userName).then(function (res) {}, function (err) {
-        XhrNotificationService.notify(err);
-      });
+      HelpdeskService.resendInviteEmail(vm.user.displayName, vm.user.userName).then(angular.noop, XhrNotificationService.notify);
     }
 
-    function reviewEntitlementsAndLicenses() {
-      // Messaging
-      var paidOrFree;
-      if (hasEntitlement('webex-squared')) {
-        vm.messaging.entitled = true;
-        if (hasEntitlement('squared-room-moderation')) {
-          paidOrFree = isLicensedForService('MS') ? 'paid' : 'free';
-          vm.messaging.entitlements.push($translate.instant('helpdesk.entitlements.squared-room-moderation.' + paidOrFree));
-        } else {
-          vm.messaging.entitlements.push($translate.instant('helpdesk.entitlements.webex-squared'));
-        }
-      }
+    function initUserView(user) {
+      vm.user = user;
+      vm.resendInviteEnabled = _.includes(user.statuses, 'helpdesk.userStatuses.pending');
+      vm.messageCard = HelpdeskCardsService.getMessageCardForUser(user);
+      vm.meetingCard = HelpdeskCardsService.getMeetingCardForUser(user);
+      vm.callCard = HelpdeskCardsService.getCallCardForUser(user);
+      vm.hybridServicesCard = HelpdeskCardsService.getHybridServicesCardForUser(user);
 
-      // Meeting
-      if (hasEntitlement('squared-syncup')) {
-        vm.meeting.entitled = true;
-        paidOrFree = isLicensedForService('CF') ? 'paid' : 'free';
-        vm.meeting.entitlements.push($translate.instant('helpdesk.entitlements.squared-syncup.' + paidOrFree));
-      }
-
-      // Call
-      if (hasEntitlement('ciscouc')) {
-        vm.call.entitled = true;
-        paidOrFree = isLicensedForService('CO') ? 'paid' : 'free';
-        vm.call.entitlements.push($translate.instant('helpdesk.entitlements.ciscouc.' + paidOrFree));
-      }
-
-      // Hybrid Services
-      if (hasEntitlement('squared-fusion-cal')) {
-        vm.hybrid.entitled = true;
-        vm.hybrid.cal.entitled = true;
-      }
-      if (hasEntitlement('squared-fusion-uc')) {
-        vm.hybrid.entitled = true;
-        vm.hybrid.uc.entitled = true;
-        vm.hybrid.ec.entitled = hasEntitlement('squared-fusion-uc');
-      }
-      if (vm.hybrid.entitled) {
-        USSService2.getStatusesForUserInOrg(userId, orgId).then(function (statuses) {
+      if (vm.hybridServicesCard.entitled) {
+        USSService2.getStatusesForUserInOrg(vm.userId, vm.orgId).then(function (statuses) {
           _.each(statuses, function (status) {
             status.collapsedState = USSService2.decorateWithStatus(status);
             switch (status.serviceId) {
             case 'squared-fusion-cal':
-              vm.hybrid.cal.status = status;
+              vm.hybridServicesCard.cal.status = status;
               break;
             case 'squared-fusion-uc':
-              vm.hybrid.uc.status = status;
+              vm.hybridServicesCard.uc.status = status;
               break;
             case 'squared-fusion-ec':
-              vm.hybrid.ec.status = status;
+              vm.hybridServicesCard.ec.status = status;
               break;
             }
           });
-        }, function (err) {
-          XhrNotificationService.notify(err);
-        });
+        }, XhrNotificationService.notify);
       }
-    }
-
-    function hasEntitlement(entitlement) {
-      if (vm.user && vm.user.entitlements) {
-        return _.includes(vm.user.entitlements, entitlement);
-      }
-      return false;
-    }
-
-    function isLicensedForService(licPrefix) {
-      if (vm.user && vm.user.entitlements) {
-        var userLicenses = vm.user.licenseID;
-        if (userLicenses) {
-          for (var l = userLicenses.length - 1; l >= 0; l--) {
-            var licensePrefix = userLicenses[l].substring(0, 2);
-            if (licensePrefix === licPrefix) {
-              return true;
-            }
-          }
-        }
-      }
-      return false;
     }
   }
 
