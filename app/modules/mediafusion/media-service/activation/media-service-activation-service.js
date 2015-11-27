38a48f8cc03c56895c52640c2e3457b61e322534
Krishna Kumar <kribalac@cisco.com>
2015-11-27 16:50:15 +0530

    Two page changes for media fusion

---
+++ app/modules/mediafusion/media-service/activation/media-service-activation-service.js
@@ -0,0 +1,54 @@
+'use strict';
+
+angular.module('Mediafusion')
+  .service('MediaServiceActivation', ['$http', 'MediafusionConfigService', 'Authinfo',
+    function MediaServiceActivation($http, config, Authinfo) {
+
+      var setServiceEnabled = function (serviceId, enabled) {
+        return $http
+          .patch(config.getUrl() + '/organizations/' + Authinfo.getOrgId() + '/services/' + serviceId, {
+            enabled: enabled
+          });
+
+      };
+
+      var isServiceEnabled = function (serviceId, callback) {
+        $http
+          .get(config.getUrl() + '/organizations/' + Authinfo.getOrgId() + '/services')
+          .success(function (data) {
+            var service = _.find(data.items, {
+              id: serviceId
+            });
+            if (service === undefined) {
+              callback(false);
+            } else {
+              callback(null, service.enabled);
+            }
+          })
+          .error(function () {
+            callback(arguments);
+          });
+      };
+
+      var getUserIdentityOrgToMediaAgentOrgMapping = function () {
+        var url = config.getCalliopeUrl() + '/identity2agent/' + Authinfo.getOrgId();
+        return $http.get(url);
+      };
+
+      var setUserIdentityOrgToMediaAgentOrgMapping = function (mediaAgentOrgIdsArray) {
+        var url = config.getCalliopeUrl() + '/identity2agent';
+        return $http
+          .put(url, {
+            identityOrgId: Authinfo.getOrgId(),
+            mediaAgentOrgIds: mediaAgentOrgIdsArray
+          });
+      };
+
+      return {
+        isServiceEnabled: isServiceEnabled,
+        setServiceEnabled: setServiceEnabled,
+        getUserIdentityOrgToMediaAgentOrgMapping: getUserIdentityOrgToMediaAgentOrgMapping,
+        setUserIdentityOrgToMediaAgentOrgMapping: setUserIdentityOrgToMediaAgentOrgMapping
+      };
+    }
+  ]);
---
+++ app/modules/mediafusion/media-service/settings/media-service-settings-controller.js
@@ -0,0 +1,94 @@
+(function () {
+  'use strict';
+
+  /* @ngInject */
+  function MediaServiceSettingsController($state, $modal, MediaServiceDescriptor, Authinfo, $stateParams, NotificationConfigService,
+    MailValidatorService, XhrNotificationService, Notification) {
+    var vm = this;
+    vm.config = "";
+    vm.wx2users = "";
+    vm.serviceType = "mf_mgmt";
+    vm.serviceId = "squared-fusion-media";
+    vm.cluster = $stateParams.cluster;
+
+    vm.disableMediaService = function (serviceId) {
+      MediaServiceDescriptor.setServiceEnabled(serviceId, false).then(
+        function success() {
+          $state.go("mediafusion.list", {
+            serviceType: "mf_mgmt"
+          }, {
+            reload: true
+          });
+        },
+        function error(data, status) {
+          XhrNotificationService.notify(error);
+        });
+    };
+
+    vm.confirmDisable = function (serviceId) {
+      $modal.open({
+        // TODO: update correct disable-dialog html
+        templateUrl: "modules/mediafusion/media-service/confirm-disable-dialog.html",
+        controller: DisableConfirmController,
+        controllerAs: "disableConfirmDialog",
+      }).result.then(function () {
+        vm.disableMediaService(serviceId);
+      });
+    };
+
+    NotificationConfigService.read(function (err, config) {
+      vm.loading = false;
+      if (err) {
+        return XhrNotificationService.notify(err);
+      }
+      vm.config = config || {};
+      if (vm.config.wx2users.length > 0) {
+        vm.wx2users = _.map(vm.config.wx2users.split(','), function (user) {
+          return {
+            text: user
+          };
+        });
+      } else {
+        vm.wx2users = [];
+      }
+    });
+
+    vm.writeConfig = function () {
+      vm.config.wx2users = _.map(vm.wx2users, function (data) {
+        return data.text;
+      }).toString();
+      if (vm.config.wx2users && !MailValidatorService.isValidEmailCsv(vm.config.wx2users)) {
+        Notification.error("hercules.errors.invalidEmail");
+      } else {
+        vm.savingEmail = true;
+        NotificationConfigService.write(vm.config, function (err) {
+          vm.savingEmail = false;
+          if (err) {
+            return XhrNotificationService.notify(err);
+          }
+        });
+      }
+    };
+
+    vm.invalidEmail = function (tag) {
+      Notification.error(tag.text + " is not a valid email");
+    };
+  }
+
+  /* @ngInject */
+  function DisableConfirmController(MediaServiceDescriptor, $modalInstance) {
+    var modalVm = this;
+    modalVm.serviceIconClass = MediaServiceDescriptor.serviceIcon();
+
+    modalVm.ok = function () {
+      $modalInstance.close();
+    };
+    modalVm.cancel = function () {
+      $modalInstance.dismiss();
+    };
+  }
+
+  angular
+    .module('Mediafusion')
+    .controller('MediaServiceSettingsController', MediaServiceSettingsController);
+}());
