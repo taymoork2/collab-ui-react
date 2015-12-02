4650fb480098d86c809b11455fe194ecf4b9e3ad
lhsieh <lhsieh@cisco.com>
2015-12-02 15:56:29 -0800

    Based on team decision, turn off spinner/loading indicator for the Config and the Report iframe page.

--- app/modules/webex/siteReportsIframe/siteReportIframe.ctrl.js
+++ app/modules/webex/siteReportsIframe/siteReportIframe.ctrl.js
@@ -34,6 +34,7 @@
       _this.funcName = "ReportsIframeCtrl()";
       _this.logMsg = "";
 
+      /*
       $scope.showSpinner = (
         ("storage_utilization" == $stateParams.reportPageId) ||
         ("support_center_support_sessions" == $stateParams.reportPageId) ||
@@ -42,7 +43,8 @@
         ("support_center_csr_activity" == $stateParams.reportPageId) ||
         ("support_center_url_referral" == $stateParams.reportPageId)
       ) ? false : true;
-      // $scope.showSpinner = false;
+      */
+      $scope.showSpinner = false;
 
       $scope.siteUrl = $stateParams.siteUrl;
       $scope.indexPageSref = "webex-reports({siteUrl:'" + $stateParams.siteUrl + "'})";
--- app/modules/webex/utils/iFrameResizeable.directive.js
+++ app/modules/webex/utils/iFrameResizeable.directive.js
@@ -7,7 +7,7 @@ angular.module('WebExUtils').directive('iFrameResizable', function ($window) {
   return function ($scope) {
     $scope.initializeWindowSize = function () {
       var innerHeight = $window.innerHeight;
-      // var iframeTopMargin = ($scope.showSpinner) ? 214 : 200;
+      // var iframeTopMargin = 200;
       var iframeTopMargin = 214;
 
       $scope.iframeHeight = (iframeTopMargin >= innerHeight) ? 0 : innerHeight - iframeTopMargin;
