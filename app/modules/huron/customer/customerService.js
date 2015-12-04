2dc57c04c47bc3ce4cc11781e5f8f2272d1804a7
Brian Spence <brspence@cisco.com>
2015-12-03 18:30:20 -0600

    BLUE-1854: disable advanced spark call checkbox in edit trial
    Add metric event for trial edit
    Add trial data to metric events
    Add self-healing for Huron customer create

--- app/modules/huron/customer/customerService.js
+++ app/modules/huron/customer/customerService.js
@@ -6,8 +6,7 @@
     .factory('HuronCustomer', HuronCustomer);
 
   /* @ngInject */
-  function HuronCustomer(Authinfo, CustomerCommonService) {
-    var domainRegex = /[^A-Za-z0-9\-]/g;
+  function HuronCustomer(Authinfo, CustomerCommonService, $q) {
 
     var customerPayload = {
       'uuid': null,
@@ -18,34 +17,55 @@
       }
     };
 
-    return {
-      create: function (uuid, name, email) {
-        var customer = angular.copy(customerPayload);
-        customer.uuid = uuid;
-        customer.name = name;
-
-        // Set customer.voicemail.pilotNumber to customer's uuid, so that it is unique.
-        // The pilot number will be set in service setup.
-        customer.voicemail.pilotNumber = uuid;
-        return CustomerCommonService.save({}, customer).$promise;
-      },
-
-      get: function () {
-        return CustomerCommonService.get({
-          customerId: Authinfo.getOrgId()
-        }).$promise;
-      },
-
-      put: function (name) {
-        var customer = angular.copy(customerPayload);
-        customer.uuid = undefined;
-        customer.name = name;
-        customer.voicemail = undefined;
-
-        return CustomerCommonService.update({
-          customerId: Authinfo.getOrgId()
-        }, customer).$promise;
-      }
+    var service = {
+      create: createCustomer,
+      get: getCustomer,
+      put: putCustomer
     };
+
+    return service;
+
+    function createCustomer(uuid, name, email) {
+      var customer = angular.copy(customerPayload);
+      customer.uuid = uuid;
+      customer.name = name;
+
+      // Set customer.voicemail.pilotNumber to customer's uuid, so that it is unique.
+      // The pilot number will be set in service setup.
+      customer.voicemail.pilotNumber = uuid;
+      return CustomerCommonService.save({}, customer).$promise
+        .catch(function (response) {
+          // If we have a conflict on create
+          if (_.get(response, 'status') === 409) {
+            // Query the customer we're trying to create
+            return getCustomer(uuid)
+              .then(function (getCustomer) {
+                // Update the customer if it matches our name
+                if (_.get(getCustomer, 'name') === name) {
+                  return putCustomer(name, uuid);
+                }
+              });
+          }
+          // Otherwise return original error
+          return $q.reject(response);
+        });
+    }
+
+    function getCustomer(uuid) {
+      return CustomerCommonService.get({
+        customerId: uuid || Authinfo.getOrgId()
+      }).$promise;
+    }
+
+    function putCustomer(name, uuid) {
+      var customer = angular.copy(customerPayload);
+      customer.uuid = undefined;
+      customer.name = name;
+      customer.voicemail = undefined;
+
+      return CustomerCommonService.update({
+        customerId: uuid || Authinfo.getOrgId()
+      }, customer).$promise;
+    }
   }
 })();
