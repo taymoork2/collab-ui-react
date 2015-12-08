73eb2ddccca8c55ae87ee33350f6f290b95f59c4
lhsieh <lhsieh@cisco.com>
2015-12-07 21:23:33 -0800

    US52126: Change display for license over limit

--- app/modules/webex/utils/webExUtils.fact.js
+++ app/modules/webex/utils/webExUtils.fact.js
@@ -231,11 +231,6 @@
                   var licenseVolume = license.volume;
                   var licenseUsage = license.usage;
                   var licensesAvailable = licenseVolume - licenseUsage;
-                  /*
-                  var licensesAvailable = (
-                    (licenseVolume - licenseUsage) < 0
-                  ) ? 0 : licenseVolume - licenseUsage;
-                  */
 
                   licenseInfo = {
                     volume: licenseVolume,
