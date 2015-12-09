b3a04088cff9173f56c3feb0fb83861c43269165
sani <sani@cisco.com>
2015-12-09 11:24:44 -0600

    US213253 - UI Create Simple AA from Template E2E test update

--- test/e2e-protractor/pages/autoattendant.page.js
+++ test/e2e-protractor/pages/autoattendant.page.js
@@ -10,6 +10,12 @@ var AutoAttendantPage = function () {
   this.numberDropDownOptions = element(by.css('.select-options')).all(by.tagName('li'));
   this.saveButton = element(by.name('saveButton'));
   this.numberIconClose = element.all(by.css('.icon-close')).last();
+  this.sayMessage = element(by.cssContainingText('.aa-message-panel', 'Say Message'));
+  this.sayMessageInput = element(by.name('sayMessageInput'));
+  this.sayMessageLanguage = element(by.css('select[name="languageSelect"] + div a.select-toggle'));
+  this.languageDropDownOptions = element(by.css('select[name="languageSelect"] + div div.dropdown-menu')).all(by.tagName('li')).first();
+  this.sayMessageVoice = element(by.css('select[name="voiceSelect"] + div a.select-toggle'));
+  this.sayMessageVoiceOptions = element(by.css('select[name="voiceSelect"] + div div.dropdown-menu')).all(by.tagName('li')).first();
 };
 
 module.exports = AutoAttendantPage;
