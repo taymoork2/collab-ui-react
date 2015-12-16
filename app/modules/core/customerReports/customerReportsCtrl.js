05a77eddf97aad4442f233f9338651f68e026aac
khempel <khempel@cisco.com>
2015-12-16 13:26:12 -0600

    US9747 & US9751 - Final Room and File graph updates

--- app/modules/core/customerReports/customerReportsCtrl.js
+++ app/modules/core/customerReports/customerReportsCtrl.js
@@ -243,7 +243,7 @@
         } else if (response.length === 0) {
           vm.filesSharedStatus = EMPTY;
         } else {
-          var tempFilesSharedChart = CustomerGraphService.setAvgRoomsGraph(response, filesSharedChart);
+          var tempFilesSharedChart = CustomerGraphService.setFilesSharedGraph(response, filesSharedChart);
           if (tempFilesSharedChart !== null && angular.isDefined(tempFilesSharedChart)) {
             filesSharedChart = tempFilesSharedChart;
           }
