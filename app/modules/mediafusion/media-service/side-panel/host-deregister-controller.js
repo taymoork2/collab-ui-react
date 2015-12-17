ec440950fd343b965613474a13d797f710b4c558
napal <napal@localhost.localdomain>
2015-12-17 16:08:01 +0530

    Media Service- host deregister changes

---
+++ app/modules/mediafusion/media-service/side-panel/host-deregister-controller.js
@@ -0,0 +1,37 @@
+(function () {
+  'use strict';
+
+  /* @ngInject */
+  function HostDeregisterController(cluster, MediaClusterService, XhrNotificationService, $translate, $modalInstance, $window) {
+    var vm = this;
+    window.x = $window;
+    vm.deregisterAreYouSure = $translate.instant(
+      'mediaFusion.clusters.deregisterAreYouSure', {
+        clusterName: cluster.name
+      });
+    vm.saving = false;
+    vm.deregister = function () {
+      vm.saving = true;
+      MediaClusterService
+        .deleteCluster(cluster.id)
+        .then(function () {
+          $modalInstance.close();
+          vm.saving = false;
+        }, function (err) {
+          vm.error = $translate.instant('mediaFusion.clusters.deregisterErrorGeneric', {
+              clusterName: cluster.name,
+              errorMessage: XhrNotificationService.getMessages(err).join(', ')
+            });
+          vm.saving = false;
+        });
+      return false;
+    };
+
+    vm.close = $modalInstance.close;
+  }
+
+  angular
+    .module('Mediafusion')
+    .controller('HostDeregisterController', HostDeregisterController);
+
+}());
