f99a7b898b432d2b53f342e513b32d48e87bee1a
Thu Nguyen <thunguy@cisco.com>
2015-12-18 10:18:24 -0600

    Add unit tests for user international dialing configuration.

---
+++ app/modules/huron/internationalDialing/internationalDialingCtrl.spec.js
@@ -0,0 +1,48 @@
+'use strict';
+
+describe('Controller: InternationalDialingInfoCtrl', function () {
+  var controller, $controller, $scope, $q, $httpBackend, InternationalDialing, Notification, HuronConfig;
+  var url;
+  var getDeferred;
+  var currentUser = getJSONFixture('core/json/currentUser.json');
+  var cosRestrictions = getJSONFixture('huron/json/user/cosRestrictions.json');
+
+  var $stateParams = {
+    currentUser: currentUser
+  };
+
+  beforeEach(module('Huron'));
+
+  beforeEach(inject(function ($rootScope, _$controller_, _$q_, _$httpBackend_, _InternationalDialing_, _Notification_, _HuronConfig_) {
+    $scope = $rootScope.$new();
+    $controller = _$controller_;
+    $httpBackend = _$httpBackend_;
+    Notification = _Notification_;
+    HuronConfig = _HuronConfig_;
+    InternationalDialing = _InternationalDialing_;
+    $q = _$q_;
+
+    url = HuronConfig.getCmiUrl() + '/voice/customers/' + currentUser.meta.organizationID + '/users/' + currentUser.id + '/features/restrictions';
+
+    spyOn(Notification, 'notify');
+    spyOn(InternationalDialing, 'listCosRestrictions').and.returnValue($q.when(cosRestrictions));
+    spyOn(InternationalDialing, 'updateCosRestriction').and.returnValue($q.when());
+
+    controller = $controller('InternationalDialingInfoCtrl', {
+      $scope: $scope,
+      $stateParams: $stateParams,
+      InternationalDialing: InternationalDialing,
+      Notification: Notification
+    });
+
+    $scope.$apply();
+  }));
+
+  it('should update international dialing when toggle is OFF', function () {
+    controller.saveInternationalDialing();
+    $scope.$apply();
+
+    expect(InternationalDialing.updateCosRestriction).toHaveBeenCalled();
+    expect(Notification.notify).toHaveBeenCalledWith(jasmine.any(Array), 'success');
+  });
+});
