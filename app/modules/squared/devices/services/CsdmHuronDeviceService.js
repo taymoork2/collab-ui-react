69d567a76ef6b58914b6fe7c5e8daccf7d199efb
Bjørn Nordlund <bnordlun@cisco.com>
2015-12-22 17:31:34 +0100

    Added user feature toggle for listing huron devices in csdm

--- app/modules/squared/devices/services/CsdmHuronDeviceService.js
+++ app/modules/squared/devices/services/CsdmHuronDeviceService.js
@@ -2,18 +2,37 @@
   'use strict';
 
   /* @ngInject  */
-  function CsdmHuronDeviceService($http, $q, Authinfo, CsdmConfigService, CsdmConverter, CsdmCacheFactory, $window) {
+  function CsdmHuronDeviceService($http, $q, Authinfo, CsdmConfigService, CsdmConverter, CsdmCacheFactory, $window, FeatureToggleService) {
     var devicesUrl = CsdmConfigService.getUrl() + '/organization/' + Authinfo.getOrgId() + '/huronDevices';
+
+    function huronEnabled() {
+      return FeatureToggleService.getFeaturesForUser(Authinfo.getUserId()).then(function (features) {
+        var feature = _.find(features.user, {
+          key: "csdm-huron"
+        });
+        if (angular.isUndefined(feature)) {
+          return false;
+        }
+        return feature.val;
+      });
+    }
 
     var deviceCache = CsdmCacheFactory.create({
       fetch: function () {
-        if ($window.location.search.indexOf("showHuronDevices=true") > -1) {
-          return $http.get(devicesUrl).then(function (res) {
-            return CsdmConverter.convertHuronDevices(res.data);
+        return huronEnabled().then(function (enabled) {
+          if (enabled) {
+            return $http.get(devicesUrl).then(function (res) {
+              return CsdmConverter.convertHuronDevices(res.data);
+            });
+          } else {
+            return $q(function (resolve) {
+              resolve([]);
+            });
+          }
+        }).catch(function (err) {
+          return $q(function (resolve) {
+            resolve([]);
           });
-        }
-        return $q(function (resolve) {
-          resolve([]);
         });
       }
     });
