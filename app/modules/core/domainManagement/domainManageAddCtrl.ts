f3d16f53510994a73a124c4a78b70011dc662c2b
Magnus Holst <maholst@cisco.com>
2016-01-13 13:15:05 +0100

    live validate domain in add dialog while typing.

--- app/modules/core/domainManagement/domainManageAddCtrl.ts
+++ app/modules/core/domainManagement/domainManageAddCtrl.ts
@@ -14,8 +14,12 @@ namespace domainManagement {
       let ctrl = this;
 
       this._domainManagementService.addDomain(this._domain).then(
-        function () { ctrl.$state.go('domainmanagement');},
-        function (err) { console.log('could not add domain (example failure): ' + ctrl._domain + err);}
+        function () {
+          ctrl.$state.go('domainmanagement');
+        },
+        function (err) {
+          console.log('could not add domain (example failure): ' + ctrl._domain + err);
+        }
       )
     }
 
@@ -27,8 +31,31 @@ namespace domainManagement {
       return this._domain;
     }
 
-    set domain(domain){
+    set domain(domain) {
       this._domain = domain;
+    }
+
+    //gui valid
+
+    public validate() {
+      if (this._domain && this._domain.length > 0) {
+
+        if (/^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)+([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9]){2,}$/g.test(this._domain)) {
+          return {valid: true, empty: false};
+        } else {
+          return {valid: false, empty: false};
+        }
+      } else {
+        return {valid: false, empty: true};
+      }
+    }
+    get isValid() {
+      let validation = this.validate();
+      return validation && validation.valid;
+    }
+
+    get addEnabled() {
+      return this._domain && this._domain.length > 0 && this.isValid;
     }
   }
   angular
