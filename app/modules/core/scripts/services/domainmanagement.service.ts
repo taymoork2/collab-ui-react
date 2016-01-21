9d2e001e3d9c8a2bb732a0247f6c4e705681c12c
ngrodum <ngrodum@cisco.com>
2016-01-21 16:01:00 +0100

    Domain Management: Wired up the reading of pending, enforced and claimed domains

--- app/modules/core/scripts/services/domainmanagement.service.ts
+++ app/modules/core/scripts/services/domainmanagement.service.ts
@@ -12,8 +12,20 @@ class DomainManagementService {
     claimed: 'claimed'
   }
 
-  constructor($http, Config, Authinfo, private $q) {
-    var _verifiedDomainsUrl = Config.getDomainManagementUrl(Authinfo.getOrgId());
+  private _scomUrl;
+
+  constructor(private $http, Config, Authinfo, private $q) {
+
+
+    var _verifiedDomainsUrl = Config.getDomainManagementUrl(Authinfo.getOrgId());  //not used anymore?
+
+    this._scomUrl = Config.getScomUrl() + '/' + Authinfo.getOrgId();
+
+    //Unverify: http://wikicentral.cisco.com/display/IDENTITY/API+-+UnVerify+Domain+Ownership
+    //POST https://identity.webex.com/organization/{orgid}/v1/actions/DomainVerification/Unverify/invoke
+
+    //Delete: (domain base64 enc) http://wikicentral.cisco.com/display/IDENTITY/Domain+Management+API+-+Delete+Domain
+    //DELETE https://<server name>/organization/{orgId}/v1/Domains/<domainValue>
   }
 
   get states() {
@@ -25,7 +37,7 @@ class DomainManagementService {
   }
 
   refreshDomainList() {
-    return this.getVerifiedDomains();
+    return this.getVerifiedDomains(false);
   }
 
   addDomain(domainToAdd) {
@@ -59,19 +71,136 @@ class DomainManagementService {
     return deferred.promise;
   }
 
-  getVerifiedDomains() {
+  getVerifiedDomains(disableCache) {
     let deferred = this.$q.defer();
 
-    this._domainList.push(
+    let scomUrl = this._scomUrl + disableCache ? '?disableCache=true' : '';
+
+    this.$http.get(scomUrl).then(data => {
+
+      this._domainList = [];
+
+      if (!data || (!data.domains && !data.verifiedDomains && !data.pendingDomains)){
+        data =
+        {
+          id: "theid",
+          meta:{ created: "adfd"},
+          displayName: "org",
+          domains: [
+            "claimed1.grodum.org",
+            "claimed2.grodum.org"
+          ],
+          verifiedDomains: [
+            "claimed1.grodum.org",
+            "verified1.grodum.org",
+            "verified2.grodum.org"
+          ],
+          pendingDomains: [
+            "verified1.grodum.org",
+            "claimed2.grodum.org",
+            "pending1.grodum.org",
+            "pending2.grodum.org"
+          ]
+        };
+      }
+
+      this.loadDomainlist(data.domains, this.states.claimed, overrideIf => (overrideIf.status != this.states.claimed));
+
+      this.loadDomainlist(data.verifiedDomains, this.states.verified, overrideIf => (overrideIf.status == this.states.pending));
+
+      this.loadDomainlist(data.pendingDomains, this.states.pending, null);
+
+     /* function(domainList, domainStatus, overridePredicate)
+
+      _.each(data.domains, claimedDom => {
+
+        let claimedDomLower = claimedDom.toLowerCase();
+        let alreadyAddedMatch = _.find(this._domainList, { text: claimedDomLower});
+
+        if (!alreadyAddedMatch || (alreadyAddedMatch.status != this.states.claimed)){
+
+          if (alreadyAddedMatch)
+            _.remove(this._domainList, {text: claimedDomLower});
+
+          this._domainList.push({
+              text: claimedDomLower,
+              code: '',
+              status: this.states.claimed
+          });
+        }
+      });
+
+      _.each(data.verifiedDomains, verifiedDom => {
+
+        let verifiedDomLower = verifiedDom.toLowerCase();
+        let alreadyAddedMatch = _.find(this._domainList, { text: verifiedDomLower});
+
+        if (!alreadyAddedMatch || (alreadyAddedMatch.status == this.states.pending)){
+
+          if (alreadyAddedMatch)
+            _.remove(this._domainList, {text: verifiedDomLower});
+
+          this._domainList.push({
+            text: verifiedDomLower,
+            code: '',
+            status: this.states.verified
+          });
+        }
+      });
+
+      _.each(data.pendingDomains, pendingDom => {
+
+        let pendingDomLower = pendingDom.toLowerCase();
+        let alreadyAddedMatch = _.find(this._domainList, { text: pendingDomLower});
+
+        if (!alreadyAddedMatch){
+          this._domainList.push({
+            text: pendingDomLower,
+            code: 'RNG31337',
+            status: this.states.pending
+          });
+        }
+      });*/
+
+      deferred.resolve(this._domainList);
+    }, error => {
+      deferred.reject(error);
+    });
+
+
+    /*this._domainList.push(
       {
         text: 'getVerifiedDomainspromise.com'.toLowerCase(),
         code: '',
         status: this.states.verified
-      });
+      });*/
 
-    deferred.resolve();
+
     return deferred.promise;
   }
+
+  private loadDomainlist(domainArray, domainStatus, overridePredicate){
+
+    _.each(domainArray, dom => {
+
+      let domLower = dom.toLowerCase();
+      let alreadyAddedMatch = _.find(this._domainList, { text: domLower});
+
+      if (!alreadyAddedMatch || (overridePredicate && overridePredicate(alreadyAddedMatch))){
+
+        if (alreadyAddedMatch)
+          _.remove(this._domainList, {text: domLower});
+
+        this._domainList.push({
+          text: domLower,
+          code: '',
+          status: domainStatus
+        });
+      }
+    });
+
+  }
+
 
   public verifyDomain(domain) {
     let deferred = this.$q.defer();
