7fe1e67a090a27df9b08ff6178c6e2185b5d8087
Tom Vasset <tvasset@cisco.com>
2016-01-25 17:59:01 +0100

    Readonly admin changes

--- app/modules/core/login/loginCtrl.js
+++ app/modules/core/login/loginCtrl.js
@@ -58,7 +58,7 @@ angular.module('Core')
                 state = 'partneroverview';
 
               } else if (Authinfo.isSupportUser()) {
-                state = 'support.status';
+                state = 'support';
               } else if (!$stateParams.customerOrgId && Authinfo.isHelpDeskUserOnly()) {
                 state = 'helpdesk.search';
               } else if (Authinfo.isPartnerUser()) {
