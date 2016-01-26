3ee944297f9f18cd778c7a5d3df953d4a216d3c9
Chris Laughbon <claughbo@cisco.com>
2016-01-26 14:42:24 -0800

    Indigo 224 - UT

---
+++ app/modules/huron/features/featureLanding/featureCardsDirective.spec.js
@@ -0,0 +1,23 @@
+'use strict';
+
+describe('Directive: featureCards', function () {
+  var $compile, $rootScope;
+
+  beforeEach(module('Huron'));
+
+  beforeEach(inject(function (_$compile_, _$rootScope_) {
+    $compile = _$compile_;
+    $rootScope = _$rootScope_;
+  }));
+
+  it('replaces the element with the appropriate content', function () {
+    // var element = $compile("<feature-cards ng-if=\"huronFeaturesCtrl.pageState == 'showFeatures'\"></feature-cards>")($rootScope);
+
+    var element = $compile("<feature-cards </feature-cards>")($rootScope);
+
+    $rootScope.$digest();
+
+    expect(element.html()).toContain('cs-card-block');
+  });
+
+});
---
+++ app/modules/huron/features/featureLanding/featuresCtrlAA.spec.js
@@ -0,0 +1,237 @@
+/**
+ * Created by sjalipar on 10/9/15.
+ */
+'use strict';
+
+describe('Features Controller', function () {
+
+  var featureCtrl, $rootScope, $scope, $modal, $q, $state, $translate, $filter, $timeout, Authinfo, Log, Notification, getDeferred, AutoAttendantCeInfoModelService, FeatureToggleService;
+  var listOfAAs = getJSONFixture('huron/json/autoAttendant/aaList.json');
+  var emptyListOfAAs = [];
+  var getAAListSuccessResp = function (data) {
+    return data;
+  };
+  var getAAListFailureResp = {
+    'data': 'Internal Server Error',
+    'status': 500,
+    'statusText': 'Internal Server Error'
+  };
+  var AAs = [{
+    'cardName': 'Main AA',
+    'numbers': ['1111'],
+    'id': 'c16a6027-caef-4429-b3af-9d61ddc71111',
+    'featureName': 'huronHuntGroup.hg',
+    'filterValue': 'AA',
+  }, {
+    'cardName': 'Second AA',
+    'numbers': ['2222'],
+    'id': 'c16a6027-caef-4429-b3af-9d61ddc72222',
+    'featureName': 'huronHuntGroup.hg',
+    'filterValue': 'AA',
+    'hasReferences': true,
+    'referenceNames': ['Main AA']
+  }, {
+    'cardName': 'Third  AA',
+    'numbers': ['3333'],
+    'id': 'c16a6027-caef-4429-b3af-9d61ddc73333',
+    'featureName': 'huronHuntGroup.hg',
+    'filterValue': 'AA'
+  }];
+
+  var cesWithNumber = getJSONFixture('huron/json/autoAttendant/callExperiencesWithNumber.json');
+
+  beforeEach(module('Huron'));
+
+  beforeEach(inject(function (_$rootScope_, $controller, _$q_, _$modal_, _$state_, _$filter_, _$timeout_, _Authinfo_, _AutoAttendantCeService_, _AutoAttendantCeInfoModelService_, _Log_, _Notification_, _FeatureToggleService_) {
+    $rootScope = _$rootScope_;
+    $scope = _$rootScope_.$new();
+    $modal = _$modal_;
+    $state = _$state_;
+    $filter = _$filter_;
+    $timeout = _$timeout_;
+    $q = _$q_;
+    Authinfo = _Authinfo_;
+    AutoAttendantCeInfoModelService = _AutoAttendantCeInfoModelService_;
+
+    Log = _Log_;
+    Notification = _Notification_;
+    FeatureToggleService = _FeatureToggleService_;
+
+    //create mock deferred object which will be used to return promises
+    getDeferred = $q.defer();
+
+    spyOn(AutoAttendantCeInfoModelService, 'getCeInfosList').and.returnValue(getDeferred.promise);
+
+    spyOn(Notification, 'error');
+
+    spyOn(FeatureToggleService, 'supports').and.callFake(function (feature) {
+      if (feature === "huronAutoAttendant") {
+        return getDeferred.promise;
+      }
+
+      if (feature === "huronHuntGroup") {
+        return null;
+      }
+      return null;
+    });
+
+    spyOn($state, 'go');
+
+    featureCtrl = $controller('HuronFeaturesCtrl', {
+      $scope: $scope,
+      $modal: $modal,
+      $state: $state,
+      $filter: $filter,
+      $timeout: $timeout,
+      Authinfo: Authinfo,
+      AutoAttendantCeInfoModelService: AutoAttendantCeInfoModelService,
+      Log: Log,
+      Notification: Notification
+    });
+
+  }));
+
+  it('should get list of AAs and if there is any data, should change the pageState to showFeatures', function () {
+
+    getDeferred.resolve(getAAListSuccessResp(listOfAAs));
+    $scope.$apply();
+
+    $timeout.flush();
+    expect(featureCtrl.pageState).toEqual('showFeatures');
+  });
+
+  it('should get list of AAs and if data received is empty, should change the pageSate to newFeature', function () {
+    getDeferred.resolve(getAAListSuccessResp(emptyListOfAAs));
+    $scope.$apply();
+    $timeout.flush();
+    expect(featureCtrl.pageState).toEqual('NewFeature');
+  });
+
+  it('should be able call delete a huntGroup function and call the $state service', function () {
+    getDeferred.resolve(getAAListSuccessResp(emptyListOfAAs));
+    $scope.$apply();
+    $timeout.flush();
+
+    featureCtrl.deleteHuronFeature(AAs[0]);
+    expect($state.go).toHaveBeenCalledWith('huronfeatures.deleteFeature', {
+      deleteFeatureName: AAs[0].cardName,
+      deleteFeatureId: AAs[0].id,
+      deleteFeatureType: 'AA'
+    });
+  });
+  it('should be able to edit a huntGroup function ', function () {
+    featureCtrl.editHuronFeature(AAs[0]);
+    expect($state.go).toHaveBeenCalledWith('huronfeatures.aabuilder', {
+      aaName: AAs[0].cardName
+    });
+  });
+
+  it('should search list of AAs', function () {
+
+    getDeferred.resolve(getAAListSuccessResp(listOfAAs));
+
+    $scope.$apply();
+    $timeout.flush();
+
+    var saveF = featureCtrl.listOfFeatures[0];
+
+    featureCtrl.searchData(featureCtrl.listOfFeatures[0].cardName);
+
+    expect(featureCtrl.listOfFeatures.length).toEqual(1);
+
+    expect(featureCtrl.listOfFeatures).toContain(saveF);
+
+  });
+
+  it('should filter a list of AAs', function () {
+    getDeferred.resolve(getAAListSuccessResp(listOfAAs));
+
+    $scope.$apply();
+    $timeout.flush();
+
+    featureCtrl.setFilter('AA');
+
+    expect(featureCtrl.listOfFeatures.length).toEqual(AAs.length);
+    expect(featureCtrl.listOfFeatures[0].cardName).toEqual(AAs[0].cardName);
+
+  });
+
+  it('should filter a list of AAs to zero length', function () {
+    getDeferred.resolve(getAAListSuccessResp(listOfAAs));
+
+    $scope.$apply();
+    $timeout.flush();
+
+    featureCtrl.setFilter('XX');
+
+    expect(featureCtrl.listOfFeatures.length).toEqual(0);
+
+  });
+  it('should receive the HURON_FEATURE_DELETED event when an AA gets deleted', function () {
+
+    var saveFeature;
+
+    getDeferred.resolve(getAAListSuccessResp(listOfAAs));
+    $scope.$apply();
+
+    $timeout.flush();
+
+    saveFeature = featureCtrl.listOfFeatures[2];
+
+    featureCtrl.deleteHuronFeature(featureCtrl.listOfFeatures[2]);
+
+    $rootScope.$broadcast('HURON_FEATURE_DELETED', {
+      deleteFeatureName: AAs[0].cardName,
+      deleteFeatureId: AAs[0].id,
+      deleteFeatureType: 'AA'
+    });
+
+    expect(featureCtrl.listOfFeatures).not.toContain(saveFeature);
+
+  });
+
+  it('should receive the HURON_FEATURE_DELETED event when an AA gets deleted with dependencies', function () {
+
+    var saveFeature;
+
+    getDeferred.resolve(getAAListSuccessResp(listOfAAs));
+    $scope.$apply();
+
+    $timeout.flush();
+
+    saveFeature = featureCtrl.listOfFeatures[1];
+
+    featureCtrl.deleteHuronFeature(featureCtrl.listOfFeatures[1]);
+
+    $rootScope.$broadcast('HURON_FEATURE_DELETED', {
+      deleteFeatureName: AAs[0].cardName,
+      deleteFeatureId: AAs[0].id,
+      deleteFeatureType: 'AA'
+    });
+
+    expect(featureCtrl.listOfFeatures[0].hasDepends).toEqual(false);
+
+    expect(featureCtrl.listOfFeatures).not.toContain(saveFeature);
+  });
+
+  it('should Not delete an AA with dependencies', function () {
+
+    var saveFeature;
+
+    getDeferred.resolve(getAAListSuccessResp(listOfAAs));
+    $scope.$apply();
+
+    $timeout.flush();
+
+    saveFeature = featureCtrl.listOfFeatures[0];
+
+    featureCtrl.deleteHuronFeature(featureCtrl.listOfFeatures[0]);
+
+    expect(Notification.error).toHaveBeenCalledWith('huronFeatureDetails.aaDeleteBlocked', jasmine.any(Object));
+
+    expect(featureCtrl.listOfFeatures[0].hasDepends).toEqual(true);
+
+    expect(featureCtrl.listOfFeatures).toContain(saveFeature);
+  });
+
+});
