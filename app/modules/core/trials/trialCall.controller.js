cbbc913bb0365b3d736289498e0db5521462747b
Admas Molla <admolla@cisco.com>
2016-02-03 15:03:41 -0800

    localized changes for shipping address
    starting writing unit tests for the device trials
    
    adding unit tests for initial state

--- app/modules/core/trials/trialCall.controller.js
+++ app/modules/core/trials/trialCall.controller.js
@@ -13,19 +13,19 @@
 
     vm.details = _trialData.details;
     vm.sx10 = _.find(vm.details.roomSystems, {
-      model: 'sx10'
+      model: 'CISCO_sx10'
     });
     vm.phone8865 = _.find(vm.details.phones, {
-      model: '8865'
+      model: 'CISCO_8865'
     });
     vm.phone8845 = _.find(vm.details.phones, {
-      model: '8845'
+      model: 'CISCO_8845'
     });
     vm.phone8841 = _.find(vm.details.phones, {
-      model: '8841'
+      model: 'CISCO_8841'
     });
     vm.phone7841 = _.find(vm.details.phones, {
-      model: '7841'
+      model: 'CISCO_7841'
     });
     vm.shippingInfo = _.find(vm.details.shippingInfo, {
       isPrimary: true
@@ -197,70 +197,95 @@
       },
     }];
 
-    vm.foo = [{
-      key: 'country',
-      type: 'input',
-      templateOptions: {
-        labelClass: 'columns medium-3',
-        inputClass: 'columns medium-6',
-        label: 'Country',
-        type: 'text',
-        required: true,
-      },
-    }, {
+    vm.shippingFields = [{
       key: 'name',
       type: 'input',
+      className: 'columns medium-7 pad-top',
       templateOptions: {
-        labelClass: 'columns medium-3',
-        inputClass: 'columns medium-6',
+        labelClass: 'columns medium-2',
+        inputClass: 'columns medium-10',
         label: 'Name',
         type: 'text',
       },
     }, {
-      key: 'phone',
+      key: 'phoneNumber',
       type: 'input',
+      className: 'columns medium-5 pad-top',
       templateOptions: {
         labelClass: 'columns medium-3',
-        inputClass: 'columns medium-6',
+        inputClass: 'columns medium-9',
         label: 'Phone',
-        type: 'number',
       },
     }, {
-      key: 'street',
-      type: 'input',
+      key: 'country',
+      type: 'select',
+      defaultValue: _.find(TrialCallService.getCountryList(), {
+        code: 'USA'
+      }),
+      className: 'columns medium-12 pad-top',
       templateOptions: {
-        labelClass: 'columns medium-3',
-        inputClass: 'columns medium-6',
-        label: 'Street',
+        labelClass: 'columns medium-2',
+        inputClass: 'columns medium-10',
+        label: 'Country',
+        type: 'text',
+        required: true,
+        labelfield: 'country',
+        labelProp: 'country',
+        valueProp: 'code',
+      },
+      expressionProperties: {
+        'templateOptions.options': function () {
+          return TrialCallService.getCountryList();
+        },
+      },
+    }, {
+      key: 'addressLine1',
+      type: 'input',
+      className: 'columns medium-12 pad-top',
+      templateOptions: {
+        labelClass: 'columns medium-2',
+        inputClass: 'columns medium-10',
+        label: 'Address',
         type: 'text',
         required: true,
       },
     }, {
       key: 'city',
       type: 'input',
+      className: 'columns medium-4 pad-top',
       templateOptions: {
         labelClass: 'columns medium-3',
-        inputClass: 'columns medium-4',
+        inputClass: 'columns medium-9',
         label: 'City',
         type: 'text',
         required: true,
       },
     }, {
       key: 'state',
-      type: 'input',
+      type: 'select',
+      className: 'columns medium-4 pad-top',
       templateOptions: {
-        labelClass: 'columns medium-3',
-        inputClass: 'columns medium-2',
+        labelClass: 'columns medium-4',
+        inputClass: 'columns medium-8',
         label: 'State',
         type: 'text',
         required: true,
+        labelfield: 'abbr',
+        labelProp: 'abbr',
+        valueProp: 'state',
+      },
+      expressionProperties: {
+        'templateOptions.options': function () {
+          return TrialCallService.getStateList();
+        }
       },
     }, {
-      key: 'zip',
+      key: 'postalCode',
       type: 'input',
+      className: 'columns medium-4 pad-top',
       templateOptions: {
         labelClass: 'columns medium-3',
-        inputClass: 'columns medium-2',
+        inputClass: 'columns medium-9',
         label: 'Zip',
         type: 'number',
         max: 99999,
--- app/modules/core/trials/trialCall.controller.spec.js
+++ app/modules/core/trials/trialCall.controller.spec.js
@@ -1,1 +1,48 @@
+'use strict';
 
+describe('Controller: TrialCallCtrl', function () {
+  var controller, $scope, $translate;
+
+  beforeEach(module('Core'));
+  beforeEach(module('core.trial'));
+
+  beforeEach(inject(function ($rootScope, $controller, _$translate_) {
+    $scope = $rootScope.$new();
+    $translate = _$translate_;
+
+    controller = $controller('TrialCallCtrl', {
+      $scope: $scope,
+      $translate: $translate,
+    });
+    $scope.$apply();
+  }));
+
+  it('should be created successfully', function () {
+    expect(controller).toBeDefined();
+  });
+
+  it('should have nothing enabled', function () {
+    var roomSystems = _.find(controller.details.roomSystems, {enabled:true});
+    var phones = _.filter(controller.details.phones, {enabled:true});
+
+    expect(roomSystems).toBeUndefined();
+    expect(phones.length).toBe(0);
+  });
+
+  // the back end expects this as an enum and enums cant start with numbers
+  it('should have all devices starting with \'CISCO_\'',function () {
+    for(var i = 0; i < controller.details.roomSystems.length; i++){
+        var model = controller.details.roomSystems[i].model;
+        expect(model.indexOf('CISCO_')).toBe(0);
+    }
+    for(var i = 0; i < controller.details.phones.length; i++){
+        var model = controller.details.phones[i].model;
+        expect(model.indexOf('CISCO_')).toBe(0);
+    }
+  });
+
+  it('should always have a recipientType === PARTNER || CUSTOMER', function(){
+    expect(controller.details.shippingInfo.recipientType).toBeDefined();
+    expect(controller.details.shippingInfo.recipientType).toBe('CUSTOMER' || 'PARTNER');
+  });
+});
