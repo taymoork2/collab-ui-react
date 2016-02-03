db0cac29e3f14acdd2735135573067c84a4ab075
Magnus Holst <maholst@cisco.com>
2016-02-03 11:47:38 +0100

    Domain Management: remove access at /#/domainmanagement
    it will only be accessible from the /#/services/call/settings

--- app/scripts/appconfig.js
+++ app/scripts/appconfig.js
@@ -189,7 +189,6 @@ angular
         })
         .state('domainmanagement', {
           //abstract: true,
-          url: '/domainmanagement',
           templateUrl: 'modules/core/domainManagement/domainManagement.tpl.html',
           controller: 'DomainManagementCtrl',
           controllerAs: 'dv',
