0018b8408d175e313ada1cf2b4ab86308387a3ad
Admas Molla <admolla@cisco.com>
2016-02-04 12:29:09 -0800

    updating other tests

--- app/modules/core/trials/trialCall.controller.spec.js
+++ app/modules/core/trials/trialCall.controller.spec.js
@@ -22,26 +22,30 @@ describe('Controller: TrialCallCtrl', function () {
   });
 
   it('should have nothing enabled', function () {
-    var roomSystems = _.find(controller.details.roomSystems, {enabled:true});
-    var phones = _.filter(controller.details.phones, {enabled:true});
+    var roomSystems = _.find(controller.details.roomSystems, {
+      enabled: true
+    });
+    var phones = _.filter(controller.details.phones, {
+      enabled: true
+    });
 
     expect(roomSystems).toBeUndefined();
     expect(phones.length).toBe(0);
   });
 
   // the back end expects this as an enum and enums cant start with numbers
-  it('should have all devices starting with \'CISCO_\'',function () {
-    for(var i = 0; i < controller.details.roomSystems.length; i++){
-        var model = controller.details.roomSystems[i].model;
-        expect(model.indexOf('CISCO_')).toBe(0);
+  it('should have all devices starting with \'CISCO_\'', function () {
+    for (var i = 0; i < controller.details.roomSystems.length; i++) {
+      var rsModel = controller.details.roomSystems[i].model;
+      expect(_.startsWith(rsModel, 'CISCO_')).toBeTruthy();
     }
-    for(var i = 0; i < controller.details.phones.length; i++){
-        var model = controller.details.phones[i].model;
-        expect(model.indexOf('CISCO_')).toBe(0);
+    for (var j = 0; j < controller.details.phones.length; j++) {
+      var phoneModel = controller.details.phones[j].model;
+      expect(_.startsWith(phoneModel, 'CISCO_')).toBeTruthy();
     }
   });
 
-  it('should always have a recipientType === PARTNER || CUSTOMER', function(){
+  it('should always have a recipientType === PARTNER || CUSTOMER', function () {
     expect(controller.details.shippingInfo.recipientType).toBeDefined();
     expect(controller.details.shippingInfo.recipientType).toBe('CUSTOMER' || 'PARTNER');
   });
