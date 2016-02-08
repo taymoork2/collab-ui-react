f667ff12bb410aa9add8e4fd934f4c1c1788d3aa
Brad DeGrazia <degrazia@cisco.com>
2016-02-08 12:53:20 -0800

    Improve test to switch to trials, and to handle white space in trial names.

--- test/e2e-protractor/manual/remove_partner_trials_spec.js
+++ test/e2e-protractor/manual/remove_partner_trials_spec.js
@@ -18,14 +18,16 @@ describe('Remove partner trials from partner org page', function () {
   it('should remove all trials', function () {
     for (var i = 0; i < 1; i++) {
       navigation.clickCustomers();
+      utils.click(partner.trialFilter);
       utils.expectIsNotDisplayed(element(by.css('.icon-spinner')));
+
       //      utils.click(element(by.css('.ui-grid-icon-up-dir')));
 
       element.all(by.css('.ui-grid .ui-grid-row')).each(function (elem, index) {
         elem.getText().then(function (text) {
           var id = text.split('\n');
           if ((id[0] !== undefined) && (id[0].length > 0) && (id[0] !== 'Atlas Test Partner Organization My Organization')) {
-            var a = element(by.id(id[0] + 'LaunchCustomerButton'));
+            var a = element(by.css("li[id='" + id[0] + "LaunchCustomerButton" + "']"));
             a.getInnerHtml().then(function (inner) {
               var patt = /href="#\/login\/\s*(.*?)\s*\//;
               var result = patt.exec(inner);
