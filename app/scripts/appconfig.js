8d656ffa0cb0113747a46508771bbe87197c6b93
jalusk <jalusk@cisco.com>
2016-02-08 13:48:28 -0600

    feat(language): default language detection from the browser

--- app/scripts/appconfig.js
+++ app/scripts/appconfig.js
@@ -144,7 +144,7 @@ angular
 
       $translateProvider.addInterpolation('$translateMessageFormatInterpolation');
 
-      var defaultLang = 'en_US';
+      var defaultLang = window.navigator['language' || 'userLanguage'].replace("-", "_") || 'en_US';
 
       //Tell the module what language to use by default
       $translateProvider.preferredLanguage(defaultLang);
