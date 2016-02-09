d704ad2aeb84a25cc966785901ffe987809ffccf
zamamoha <zamamoha@cisco.com>
2016-02-09 17:13:47 -0600

    Encoding Email Address in Login Page

--- app/modules/core/scripts/services/config.js
+++ app/modules/core/scripts/services/config.js
@@ -870,7 +870,7 @@ angular.module('Core')
             this.oauthClientRegistration.atlas.scope,
             encodeURIComponent(acu),
             this.getOauthServiceType(),
-            email
+            encodeURIComponent(email)
           ];
           return Utils.sprintf(this.oauthUrl.oauth2LoginUrlPattern, params);
         },
