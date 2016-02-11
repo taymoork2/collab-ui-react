513de73fb7f861082d625e93f9ed5261688440dc
tri nguyen <tringu3@cisco.com>
2016-02-11 15:30:41 -0800

    took out eslint comment and use localhost for url in config.sj

--- app/modules/core/scripts/services/config.js
+++ app/modules/core/scripts/services/config.js
@@ -20,7 +20,7 @@ angular.module('Core')
         },
 
         adminServiceUrl: {
-          dev: 'https://atlas-integration.wbx2.com/admin/api/v1/',
+          dev: 'http://localhost:8080/atlas-server/admin/api/v1/',
           cfe: 'https://atlas-e.wbx2.com/admin/api/v1/',
           integration: 'https://atlas-integration.wbx2.com/admin/api/v1/',
           prod: 'https://atlas-a.wbx2.com/admin/api/v1/'
