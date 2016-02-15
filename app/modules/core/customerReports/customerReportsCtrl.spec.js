7de5c4bc8ddceaa90fd3fb146259bcdbb3bc52d2
khempel <khempel@cisco.com>
2016-02-15 10:32:07 -0600

    US9752 - unit test update

--- app/modules/core/customerReports/customerReportsCtrl.spec.js
+++ app/modules/core/customerReports/customerReportsCtrl.spec.js
@@ -280,9 +280,7 @@ describe('Controller: Customer Reports Ctrl', function () {
         controller.timeSelected = timeOptions[2];
         controller.mediaUpdate();
 
-        expect(CustomerReportService.getMediaQualityData).toHaveBeenCalledWith(timeOptions[2]);
         expect(CustomerGraphService.setMediaQualityGraph).toHaveBeenCalled();
-
         expect(CustomerGraphService.setActiveUsersGraph).not.toHaveBeenCalled();
         expect(CustomerGraphService.setAvgRoomsGraph).not.toHaveBeenCalled();
         expect(CustomerGraphService.setFilesSharedGraph).not.toHaveBeenCalled();
