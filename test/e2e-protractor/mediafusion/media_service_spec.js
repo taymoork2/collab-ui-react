59af3e812e1a36ef28f6c4d6ab958fa46983d166
Naveen Amalraj <namalraj@cisco.com>
2016-02-15 17:51:48 +0530

    Protractor Test Cases for Media Service

--- test/e2e-protractor/mediafusion/media_service_spec.js
+++ test/e2e-protractor/mediafusion/media_service_spec.js
@@ -1,73 +1,55 @@
 'use strict';
 
-describe('Validate Media Service Managemnt Page', function () {
+describe('Validate Media Service Managemnt Page', function() {
 
-  afterEach(function () {
+  afterEach(function() {
 
     utils.dumpConsoleErrors();
   });
 
-  describe('Listing Media Service Clusters on page load', function () {
+  describe('Listing Media Service Clusters on page load', function() {
 
-    it('login as media super admin', function () {
-
+    it('login as media super admin', function() {
       login.login('media-super-admin');
-
     }, 120000);
 
-    it('Clicking on Media Service Management tab should change the view', function () {
-
+    it('Clicking on Media Service Management tab should change the view', function() {
       navigation.clickMediaServiceManagement();
-
     });
 
-    it('Check if Media Service is Activated', function () {
-
+    it('Check if Media Service is Activated', function() {
       mediaservice.checkForActivation();
-
     });
 
-    it('Check if Media Clusters are populated', function () {
-
+    it('Check if Media Clusters are populated', function() {
       mediaservice.checkClusterSize();
-
     });
 
-    it('Should be able to view Cluster Details after clicking a Row ', function () {
-
+    it('Should be able to view Cluster Details after clicking a Row ', function() {
       mediaservice.clickOnMediaService();
       utils.expectIsDisplayed(mediaservice.groupDetails);
-
     });
 
-    it('Clicking on Alarms  should show the Alarm Details Page ', function () {
-
+    it('Clicking on Alarms  should show the Alarm Details Page ', function() {
       mediaservice.alarmDetailsPage();
-
     });
 
-    it('Clicking on Host  should show the Host Details Page ', function () {
-
+    it('Clicking on Host  should show the Host Details Page ', function() {
       utils.click(mediaservice.hostDetails);
       utils.expectIsDisplayed(mediaservice.hostTitle);
       utils.click(mediaservice.closeSidePanel);
-
     });
 
-    it('Clicking on Settings Tab should change the view', function () {
-
+    it('Clicking on Settings Tab should change the view', function() {
       navigation.clickMediaServiceSettingsTab();
       utils.expectIsDisplayed(mediaservice.emailnotification);
-
     });
 
   });
 
-  describe('Logout', function () {
-    it('Should log out', function () {
-
+  describe('Logout', function() {
+    it('Should log out', function() {
       navigation.logout();
-
     });
 
   });
