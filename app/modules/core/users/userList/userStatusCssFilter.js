cf5bd42f512b604fff1f1ec5596833c16f3d9532
Shivangi Sharma <fshivang@cisco.com>
2016-02-16 12:28:12 -0600

    US10699 User's page: add green dot for active, add yellow dot for invite pending.

--- app/modules/core/users/userList/userStatusCssFilter.js
+++
@@ -1,5 +0,0 @@
-angular.module('Core').filter('userStatusCssFilter', function ($filter) {
-  return function (status) {
-    return (typeof status === 'undefined' || status === null || status == 'active') ? 'user-status-green' : 'user-status-yellow';
-  };
-});
