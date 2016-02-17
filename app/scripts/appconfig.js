4ef055e0023d7d4b8ab0d0623500aa0efde70715
Brian Spence <brspence@cisco.com>
2016-02-17 13:42:44 -0600

    Revert "feat(language): default language detection from the browser"
    This reverts commit 8d656ffa0cb0113747a46508771bbe87197c6b93.

--- app/scripts/appconfig.js
+++ app/scripts/appconfig.js
@@ -144,7 +144,7 @@ angular
 
       $translateProvider.addInterpolation('$translateMessageFormatInterpolation');
 
-      var defaultLang = window.navigator['language' || 'userLanguage'].replace("-", "_") || 'en_US';
+      var defaultLang = 'en_US';
 
       //Tell the module what language to use by default
       $translateProvider.preferredLanguage(defaultLang);
