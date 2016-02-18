869a69a4837fb10992628577085d9a8ccdecab28
chrispha <chrispha@cisco.com>
2016-02-17 22:23:38 -0800

    added in updated function

--- app/modules/core/users/userServices/multipleSubscriptions.controller.js
+++ app/modules/core/users/userServices/multipleSubscriptions.controller.js
@@ -9,6 +9,12 @@
   function MultipleSubscriptionsCtrl(Orgservice) {
     var vm = this;
 
+    vm.multiSubscriptions = {
+      oneBilling: false,
+      selected: '',
+      options: []
+    };
+
     init();
 
     function init() {
@@ -20,6 +26,26 @@
       }).catch(function (response) {
         Notification.errorResponse(response, 'onboardModal.subscriptionIdError');
       });
+
+      function showMultiSubscriptions(billingServiceId, isTrial) {
+        var isSelected = false;
+
+        var isTrialSubscription = (_.isUndefined(billingServiceId) || _.isEmpty(billingServiceId)) && isTrial &&
+          (_.eq('Trial', vm.multiSubscriptions.selected));
+        if (_.isArray(billingServiceId)) {
+          for (var i in billingServiceId) {
+            if (_.eq(billingServiceId[i], vm.multiSubscriptions.selected)) {
+              isSelected = true;
+              break;
+            }
+          }
+        } else {
+          isSelected = (_.eq(billingServiceId, vm.multiSubscriptions.selected));
+        }
+
+        return vm.multiSubscriptions.oneBilling || isSelected || isTrialSubscription;
+      }
+      
     }
   }
 
