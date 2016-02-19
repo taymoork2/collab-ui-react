73c3a38ba616f8efe335ed7bc5f52710b3850071
tri nguyen <tringu3@cisco.com>
2016-02-18 21:38:58 -0800

    fix integration jenkins build errors

--- test/e2e-protractor/squared/dr_login_forward_spec.js
+++ test/e2e-protractor/squared/dr_login_forward_spec.js
@@ -1,16 +1,18 @@
+'use strict';
+
 describe('Digital River Forward with Login to CI', function () {
   it('should redirect to digitalriver', function () {
 
     browser.get('#/dr-login-forward');
 
     utils.sendKeys(login.emailField, 'sqtest-admin@squared.example.com');
-    login.loginButton.click();
+    login.clickLoginButton();
 
-    browser.driver.wait(login.isLoginPasswordPresent, TIMEOUT);
+    browser.driver.wait(login.isLoginPasswordPresent);
     login.setLoginPassword('P@ssword123');
     login.clickLoginSubmit();
 
-    navigation.expectDriverCurrentUrl("www.digitalriver.com");
+    navigation.expectDriverCurrentUrl('www.digitalriver.com');
 
   });
 });
