b438c61f35efff5f613dba014691927983bd9c14
mapennin <mapennin@cisco.com>
2016-02-22 15:32:35 -0500

    Don't display conversion button if no trials

--- app/modules/core/trials/trialNoticeBanner.controller.js
+++ app/modules/core/trials/trialNoticeBanner.controller.js
@@ -46,7 +46,7 @@
     }
 
     function canShow() {
-      return ft.atlasTrialConversion && Authinfo.isUserAdmin();
+      return ft.atlasTrialConversion && Authinfo.isUserAdmin() && TrialService.getTrialIds().length;
     }
 
     function sendRequest() {
