3700bbf5eb4d92b9aad665efaccc0f238b66a6db
sani <sani@cisco.com>
2016-02-24 15:31:52 -0600

    US219951 UI - Create & manage Schedule thru modal

--- app/modules/huron/features/autoAttendant/services/aaICalService.spec.js
+++ app/modules/huron/features/autoAttendant/services/aaICalService.spec.js
@@ -3,7 +3,7 @@
 describe('Service: AACalendarService', function () {
   var AAICalService, ical;
   // require('jasmine-collection-matchers');
-
+  var starttime, endtime;
   var defaultRange = {
     days: [{
       label: 'Monday',
@@ -35,6 +35,10 @@ describe('Service: AACalendarService', function () {
   beforeEach(inject(function (_AAICalService_, _ical_) {
     AAICalService = _AAICalService_;
     ical = _ical_;
+    var date = new Date();
+    starttime = new Date(date.getFullYear(), date.getMonth(), date.getDate(), '8', 0, 0);
+    endtime = new Date(date.getFullYear(), date.getMonth(), date.getDate(), '17', 0, 0);
+
   }));
 
   describe('createCalendar', function () {
@@ -62,8 +66,8 @@ describe('Service: AACalendarService', function () {
       range.days[2].active = true;
       range.days[3].active = true;
       range.days[4].active = true;
-      range.starttime = new Date(2015, 7, 14, 8, 30, 0);
-      range.endtime = new Date(2015, 7, 14, 17, 0, 0);
+      range.starttime = starttime;
+      range.endtime = endtime;
       AAICalService.addHoursRange(calendar, range);
       var calendarRaw = {};
       calendarRaw.scheduleData = calendar.toString();
@@ -75,8 +79,8 @@ describe('Service: AACalendarService', function () {
     it('add hours range without days to the calendar and should add nothing to the calendar', function () {
       var calendar = AAICalService.createCalendar();
       var range = AAICalService.getDefaultRange();
-      range.starttime = new Date(2015, 7, 14, 8, 30, 0);
-      range.endtime = new Date(2015, 7, 14, 17, 0, 0);
+      range.starttime = starttime;
+      range.endtime = endtime;
       AAICalService.addHoursRange(calendar, range);
       expect(calendar).toEqual(AAICalService.createCalendar());
     });
@@ -89,7 +93,8 @@ describe('Service: AACalendarService', function () {
       range.days[2].active = true;
       range.days[3].active = true;
       range.days[4].active = true;
-      range.endtime = new Date(2015, 7, 14, 17, 0, 0);
+
+      range.endtime = endtime;
       AAICalService.addHoursRange(calendar, range);
       expect(calendar).toEqual(AAICalService.createCalendar());
     });
@@ -102,7 +107,8 @@ describe('Service: AACalendarService', function () {
       range.days[2].active = true;
       range.days[3].active = true;
       range.days[4].active = true;
-      range.starttime = new Date(2015, 7, 14, 8, 30, 0);
+      range.starttime = starttime;
+
       AAICalService.addHoursRange(calendar, range);
       expect(calendar).toEqual(AAICalService.createCalendar());
     });
@@ -115,14 +121,14 @@ describe('Service: AACalendarService', function () {
       range1.days[2].active = true;
       range1.days[3].active = true;
       range1.days[4].active = true;
-      range1.starttime = new Date(2015, 7, 14, 8, 30, 0);
-      range1.endtime = new Date(2015, 7, 14, 17, 0, 0);
+      range1.starttime = starttime;
+      range1.endtime = endtime;
       AAICalService.addHoursRange(calendar, range1);
       var range2 = AAICalService.getDefaultRange();
       range2.days[5].active = true;
       range2.days[6].active = true;
-      range2.starttime = new Date(2015, 7, 14, 10, 0, 0);
-      range2.endtime = new Date(2015, 7, 14, 19, 20, 0);
+      range2.starttime = starttime;
+      range2.endtime = endtime;
       AAICalService.addHoursRange(calendar, range2);
       var calendarRaw = {};
       calendarRaw.scheduleData = calendar.toString();
