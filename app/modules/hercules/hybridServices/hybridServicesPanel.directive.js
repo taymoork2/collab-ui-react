7cd389d536a0f52ce8adf2a00b85366d71286965
Brad DeGrazia <degrazia@cisco.com>
2016-03-01 12:11:19 -0800

    Fix sort order on Safari

--- app/modules/hercules/hybridServices/hybridServicesPanel.directive.js
+++ app/modules/hercules/hybridServices/hybridServicesPanel.directive.js
@@ -35,8 +35,9 @@
           vm.extensions = extensions || [];
           // Sort items so they show in proper order (UC first, Calendar Last, everything else in the middle)
           vm.extensions.sort(function (a, b) {
-            if (a.id == 'squared-fusion-uc') return -1;
-            if (a.id == 'squared-fusion-cal') return 1;
+            if (a.id === 'squared-fusion-uc') return -1;
+            if (a.id === 'squared-fusion-cal') return 1;
+            if (a.id === 'squared-fusion-ec' && b.id =='squared-fusion-cal' ) return -1;
             return 0;
           });
         });
