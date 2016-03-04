4ec5f42f71f7da0d33ed59577ea1d0e93fb8d45a
Stig Murberg <stig.murberg@gmail.com>
2016-03-04 16:57:25 +0100

    Extracted oauth related config

--- app/modules/core/config/config.js
+++ app/modules/core/config/config.js
@@ -8,35 +8,11 @@
   function Config($location, Utils, $filter, Storage, serviceUrlMapping) {
     var TEST_ENV_CONFIG = 'TEST_ENV_CONFIG';
 
-    var oauth2Scope = encodeURIComponent('webexsquare:admin ciscouc:admin Identity:SCIM Identity:Config Identity:Organization cloudMeetings:login webex-messenger:get_webextoken ccc_config:admin');
-
     var getCurrentHostname = function () {
       return $location.host() || '';
     };
 
     var config = {
-      oauthClientRegistration: {
-        atlas: {
-          id: 'C80fb9c7096bd8474627317ee1d7a817eff372ca9c9cee3ce43c3ea3e8d1511ec',
-          secret: 'c10c371b4641010a750073b3c8e65a7fff0567400d316055828d3c74925b0857',
-          scope: oauth2Scope
-        },
-        cfe: {
-          id: 'C5469b72a6de8f8f0c5a23e50b073063ea872969fc74bb461d0ea0438feab9c03',
-          secret: 'b485aae87723fc2c355547dce67bbe2635ff8052232ad812a689f2f9b9efa048',
-          scope: oauth2Scope
-        }
-      },
-
-      oauthUrl: {
-        ciRedirectUrl: 'redirect_uri=%s',
-        oauth2UrlAtlas: 'https://idbroker.webex.com/idb/oauth2/v1/',
-        oauth2UrlCfe: 'https://idbrokerbts.webex.com/idb/oauth2/v1/',
-        oauth2LoginUrlPattern: '%sauthorize?response_type=code&client_id=%s&scope=%s&redirect_uri=%s&state=random-string&service=%s&email=%s',
-        oauth2ClientUrlPattern: 'grant_type=client_credentials&scope=',
-        oauth2CodeUrlPattern: 'grant_type=authorization_code&code=%s&scope=',
-        oauth2AccessCodeUrlPattern: 'grant_type=refresh_token&refresh_token=%s&scope=%s'
-      },
 
       feedbackNavConfig: {
         mailto: 'sq-admin-support@cisco.com',
@@ -50,8 +26,6 @@
       consumerOrgId: 'consumer',
 
       logoutUrl: 'https://idbroker.webex.com/idb/saml2/jsp/doSSO.jsp?type=logout&service=webex-squared&goto=',
-
-      oauthDeleteTokenUrl: 'https://idbroker.webex.com/idb/oauth2/v1/revoke',
 
       ssoSetupUrl: 'https://idbroker.webex.com/idb/idbconfig/',
 
@@ -638,87 +612,9 @@
         return faultServiceUrl[this.getEnv()];
       },
 
-      getClientSecret: function () {
-        var clientSecret = {
-          'dev': this.oauthClientRegistration.atlas.secret,
-          'cfe': this.oauthClientRegistration.cfe.secret,
-          'integration': this.oauthClientRegistration.atlas.secret,
-          'prod': this.oauthClientRegistration.atlas.secret
-        };
-
-        return clientSecret[this.getEnv()];
-      },
-
-      getClientId: function () {
-        var clientId = {
-          'dev': this.oauthClientRegistration.atlas.id,
-          'cfe': this.oauthClientRegistration.cfe.id,
-          'integration': this.oauthClientRegistration.atlas.id,
-          'prod': this.oauthClientRegistration.atlas.id
-        };
-
-        return clientId[this.getEnv()];
-      },
-
-      getOauth2Url: function () {
-        var oAuth2Url = {
-          'dev': this.oauthUrl.oauth2UrlAtlas,
-          'cfe': this.oauthUrl.oauth2UrlCfe,
-          'integration': this.oauthUrl.oauth2UrlAtlas,
-          'prod': this.oauthUrl.oauth2UrlAtlas
-        };
-
-        return oAuth2Url[this.getEnv()];
-      },
-
-      /**
-       * Method to get Oauth Login Url with email specified
-       * @param {string} email
-       */
-      getOauthLoginUrl: function (email) {
-        var acu = serviceUrlMapping.adminClientUrl[this.getEnv()];
-        var params = [
-          this.getOauth2Url(),
-          this.getClientId(),
-          this.oauthClientRegistration.atlas.scope,
-          encodeURIComponent(acu),
-          this.getOauthServiceType(),
-          encodeURIComponent(email)
-        ];
-
-        return Utils.sprintf(this.oauthUrl.oauth2LoginUrlPattern, params);
-      },
-
-      getRedirectUrl: function () {
-        var acu = serviceUrlMapping.adminClientUrl[this.getEnv()];
-        var params = [encodeURIComponent(acu)];
-        return Utils.sprintf(this.oauthUrl.ciRedirectUrl, params);
-      },
-
-      getOauthCodeUrl: function (code) {
-        var params = [code];
-        return Utils.sprintf(this.oauthUrl.oauth2CodeUrlPattern, params);
-      },
-
-      getOauthAccessCodeUrl: function (refresh_token) {
-        var params = [
-          refresh_token,
-          this.oauthClientRegistration.atlas.scope
-        ];
-        return Utils.sprintf(this.oauthUrl.oauth2AccessCodeUrlPattern, params);
-      },
-
-      getOauthServiceType: function () {
-        return 'spark';
-      },
-
       getLogoutUrl: function () {
-        var acu = serviceUrlMapping.adminClientUrl[this.getEnv()];
+        var acu = this.getAdminPortalUrl();
         return this.logoutUrl + encodeURIComponent(acu);
-      },
-
-      getOauthDeleteTokenUrl: function () {
-        return this.oauthDeleteTokenUrl;
       },
 
       getAdminPortalUrl: function () {
@@ -865,10 +761,6 @@
       getWebexAdvancedEditUrl: function (siteURL) {
         var params = [siteURL];
         return Utils.sprintf(this.webexUrl.siteAdminDeepUrl, params);
-      },
-
-      getOAuthClientRegistrationCredentials: function () {
-        return Utils.Base64.encode(this.getClientId() + ':' + this.getClientSecret());
       },
 
       getSunlightConfigServiceUrl: function () {
---
+++ app/modules/core/config/config.url.spec.js
@@ -0,0 +1,321 @@
+'use strict';
+
+describe('Config - URL', function () {
+
+  beforeEach(module('wx2AdminWebClientApp'));
+
+  var Config, $location;
+  beforeEach(inject(function (_$location_, _Config_) {
+    Config = _Config_;
+    $location = _$location_;
+    spyOn($location, 'host');
+  }));
+
+  var devHost = 'localhost';
+  var cfeHost = 'cfe-admin.ciscospark.com';
+  var intHost = 'int-admin.ciscospark.com';
+  var prodHost = 'admin.ciscospark.com';
+
+  var scope = encodeURIComponent('webexsquare:admin ciscouc:admin Identity:SCIM Identity:Config Identity:Organization cloudMeetings:login webex-messenger:get_webextoken ccc_config:admin');
+  var orgId = 'abc123efg456';
+  var siteURL = 'webex.com';
+
+  var whenCalling = function (fn, arg) {
+    var hosts = {
+      'dev': devHost,
+      'cfe': cfeHost,
+      'integration': intHost,
+      'prod': prodHost
+    };
+    return {
+      expectUrlToBe: function (obj) {
+        _.each(obj, function (expected, env) {
+          var host = hosts[env];
+          if (!host) {
+            throw new Error("Unknown environment " + env);
+          }
+          if (!Config[fn]) {
+            throw new Error("Unknown method " + fn);
+          }
+          $location.host.and.returnValue(host);
+          var actual = Config[fn](arg);
+
+          if (expected != actual) {
+            throw new Error("Expected " + fn + " in " + env + " to be '" + expected + "' but it was '" + actual + "'");
+          }
+        });
+      }
+    };
+  };
+
+  it('should return correct identity user service url', function () {
+    whenCalling('getScimUrl', orgId).expectUrlToBe({
+      dev: 'https://identity.webex.com/identity/scim/abc123efg456/v1/Users',
+      cfe: 'https://identitybts.webex.com/identity/scim/abc123efg456/v1/Users',
+      integration: 'https://identity.webex.com/identity/scim/abc123efg456/v1/Users',
+      prod: 'https://identity.webex.com/identity/scim/abc123efg456/v1/Users'
+    });
+  });
+
+  it('should return correct user reports url', function () {
+    whenCalling('getUserReportsUrl', orgId).expectUrlToBe({
+      dev: 'https://identity.webex.com/identity/config/abc123efg456/v1/UserReports',
+      cfe: 'https://identitybts.webex.com/identity/config/abc123efg456/v1/UserReports',
+      integration: 'https://identity.webex.com/identity/config/abc123efg456/v1/UserReports',
+      prod: 'https://identity.webex.com/identity/config/abc123efg456/v1/UserReports'
+    });
+  });
+
+  it('should return correct identity org service url', function () {
+    whenCalling('getScomUrl', orgId).expectUrlToBe({
+      dev: 'https://identity.webex.com/organization/scim/v1/Orgs',
+      cfe: 'https://identitybts.webex.com/organization/scim/v1/Orgs',
+      integration: 'https://identity.webex.com/organization/scim/v1/Orgs',
+      prod: 'https://identity.webex.com/organization/scim/v1/Orgs'
+    });
+  });
+
+  it('should return correct admin service url', function () {
+    whenCalling('getAdminServiceUrl').expectUrlToBe({
+      dev: 'https://atlas-integration.wbx2.com/admin/api/v1/',
+      cfe: 'https://atlas-e.wbx2.com/admin/api/v1/',
+      integration: 'https://atlas-integration.wbx2.com/admin/api/v1/',
+      prod: 'https://atlas-a.wbx2.com/admin/api/v1/',
+    });
+  });
+
+  it('should return correct csdm service url', function () {
+    whenCalling('getCsdmServiceUrl').expectUrlToBe({
+      dev: 'https://csdm-integration.wbx2.com/csdm/api/v1',
+      cfe: 'https://csdm-e.wbx2.com/csdm/api/v1',
+      integration: 'https://csdm-integration.wbx2.com/csdm/api/v1',
+      prod: 'https://csdm-a.wbx2.com/csdm/api/v1'
+    });
+  });
+
+  it('should always return locus integration service url', function () {
+    whenCalling('getLocusServiceUrl').expectUrlToBe({
+      dev: 'https://admin-portal-test-public.wbx2.com/locus',
+      cfe: 'https://admin-portal-test-public.wbx2.com/locus',
+      integration: 'https://admin-portal-test-public.wbx2.com/locus',
+      prod: 'https://admin-portal-test-public.wbx2.com/locus'
+    });
+  });
+
+  it('should return correct feature toggle service url', function () {
+    whenCalling('getFeatureToggleUrl').expectUrlToBe({
+      dev: 'https://locus-a.wbx2.com',
+      cfe: 'https://locus-e.wbx2.com',
+      integration: 'https://locus-a.wbx2.com',
+      prod: 'https://locus-a.wbx2.com'
+    });
+  });
+
+  it('should return correct meeting service url', function () {
+    whenCalling('getMeetingServiceUrl').expectUrlToBe({
+      dev: 'http://mf-meeting-service.mb-lab.huron.uno/admin/api/v1',
+      cfe: 'https://mf-meeting-service.mb-lab.huron.uno/admin/api/v1',
+      integration: 'https://mf-meeting-service.mb-lab.huron.uno/admin/api/v1',
+      prod: 'https://mf-meeting-service.mb-lab.huron.uno/admin/api/v1'
+    });
+  });
+
+  it('should return correct meeting service url', function () {
+    whenCalling('getMeetingInfoServiceUrl').expectUrlToBe({
+      dev: 'http://mf-meeting-service.mb-lab.huron.uno/admin/api/v1',
+      cfe: 'https://mf-meeting-service.mb-lab.huron.uno/admin/api/v1',
+      integration: 'https://mf-meeting-service.mb-lab.huron.uno/admin/api/v1',
+      prod: 'https://mf-meeting-service.mb-lab.huron.uno/admin/api/v1'
+    });
+  });
+
+  it('should return correct logout url', function () {
+    whenCalling('getLogoutUrl').expectUrlToBe({
+      dev: 'https://idbroker.webex.com/idb/saml2/jsp/doSSO.jsp?type=logout&service=webex-squared&goto=http%3A%2F%2F127.0.0.1%3A8000',
+      cfe: 'https://idbroker.webex.com/idb/saml2/jsp/doSSO.jsp?type=logout&service=webex-squared&goto=https%3A%2F%2Fcfe-admin.ciscospark.com',
+      integration: 'https://idbroker.webex.com/idb/saml2/jsp/doSSO.jsp?type=logout&service=webex-squared&goto=https%3A%2F%2Fint-admin.ciscospark.com%2F',
+      prod: 'https://idbroker.webex.com/idb/saml2/jsp/doSSO.jsp?type=logout&service=webex-squared&goto=https%3A%2F%2Fadmin.ciscospark.com%2F'
+    });
+  });
+
+  it('should return correct sso setup url', function () {
+    whenCalling('getSSOSetupUrl').expectUrlToBe({
+      dev: 'https://idbroker.webex.com/idb/idbconfig/',
+      cfe: 'https://idbroker.webex.com/idb/idbconfig/',
+      integration: 'https://idbroker.webex.com/idb/idbconfig/',
+      prod: 'https://idbroker.webex.com/idb/idbconfig/'
+    });
+  });
+
+  it('should return correct sso test url', function () {
+    whenCalling('getSSOTestUrl').expectUrlToBe({
+      dev: 'https://idbroker.webex.com/idb/saml2/jsp/spSSOInit.jsp',
+      cfe: 'https://idbroker.webex.com/idb/saml2/jsp/spSSOInit.jsp',
+      integration: 'https://idbroker.webex.com/idb/saml2/jsp/spSSOInit.jsp',
+      prod: 'https://idbroker.webex.com/idb/saml2/jsp/spSSOInit.jsp'
+    });
+  });
+
+  it('should return correct health check service url', function () {
+    whenCalling('getHealthCheckUrlServiceUrl').expectUrlToBe({
+      dev: 'https://ciscospark.statuspage.io/index.json',
+      cfe: 'https://ciscospark.statuspage.io/index.json',
+      integration: 'https://ciscospark.statuspage.io/index.json',
+      prod: 'https://ciscospark.statuspage.io/index.json'
+    });
+  });
+
+  it('should return correct log metrics url', function () {
+    whenCalling('getLogMetricsUrl').expectUrlToBe({
+      dev: 'https://metrics-a.wbx2.com/metrics/api/v1/metrics',
+      cfe: 'https://metrics-a.wbx2.com/metrics/api/v1/metrics',
+      integration: 'https://metrics-a.wbx2.com/metrics/api/v1/metrics',
+      prod: 'https://metrics-a.wbx2.com/metrics/api/v1/metrics'
+    });
+  });
+
+  it('should return correct call flow service url', function () {
+    whenCalling('getCallflowServiceUrl').expectUrlToBe({
+      dev: 'https://admin-portal-test-public.wbx2.com/atlas-server/admin/api/v1/',
+      cfe: 'https://admin-portal-test-public.wbx2.com/atlas-server/admin/api/v1/',
+      integration: 'https://admin-portal-test-public.wbx2.com/atlas-server/admin/api/v1/',
+      prod: 'https://admin-portal-test-public.wbx2.com/atlas-server/admin/api/v1/'
+    });
+  });
+
+  it('should return correct status page url', function () {
+    whenCalling('getStatusPageUrl').expectUrlToBe({
+      dev: 'http://status.ciscospark.com/',
+      cfe: 'http://status.ciscospark.com/',
+      integration: 'http://status.ciscospark.com/',
+      prod: 'http://status.ciscospark.com/'
+    });
+  });
+
+  it('should return correct squared app url', function () {
+    whenCalling('getSquaredAppUrl').expectUrlToBe({
+      dev: 'squared://',
+      cfe: 'squared://',
+      integration: 'squared://',
+      prod: 'squared://'
+    });
+  });
+
+  it('should return correct itunes store url', function () {
+    whenCalling('getItunesStoreUrl').expectUrlToBe({
+      dev: 'http://cs.co/sqios',
+      cfe: 'http://cs.co/sqios',
+      integration: 'http://cs.co/sqios',
+      prod: 'http://cs.co/sqios'
+    });
+  });
+
+  it('should return correct android store url', function () {
+    whenCalling('getAndroidStoreUrl').expectUrlToBe({
+      dev: 'http://cs.co/sqandroid',
+      cfe: 'http://cs.co/sqandroid',
+      integration: 'http://cs.co/sqandroid',
+      prod: 'http://cs.co/sqandroid'
+    });
+  });
+
+  it('should return correct android app intent url', function () {
+    whenCalling('getAndroidAppIntent').expectUrlToBe({
+      dev: 'intent://view?id=123#Intent;package=com.cisco.wx2.android;scheme=squared;end;',
+      cfe: 'intent://view?id=123#Intent;package=com.cisco.wx2.android;scheme=squared;end;',
+      integration: 'intent://view?id=123#Intent;package=com.cisco.wx2.android;scheme=squared;end;',
+      prod: 'intent://view?id=123#Intent;package=com.cisco.wx2.android;scheme=squared;end;'
+    });
+  });
+
+  it('should return correct web client url', function () {
+    whenCalling('getWebClientUrl').expectUrlToBe({
+      dev: 'https://web.ciscospark.com/',
+      cfe: 'https://web.ciscospark.com/',
+      integration: 'https://web.ciscospark.com/',
+      prod: 'https://web.ciscospark.com/'
+    });
+  });
+
+  it('should return correct hercules url', function () {
+    whenCalling('getHerculesUrl').expectUrlToBe({
+      dev: 'https://hercules-integration.wbx2.com/v1',
+      cfe: 'https://hercules-e.wbx2.com/v1',
+      integration: 'https://hercules-integration.wbx2.com/v1',
+      prod: 'https://hercules-a.wbx2.com/v1'
+    });
+  });
+
+  it('should return correct USS url', function () {
+    whenCalling('getUssUrl').expectUrlToBe({
+      dev: 'https://uss-integration.wbx2.com/',
+      cfe: 'https://uss-e.wbx2.com/',
+      integration: 'https://uss-integration.wbx2.com/',
+      prod: 'https://uss-a.wbx2.com/'
+    });
+  });
+
+  it('should return correct calliope url', function () {
+    whenCalling('getCalliopeUrl').expectUrlToBe({
+      dev: 'https://calliope-integration.wbx2.com/calliope/api/authorization/v1',
+      cfe: 'https://calliope-e.wbx2.com/calliope/api/authorization/v1',
+      integration: 'https://calliope-integration.wbx2.com/calliope/api/authorization/v1',
+      prod: 'https://calliope-a.wbx2.com/calliope/api/authorization/v1'
+    });
+  });
+
+  it('should return correct certs url', function () {
+    whenCalling('getCertsUrl').expectUrlToBe({
+      dev: 'https://certs-integration.wbx2.com/',
+      cfe: 'https://certs-e.wbx2.com/',
+      integration: 'https://certs-integration.wbx2.com/',
+      prod: 'https://certs-a.wbx2.com/'
+    });
+  });
+
+  it('should return correct wdm url', function () {
+    whenCalling('getWdmUrl').expectUrlToBe({
+      dev: 'https://wdm-a.wbx2.com/wdm/api/v1',
+      cfe: 'http://wdm.cfe.wbx2.com/wdm/api/v1',
+      integration: 'https://wdm-a.wbx2.com/wdm/api/v1',
+      prod: 'https://wdm-a.wbx2.com/wdm/api/v1'
+    });
+  });
+
+  it('should return correct utilization url', function () {
+    whenCalling('getUtilizationServiceUrl').expectUrlToBe({
+      dev: 'http://mf-meeting-service.mb-lab.huron.uno/admin/api/v1',
+      cfe: 'https://mf-meeting-service.mb-lab.huron.uno/admin/api/v1',
+      integration: 'https://mf-meeting-service.mb-lab.huron.uno/admin/api/v1',
+      prod: 'https://mf-meeting-service.mb-lab.huron.uno/admin/api/v1'
+    });
+  });
+
+  it('should return correct webex advanced home url', function () {
+    whenCalling('getWebexAdvancedHomeUrl', siteURL).expectUrlToBe({
+      dev: 'https://webex.com/dispatcher/AtlasIntegration.do?cmd=GoToSiteAdminHomePage',
+      cfe: 'https://webex.com/dispatcher/AtlasIntegration.do?cmd=GoToSiteAdminHomePage',
+      integration: 'https://webex.com/dispatcher/AtlasIntegration.do?cmd=GoToSiteAdminHomePage',
+      prod: 'https://webex.com/dispatcher/AtlasIntegration.do?cmd=GoToSiteAdminHomePage'
+    });
+  });
+
+  it('should return correct webex advanced edit url', function () {
+    whenCalling('getWebexAdvancedEditUrl', siteURL).expectUrlToBe({
+      dev: 'https://webex.com/dispatcher/AtlasIntegration.do?cmd=GoToSiteAdminEditUserPage',
+      cfe: 'https://webex.com/dispatcher/AtlasIntegration.do?cmd=GoToSiteAdminEditUserPage',
+      integration: 'https://webex.com/dispatcher/AtlasIntegration.do?cmd=GoToSiteAdminEditUserPage',
+      prod: 'https://webex.com/dispatcher/AtlasIntegration.do?cmd=GoToSiteAdminEditUserPage'
+    });
+  });
+
+  it('should return correct customer care url', function () {
+    whenCalling('getSunlightConfigServiceUrl').expectUrlToBe({
+      dev: 'https://config.rciad.ciscoccservice.com/config/v1',
+      cfe: 'https://config.rciad.ciscoccservice.com/config/v1',
+      integration: 'https://config.rciad.ciscoccservice.com/config/v1',
+      prod: 'https://config.rciad.ciscoccservice.com/config/v1'
+    });
+  });
+
+});
