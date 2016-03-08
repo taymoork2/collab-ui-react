7e7f0a3b137205e133e94ae386ae977b26070a3e
Charles Lin (charlin) <charlin@cisco.com>
2016-03-08 01:45:50 -0800

    e2e test to detect enabled csv col and site also detect t30 and t31 sites where csv import/export is not available

--- test/e2e-protractor/pages/webexsitelist.page.js
+++ test/e2e-protractor/pages/webexsitelist.page.js
@@ -1,13 +1,27 @@
 var SiteListPage = function () {
   this.t31CSVToggleUser = {
-    siteUrl: 't30citestprov6.webex.com',
-    testAdminUsername: 't31r1-csvtoggle-adm@mailinator.com',
+    // siteUrl: 't30citestprov6.webex.com',
+    // testAdminUsername: 't31r1-csvtoggle-adm@mailinator.com',
+    siteUrl: 'sjsite14.webex.com',
+    testAdminUsername: 'sjsite14-lhsieh@mailinator.com',
     testAdminPassword: 'Cisco!23'
   };
 
   this.siteListPageId = element(by.id('site-list'));
   this.conferencingLink = element(by.css('a[href="#site-list"]'));
   this.csvColumnId = element(by.id('id-siteCsvColumnHeader'));
+
+  this.csvExportId = element(by.id(this.t31CSVToggleUser.siteUrl + "_export"));
+  this.csvImportId = element(by.id(this.t31CSVToggleUser.siteUrl + "_import"));
+
+  this.xLaunchExportUsersT31CSV = element(by.id(this.t31CSVToggleUser.siteUrl + "_xlaunch-export-users"));
+  this.xLaunchImportUsersT31CSV = element(by.id(this.t31CSVToggleUser.siteUrl + "_xlaunch-import-users"));
+
+  //check T30citestprov9.webex.com 'Not Available'
+  this.t30csvNotAvail = element(by.id("t30citestprov9.webex.com" + "_NA"));
+
+  //check cisjsite031.webex.com 'Not Available'
+  this.t31csvNotAvail = element(by.id("cisjsite031.webex.com" + "_NA"));
 };
 
 module.exports = SiteListPage;
--- test/e2e-protractor/webex/siteList/webexsitelist_spec.js
+++ test/e2e-protractor/webex/siteList/webexsitelist_spec.js
@@ -19,6 +19,32 @@ describe('WebEx site list', function () {
     utils.wait(sitelist.csvColumnId);
   });
 
+  it('should detect the Import label', function () {
+    utils.wait(sitelist.csvImportId);
+  });
+
+  it('should detect the Export label', function () {
+    utils.wait(sitelist.csvExportId);
+  });
+
+  xit('should detect the Export link', function () {
+    utils.wait(sitelist.xLaunchExportUsersT31CSV);
+  });
+
+  xit('should detect the Import link', function () {
+    utils.wait(sitelist.xLaunchImportUsersT31CSV);
+  });
+
+  //check T30 site where CSV = Not Available
+  it('T30 site should display Not Available', function () {
+    utils.wait(sitelist.t30csvNotAvail);
+  });
+
+  //check T31 site where CSV = Not Available
+  it('T31 site but user not yet configured to be admin of this site should display Not Available', function () {
+    utils.wait(sitelist.t31csvNotAvail);
+  });
+
   xit('should log out', function () {
     navigation.logout();
   });
