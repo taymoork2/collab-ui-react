04ccd0475b33aadca1fce0ff7fd2a169629fc2b6
Admas Molla <admolla@cisco.com>
2016-03-09 10:22:34 -0800

    jsbeautify

--- app/modules/core/digitalriver/login/enterEmailAddr/enterEmailAddr.controller.spec.js
+++ app/modules/core/digitalriver/login/enterEmailAddr/enterEmailAddr.controller.spec.js
@@ -1,8 +1,8 @@
 (function () {
   'use strict';
 
-  fdescribe('Controller: enterEmailAddrController', function () {
-    var controller, DigitalRiverService, $window, $controller, $rootScope,$q;
+  describe('Controller: enterEmailAddrController', function () {
+    var controller, DigitalRiverService, $window, $controller, $rootScope, $q;
 
     beforeEach(module('Core'));
 
@@ -10,12 +10,16 @@
       $rootScope = _$rootScope_;
       DigitalRiverService = _DigitalRiverService_;
       $controller = _$controller_;
-      $window = {location:{href:''}};
+      $window = {
+        location: {
+          href: ''
+        }
+      };
       $q = _$q_;
       spyOn(DigitalRiverService, 'getUserFromEmail').and.returnValue($q.when());
     }));
 
-    function initController(){
+    function initController() {
       controller = $controller('enterEmailAddrController', {
         $window: $window
       });
@@ -45,42 +49,53 @@
       });
     });
 
-    describe('getUserFromEmail', function(){
-      beforeEach(function(){
+    describe('getUserFromEmail', function () {
+      beforeEach(function () {
         initController();
         controller.email = 'foo@bar.com';
       });
 
-      it('should error out on rejection', function(done){
+      it('should error out on rejection', function (done) {
         DigitalRiverService.getUserFromEmail.and.returnValue($q.reject());
-        controller.handleEnterEmailAddr().then(function(){
+        controller.handleEnterEmailAddr().then(function () {
           expect(controller.error).toBe('digitalRiver.validation.unexpectedError');
           done();
         });
         $rootScope.$apply();
       });
 
-      it('should error out on success with no data', function(done){
+      it('should error out on success with no data', function (done) {
         DigitalRiverService.getUserFromEmail.and.returnValue($q.when());
-        controller.handleEnterEmailAddr().then(function(){
+        controller.handleEnterEmailAddr().then(function () {
           expect(controller.error).toBe('digitalRiver.validation.unexpectedError');
           done();
         });
         $rootScope.$apply();
       });
 
-      it('should redirect to create-account on success with improper data', function(done){
-        DigitalRiverService.getUserFromEmail.and.returnValue($q.when({data:{success:true}}));
-        controller.handleEnterEmailAddr().then(function(){
+      it('should redirect to create-account on success with improper data', function (done) {
+        DigitalRiverService.getUserFromEmail.and.returnValue($q.when({
+          data: {
+            success: true
+          }
+        }));
+        controller.handleEnterEmailAddr().then(function () {
           expect($window.location.href).toContain('create-account');
           done();
         });
         $rootScope.$apply();
       });
 
-      it('should redirect to dr-login-forward on success with proper data', function(done){
-        DigitalRiverService.getUserFromEmail.and.returnValue($q.when({data:{data:{exists:true},success:true}}));
-        controller.handleEnterEmailAddr().then(function(){
+      it('should redirect to dr-login-forward on success with proper data', function (done) {
+        DigitalRiverService.getUserFromEmail.and.returnValue($q.when({
+          data: {
+            data: {
+              exists: true
+            },
+            success: true
+          }
+        }));
+        controller.handleEnterEmailAddr().then(function () {
           expect($window.location.href).toContain('/#/dr-login-forward');
           done();
         });
