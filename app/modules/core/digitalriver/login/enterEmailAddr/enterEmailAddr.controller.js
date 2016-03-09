a61c49479501ca1df40ce850ecc731ec321f7984
Admas Molla <admolla@cisco.com>
2016-03-09 10:10:27 -0800

    converting enterEmailAddr into unit tests

--- app/modules/core/digitalriver/login/enterEmailAddr/enterEmailAddr.controller.js
+++ app/modules/core/digitalriver/login/enterEmailAddr/enterEmailAddr.controller.js
@@ -13,27 +13,30 @@
     // TODO: Remove this after the go-live.
     vm.drReferrer = $location.search().referrer === DigitalRiverService.getDrReferrer();
 
-    vm.emailPlaceholder = function () {
-      return $translate.instant('digitalRiver.enterEmailAddr.emailPlaceholder');
-    };
+    vm.emailPlaceholder = emailPlaceholder;
+    vm.handleEnterEmailAddr = handleEnterEmailAddr;
 
-    vm.handleEnterEmailAddr = function () {
+    function emailPlaceholder() {
+      return $translate.instant('digitalRiver.enterEmailAddr.emailPlaceholder');
+    }
+
+    function handleEnterEmailAddr() {
       if (!vm.email || 0 === vm.email.trim().length) {
         vm.error = $translate.instant('digitalRiver.enterEmailAddr.validation.emptyEmail');
         return;
       }
 
-      DigitalRiverService.getUserFromEmail(vm.email)
+      return DigitalRiverService.getUserFromEmail(vm.email)
         .then(function (result) {
-          if (result.data.success === true) {
-            $window.location.href = (_.get(result, 'data.data.exists', false) === true ? "/#/dr-login-forward" : "/#/create-account") + "?email=" + vm.email + "&referrer=" + DigitalRiverService.getDrReferrer();
+          if (_.get(result, 'data.success') === true) {
+            $window.location.href = (_.get(result, 'data.data.exists', false) === true ? '/#/dr-login-forward' : '/#/create-account') + '?email=' + vm.email + '&referrer=' + DigitalRiverService.getDrReferrer();
           } else {
             vm.error = _.get(result, 'data.message', $translate.instant('digitalRiver.validation.unexpectedError'));
           }
         }, function (result, status) {
           vm.error = _.get(result, 'data.message', $translate.instant('digitalRiver.validation.unexpectedError'));
         });
-    };
+    }
 
   }
 })();
--- app/modules/core/digitalriver/login/enterEmailAddr/enterEmailAddr.controller.spec.js
+++ app/modules/core/digitalriver/login/enterEmailAddr/enterEmailAddr.controller.spec.js
@@ -1,30 +1,37 @@
 (function () {
   'use strict';
 
-  describe('Controller: enterEmailAddrController', function () {
-    var controller, DigitalRiverService;
+  fdescribe('Controller: enterEmailAddrController', function () {
+    var controller, DigitalRiverService, $window, $controller, $rootScope,$q;
 
     beforeEach(module('Core'));
 
-    beforeEach(inject(function (_$controller_, _$location_, _$window_, _$translate_, _DigitalRiverService_, $q) {
+    beforeEach(inject(function (_$rootScope_, _$controller_, _DigitalRiverService_, _$q_) {
+      $rootScope = _$rootScope_;
       DigitalRiverService = _DigitalRiverService_;
-      controller = _$controller_('enterEmailAddrController', {
-        $location: _$location_,
-        $window: _$window_,
-        $translate: _$translate_,
-        DigitalRiverService: DigitalRiverService
-      });
-      spyOn(DigitalRiverService, "getUserFromEmail").and.returnValue($q.when());
+      $controller = _$controller_;
+      $window = {location:{href:''}};
+      $q = _$q_;
+      spyOn(DigitalRiverService, 'getUserFromEmail').and.returnValue($q.when());
     }));
 
+    function initController(){
+      controller = $controller('enterEmailAddrController', {
+        $window: $window
+      });
+      $rootScope.$apply();
+    }
+
     describe('emailPlaceholder', function () {
+      beforeEach(initController);
+
       it('should return the correct value', function () {
         expect(controller.emailPlaceholder()).toEqual('digitalRiver.enterEmailAddr.emailPlaceholder');
       });
     });
 
     describe('handleEnterEmailAddr', function () {
-
+      beforeEach(initController);
       it('should validate an empty email', function () {
         controller.handleEnterEmailAddr();
         expect(controller.error).toEqual('digitalRiver.enterEmailAddr.validation.emptyEmail');
@@ -36,7 +43,49 @@
         expect(controller.error).not.toBeDefined();
         expect(DigitalRiverService.getUserFromEmail).toHaveBeenCalled();
       });
+    });
 
+    describe('getUserFromEmail', function(){
+      beforeEach(function(){
+        initController();
+        controller.email = 'foo@bar.com';
+      });
+
+      it('should error out on rejection', function(done){
+        DigitalRiverService.getUserFromEmail.and.returnValue($q.reject());
+        controller.handleEnterEmailAddr().then(function(){
+          expect(controller.error).toBe('digitalRiver.validation.unexpectedError');
+          done();
+        });
+        $rootScope.$apply();
+      });
+
+      it('should error out on success with no data', function(done){
+        DigitalRiverService.getUserFromEmail.and.returnValue($q.when());
+        controller.handleEnterEmailAddr().then(function(){
+          expect(controller.error).toBe('digitalRiver.validation.unexpectedError');
+          done();
+        });
+        $rootScope.$apply();
+      });
+
+      it('should redirect to create-account on success with improper data', function(done){
+        DigitalRiverService.getUserFromEmail.and.returnValue($q.when({data:{success:true}}));
+        controller.handleEnterEmailAddr().then(function(){
+          expect($window.location.href).toContain('create-account');
+          done();
+        });
+        $rootScope.$apply();
+      });
+
+      it('should redirect to dr-login-forward on success with proper data', function(done){
+        DigitalRiverService.getUserFromEmail.and.returnValue($q.when({data:{data:{exists:true},success:true}}));
+        controller.handleEnterEmailAddr().then(function(){
+          expect($window.location.href).toContain('/#/dr-login-forward');
+          done();
+        });
+        $rootScope.$apply();
+      });
     });
 
   });
