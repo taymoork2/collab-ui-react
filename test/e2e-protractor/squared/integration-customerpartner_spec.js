62fb79f6764eef120b01c80c944c1c693dd17f46
Stig Murberg <stig.murberg@gmail.com>
2016-03-09 13:15:45 +0100

    Disabling spec (as discussed with Raji) as the org is borked and needs CI intervention to be fixed

--- test/e2e-protractor/squared/integration-customerpartner_spec.js
+++ test/e2e-protractor/squared/integration-customerpartner_spec.js
@@ -49,7 +49,7 @@ xdescribe('CS Admin flow', function () {
 
 });
 
-describe('CS User flow', function () {
+xdescribe('CS User flow', function () {
 
   it('should just login', function () {
     login.login('customer-support-user', '#/partner/customers');
