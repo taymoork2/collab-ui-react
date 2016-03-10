141e51ac55bba0509c64f211d5e0ffb0107a1f3d
Sumitra Krishnan <sumitkri@cisco.com>
2016-03-10 12:56:39 -0800

    More e2e test for multi center license column

--- test/e2e-protractor/pages/webexsitelist.page.js
+++ test/e2e-protractor/pages/webexsitelist.page.js
@@ -5,7 +5,13 @@ var SiteListPage = function () {
     testAdminPassword: 'Cisco!23'
   };
 
-  this.multiCenterLicenseUser = {
+  this.multiCenterLicenseUser_single = {
+    siteUrl: 't30citestprov44.webex.com',
+    testAdminUsername: 'customerqaprov+160302qaprov44@gmail.com',
+    testAdminPassword: 'Cisco!23'
+  };
+
+  this.multiCenterLicenseUser_multiple = {
     siteUrl: 't30citestprov6.webex.com',
     testAdminUsername: 't31r1-csvtoggle-adm@mailinator.com',
     testAdminPassword: 'Cisco!23'
@@ -15,6 +21,7 @@ var SiteListPage = function () {
   this.conferencingLink = element(by.css('a[href="#site-list"]'));
   this.csvColumnId = element(by.id('id-siteCsvColumnHeader'));
   this.licenseTypesColumnId = element(by.id('site-license-types'));
+  this.singleLicenseSiteId = element(by.id('t30citestprov44.webex.com_licenseType'));
   this.multiLicenseSiteId = element(by.id('t30citestprov6.webex.com_licenseType'));
 };
 
