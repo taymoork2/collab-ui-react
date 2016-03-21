7fb84782917a827133cc8000d256ac32c4544d53
lhsieh <lhsieh@cisco.com>
2016-03-21 15:54:44 -0700

    WebEx CSV e2e protractor clean up.

--- test/e2e-protractor/webex/siteList/webexsitelist_spec.js
+++ test/e2e-protractor/webex/siteList/webexsitelist_spec.js
@@ -26,7 +26,7 @@ describe('Services > Webex page aka Site List page', function () {
       utils.wait(sitelist.csvColumnId);
     });
 
-    it('should detect CSV Export & Import status being given', function () {
+    it('should detect CSV Export & Import status is given', function () {
       utils.wait(sitelist.csvImportId);
       utils.wait(sitelist.csvExportId);
     });
