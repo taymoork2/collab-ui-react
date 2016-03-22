01e91292fa133aab6805eb74366b9bdd491facd9
zamamoha <zamamoha@cisco.com>
2016-03-22 11:25:44 -0500

    US220499 Modal Ctrl and Template

---
+++ app/modules/huron/features/autoAttendant/schedule/aaScheduleImportCtrl.js
@@ -0,0 +1,39 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('uc.autoattendant')
+    .controller('AAScheduleImportCtrl', AAScheduleImportCtrl);
+
+  /* @ngInject */
+  function AAScheduleImportCtrl($modalInstance, AutoAttendantCeInfoModelService, AutoAttendantCeService, AACalendarService, AAICalService) {
+
+    var vm = this;
+    vm.selectPlaceholder = 'Select Auto Attendant';
+    vm.options = [];
+    vm.continueImport = continueImport;
+    
+    AutoAttendantCeInfoModelService.getCeInfosList().then(function (data) {
+      data.aaRecords.forEach(function (value) {
+          var obj = {};
+          obj.label = value.callExperienceName;
+          obj.value = value.callExperienceURL;
+          vm.options.push(obj);
+      });
+    });
+    
+    function continueImport() {
+      AutoAttendantCeService.readCe(vm.selected.value).then(function (data) {
+        if (data.scheduleId) {
+          AACalendarService.readCalendar(data.scheduleId).then(function (calendar) {
+            var allHours = AAICalService.getHoursRanges(calendar);
+            $modalInstance.close(allHours);
+          });
+        } else {
+          $modalInstance.close(undefined);
+        }
+        
+      });
+    }
+  }
+})();
