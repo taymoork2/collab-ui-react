0230c15044996a08c41712aa8f4e4351b1db19f6
Brian Spence <brspence@cisco.com>
2016-03-23 15:55:21 -0500

    chore(bower): Format list-bower-components output

--- bin/helpers/list-bower-components.js
+++ bin/helpers/list-bower-components.js
@@ -3,10 +3,11 @@
 var glob = require('glob');
 var _ = require('lodash');
 var fs = require('fs');
+var sprintf = require('sprintf-js').sprintf;
 
-glob('bower_components/**/bower.json', {}, function (er, files) {
+glob('bower_components/*/bower.json', {}, function (er, files) {
   _.forEach(files, function (file) {
     var bower = JSON.parse(fs.readFileSync(file));
-    console.log(bower.name, bower.version);
+    console.log(sprintf('%-50s %-15s %-80s', bower.name, bower.version, file));
   });
 });
