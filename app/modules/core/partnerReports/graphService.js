514b0a8b81682eb689c2a5c41f9cb0a4359c9ef4
khempel <khempel@cisco.com>
2016-03-25 15:45:58 -0500

    ATLAS-455 - Changing how show/hiding of reports occurs to prevent disappearing reports

--- app/modules/core/partnerReports/graphService.js
+++ app/modules/core/partnerReports/graphService.js
@@ -282,11 +282,9 @@
     function modifyPopulation(data, overallPopulation) {
       if (angular.isArray(data)) {
         angular.forEach(data, function (item) {
-          var comparison = item.percentage - overallPopulation;
-          item.absCompare = Math.abs(comparison);
           item.overallPopulation = overallPopulation;
           if (item.colorOne === null || item.colorOne === undefined) {
-            if (comparison >= 0) {
+            if ((item.percentage - overallPopulation) >= 0) {
               item.colorOne = chartColors.brandInfo;
             } else {
               item.colorOne = chartColors.brandDanger;
