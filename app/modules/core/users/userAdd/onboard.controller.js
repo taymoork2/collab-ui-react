5d8f84358ebe6b7cca357197130fc421e95a7ab2
Shawn Pu <xipu@cisco.com>
2016-04-01 15:26:51 -0700

    unlock MessengerSync on altas, user update/csv bulk update and the associated strings, test, DI

--- app/modules/core/users/userAdd/onboard.controller.js
+++ app/modules/core/users/userAdd/onboard.controller.js
@@ -6,15 +6,13 @@
     .controller('OnboardCtrl', OnboardCtrl);
 
   /*@ngInject*/
-  function OnboardCtrl($scope, $state, $stateParams, $q, $http, $window, Log, Authinfo, $rootScope, $translate, LogMetricsService, Config, GroupService, Notification, OnboardService, Userservice, $timeout, Utils, Orgservice, TelephonyInfoService, FeatureToggleService, NAME_DELIMITER, SyncService, TelephoneNumberService, DialPlanService, CsvDownloadService, TrackingId, chartColors) {
+  function OnboardCtrl($scope, $state, $stateParams, $q, $http, $window, Log, Authinfo, $rootScope, $translate, LogMetricsService, Config, GroupService, Notification, OnboardService, Userservice, $timeout, Utils, Orgservice, TelephonyInfoService, FeatureToggleService, NAME_DELIMITER, TelephoneNumberService, DialPlanService, CsvDownloadService, TrackingId, chartColors) {
     $scope.hasAccount = Authinfo.hasAccount();
     $scope.usrlist = [];
     $scope.internalNumberPool = [];
     $scope.externalNumberPool = [];
-    $scope.isMsgrSyncEnabled = false;
     $scope.telephonyInfo = {};
 
-    $scope.getMessengerSyncStatus = getMessengerSyncStatus;
     $scope.loadInternalNumberPool = loadInternalNumberPool;
     $scope.loadExternalNumberPool = loadExternalNumberPool;
     $scope.checkDnOverlapsSteeringDigit = checkDnOverlapsSteeringDigit;
@@ -292,15 +290,6 @@
       $scope.model.userInfoValid = false;
     }
 
-    function getMessengerSyncStatus() {
-      SyncService.isMessengerSyncEnabled()
-        .then(function (isIt) {
-          $scope.isMsgrSyncEnabled = isIt;
-        }, function (errorMsg) {
-          Log.error(errorMsg);
-        });
-    }
-
     function ServiceFeature(label, value, name, license) {
       this.label = label;
       this.value = value;
@@ -350,10 +339,6 @@
     if ($scope.currentUser) {
       userEnts = $scope.currentUser.entitlements;
       userLicenseIds = $scope.currentUser.licenseID;
-    }
-
-    if (null !== Authinfo.getOrgId()) {
-      getMessengerSyncStatus();
     }
 
     function populateConf() {
