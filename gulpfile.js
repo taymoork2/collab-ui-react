ad9497fca558ac5f36da01d9c500e827d92bdb2c
Thomas Bassetto <thobasse@cisco.com>
2016-04-04 08:18:39 +0200

    Add checks for NodeJS and npm versions

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,23 @@
 'use strict';
 
 var gulp = require('gulp');
+var gutil = require('gulp-util');
+var semver = require('semver');
 var taskListing = require('gulp-task-listing');
 var requireDir = require('require-dir');
+
+// Check requirements
+if (!semver.satisfies(process.version, '>=4')) {
+  gutil.log(gutil.colors.red('NodeJS version should be at least version 4, current is:'), gutil.colors.red.bold(process.version));
+  process.exit(1);
+}
+var spawn = require('child_process').spawnSync;
+var child = spawn('npm', ['--version']);
+var npmVersion = child.stdout.toString();
+if (child.status !== 0 || !semver.satisfies(npmVersion, '=2')) {
+  gutil.log(gutil.colors.red('npm version should strictly be version 2, current is:'), gutil.colors.red.bold(npmVersion));
+  process.exit(1);
+}
 
 // Tasks are imported from the 'gulp' directory
 requireDir('./gulp', {
