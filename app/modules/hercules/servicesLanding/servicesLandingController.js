ce84e93a3b6ec250d0cfa3e0a3328b17f141979d
Magnus Holst <maholst@cisco.com>
2016-04-11 15:38:37 +0200

    Services Landing:  Code formatting

--- app/modules/hercules/servicesLanding/servicesLandingController.js
+++ app/modules/hercules/servicesLanding/servicesLandingController.js
@@ -21,68 +21,68 @@
     $scope.showLayout = "all";
     $scope.showDropdown = false;
     $scope.activeCards = {
-      "Calendar":true,
-      "Call":false,
-      "Context":false,
-      "Media":true
-    }
+      "Calendar": true,
+      "Call": false,
+      "Context": false,
+      "Media": true
+    };
     $scope.cards = [
       {
-        "link":"services/expressway-management",
-        "title":"Management",
-        "icon":"icon-circle-calendar",
-        "descr":"Management Service is the global configuration service for Calendar and Call Services.",
-        "active":true
+        "link": "services/expressway-management",
+        "title": "Management",
+        "icon": "icon-circle-calendar",
+        "descr": "Management Service is the global configuration service for Calendar and Call Services.",
+        "active": true
       },
       {
-        "link":"services/calendar",
-        "title":"Calendar",
-        "icon":"icon-circle-calendar",
-        "descr":"(Integrate Microsoft Exchange) Collaborate instantly. Just add @webex to attach a WebEx to your invitation. Add @spark to create a Spark room.",
-        "active":true
+        "link": "services/calendar",
+        "title": "Calendar",
+        "icon": "icon-circle-calendar",
+        "descr": "(Integrate Microsoft Exchange) Collaborate instantly. Just add @webex to attach a WebEx to your invitation. Add @spark to create a Spark room.",
+        "active": true
       },
       {
-        "link":"services/call",
-        "title":"Call",
-        "icon":"icon-circle-call",
-        "descr":"Enables Zero-Touch Meetings, desktop sharing from Cisco Spark clients while on a work phone (Aware).  Place/answer work calls from Cisco Spark clients (Connect).",
-        "active":false
+        "link": "services/call",
+        "title": "Call",
+        "icon": "icon-circle-call",
+        "descr": "Enables Zero-Touch Meetings, desktop sharing from Cisco Spark clients while on a work phone (Aware).  Place/answer work calls from Cisco Spark clients (Connect).",
+        "active": false
       },
       {
-        "link":"services/expressway-management",
-        "title":"Media",
-        "icon":"icon-circle-call",
-        "descr":"Lorem ipsum dolor sit amet, consectetur a the adipiscing elit. Donec placera ultricvenenatis. Aliquam mauris. Lorem ipsum dolor.",
-        "active":false
+        "link": "services/expressway-management",
+        "title": "Media",
+        "icon": "icon-circle-call",
+        "descr": "Lorem ipsum dolor sit amet, consectetur a the adipiscing elit. Donec placera ultricvenenatis. Aliquam mauris. Lorem ipsum dolor.",
+        "active": false
       },
       {
-        "link":"services/expressway-management",
-        "title":"Context",
-        "icon":"icon-circle-data",
-        "descr":"Set up the Media Service to host meetings in the Cloud and On-premise.",
-        "active":true
+        "link": "services/expressway-management",
+        "title": "Context",
+        "icon": "icon-circle-data",
+        "descr": "Set up the Media Service to host meetings in the Cloud and On-premise.",
+        "active": true
       }
     ];
 
     //vm.pageTitle = $translate.instant('overview.pageTitle');
 
-    $scope.changeLayout = function(changeTo){
+    $scope.changeLayout = function (changeTo) {
       $scope.showLayout = changeTo;
-    }
+    };
 
-    $scope.changeDropdown = function(){
+    $scope.changeDropdown = function () {
       console.log("change dropdown");
       $scope.showDropdown = !$scope.showDropdown;
-    }
+    };
 
-    $scope.getFilter = function(card){
+    $scope.getFilter = function (card) {
       var filter;
-      if($scope.showLayout == 'active' && card.active == true){
+      if ($scope.showLayout == 'active' && card.active === true) {
         filter = true;
-      } else if ($scope.showLayout == 'all'){
+      } else if ($scope.showLayout == 'all') {
         filter = true;
       }
       return filter;
-    }
+    };
   }
 })();
