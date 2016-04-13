4907f83cf2bfe2000addcdecbb440414649e4d87
sjalipar <sjalipar@cisco.com>
2016-04-13 14:53:57 +0530

    unit test for deatails control

---
+++ app/modules/sunlight/details/detailsHeaderCtrl.spec.js
@@ -0,0 +1,17 @@
+/**
+ * Created by sjalipar on 4/13/16.
+ */
+describe('Care details ctrl', function () {
+  var detailsCtrl;
+  beforeEach(module('Sunlight'));
+  beforeEach(inject(function ($controller) {
+    detailsCtrl = $controller('DetailsHeaderCtrl', function () {});
+  }));
+
+  it('should define header title and tabs', function () {
+    expect(detailsCtrl.title).toEqual('sunlightDetails.title');
+    expect(detailsCtrl.tabs.length).toEqual(2);
+    expect(detailsCtrl.tabs[0].title).toEqual('sunlightDetails.featuresTitle');
+    expect(detailsCtrl.tabs[1].title).toEqual('sunlightDetails.settingsTitle');
+  });
+});
