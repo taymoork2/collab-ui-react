b2561e0aad064512a7081cc2f268ee7ce9ed03fc
Magnus Holst <maholst@cisco.com>
2016-04-13 15:05:47 +0200

    TypeScript & Services Landing: Concatenate TS files to fix load order of dependencies.

--- gulp/utils/typeScript.gulp.js
+++ gulp/utils/typeScript.gulp.js
@@ -6,6 +6,7 @@
 /* eslint no-param-reassign:0 */
 
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 var config = require('../gulp.config')();
 var $ = require('gulp-load-plugins')({
   lazy: true
@@ -14,7 +15,7 @@ var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var args = require('yargs').argv;
 
-function compile(files, dest) {
+function compile(files, dest, concatOut) {
   var reporter = $.typescript.reporter.defaultReporter();
   var SPEC_SUFFIX = '.spec';
 
@@ -22,7 +23,7 @@ function compile(files, dest) {
     .src(files, {
       base: config.app
     })
-    .pipe($.if(args.verbose, $.print()))
+    .pipe($.if(true ||args.verbose, $.print()))
     .pipe($.sourcemaps.init())
     .pipe($.typescript({
       "removeComments": false,
@@ -30,16 +31,18 @@ function compile(files, dest) {
       "target": "ES5",
       "sourceMap": true,
       "showOutput": "silent",
-      "listFiles": false
+      "listFiles": false,
+      "sortOutput": true
     }, undefined, reporter))
-    .pipe($.rename(
+    .pipe($.if(!concatOut,$.rename(
       function (path) {
         if (path.basename.indexOf(SPEC_SUFFIX) > -1) {
           path.basename = path.basename.replace(SPEC_SUFFIX, '');
           path.extname = config.typeScript.compiledTestSuffix;
         }
       }
-    ))
+    )))
+    .pipe($.if(concatOut,concat('modules/core/ts-output.js')))
     .pipe($.sourcemaps.write())
     .pipe(gulp.dest(dest))
     .pipe(reload({
