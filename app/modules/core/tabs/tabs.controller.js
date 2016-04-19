0be73f3ce87a4e98694b284c7b869a71e7e81961
Magnus Holst <maholst@cisco.com>
2016-04-19 16:29:25 +0200

    Services Landing: added feature toggle support for tabs (buttons on left side of atlas)
    code need formatting

--- app/modules/core/tabs/tabs.controller.js
+++ app/modules/core/tabs/tabs.controller.js
@@ -5,8 +5,10 @@
     .controller('TabsCtrl', TabsCtrl);
 
   /* @ngInject */
-  function TabsCtrl($rootScope, $scope, $location, Utils, Authinfo) {
-
+  function TabsCtrl($rootScope, $scope, $location, Utils, Authinfo, FeatureToggleService) {
+    var tc = this;
+    tc.features = [];
+    tc.tabs = [];
     initTabs();
 
     $scope.$on('AuthinfoUpdated', initTabs);
@@ -18,8 +20,8 @@
 
       var tab = _.find($scope.tabs, function (tab) {
         return matchesLocationPath(tab.link) || _.some(tab.subPages, function (subTab) {
-          return matchesLocationPath(subTab.link);
-        });
+            return matchesLocationPath(subTab.link);
+          });
       });
 
       if (tab) {
@@ -37,9 +39,67 @@
       });
     }
 
-    function initTabs() {
-      $scope.tabs = Authinfo.getTabs();
+    function updateScopeTabs() {
+      $scope.tabs = filterFeatureToggledTabs(tc.tabs, tc.features);
       setActiveTab();
+    }
+
+    function filterFeatureToggledTabs(tabs, features) {
+      return _.filter(tabs, function (tab) {
+        return !tab.feature || _.any(features, {
+            feature: tab.feature.replace(/^!/, ''),
+            enabled: !/^!/.test(tab.feature)
+          });
+      });
+    }
+
+    function updateFeatureTogglesFromTabs(tabs, features) {
+      // return s && s.replace(/^!/g, '');
+      var newFeatures = _.chain(tabs)
+        .map('feature')
+        .compact()
+        .invoke(String.prototype.replace, /^!/, '')
+        .unique()
+        .map(function (feature) {
+          return {feature: feature, enabled: false};
+        }).value();
+      console.log('fadfa',newFeatures,tabs);
+
+    _.forEach(newFeatures,function (feature) {
+        var existing = _.find(features, {feature: feature.feature});
+        if (existing) {
+          console.log('nooop');
+          //noop , keep the enabled flag from previous load.
+        } else {
+          console.log('push', feature, features);
+          features.push(feature);
+        }
+      });
+      console.log('ff2', features);
+    }
+
+    function getFeatureToggles(features) {
+      _.forEach(features, function (feature) {
+        FeatureToggleService.supports(feature.feature).then(function (supports) {
+          console.log('featur toggle support',supports, feature);
+          feature.enabled = !!supports;
+          updateScopeTabs();
+        });
+      });
+    }
+
+
+    function initTabs() {
+      tc.tabs = Authinfo.getTabs();
+      console.log(tc, tc.tabs);
+      updateFeatureTogglesFromTabs(tc.tabs, tc.features);
+      getFeatureToggles(tc.features);
+      console.log('after get toglles',tc, tc.features);
+      updateScopeTabs();
+      // $scope.tabs = filterFeatureToggledTabs(tabs, tc.features);
+      // console.log('features', features, filterFeatureToggledTabs(tabs, features));
+      // _.forEach($scope.tabs, function (tab) {console.log(tab);});
+      // setActiveTab();
     }
   }
 })();
