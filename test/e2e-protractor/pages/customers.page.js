08498f29b7f792e9a4058a94a8f0ee4504c0a479
Shafi Razzack <shrazzac@cisco.com>
2016-04-27 14:20:07 -0700

    code review changes

--- test/e2e-protractor/pages/customers.page.js
+++ test/e2e-protractor/pages/customers.page.js
@@ -1,22 +1,10 @@
 'use strict';
 
 var CustomersPage = function () {
-
+  
   this.viewCustomer = element(by.id('launchCustomer'));
-
   this.clickViewCustomer = function (alias) {
     utils.clickUser(alias);
-  };
-
-  this.username = element(by.css('li.user-name'));
-
-  this.checkLoggedinUsername = function (emailAlias) {
-    browser.getAllWindowHandles().then(function (handles) {
-      var newTabHandle = handles[1];
-      browser.switchTo().window(newTabHandle).then(function () {
-        utils.expectText(global.customers.username, emailAlias);
-      });
-    });
   };
 };
 
--- test/e2e-protractor/squared/salesadmin-customer-launch_spec.js
+++ test/e2e-protractor/squared/salesadmin-customer-launch_spec.js
@@ -3,20 +3,20 @@
 /* global LONG_TIMEOUT */
 
 describe('Launch customer as a sales admin', function () {
-  var token;
-
   it('should login as a partner sales account admin', function () {
-    login.login('partner-reports-sales-admin', '#/partner/customers')
-      .then(function (bearerToken) {
-        token = bearerToken;
-      });
+    login.login('partner-reports-sales-admin', '#/partner/customers');
   });
 
   describe('Launch customer ', function () {
     it('should launch customer', function () {
       customers.clickViewCustomer('Atlas_Test_Sales_Admin_Org_Create');
       utils.click(customers.viewCustomer);
-      customers.checkLoggedinUsername('kingkuntauser4+1715@gmail.com');
+      browser.getAllWindowHandles().then(function (handles) {
+        var newTabHandle = handles[1];
+        browser.switchTo().window(newTabHandle).then(function () {
+          utils.expectIsDisplayed(landing.serviceSetup);
+        });
+      });
     });
   });
 });
