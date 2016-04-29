ce4e8f6a8e74bf4bf6ff401a4134994e37ca539f
Helge Langehaug <hellange@cisco.com>
2016-04-29 10:06:52 +0200

    Read-only interception tests have been fixed because they gave false positives.

--- app/modules/core/scripts/services/readonly.interceptor.spec.js
+++ app/modules/core/scripts/services/readonly.interceptor.spec.js
@@ -3,47 +3,86 @@
 describe('ReadonlyInterceptor', function () {
   beforeEach(module('Core'));
 
-  var $httpBackend, Interceptor, Authinfo, Notification;
+  var $httpBackend, Interceptor, Authinfo, Notification, $q;
 
-  beforeEach(inject(function ($injector, _ReadonlyInterceptor_, _Authinfo_, _Notification_) {
+  beforeEach(inject(function (_$q_, _$httpBackend_, $injector, _ReadonlyInterceptor_, _Authinfo_, _Notification_) {
     Interceptor = _ReadonlyInterceptor_;
     Authinfo = _Authinfo_;
     Notification = _Notification_;
+    $httpBackend = _$httpBackend_;
+    $q = _$q_;
+    $q.reject = sinon.spy();
   }));
 
-  it('write operations are intercepted with special notification in read only mode', function () {
-    sinon.stub().returns(false);
-    Authinfo.isReadOnlyAdmin = sinon.stub().returns(true);
-    Notification.notifyReadOnly = sinon.stub();
-    Interceptor.request({
-      data: "x",
-      method: "POST"
+  describe('read-only mode', function () {
+
+    beforeEach(function () {
+      Authinfo.isReadOnlyAdmin = sinon.stub().returns(true);
+      Notification.notifyReadOnly = sinon.spy();
     });
-    expect(Notification.notifyReadOnly.calledOnce);
+
+    it('intercepts POST operations and creates a read-only notification', function () {
+      Notification.notifyReadOnly = sinon.spy();
+
+      Interceptor.request({
+        data: "x",
+        method: "POST"
+      });
+      expect($q.reject.callCount).toEqual(1);
+      expect(Notification.notifyReadOnly.callCount).toEqual(1);
+    });
+
+    it('intercepts PUT operations and creates a read-only notification', function () {
+      Interceptor.request({
+        data: "x",
+        method: "PUT"
+      });
+      expect($q.reject.callCount).toEqual(1);
+      expect(Notification.notifyReadOnly.callCount).toEqual(1);
+    });
+
+    it('intercepts DELETE operations and creates a read-only notification', function () {
+      Interceptor.request({
+        data: "x",
+        method: "DELETE"
+      });
+      expect($q.reject.callCount).toEqual(1);
+      expect(Notification.notifyReadOnly.callCount).toEqual(1);
+    });
+
+    it('intercepts PATCH operations and creates a read-only notification', function () {
+      Interceptor.request({
+        data: "x",
+        method: "PATCH"
+      });
+      expect($q.reject.callCount).toEqual(1);
+      expect(Notification.notifyReadOnly.callCount).toEqual(1);
+    });
+
+    it('does not intercept read operations', function () {
+      var config = {
+        data: "x",
+        method: "GET"
+      };
+      var response = Interceptor.request(config);
+      expect($q.reject.callCount).toEqual(0);
+      expect(Notification.notifyReadOnly.callCount).toEqual(0);
+    });
+
   });
 
-  it('read operations are not intercepted with special notification in read only mode', function () {
-    sinon.stub().returns(false);
-    Authinfo.isReadOnlyAdmin = sinon.stub().returns(true);
-    Notification.notifyReadOnly = sinon.stub();
-    var config = {
-      data: "x",
-      method: "POST"
-    };
-    var response = Interceptor.request(config);
-    expect(Notification.notifyReadOnly.notCalled);
-  });
+  describe('while not in read-only mode', function () {
 
-  it('write operations are NOT intercepted with special notification when NOT in read only mode', function () {
-    sinon.stub().returns(false);
-    Authinfo.isReadOnlyAdmin = sinon.stub().returns(false);
-    Notification.notifyReadOnly = sinon.stub();
-    var config = {
-      data: "x",
-      method: "POST"
-    };
-    var response = Interceptor.request(config);
-    expect(Notification.notifyReadOnly.notCalled);
+    it('does not manipulate requests', function () {
+      Authinfo.isReadOnlyAdmin = sinon.stub().returns(false);
+      Notification.notifyReadOnly = sinon.spy();
+      var config = {
+        data: "x",
+        method: "POST"
+      };
+      var response = Interceptor.request(config);
+      expect($q.reject.callCount).toEqual(0);
+      expect(Notification.notifyReadOnly.callCount).toEqual(0);
+    });
   });
-
 });
