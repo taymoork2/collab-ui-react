b6da811d09962dbc0da908ccbb68c46bce42d647
afroyhau <afroyhau@cisco.com>
2016-05-03 10:18:48 +0200

    Added allowed list for readonly interceptor

--- app/modules/core/scripts/services/readonly.interceptor.js
+++ app/modules/core/scripts/services/readonly.interceptor.js
@@ -4,8 +4,14 @@
   /*ngInject*/
   function ReadonlyInterceptor($q, Authinfo, Notification) {
 
+    var allowedList = [
+      '/conversation/api/v1/users/deskFeedbackUrl',
+      '/idb/oauth2/v1/revoke'
+    ];
+
     function rejectOnNotRead(config) {
-      if (_.isFunction(Authinfo.isReadOnlyAdmin) && Authinfo.isReadOnlyAdmin() && isWriteOp(config.method)) {
+      if (_.isFunction(Authinfo.isReadOnlyAdmin) && Authinfo.isReadOnlyAdmin() && isWriteOp(config.method) && !isInAllowedList(config.url)) {
+        console.log('Rejecting URL', config.url);
         Notification.notifyReadOnly(config);
         return $q.reject(config);
       } else {
@@ -15,6 +21,17 @@
 
     function isWriteOp(method) {
       return (method === 'POST' || method === 'PUT' || method === 'PATCH' || method === 'DELETE');
+    }
+
+    function isInAllowedList(url) {
+      var found = _.find(allowedList, function (p) {
+        return _.endsWith(url, p);
+      });
+      if (found) {
+        return true;
+      } else {
+        return false;
+      }
     }
 
     return {
