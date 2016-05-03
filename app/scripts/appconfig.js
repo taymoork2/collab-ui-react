56cfcc0a959dfec642abe6b24a8420b1550f40b6
Thomas Bassetto <thobasse@cisco.com>
2016-05-03 15:37:17 +0200

    Better use of $translateProvider to handle the default and fallback languages

--- app/scripts/appconfig.js
+++ app/scripts/appconfig.js
@@ -332,10 +332,10 @@
           });
 
         $httpProvider.interceptors.push('TrackingIdInterceptor');
-        $httpProvider.interceptors.push('ResponseInterceptor');
-        $httpProvider.interceptors.push('TimingInterceptor');
-        $httpProvider.interceptors.push('ServerErrorInterceptor');
-        $httpProvider.interceptors.push('ReadonlyInterceptor');
+        // $httpProvider.interceptors.push('ResponseInterceptor');
+        // $httpProvider.interceptors.push('TimingInterceptor');
+        // $httpProvider.interceptors.push('ServerErrorInterceptor');
+        // $httpProvider.interceptors.push('ReadonlyInterceptor');
 
         // See ... http://angular-translate.github.io/docs/#/guide/19_security
         $translateProvider.useSanitizeValueStrategy('escapeParameters');
@@ -347,10 +347,8 @@
 
         $translateProvider.addInterpolation('$translateMessageFormatInterpolation');
 
-        var defaultLang = 'en_US';
-
-        //Tell the module what language to use by default
-        $translateProvider.preferredLanguage(defaultLang);
+        $translateProvider.determinePreferredLanguage();
+        $translateProvider.fallbackLanguage('en_US');
       }
     ]);
 
