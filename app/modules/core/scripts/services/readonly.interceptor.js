9701f328b3dbfb27faafede05d65a410f0084ca3
Thomas Bassetto <thobasse@cisco.com>
2016-05-04 09:37:22 +0200

    Fix circular dependencies between $translateProvider and HTTP interceptors

--- app/modules/core/scripts/services/readonly.interceptor.js
+++ app/modules/core/scripts/services/readonly.interceptor.js
@@ -1,10 +1,21 @@
 (function () {
   'use strict';
 
+  angular
+    .module('Core')
+    .factory('ReadonlyInterceptor', ReadonlyInterceptor);
+
   /*ngInject*/
-  function ReadonlyInterceptor($q, Authinfo, Notification) {
+  function ReadonlyInterceptor($q, $injector) {
+
+    return {
+      request: rejectOnNotRead
+    };
 
     function rejectOnNotRead(config) {
+      // injected manually to get around circular dependency problem with $translateProvider
+      var Authinfo = $injector.get('Authinfo');
+      var Notification = $injector.get('Notification');
       if (_.isFunction(Authinfo.isReadOnlyAdmin) && Authinfo.isReadOnlyAdmin() && isWriteOp(config.method)) {
         Notification.notifyReadOnly(config);
         return $q.reject(config);
@@ -16,12 +27,6 @@
     function isWriteOp(method) {
       return (method === 'POST' || method === 'PUT' || method === 'PATCH' || method === 'DELETE');
     }
-
-    return {
-      request: rejectOnNotRead
-    };
   }
-
-  angular.module('Core').factory('ReadonlyInterceptor', ReadonlyInterceptor);
 
 }());
