969505ccb648ff6369b353dac932b1906644ae9a
ngrodum <ngrodum@cisco.com>
2016-05-12 15:27:33 +0200

    Global Settings: Added gui for Security section

--- app/modules/core/settings/securitySetting.ts
+++ app/modules/core/settings/securitySetting.ts
@@ -3,6 +3,8 @@ namespace globalsettings {
 
   export class SecuritySetting extends Setting {
 
+    ckbox1 = true;
+
     constructor(ctrl:SettingsController) {
       super('security', ctrl);
       this.subsectionDescription = '';
