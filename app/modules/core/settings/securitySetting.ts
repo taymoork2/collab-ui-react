79df266ac36be09f3967f6f226f33da0bcb0c67e
ngrodum <ngrodum@cisco.com>
2016-05-13 13:20:40 +0200

    Global Settings: Security section: Wiring up the checkbox to js
    (cherry picked from commit 587c5fe7b75bcc69a3627ba2809cef152e54df87)

--- app/modules/core/settings/securitySetting.ts
+++ app/modules/core/settings/securitySetting.ts
@@ -3,11 +3,22 @@ namespace globalsettings {
 
   export class SecuritySetting extends Setting {
 
-    ckbox1 = true;
+    requireProtectedDevices:boolean = undefined;
 
     constructor(ctrl:SettingsController) {
       super('security', ctrl);
       this.subsectionDescription = '';
+
+      //get the current setting:
+      setTimeout( this.settingLoaded.bind(this), 2400, true);
+    }
+
+    settingLoaded(requireProtectedDevices:boolean) {
+      this.requireProtectedDevices = requireProtectedDevices;
+    }
+
+    requireProtectedDevicesUpdate(){
+    //  console.log('dev changed to', this.requireProtectedDevices);
     }
   }
 }
