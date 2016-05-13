11ad700d027e5fe67894cc64294e22768d624f2b
ngrodum <ngrodum@cisco.com>
2016-05-13 13:45:51 +0200

    Global Settings: Added gui for privacy section, wired up in js.
    (cherry picked from commit babf3bd2c4838cbaf920560396b2a51fc4ed72a2)

--- app/modules/core/settings/privacySetting.ts
+++ app/modules/core/settings/privacySetting.ts
@@ -3,9 +3,30 @@ namespace globalsettings {
 
   export class PrivacySetting extends Setting {
 
+    allowReadOnlyOrgAccess:boolean = undefined;
+    sendUsageData:boolean = undefined;
+
     constructor(ctrl:SettingsController) {
       super('privacy', ctrl);
-      this.subsectionDescription = '';
+
+      setTimeout( this.allowReadOnlyOrgAccessLoaded.bind(this), 800, true);
+      setTimeout( this.sendUsageDataLoaded.bind(this), 1200, false);
+    }
+
+    allowReadOnlyOrgAccessLoaded(allowReadOnlyOrgAccess:boolean) {
+      this.allowReadOnlyOrgAccess = allowReadOnlyOrgAccess;
+    }
+
+    allowReadOnlyOrgAccessUpdate(){
+      alert('allowReadOnlyOrgAccess changed to' + this.allowReadOnlyOrgAccess);
+    }
+
+    sendUsageDataLoaded(sendUsageData:boolean) {
+      this.sendUsageData = sendUsageData;
+    }
+
+    sendUsageDataUpdate(){
+        alert('sendUsageData changed to' + this.sendUsageData);
     }
   }
 }
