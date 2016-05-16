14fa06c274c73c868bb1c446c293029f0228b43b
Brad DeGrazia <degrazia@cisco.com>
2016-05-16 14:00:45 -0700

    update with PR comments

--- app/modules/squared/scripts/controllers/invite.js
+++ app/modules/squared/scripts/controllers/invite.js
@@ -5,43 +5,18 @@
     .module('Squared')
     .controller('InviteCtrl', InviteCtrl);
 
-  function InviteCtrl($location, ipCookie, Utils, Inviteservice, Config, Log, UrlConfig, WindowLocation) {
-    var redirect = function () {
-      var redirectUrl = null;
+  function InviteCtrl($location, Utils, Log, UrlConfig, WindowLocation) {
+    var redirectUrl = null;
 
-      if (Utils.isIPhone()) {
-        redirectUrl = UrlConfig.getItunesStoreUrl();
-      } else if (Utils.isAndroid()) {
-        redirectUrl = UrlConfig.getAndroidStoreUrl();
-      } else {
-        redirectUrl = UrlConfig.getWebClientUrl();
-      }
-      Log.info('Redirect to: ' + redirectUrl);
-
-      WindowLocation.set(redirectUrl);
-    };
-
-    redirect();
-
-    // check if cookie already exists.  Only call backend if not.
-    var cookieName = 'invdata';
-    var inviteCookie = ipCookie(cookieName);
-
-    if (inviteCookie === undefined) {
-
-      inviteCookie = {
-        userEmail: null,
-        displayName: null,
-        orgId: null,
-        entitlements: null
-      };
-      var cookieOptions = {
-        domain: Config.isDev() ? null : '.wbx2.com',
-        expires: 1 // 1 day
-      };
-
+    if (Utils.isIPhone()) {
+      redirectUrl = UrlConfig.getItunesStoreUrl();
+    } else if (Utils.isAndroid()) {
+      redirectUrl = UrlConfig.getAndroidStoreUrl();
     } else {
-      redirect();
+      redirectUrl = UrlConfig.getWebClientUrl();
     }
+    Log.info('Redirect to: ' + redirectUrl);
+
+    WindowLocation.set(redirectUrl);
   }
 })();
