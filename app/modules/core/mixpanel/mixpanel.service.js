3de79aa8f80f9b404ad0519694d091062de12f0f
chrispha <chrispha@cisco.com>
2016-05-27 10:06:08 -0700

    updated to use single promise

--- app/modules/core/mixpanel/mixpanel.service.js
+++ app/modules/core/mixpanel/mixpanel.service.js
@@ -8,11 +8,14 @@
     .service('Mixpanel', Mixpanel);
 
   /* @ngInject */
-  function Mixpanel(Config, Orgservice) {
+  function Mixpanel($q, Config, Orgservice) {
 
     var service = {
-      trackEvent: trackEvent
+      trackEvent: trackEvent,
+      getTestOrg: getTestOrg
     };
+
+    var isTestOrgPromise = null;
 
     return service;
 
@@ -20,14 +23,31 @@
      *	Saves mixpanel data in production
      */
     function trackEvent(eventName, attributes) {
+      if (Config.isE2E()) {
+        return;
+      }
+
       if (Config.isProd()) {
         mixpanel.track('Prod Org: ' + eventName, _.toArray(attributes));
-      } 
-      Orgservice.getOrg(function (response) {
-        if (response.isTestOrg) {
-          mixpanel.track('Test Org: ' + eventName, _.toArray(attributes));
-        }
-      });
+      } else {
+        getTestOrg().then(function (isTestOrg) {
+          if (isTestOrg) {
+            mixpanel.track('Test Org: ' + eventName, _.toArray(attributes));
+          }
+        });
+      }
+
+    }
+
+    function getTestOrg() {
+      if (!isTestOrgPromise) {
+        isTestOrgPromise = $q(function (resolve, reject) {
+          Orgservice.getOrg(function (response) {
+            resolve(response.isTestOrg);
+          });
+        });
+      }
+      return isTestOrgPromise;
     }
   }
 
