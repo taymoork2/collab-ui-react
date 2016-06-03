1bfe1209c2851f8996ba4908c9a56f5ea239fe88
Coco Mycek <cmycek@cisco.com>
2016-06-03 16:27:32 -0400

    AUTOATTN-640: Fix static analysis #2455

--- app/modules/huron/features/autoAttendant/builder/aaBuilderActionsCtrl.js
+++ app/modules/huron/features/autoAttendant/builder/aaBuilderActionsCtrl.js
@@ -6,7 +6,7 @@
     .controller('AABuilderActionsCtrl', AABuilderActionsCtrl);
 
   /* @ngInject */
-  function AABuilderActionsCtrl($scope, $translate, $controller, AAUiModelService, AACommonService) {
+  function AABuilderActionsCtrl($scope, $translate, $controller, AAUiModelService, AACommonService, AutoAttendantCeMenuModelService) {
 
     var vm = this;
 
