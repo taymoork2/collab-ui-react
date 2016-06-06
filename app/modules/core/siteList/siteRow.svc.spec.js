2b82f5aef68c6c1e8b733e818742d63b2a372c42
Sumitra Krishnan <sumitkri@cisco.com>
2016-06-06 13:24:44 -0700

    Adding unit test for controller

--- app/modules/core/siteList/siteRow.svc.spec.js
+++ app/modules/core/siteList/siteRow.svc.spec.js
@@ -1,1 +1,7 @@
+'use strict';
 
+describe('Service: WebExSiteRowService', function () {
+  beforeEach(module('Core'));
+  beforeEach(module('Huron'));
+  beforeEach(module('WebExApp'));
+});
--- app/modules/core/siteList/siteRowCtrl.spec.js
+++ app/modules/core/siteList/siteRowCtrl.spec.js
@@ -1,1 +1,34 @@
+'use strict';
 
+describe('Controller: WebExSiteRowCtrl', function () {
+  var controller, $scope, $q, FeatureToggleService, WebExSiteRowService;
+  beforeEach(module('Core'));
+  beforeEach(module('Huron'));
+  beforeEach(module('WebExApp'));
+
+  beforeEach(inject(function ($rootScope, $controller, _$q_, _FeatureToggleService_, _WebExSiteRowService_) {
+    $scope = $rootScope.$new();
+
+    FeatureToggleService = _FeatureToggleService_;
+    WebExSiteRowService = _WebExSiteRowService_;
+    $q = _$q_;
+
+    spyOn(FeatureToggleService, 'supports').and.returnValue($q.when(true));
+    spyOn(WebExSiteRowService, 'getConferenceServices');
+    spyOn(WebExSiteRowService, 'configureGrid');
+    spyOn(WebExSiteRowService, 'getGridOptions');
+    spyOn(WebExSiteRowService, 'getShowGridData').and.returnValue(true);
+
+    controller = $controller('WebExSiteRowCtrl', {
+      $scope: $scope,
+      FeatureToggleService: FeatureToggleService,
+      WebExSiteRowService: WebExSiteRowService
+    });
+
+    $scope.$apply();
+  }));
+
+  it('can correctly initialize WebExSiteRowCtrl', function () {
+    expect(controller).toBeDefined();
+  });
+});
