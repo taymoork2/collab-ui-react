0032c841587f5fc307e4bd62dd09db8eb605d90b
Magnus Holst <maholst@cisco.com>
2016-06-07 10:46:23 +0200

    My Company Page: refactored headerCtrl - remove scope usage.
    Parts of the header still use scope set by other controllers.

--- app/modules/core/scripts/controllers/headerCtrl.js
+++ app/modules/core/scripts/controllers/headerCtrl.js
@@ -18,11 +18,11 @@
     init();
 
     function init() {
-      $scope.icon = 'icon-cisco-logo';
+      vm.icon = 'icon-cisco-logo';
       $translate('loginPage.title').then(function (title) {
-        $scope.headerTitle = title;
+        vm.headerTitle = title;
       });
-      $scope.navStyle = 'admin';
+      vm.navStyle = 'admin';
       initFeatureToggles();
     }
 
