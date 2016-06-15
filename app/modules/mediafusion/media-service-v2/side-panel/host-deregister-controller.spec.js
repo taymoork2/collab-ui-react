d564a95df28b00f39e8aafc142b8d364b986558a
Krishna Kumar <kribalac@cisco.com>
2016-06-15 16:49:43 +0530

    Media fusion - FMS v2 changes

---
+++ app/modules/mediafusion/media-service-v2/side-panel/host-deregister-controller.spec.js
@@ -0,0 +1,94 @@
+'use strict';
+
+describe('Controller: HostDeregisterControllerV2', function () {
+
+  beforeEach(module('wx2AdminWebClientApp'));
+
+  var vm, $rootScope, $scope, httpBackend, controller, cluster, orgName, MediaClusterServiceV2, XhrNotificationService, $q, $translate, modalInstanceMock, windowMock, log;
+
+  beforeEach(inject(function (_$rootScope_, $httpBackend, $controller, _XhrNotificationService_, _$q_, _$translate_, $log) {
+    $rootScope = _$rootScope_;
+    cluster = {
+      id: 'a',
+      name: 'b'
+    };
+    orgName = '123';
+
+    MediaClusterServiceV2 = {
+
+      deleteCluster: sinon.stub()
+    };
+    XhrNotificationService = _XhrNotificationService_;
+    $q = _$q_;
+    $translate = _$translate_;
+
+    modalInstanceMock = {
+      close: sinon.stub()
+    };
+    windowMock = {
+      open: sinon.stub()
+    };
+    log = $log;
+    log.reset();
+    httpBackend = $httpBackend;
+    httpBackend
+      .when('GET', 'l10n/en_US.json')
+      .respond({});
+    controller = $controller('HostDeregisterControllerV2', {
+      $scope: $rootScope.$new(),
+
+      cluster: cluster,
+      orgName: orgName,
+      MediaClusterServiceV2: MediaClusterServiceV2,
+      XhrNotificationService: XhrNotificationService,
+      $q: $q,
+      $translate: $translate,
+      $modalInstance: modalInstanceMock,
+      $window: windowMock,
+      log: log
+    });
+
+  }));
+
+  it('check if HostDeregisterControllerV2 is Defined', function () {
+    expect(controller).toBeDefined();
+  });
+
+  it('check if Deregister is called', function () {
+    spyOn(MediaClusterServiceV2, 'deleteCluster').and.returnValue($q.when());
+    controller.deregister();
+    expect(MediaClusterServiceV2.deleteCluster).toHaveBeenCalled();
+    expect(controller.saving).toBe(true);
+
+  });
+
+  it('check if Deregister is called with  clusterId', function () {
+
+    spyOn(MediaClusterServiceV2, 'deleteCluster').and.returnValue($q.when());
+    controller.deregister();
+    expect(MediaClusterServiceV2.deleteCluster).toHaveBeenCalledWith(cluster.id);
+  });
+
+  it('Should go to success module of deregister', function () {
+    var deregisterDefered = $q.defer();
+    spyOn(MediaClusterServiceV2, 'deleteCluster').and.returnValue(deregisterDefered.promise);
+    deregisterDefered.resolve();
+    httpBackend.flush();
+
+    controller.deregister();
+    //  httpBackend.verifyNoOutstandingRequest();
+    httpBackend.verifyNoOutstandingExpectation();
+    expect(controller.saving).toBe(false);
+  });
+  it('Should go to failure module of deregister', function () {
+    var deregisterDefered = $q.defer();
+    spyOn(MediaClusterServiceV2, 'deleteCluster').and.returnValue(deregisterDefered.promise);
+    deregisterDefered.reject();
+    httpBackend.flush();
+
+    controller.deregister();
+    //  httpBackend.verifyNoOutstandingRequest();
+    httpBackend.verifyNoOutstandingExpectation();
+    expect(controller.saving).toBe(false);
+  });
+});
--- app/modules/mediafusion/media-service-v2/side-panel/reassign-cluster-controller.spec.js
+++ app/modules/mediafusion/media-service-v2/side-panel/reassign-cluster-controller.spec.js
@@ -1,12 +1,12 @@
 'use strict';
 
-describe('Controller: ReassignClusterController', function () {
+describe('Controller: ReassignClusterControllerV2', function () {
 
   beforeEach(module('wx2AdminWebClientApp'));
 
-  var vm, controller, cluster, MediaClusterService, XhrNotificationService, $q, $translate, modalInstanceMock, windowMock, log;
+  var vm, controller, cluster, MediaClusterServiceV2, XhrNotificationService, $q, $translate, modalInstanceMock, windowMock, log;
 
-  beforeEach(inject(function ($controller, _MediaClusterService_, _XhrNotificationService_, _$q_, _$translate_, $log) {
+  beforeEach(inject(function ($controller, _MediaClusterServiceV2_, _XhrNotificationService_, _$q_, _$translate_, $log) {
     cluster = {
       id: 'a',
       name: 'b',
@@ -16,13 +16,13 @@ describe('Controller: ReassignClusterController', function () {
       assigned_property_sets: '606599c8-8e72-491f-9212-153a0877eb84' // or whatever you prefer
     };
 
-    /* MediaClusterService = {
+    /* MediaClusterServiceV2 = {
 
        getGroups: sinon.stub(),
        removeGroupAssignment: sinon.stub(),
        updateGroupAssignment: sinon.stub()
      };*/
-    MediaClusterService = _MediaClusterService_;
+    MediaClusterServiceV2 = _MediaClusterServiceV2_;
     XhrNotificationService = _XhrNotificationService_;
     $q = _$q_;
     $translate = _$translate_;
@@ -36,9 +36,9 @@ describe('Controller: ReassignClusterController', function () {
     log = $log;
     log.reset();
 
-    controller = $controller('ReassignClusterController', {
+    controller = $controller('ReassignClusterControllerV2', {
       cluster: cluster,
-      MediaClusterService: MediaClusterService,
+      MediaClusterServiceV2: MediaClusterServiceV2,
       XhrNotificationService: XhrNotificationService,
       $q: $q,
       $translate: $translate,
@@ -49,24 +49,24 @@ describe('Controller: ReassignClusterController', function () {
 
   }));
 
-  it('check if ReassignClusterController is Defined', function () {
+  it('check if ReassignClusterControllerV2 is Defined', function () {
     expect(controller).toBeDefined();
   });
 
   it('check if getGroups is called', function () {
-    spyOn(MediaClusterService, 'getGroups').and.returnValue($q.when());
+    spyOn(MediaClusterServiceV2, 'getGroups').and.returnValue($q.when());
     controller.getGroups();
-    expect(MediaClusterService.getGroups).toHaveBeenCalled();
+    expect(MediaClusterServiceV2.getGroups).toHaveBeenCalled();
 
   });
 
   it('check if removeGroupAssignment is called with  clusterId', function () {
 
-    spyOn(MediaClusterService, 'removeGroupAssignment').and.returnValue($q.when());
-    spyOn(MediaClusterService, 'updateGroupAssignment').and.returnValue($q.when());
+    spyOn(MediaClusterServiceV2, 'removeGroupAssignment').and.returnValue($q.when());
+    spyOn(MediaClusterServiceV2, 'updateGroupAssignment').and.returnValue($q.when());
     controller.reassign();
     expect(controller.saving).toBe(true);
-    expect(MediaClusterService.removeGroupAssignment).toHaveBeenCalledWith(cluster.id, cluster.assigned_property_sets[0]);
+    expect(MediaClusterServiceV2.removeGroupAssignment).toHaveBeenCalledWith(cluster.id, cluster.assigned_property_sets[0]);
 
   });
 
