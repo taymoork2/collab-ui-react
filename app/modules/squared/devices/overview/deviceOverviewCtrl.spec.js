159dd3788ed8fdce4f5f835908e6e6f076dc744e
Wouter Lammers <woulamme@cisco.com>
2016-06-21 15:08:57 +0200

    Started writing tests for editing tags

--- app/modules/squared/devices/overview/deviceOverviewCtrl.spec.js
+++ app/modules/squared/devices/overview/deviceOverviewCtrl.spec.js
@@ -2,7 +2,7 @@
 
 describe('Controller: DeviceOverviewCtrl', function () {
   var $scope, $controller, controller, $httpBackend;
-  var $q, CsdmConfigService;
+  var $q, CsdmConfigService, CsdmDeviceService;
 
   beforeEach(module('Hercules'));
   beforeEach(module('Squared'));
@@ -11,12 +11,13 @@ describe('Controller: DeviceOverviewCtrl', function () {
   beforeEach(initSpies);
   beforeEach(initController);
 
-  function dependencies(_$q_, $rootScope, _$controller_, _$httpBackend_, _CsdmConfigService_) {
+  function dependencies(_$q_, $rootScope, _$controller_, _$httpBackend_, _CsdmConfigService_, _CsdmDeviceService_) {
     $scope = $rootScope.$new();
     $controller = _$controller_;
     $httpBackend = _$httpBackend_;
     $q = _$q_;
     CsdmConfigService = _CsdmConfigService_;
+    CsdmDeviceService = _CsdmDeviceService_;
   }
 
   function initSpies() {
@@ -42,5 +43,27 @@ describe('Controller: DeviceOverviewCtrl', function () {
 
   it('should init controller', function () {
     expect(controller).toBeDefined();
+  });
+
+  describe('Tags', function () {
+    it('should ignore only whitespace tags', function () {
+      controller.newTag = ' ';
+      controller.addTag();
+      expect(controller.isAddingTag).toBeFalsy();
+      expect(controller.newTag).toBeUndefined();
+    });
+
+    it('should post new tags to CsdmDeviceService', function () {
+      controller.newTag = 'new tag';
+      controller.currentDevice = {
+        tags: [],
+        url: 'testUrl'
+      };
+      spyOn(CsdmDeviceService, 'updateTags').and.returnValue($q.resolve());
+      controller.addTag();
+      $scope.$apply();
+      expect(CsdmDeviceService.updateTags).toHaveBeenCalledTimes(1);
+      expect(CsdmDeviceService.updateTags).toHaveBeenCalledWith('test2Url', ['new tag']);
+    });
   });
 });
