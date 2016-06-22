b84348a20fea222045cfbb93dac8bcdcca50eb6e
Zack Zatkin-Gold <zzatking@cisco.com>
2016-06-22 10:57:32 -0700

    US12857: Front End - In-service Retention Policy
    This user story involves implementing the new data retention endpoint in
    the global settings page.  This endpoint differs from the usual endpoint
    that is used by the first-time wizard since the first-time wizard should
    not be revisited more than once (hence why it's a first-time wizard).
    
    When a user selects a retention policy that is lower than the selected
    one, a modal window will pop-up and alert the user that the desired
    changes will remove messages older than the retention policy selected.
    If the user cancels the changes, the dropdown will revert the changes.
    
    The retention changes will go in by AJAX - there is no requirement to
    click on a Save button.
    
    I have also taken the time to rename "dataPolicy" to "retention"
    throughout the global settings page, for better readability.

--- app/modules/core/scripts/services/retention.service.js
+++ app/modules/core/scripts/services/retention.service.js
@@ -6,8 +6,19 @@
     .service('RetentionService', RetentionService);
 
   /* @ngInject */
-  function RetentionService($http, $q, UrlConfig) {
+  function RetentionService($resource, $q, UrlConfig) {
     var accountUrl = UrlConfig.getAdminServiceUrl();
+
+    var retentionResource = $resource(accountUrl + 'organizations/:orgId/settings/msgDataRetention', {
+      orgId: '@orgId'
+    }, {
+      get: {
+        method: 'GET'
+      },
+      update: {
+        method: 'PUT'
+      }
+    });
 
     var service = {
       getRetention: getRetention,
@@ -16,17 +27,13 @@
 
     return service;
 
-    function getRetentionUrl(org) {
-      return accountUrl + 'organizations/' + org + '/settings/msgDataRetention';
-    }
-
     function getRetention(org) {
       if (!org || org == '')
         return $q.reject("No organization was provided.");
 
-      var url = getRetentionUrl(org);
-
-      return $http.get(url);
+      return retentionResource.get({
+        orgId: org
+      }).$promise;
     }
 
     function setRetention(org, days) {
@@ -35,13 +42,13 @@
       if (!days || days == '')
         return $q.reject("No retention was provided.");
 
-      var url = getRetentionUrl(org);
-
       var payload = {
-        'msgDataRetention': days
+        msgDataRetention: days
       };
 
-      return $http.put(url, payload);
+      return retentionResource.update({
+        orgId: org
+      }, payload).$promise;
     }
   }
 })();
