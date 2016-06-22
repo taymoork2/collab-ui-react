d55133052eeaa8e162b14bff7ff6b011816cb36b
nbeeri <nbeeri@cisco.com>
2016-06-22 19:09:35 +0530

    Resolved UT

--- app/modules/mediafusion/media-service-v2/side-panel/delete-cluster-controller.spec.js
+++ app/modules/mediafusion/media-service-v2/side-panel/delete-cluster-controller.spec.js
@@ -1,30 +1,21 @@
 'use strict';
-
 describe('Controller: DeleteClusterControllerV2', function () {
-
   beforeEach(module('wx2AdminWebClientApp'));
-
   var $scope, vm, controller, clusterId, groupDetail, groupName, MediaClusterServiceV2, XhrNotificationService, $q, $translate, modalInstanceMock, windowMock, log;
   var getGroupsDefered, redirectTargetPromise, httpBackend;
-  beforeEach(inject(function ($rootScope, $httpBackend, $controller, _XhrNotificationService_, _$q_, _$translate_, $log) {
+  beforeEach(inject(function (_MediaClusterServiceV2_, $rootScope, $httpBackend, $controller, _XhrNotificationService_, _$q_, _$translate_, $log) {
     $scope = $rootScope.$new();
     groupName = 'MF_TEAM';
     clusterId = 'clusterId';
     $q = _$q_;
     httpBackend = $httpBackend;
     getGroupsDefered = $q.defer();
-
     redirectTargetPromise = {
       then: sinon.stub()
     };
-    MediaClusterServiceV2 = {
-      getGroups: sinon.stub().returns(getGroupsDefered.promise),
-      deleteGroup: sinon.stub().returns(redirectTargetPromise)
-    };
-
     httpBackend.when('GET', /^\w+.*/).respond({});
-
     XhrNotificationService = _XhrNotificationService_;
+    MediaClusterServiceV2 = _MediaClusterServiceV2_;
     $translate = _$translate_;
     modalInstanceMock = {
       close: sinon.stub()
@@ -34,7 +25,6 @@ describe('Controller: DeleteClusterControllerV2', function () {
     };
     log = $log;
     log.reset();
-
     controller = $controller('DeleteClusterControllerV2', {
       $scope: $scope,
       groupName: groupName,
@@ -48,15 +38,17 @@ describe('Controller: DeleteClusterControllerV2', function () {
       log: log
     });
   }));
-
   afterEach(function () {
     httpBackend.verifyNoOutstandingRequest();
-
     httpBackend.verifyNoOutstandingExpectation();
   });
-
   it('check if DeleteClusterControllerV2 is Defined', function () {
     expect(controller).toBeDefined();
   });
-
+  it('delete should call the deleteV2Cluster of MediaClusterServiceV2 ', function () {
+    spyOn(MediaClusterServiceV2, 'deleteV2Cluster').and.returnValue(redirectTargetPromise);
+    controller.delete();
+    expect(controller.saving).toBe(true);
+    expect(MediaClusterServiceV2.deleteV2Cluster).toHaveBeenCalled();
+  });
 });
