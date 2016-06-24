5bdf6d28c70f066c5c46fbf22a3b7eed908c211d
lhsieh <lhsieh@cisco.com>
2016-06-23 21:19:00 -0700

    Remove unnecessary variables.

--- test/e2e-protractor/pages/webexsitelist.page.js
+++ test/e2e-protractor/pages/webexsitelist.page.js
@@ -1,8 +1,6 @@
 var WebExSiteListPage = function () {
   this.siteListPageId = element(by.id('site-list'));
   this.conferencingLink = element(by.css('a[href="#site-list"]'));
-  this.license_MC200 = "Meeting Center 200";
-  this.license_Multiple = "Multiple...";
 };
 
 module.exports = WebExSiteListPage;
