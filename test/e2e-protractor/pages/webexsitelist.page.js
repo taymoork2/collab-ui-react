44695e251b79a0238ae9a1f4a81aa36c8b981989
lhsieh <lhsieh@cisco.com>
2016-06-23 21:10:59 -0700

    WebEx e2e tests for detecting non-CI sites.

--- test/e2e-protractor/pages/webexsitelist.page.js
+++ test/e2e-protractor/pages/webexsitelist.page.js
@@ -1,7 +1,6 @@
 var WebExSiteListPage = function () {
   this.siteListPageId = element(by.id('site-list'));
   this.conferencingLink = element(by.css('a[href="#site-list"]'));
-  this.licenseTypesColumnId = element(by.id('site-license-types'));
   this.license_MC200 = "Meeting Center 200";
   this.license_Multiple = "Multiple...";
 };
