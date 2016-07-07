fe32744a07fb4585241c8422d387fbc98f49d12f
jkallam <jkallam@cisco.com>
2016-07-07 13:03:13 -0700

    Added Cross Launch Url for non-CI sites

--- app/modules/core/siteList/siteRow.svc.js
+++ app/modules/core/siteList/siteRow.svc.js
@@ -58,10 +58,10 @@
     this.logSiteRows = function () {
       var funcName = "logSiteRows()";
       var logMsg = funcName + "\n" + JSON.stringify(_this.siteRows.gridData);
-      //$log.log(logMsg);
-      //$log.log("_this.siteRows.showGridData = " + _this.siteRows.showGridData + "\n");
-      //$log.log("_this.siteRows.gridOptions = " + JSON.stringify(_this.siteRows.gridOptions) + "\n");
-      //$log.log("_this.siteRows.showCSVIconAndResults = " + _this.siteRows.showCSVIconAndResults + "\n");
+      //      $log.log(logMsg);
+      //      $log.log("_this.siteRows.showGridData = " + _this.siteRows.showGridData + "\n");
+      //      $log.log("_this.siteRows.gridOptions = " + JSON.stringify(_this.siteRows.gridOptions) + "\n");
+      //      $log.log("_this.siteRows.showCSVIconAndResults = " + _this.siteRows.showCSVIconAndResults + "\n");
 
     };
 
@@ -128,11 +128,11 @@
 
     this.getConferenceServices = function () {
       var funcName = "getConferenceServices()";
-      var logMsg = funcName + "\n" +
-        "conferenceServices=\n" + JSON.stringify(conferenceServices);
-      //$log.log(logMsg);
 
       var conferenceServices = Authinfo.getConferenceServicesWithoutSiteUrl();
+      var logMsg = funcName + "\n" +
+        "conferenceServices=\n" + JSON.stringify(conferenceServices);
+      //      $log.log(logMsg);
 
       conferenceServices.forEach(
         function checkConferenceService(conferenceService) {
@@ -172,6 +172,7 @@
             conferenceService.advancedSettings = null;
             conferenceService.webexAdvancedUrl = null;
             conferenceService.siteUrl = newSiteUrl;
+            conferenceService.siteAdminUrl = null;
 
             conferenceService.showLicenseTypes = false;
             conferenceService.multipleWebexServicesLicensed = false;
@@ -564,6 +565,10 @@
       var siteUrl = siteRow.siteUrl;
 
       var isCISite = WebExUtilsFact.isCIEnabledSite(siteUrl);
+
+      var protocol = "https://";
+      var constant = "/wbxadmin/default.do?siteurl=";
+      siteRow.siteAdminUrl = protocol + siteUrl + constant + WebExUtilsFact.getSiteName(siteUrl);
 
       siteRow.isCI = isCISite;
 
