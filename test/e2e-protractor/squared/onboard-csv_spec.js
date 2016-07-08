0bfe4ad3317cf38bcbbd763ed6c86a5ed9472345
Magnus Holst <maholst@cisco.com>
2016-07-08 10:11:16 +0200

    ignore 2 tests related to access to FTSW
    these two tests are currently invalid,
    the access to FTSW will only be when the !isSetupDone on an org.

--- test/e2e-protractor/squared/onboard-csv_spec.js
+++ test/e2e-protractor/squared/onboard-csv_spec.js
@@ -26,8 +26,8 @@ describe('Onboard Users using CSV File', function () {
     login.login('account-admin', '#/users');
   });
 
-  it('should open add users tab', function () {
-    utils.click(landing.serviceSetup);
+  xit('should open add users tab', function () {
+    utils.click(landing.serviceSetup); //TODO this is invalid, since the wizard will only be shown when !isSetupDone
     utils.click(navigation.addUsers);
     utils.expectTextToBeSet(wizard.mainviewTitle, 'Add Users');
     utils.click(inviteusers.bulkUpload);
