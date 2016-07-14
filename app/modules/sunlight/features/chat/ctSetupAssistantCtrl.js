41756c41db5604694aabe8155612d784aefe4862
sjalipar <sjalipar@cisco.com>
2016-07-15 00:18:04 +0530

    modify e2e for chat template profile page

--- app/modules/sunlight/features/chat/ctSetupAssistantCtrl.js
+++ app/modules/sunlight/features/chat/ctSetupAssistantCtrl.js
@@ -38,7 +38,7 @@
       'chatStatusMessages',
       'summary'
     ];
-    vm.currentState = vm.states[1];
+    vm.currentState = vm.states[0];
     vm.animationTimeout = 10;
     vm.escapeKey = 27;
 
