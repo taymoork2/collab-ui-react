13f0020da858e18e0a89303d7b53aa5353e88c37
sjalipar <sjalipar@cisco.com>
2016-07-14 23:54:04 +0530

    refactor chat template profile page

--- app/modules/sunlight/features/chat/ctSetupAssistantCtrl.js
+++ app/modules/sunlight/features/chat/ctSetupAssistantCtrl.js
@@ -38,7 +38,7 @@
       'chatStatusMessages',
       'summary'
     ];
-    vm.currentState = vm.states[0];
+    vm.currentState = vm.states[1];
     vm.animationTimeout = 10;
     vm.escapeKey = 27;
 
