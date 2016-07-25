c7ca1c1091ea37dc143a2795faf578c074776e58
Mike Park <mipark2@cisco.com>
2016-07-25 11:07:20 -0700

    refactor(build,karma): mv common code out of 'karma.shim.common.js' and 'karma.shim.tpl.js'
    chore(build,karma): add gitignore pattern 'flist-split.*.karma.shim.js'

--- karma/karma.shim.default.js
+++ karma/karma.shim.default.js
@@ -1,20 +1,4 @@
-// Bootstrap the app
-require('../app/bootstrap');
-require('angular-mocks');
-
-// Load the rest of the app
-require('../app/scripts/main');
-
-// Test Dependencies
-require('jasmine-jquery/lib/jasmine-jquery.js');
-require('sinon');
-require('karma-sinon');
-require('jasmine-sinon');
-require('jasmine-promise-matchers/dist/jasmine-promise-matchers.js');
-require('bardjs/dist/bard.js');
-
-// Initial test setup
-require('../test/global.spec.js');
+require('./karma.shim.common.js');
 
 requireAll(require.context('../app', true, /\.spec\.(js|ts)$/));
 
