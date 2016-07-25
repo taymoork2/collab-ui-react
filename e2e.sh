bff5fcde0b8b39f53342108ca0a25a358e9e8c28
Mike Park <mipark2@cisco.com>
2016-07-25 16:57:07 -0700

    chore(build,ktest-all): use absolute path for 'ktest'
    chore(build,ktest): offset num cores by -1
    
    chore(build,ktest): bring back usage help
    
    chore(build,e2e): use 'curl' instead of 'nc'
    
    - jenkins builder does not have 'nc' installed, so use 'curl' instead as
      alternate method of detecting when web server is up
    
    chore(build,e2e): fix typo - 'serve-dist'

--- e2e.sh
+++ e2e.sh
@@ -6,8 +6,11 @@ npm run webdriver
 # Cleanup tcp processed from previous jobs
 npm run kill
 
-npm run server-dist &
-while ! nc -z 127.0.0.1 8000; do sleep 1; done
+npm run serve-dist &
+# poll on web webserver at root context until it comes up
+while [ $( curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:8000/ ) -ne 200 ]; do
+    sleep 2;
+done
 
 # Run Protractor
 npm run e2e -- --verbose --sauce
