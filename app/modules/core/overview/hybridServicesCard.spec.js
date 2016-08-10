b22b3212a45ce7f3de6d4e0cf8ddee87fdffcca2
Kjetil Ree <kjeree@cisco.com>
2016-08-10 14:41:14 +0200

    Update tests, linting.

--- app/modules/core/overview/hybridServicesCard.spec.js
+++ app/modules/core/overview/hybridServicesCard.spec.js
@@ -1,28 +1,26 @@
 describe('OverviewHybridServicesCard', function () {
 
-  var OverviewHybridServicesCard, $rootScope, FusionClusterService, $httpBackend, $q;
+  var OverviewHybridServicesCard, $rootScope, FusionClusterService, $q;
 
   beforeEach(angular.mock.module('Hercules'));
   beforeEach(angular.mock.module('Squared'));
   beforeEach(angular.mock.module('Core'));
 
-  function dependencies(_OverviewHybridServicesCard_, _$rootScope_, _FusionClusterService_, _$httpBackend_, _$q_) {
+  function dependencies(_OverviewHybridServicesCard_, _$rootScope_, _FusionClusterService_, _$q_) {
     OverviewHybridServicesCard = _OverviewHybridServicesCard_;
     $rootScope = _$rootScope_;
     FusionClusterService = _FusionClusterService_;
-    $httpBackend = _$httpBackend_;
     $q = _$q_;
   }
 
   function initSpies() {
-    $httpBackend.expectGET('https://hercules-integration.wbx2.com/hercules/api/v2/organizations/null?fields=@wide').respond(['']);
     spyOn(FusionClusterService, 'getAll');
   }
 
   beforeEach(inject(dependencies));
   beforeEach(inject(initSpies));
 
-  it('should not show card when no services are set up ', function() {
+  it('should not show card when no services are set up', function () {
     var clustersWithNothingInstalled = getJSONFixture('hercules/nothing-provisioned-cluster-list.json');
     FusionClusterService.getAll.and.returnValue($q.resolve(clustersWithNothingInstalled));
     var card = OverviewHybridServicesCard.createCard();
@@ -31,7 +29,7 @@ describe('OverviewHybridServicesCard', function () {
     expect(card.enabled).toBe(false);
   });
 
-  it('should not show a service that is not set up ', function() {
+  it('should not show a service that is not set up', function () {
     var clustersWithNoServices = getJSONFixture('hercules/nothing-provisioned-cluster-list.json');
     FusionClusterService.getAll.and.returnValue($q.resolve(clustersWithNoServices));
     var card = OverviewHybridServicesCard.createCard();
@@ -43,7 +41,7 @@ describe('OverviewHybridServicesCard', function () {
 
   });
 
-  it('should show the card when services are set up', function() {
+  it('should show the card when services are set up', function () {
     spyOn(FusionClusterService, 'processClustersToSeeIfServiceIsSetup');
     FusionClusterService.processClustersToSeeIfServiceIsSetup.and.returnValue(true);
     var clustersWithManyServices = getJSONFixture('hercules/disco-systems-cluster-list.json');
@@ -55,8 +53,8 @@ describe('OverviewHybridServicesCard', function () {
     expect(card.enabled).toBe(true);
   });
 
-  it('should not show Call if it is not set up, but still show Calendar', function() {
-    spyOn(FusionClusterService, 'processClustersToSeeIfServiceIsSetup').and.callFake(function(serviceId) {
+  it('should not show Call if it is not set up, but still show Calendar', function () {
+    spyOn(FusionClusterService, 'processClustersToSeeIfServiceIsSetup').and.callFake(function (serviceId) {
       return serviceId === 'squared-fusion-cal';
     });
     var clustersWithManyServices = getJSONFixture('hercules/disco-systems-cluster-list.json');
