35c68e602db30c1f312198711b81dbd22109b806
Brian Spence <brspence@cisco.com>
2016-08-10 18:02:01 -0500

    feat(directoryNumber): New directory number component

--- app/modules/huron/lines/lineOverview/lineOverview.component.ts
+++ app/modules/huron/lines/lineOverview/lineOverview.component.ts
@@ -1,9 +1,32 @@
-import directoryNumber from '../directoryNumber/directoryNumber.component';
+import directoryNumber from '../../directoryNumber/directoryNumber.component';
+
+interface IDirectoryNumber {
+  uuid: string,
+  pattern: string,
+}
 
 class LineOverviewCtrl {
 
-  private $onInit(): void {
+  public esnPrefix: string;
+  public internalIsWarn: boolean;
+  public internalNumber: IDirectoryNumber;
+  public internalOptions: IDirectoryNumber[];
+  public internalWarnMsg: string;
+  public externalNumber: IDirectoryNumber;
+  public externalOptions: IDirectoryNumber[];
+  public showExtensions: boolean;
 
+  private $onInit(): void {
+    this.initDirectoryNumber();
+  }
+
+  private initDirectoryNumber(): void {
+    this.showExtensions = true;
+  }
+
+  public setDirectoryNumbers(internalNumber: IDirectoryNumber, externalNumber: IDirectoryNumber): void {
+    this.internalNumber = internalNumber;
+    this.externalNumber = externalNumber;
   }
 }
 
