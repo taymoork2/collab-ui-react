423463eea62829d82ce7ac28ff81fbf45140e01e
lhsieh <lhsieh@cisco.com>
2016-08-16 20:03:04 -0700

    Update unit test.

--- app/modules/core/leaderBoard/leaderBoardCtrl.spec.js
+++ app/modules/core/leaderBoard/leaderBoardCtrl.spec.js
@@ -4,18 +4,20 @@ describe('LeaderBoard', function () {
   beforeEach(angular.mock.module('Core'));
   beforeEach(angular.mock.module('Huron'));
 
-  var $controller, $q, $scope, Orgservice, TrialService;
+  var $controller, $q, $scope, Orgservice, TrialService, WebExUtilsFact;
   var usageOnlySharedDevicesFixture = getJSONFixture('core/json/organizations/usageOnlySharedDevices.json');
 
-  beforeEach(inject(function (_$controller_, _$q_, $rootScope, _Orgservice_, _TrialService_) {
+  beforeEach(inject(function (_$controller_, _$q_, $rootScope, _Orgservice_, _TrialService_, _WebExUtilsFact_) {
     $scope = $rootScope.$new();
     $controller = _$controller_;
     $q = _$q_;
     Orgservice = _Orgservice_;
     TrialService = _TrialService_;
+    WebExUtilsFact = _WebExUtilsFact_;
 
     spyOn(Orgservice, 'getLicensesUsage').and.returnValue($q.when(usageOnlySharedDevicesFixture));
     spyOn(TrialService, 'getDaysLeftForCurrentUser').and.returnValue($q.when());
+    spyOn(WebExUtilsFact, 'isCIEnabledSite').andReturnValue(true);
   }));
 
   function initController() {
