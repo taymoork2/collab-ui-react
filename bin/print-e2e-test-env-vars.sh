e2720e3c1f0ba84c884a2e56ecaf8b041ac48a2f
Mike Park <mipark2@cisco.com>
2016-08-25 13:53:54 -0700

    feat(build): revamp for markdown build notifications
    - print-e2e-test-env-vars.sh
      - chore - shell lint cleanups
      - refactor - use only one method for determining last e2e run number
    
    - sauce-results-helpers
      - fix - grep expression for 'grep_for_sauce_link_lines' (too loose)
      - new - 'get_saved_e2e_exit_code'
    
    - webhook-helpers
      - new - add functions to make curl-ing of Spark api easier
        - chore - reimpl 'add_key_val_to_json' to python
          - more jenkins slave executors have python installed, so this opens up the possibilities for webhook-helpers to be used more broadly
        - new - '*file_to_json'
          - simplify CLI usage for stringifying a file
        - new - 'post_*file_as_message_to_room_id'
          - simplify CLI usage for posting a payload to a room id
    
    - jenkins-helpers
      - new - add curl-ing of JSON api for jenkins build urls
        - IMPORTANT: the following environment vars must now be defined at build run-time (NEVER COMMIT THEM TO SOURCE):
          - SPARK__MSG_BOT_API_TOKEN
            - used by curl for using Spark messaging/rooms REST API
            - see: https://developer.ciscospark.com/apps.html
          - JENKINS__USER_ID
          - JENKINS__USER_API_TOKEN
            - used by curl for accessing REST API of jenkins build urls
            - see: https://sqbu-jenkins.cisco.com:8443/user/${CEC_user_id}/configure
        - IMPORTANT: a hard requirement is now placed on any jenkins job that sources 'jenkins-helpers' and uses any 'jq_*' function
          - new - 'jq_*'
            - helper methods for accessing properties from JSON payload file
      - new - 'print_build_*'
        - helper functions for printing printing sections of markdown payload related to a jenkins build
    
    - markdown-helpers
      - new - add 'tr_user_id_to_mention_by_email'
        - helper function for transforming a CEC id into Spark API-friendly markdown for triggering an @-mention in a Spark room
      - fix - start from index 1 for displaying run number for failed test links
    
    - secrets-helpers
      - for providing auth-related env values

--- bin/print-e2e-test-env-vars.sh
+++ bin/print-e2e-test-env-vars.sh
@@ -5,25 +5,22 @@ if [ -z "${WX2_ADMIN_WEB_CLIENT_HOME}" ]; then
     exit 1
 fi
 
-source ${WX2_ADMIN_WEB_CLIENT_HOME}/.jenkins-build-env-vars
-source ${WX2_ADMIN_WEB_CLIENT_HOME}/bin/include/jenkins-helpers
-
-# log whether tests succeded (will determine if job is marked 'UNSTABLE' or not)
-echo -n "[INFO] E2E tests succeded: "
-e2e_tests_succeeded && echo true || echo false
+. ${WX2_ADMIN_WEB_CLIENT_HOME}/.jenkins-build-env-vars
+. ${WX2_ADMIN_WEB_CLIENT_HOME}/bin/include/jenkins-helpers
+. ${WX2_ADMIN_WEB_CLIENT_HOME}/bin/include/sauce-results-helpers
 
 # reset e2e-related env-vars file (these are injected into jenkins job environment)
-> ${E2E_TEST_ENV_VARS_FILE}
+> "$E2E_TEST_ENV_VARS_FILE"
 
 # 'NUM_RETRIES' => retries used during e2e tests
-NUM_RETRIES=$(basename `get_last_retry_dir ` | cut -d- -f2)
-echo NUM_RETRIES=${NUM_RETRIES} >> ${E2E_TEST_ENV_VARS_FILE}
+NUM_RETRIES=$(get_last_run_from_log_file "$e2e_sauce_logs_file")
+echo NUM_RETRIES=${NUM_RETRIES} >> "$E2E_TEST_ENV_VARS_FILE"
 
 # 'RESULTS_URL' => url for jenkins job that publishes the e2e test results
-RESULTS_URL=`print_last_triggered_job_url`
-echo RESULTS_URL=${RESULTS_URL} >> $E2E_TEST_ENV_VARS_FILE
+RESULTS_URL=$(print_last_triggered_job_url)
+echo RESULTS_URL=${RESULTS_URL} >> "$E2E_TEST_ENV_VARS_FILE"
 
 # 'BUILD_DESCR' => summary text, along with a link to the test results job
 BUILD_DESCR="<a href=\"${RESULTS_URL}\">E2E Test Report</a>"
 BUILD_DESCR="${BUILD_DESCR} (Retries: ${NUM_RETRIES})"
-echo BUILD_DESCR=$BUILD_DESCR >> $E2E_TEST_ENV_VARS_FILE
+echo BUILD_DESCR=$BUILD_DESCR >> "$E2E_TEST_ENV_VARS_FILE"
