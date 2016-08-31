65baeae0d617f10def5b17ac7ecb3a979517593e
Wouter Lammers <woulamme@cisco.com>
2016-08-31 10:36:12 +0200

    fix(devices): Allow Huron only orgs to add devices and places
    The buttons on the devices and places pages (both for empty orgs and orgs with devices/places) were not shown in orgs that do not have the cloudberry entitlement, now they also show for orgs with only the huron entitlement. See https://jira-eng-chn-sjc1.cisco.com/jira/browse/ATLAS-1080

--- app/modules/squared/devices/addDeviceNew/ChooseSharedSpaceCtrl.js
+++ app/modules/squared/devices/addDeviceNew/ChooseSharedSpaceCtrl.js
@@ -4,7 +4,7 @@
   angular.module('Core')
     .controller('ChooseSharedSpaceCtrl', ChooseSharedSpaceCtrl);
   /* @ngInject */
-  function ChooseSharedSpaceCtrl(CsdmCodeService, CsdmPlaceService, CsdmHuronPlaceService, XhrNotificationService, $stateParams, $state, $translate, Authinfo) {
+  function ChooseSharedSpaceCtrl(CsdmCodeService, CsdmPlaceService, CsdmHuronPlaceService, XhrNotificationService, $stateParams, $translate, Authinfo) {
     var vm = this;
     vm.wizardData = $stateParams.wizard.state().data;
 
@@ -137,10 +137,6 @@
 
     vm.back = function () {
       $stateParams.wizard.back();
-    };
-
-    vm.clickUsers = function () {
-      $state.go('users.list');
     };
   }
 })();
