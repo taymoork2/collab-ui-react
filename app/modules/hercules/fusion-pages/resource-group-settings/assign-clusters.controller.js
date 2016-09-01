7be3504670a073ad7404ff423bf34cbaecdd1b96
tvasset <tvasset@cisco.com>
2016-09-01 10:21:58 -0700

    Simple assign cluster modal to exercise the backend (will be rewritten when designs are ready)

--- app/modules/hercules/fusion-pages/resource-group-settings/assign-clusters.controller.js
+++ app/modules/hercules/fusion-pages/resource-group-settings/assign-clusters.controller.js
@@ -5,12 +5,47 @@
     .controller('AssignClustersController', AssignClustersController);
 
   /* @ngInject */
-  function AssignClustersController($modalInstance, resourceGroup) {
+  function AssignClustersController($modalInstance, resourceGroup, FusionClusterService, ResourceGroupService, $translate, Notification) {
     var vm = this;
+    vm.clusters = [];
+    vm.assignments = {};
     vm.resourceGroup = resourceGroup;
-    vm.saveClusterAssignments = saveClusterAssignments;
+    vm.clusterCheckboxes = [];
+    vm.saveAssignments = saveAssignments;
 
-    function saveClusterAssignments() {
+    loadAssignments();
+
+    function loadAssignments() {
+      FusionClusterService.getAll()
+        .then(function (clusters) {
+          vm.clusters = clusters;
+          // eslint-disable-next-line no-unused-expressions
+          !_.forEach(vm.clusters, function (c) {
+            vm.assignments[c.id] = c.resourceGroupId === resourceGroup.id;
+            vm.clusterCheckboxes.push({
+              label: c.name,
+              name: c.id,
+              id: c.id
+            });
+          });
+        }, angular.noop);
+    }
+
+    function saveAssignments() {
+      // eslint-disable-next-line no-unused-expressions
+      !_.forEach(vm.clusters, function (c) {
+        if ((c.resourceGroupId === resourceGroup.id && !vm.assignments[c.id]) || (c.resourceGroupId !== resourceGroup.id && vm.assignments[c.id])) {
+          ResourceGroupService.assign(c.id, vm.assignments[c.id] ? vm.resourceGroup.id : '')
+            .then(function () {
+              Notification.success($translate.instant('hercules.assignClustersModal.' + (vm.assignments[c.id] ? 'assigned' : 'removed'), {
+                clusterName: c.name,
+                groupName: resourceGroup.name
+              }));
+            }, function () {
+              Notification.error('hercules.genericFailure');
+            });
+        }
+      });
       $modalInstance.close();
     }
   }
