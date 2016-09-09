792e61572218679e63d5be5c4941eb167e2ba938
chrispha <chrispha@cisco.com>
2016-09-08 17:25:34 -0700

    us14757: overview page enhancements

--- app/modules/core/overview/usersCard.js
+++ app/modules/core/overview/usersCard.js
@@ -6,7 +6,7 @@
     .factory('OverviewUsersCard', OverviewUsersCard);
 
   /* @ngInject */
-  function OverviewUsersCard($state, $rootScope) {
+  function OverviewUsersCard($state, $rootScope, $translate, Auth, Authinfo) {
     return {
       createCard: function createCard() {
         var card = {};
@@ -16,12 +16,43 @@
         card.cardClass = 'user-card';
         card.icon = 'icon-circle-user';
 
+        card.showLicenseCard = false;
+
         card.unlicensedUsersHandler = function (data) {
           if (data.success) {
-            card.usersToConvert = Math.max((data.resources || []).length, data.totalResults);
             // for now use the length to get the count as there is a bug in CI and totalResults is not accurate.
+            card.usersToConvert = Math.max((data.resources || []).length, data.totalResults);
+            if (card.usersToConvert === 0) {
+              card.name = 'overview.cards.licenses.title';
+              card.showLicenseCard = true;
+              unassignedLicenses();
+            }
           }
         };
+
+        function unassignedLicenses() {
+          Auth.getCustomerAccount(Authinfo.getOrgId()).then(function (data) {
+            var max = 0;
+            var license = {};
+            var licenses = data.data.customers[0].licenses;
+            _.forEach(licenses, function (data) {
+              if (data.volume >= max) {
+                max = data.volume;
+                if (data.volume === max) {
+                  if (!(license.licenseType) || license.licenseType !== 'MESSAGING') {
+                    license = data;
+                  }
+                } else {
+                  license = data;
+                }
+              }
+              card.text = $translate.instant('overview.cards.licenses.text', {
+                number: max,
+                licenseType: license.licenseType
+              });
+            });
+          });
+        }
 
         card.orgEventHandler = function (data) {
           if (data.success) {
@@ -43,6 +74,10 @@
             currentTab: 'enterpriseSettings',
             currentStep: 'init'
           });
+        };
+
+        card.manageUsers = function () {
+          $state.go('users.manage', {});
         };
 
         return card;
