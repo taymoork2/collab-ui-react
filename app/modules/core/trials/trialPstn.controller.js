1d475408808948c49ca7bd912c3eb8c3b5304bde
Shivangi Sharma <fshivang@cisco.com>
2016-09-20 13:30:48 -0700

    PROVISION 2788: PSTN provisioning should use Carrier.apiImplementation rather than deprecated Carrier.apiExists

--- app/modules/core/trials/trialPstn.controller.js
+++ app/modules/core/trials/trialPstn.controller.js
@@ -13,6 +13,7 @@
     vm.customerId = Authinfo.getOrgId();
     var pstnTokenLimit = 10;
     vm.providerImplementation = 'SWIVEL';
+    vm.swivel = $translate.instant('pstnSetup.swivel');
 
     vm.getStateInventory = getStateInventory;
     vm.searchCarrierInventory = searchCarrierInventory;
