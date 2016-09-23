9bbd8ad38acfac629e6a33926e6c419ce1547b76
tvasset <tvasset@cisco.com>
2016-09-23 04:09:13 -0700

    Added a delete resource group confirm modal (earlier we just nuked the group on button click)

---
+++ app/modules/hercules/fusion-pages/resource-group-settings/confirm-delete-resource-group.controller.js
@@ -0,0 +1,23 @@
+(function () {
+  'use strict';
+
+  angular.module('Hercules')
+    .controller('ConfirmDeleteResourceGroupController', ConfirmDeleteResourceGroupController);
+
+  /* @ngInject */
+  function ConfirmDeleteResourceGroupController($modalInstance, resourceGroup, ResourceGroupService, Notification) {
+    var vm = this;
+    vm.resourceGroup = resourceGroup;
+    vm.confirmRemove = confirmRemove;
+
+    function confirmRemove() {
+      ResourceGroupService.remove(vm.resourceGroup.id)
+        .then(function () {
+          Notification.success('hercules.resourceGroupSettings.deleteSuccess');
+          $modalInstance.close();
+        }, function () {
+          Notification.error('hercules.genericFailure');
+        });
+    }
+  }
+})();
