e2cd7f34073e2e7130aed327fb944f69f13b8a39
Helge Langehaug <hellange@cisco.com>
2016-09-29 14:01:45 +0200

    Updated device mock data with more correct distributed data.

--- app/modules/core/customerReports/deviceUsage/deviceUsageMock.service.js
+++ app/modules/core/customerReports/deviceUsage/deviceUsageMock.service.js
@@ -6,22 +6,27 @@
     .service('DeviceUsageMockService', DeviceUsageMockService);
 
   /* @ngInject */
-  function DeviceUsageMockService($q, $log, Utils) {
+  function DeviceUsageMockService($q) {
 
     function randVal() {
       return _.random(0, 50);
     }
 
-    function sample(time) {
+    function sample(time, deviceUUID) {
       return {
         'count': randVal(),
         'date': time,
-        'deviceId': Utils.getUUID(),
+        'deviceId': deviceUUID,
         'pairedCount': randVal(),
         'sharedCount': randVal(),
         'totalDuration': randVal() * 10
       };
     }
+
+    var minNoOfCallsPrDay = 0;
+    var maxNoOfCallsPrDay = 10;
+
+    var existingUniqueDeviceIds = createSetOfUniqueDeviceIds(10);
 
     function getData(startDate, endDate) {
       var data = [];
@@ -29,11 +34,22 @@
       var end = moment(endDate);
       while (start.isBefore(end)) {
         var time = start.format('YYYYMMDD');
-        data.push(sample(time));
+        var noOfCallsToday = _.random(minNoOfCallsPrDay, maxNoOfCallsPrDay);
+        for (var i = 0; i < noOfCallsToday; i++) {
+          var deviceUUID = existingUniqueDeviceIds[i];
+          data.push(sample(time, deviceUUID));
+        }
         start.add(1, "days");
       }
-      $log.info('returned mock data set:', data);
       return $q.when(data);
+    }
+
+    function createSetOfUniqueDeviceIds(noOfUniqueDevices) {
+      var devices = [];
+      _.times(noOfUniqueDevices, function (index) {
+        devices.push("1111-" + index);
+      });
+      return devices;
     }
 
     return {
