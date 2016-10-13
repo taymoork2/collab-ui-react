ccef65f07f8bb54955b3693648deced0b5a25032
Helge Langehaug <hellange@cisco.com>
2016-10-13 11:27:04 +0200

    Device usage mock updated with new API.

--- app/modules/core/customerReports/deviceUsage/deviceUsageMock.service.js
+++ app/modules/core/customerReports/deviceUsage/deviceUsageMock.service.js
@@ -6,61 +6,40 @@
     .service('DeviceUsageMockService', DeviceUsageMockService);
 
   /* @ngInject */
-  function DeviceUsageMockService($q) {
+  function DeviceUsageMockService($log, $q, DeviceUsageMockData) {
 
-    var cachedData;
-    var deviceCategory = "ce";
-
-    function deviceDaySample(time, accountId) {
-      return {
-        'callCount': _.random(1, 20),
-        'date': time,
-        'accountId': accountId,
-        'pairedCount': _.random(0, 10),
-        'deviceCategory': deviceCategory,
-        'totalDuration': _.random(1, 24) * 60 * 60 // 1 to 24 hours returned in seconds
-      };
-    }
-
-    var existingUniqueDeviceIds = createSetOfUniqueDeviceIds(1000);
-
-    function getCachedData(startDate, endDate) {
-      if (_.isEmpty(cachedData)) {
-        return getData(startDate, endDate);
-      } else {
-        return $q.when(_.cloneDeep(cachedData));
-      }
-    }
-
-
-    function getData(startDate, endDate) {
-      var data = [];
-      var start = moment(startDate);
-      var end = moment(endDate);
-      while (start.isBefore(end)) {
-        var time = start.format('YYYYMMDD');
-        var noOfActiveDevicesToday = _.random(0, existingUniqueDeviceIds.length - 1);
-        for (var i = 0; i < noOfActiveDevicesToday; i++) {
-          var accountId = existingUniqueDeviceIds[i];
-          data.push(deviceDaySample(time, accountId));
-        }
-        start.add(1, "days");
-      }
-      cachedData = data;
-      return $q.when(data);
-    }
-
-    function createSetOfUniqueDeviceIds(noOfUniqueDevices) {
-      var devices = [];
-      _.times(noOfUniqueDevices, function (index) {
-        devices.push("1111-" + index);
-      });
-      return devices;
-    }
-
-    return {
+    var service = {
       getData: getData,
-      getCachedData: getCachedData
     };
+    return service;
+
+    // Main api supposed to simulate backend
+    // Should be adjusted according to latest (preliminary) backend API
+    function getData(startDate, endDate, all) {
+      if (all === true) {
+        return $q.when(getRawData(startDate, endDate));
+      } else {
+        return $q.when(getDailySumPrType(startDate, endDate));
+      }
+    }
+
+    function getRawData(startDate, endDate) {
+      return DeviceUsageMockData.getRawData(startDate, endDate);
+    }
+
+    function getDailySumPrType(startDate, endDate) {
+      var rawData = getRawData(startDate, endDate);
+      var calculatedList = [];
+      _.each(rawData, function (d) {
+        var existingRegistration = _.find(calculatedList, { date: d.date, deviceCategory: d.deviceCategory });
+        if (existingRegistration) {
+          existingRegistration.totalDuration += d.totalDuration;
+        } else {
+          calculatedList.push(d);
+        }
+      });
+      $log.warn("After summing raw data returning only combination of date and type:", calculatedList);
+      return calculatedList;
+    }
   }
 }());
---
+++ app/modules/core/customerReports/deviceUsage/deviceUsageMockData.js
@@ -0,0 +1,70 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('Core')
+    .service('DeviceUsageMockData', DeviceUsageMockData);
+
+  /* @ngInject */
+  function DeviceUsageMockData() {
+
+    var cachedRawData;
+
+    var maxCallsPrDay = 20;
+    var maxPairedCallsPrDay = 10;
+    var existingUniqueDeviceIds = createSetOfUniqueDeviceIds(1000);
+
+    var service = {
+      getRawData: getRawData
+    };
+    return service;
+
+
+    function secondsFromDays(days) {
+      return days * 60 * 60;
+    }
+
+    function deviceDaySample(date, accountId, deviceCategory) {
+      return {
+        'callCount': _.random(1, maxCallsPrDay),
+        'date': date,
+        'accountId': accountId,
+        'pairedCount': _.random(0, maxPairedCallsPrDay),
+        'deviceCategory': deviceCategory,
+        'totalDuration': secondsFromDays(_.random(1, 24))
+      };
+    }
+
+    function getRawData(startDate, endDate) {
+      if (_.isEmpty(cachedRawData)) {
+        cachedRawData = assembleRawData(startDate, endDate);
+      }
+      return _.cloneDeep(cachedRawData);
+    }
+
+    function assembleRawData(startDate, endDate) {
+      var data = [];
+      var start = moment(startDate);
+      var end = moment(endDate);
+      while (start.isBefore(end)) {
+        var time = start.format('YYYYMMDD');
+        var noOfActiveDevicesToday = _.random(0, existingUniqueDeviceIds.length - 1);
+        for (var i = 0; i < noOfActiveDevicesToday; i++) {
+          var accountId = existingUniqueDeviceIds[i];
+          data.push(deviceDaySample(time, accountId, "ce"));
+          data.push(deviceDaySample(time, accountId, "darling"));
+        }
+        start.add(1, "days");
+      }
+      return data;
+    }
+
+    function createSetOfUniqueDeviceIds(noOfUniqueDevices) {
+      var devices = [];
+      _.times(noOfUniqueDevices, function (index) {
+        devices.push("1111-" + index);
+      });
+      return devices;
+    }
+  }
+}());
