b9ec941f4de5cdf1bffcdb3c98c38ddc40d23cca
Mike Park <mipark2@cisco.com>
2016-10-24 16:51:45 -0700

    refactor(huron): replace 'angular.isObject' -> '_.isObject'

--- app/modules/huron/pstnSetup/pstnNumbers/pstnNumbers.controller.js
+++ app/modules/huron/pstnSetup/pstnNumbers/pstnNumbers.controller.js
@@ -448,7 +448,7 @@
       $q.all(promises).then(function (results) {
         // sort our successful indexes and process from high to low
         _.forInRight(_.sortBy(results), function (indices) {
-          if (angular.isObject(indices) && _.isNumber(indices.searchResultsIndex) && _.isNumber(indices.searchResultsModelIndex)) {
+          if (_.isObject(indices) && _.isNumber(indices.searchResultsIndex) && _.isNumber(indices.searchResultsModelIndex)) {
             // clear the checkbox
             _.set(model.searchResultsModel, indices.searchResultsModelIndex, false);
             // remove from search result
