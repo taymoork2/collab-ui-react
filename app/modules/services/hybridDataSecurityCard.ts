b504150675dcd4ee7d4c9f2dd02d42d26887b333
Yelena Rabinovich <yrabinov@cisco.com>
2016-10-26 11:42:35 -0600

    feat(HDS): Removed code for displaying card, removed blank line.

--- app/modules/services/hybridDataSecurityCard.ts
+++ app/modules/services/hybridDataSecurityCard.ts
@@ -81,7 +81,6 @@ export class ServicesOverviewHybridDataSecurityCard extends ServicesOverviewHybr
       }
     }
 
-
   /* @ngInject */
   public constructor(FusionClusterStatesService) {
     super({
