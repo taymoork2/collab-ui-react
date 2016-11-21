0a20a6b2ef7159bb5e635bd6e0f1f95ea317ddb6
Kjetil Ree <kjeree@cisco.com>
2016-11-21 16:04:09 +0100

    feat(hercules): Last pieces of the Google Calendar onboadring flow for now.

---
+++ app/modules/hercules/service-settings/calendar-service-setup/first-time-google-setup.controller.spec.js
@@ -0,0 +1,55 @@
+'use strict';
+
+describe('Controller: FirstTimeGoogleSetupController', function () {
+
+  var $controller, $scope, $q, CloudConnectorServiceMock, Notification, controller, modalInstanceMock;
+
+  beforeEach(angular.mock.module('Hercules'));
+  beforeEach(angular.mock.module('Squared'));
+  beforeEach(inject(dependencies));
+  beforeEach(initSpies);
+  beforeEach(initController);
+
+  modalInstanceMock = {
+    close: sinon.stub()
+  };
+
+  CloudConnectorServiceMock = {
+    updateConfig: function () {
+      return $q.reject();
+    }
+  };
+
+  function dependencies(_$controller_, $rootScope, _$q_, _Notification_) {
+    $controller = _$controller_;
+    $scope = $rootScope.$new();
+    $q = _$q_;
+    Notification = _Notification_;
+  }
+
+  function initSpies() {
+    spyOn(Notification, 'errorWithTrackingId');
+    spyOn(modalInstanceMock, 'close');
+  }
+
+  function initController() {
+    controller = $controller('FirstTimeGoogleSetupController', {
+      $modalInstance: modalInstanceMock,
+      Notification: Notification,
+      CloudConnectorService: CloudConnectorServiceMock,
+    });
+  }
+
+  it('should show an error Notification if the save to CloudConnectorService is rejected', function () {
+    controller.uploadKey();
+    $scope.$apply();
+    expect(Notification.errorWithTrackingId.calls.count()).toBe(1);
+  });
+
+  it('should take the user to the users page if he clicks the users link', function () {
+    controller.navigateToUsers();
+    $scope.$apply();
+    expect(modalInstanceMock.close).toHaveBeenCalledWith('users.list');
+  });
+
+});
