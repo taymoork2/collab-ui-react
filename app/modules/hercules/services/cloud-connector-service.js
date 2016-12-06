74fcf3acad59072502e2a34191bd2a6d8f771cd9
Thomas Bassetto <thobasse@cisco.com>
2016-12-06 14:29:55 +0100

    style(google-calendar): fix styling of email tags

--- app/modules/hercules/services/cloud-connector-service.js
+++ app/modules/hercules/services/cloud-connector-service.js
@@ -8,7 +8,7 @@
   function CloudConnectorService($q, $timeout, Authinfo) {
 
     var serviceAccountId = 'google@example.org'; // dummy value for now
-    var isGoogleCalendarSetup = false;
+    var isGoogleCalendarSetup = true;
 
     var service = {
       isServiceSetup: isServiceSetup,
