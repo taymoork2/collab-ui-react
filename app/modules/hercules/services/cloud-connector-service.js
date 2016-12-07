73dcdb6110912ce376f788be494d67bacbdcf63a
tvasset <tvasset@cisco.com>
2016-12-07 04:37:08 -0800

    Woops, should be off :)

--- app/modules/hercules/services/cloud-connector-service.js
+++ app/modules/hercules/services/cloud-connector-service.js
@@ -7,7 +7,7 @@
 
   function CloudConnectorService($q, $timeout, Authinfo) {
     var serviceAccountId = 'google@example.org'; // dummy value for now
-    var isGoogleCalendarSetup = true;
+    var isGoogleCalendarSetup = false;
 
     return {
       isServiceSetup: isServiceSetup,
