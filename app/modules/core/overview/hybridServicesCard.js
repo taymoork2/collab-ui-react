803aa6df75caaad827f354d65a5b7983d3c375dc
tvasset <tvasset@cisco.com>
2016-12-07 05:24:47 -0800

    Chaining promises based on thobasse feedback

--- app/modules/core/overview/hybridServicesCard.js
+++ app/modules/core/overview/hybridServicesCard.js
@@ -26,36 +26,36 @@
             hasHDSFeatureToggle: FeatureToggleService.supports(FeatureToggleService.features.atlasHybridDataSecurity),
             hasGoogleCalendarFeatureToggle: FeatureToggleService.supports(FeatureToggleService.features.atlasHerculesGoogleCalendar),
           }).then(function (featureToggles) {
-            $q.all({
+            return $q.all({
               clusterList: FusionClusterService.getAll(),
               gcalService: Authinfo.isEntitled(Config.entitlements.fusion_google_cal) && featureToggles.hasGoogleCalendarFeatureToggle ? CloudConnectorService.getService('squared-fusion-gcal') : $q.resolve({}),
               featureToggles: featureToggles,
-            }).then(function (response) {
-              if (response.featureToggles.hasGoogleCalendarFeatureToggle && Authinfo.isEntitled(Config.entitlements.fusion_google_cal)) {
-                card.serviceList.push(response.gcalService);
-              }
-              if (Authinfo.isEntitled(Config.entitlements.fusion_cal)) {
-                card.serviceList.push(FusionClusterService.getStatusForService('squared-fusion-cal', response.clusterList));
-              }
-              if (Authinfo.isEntitled(Config.entitlements.fusion_uc)) {
-                card.serviceList.push(FusionClusterService.getStatusForService('squared-fusion-uc', response.clusterList));
-              }
-              if (response.featureToggles.hasMediaFeatureToggle && Authinfo.isEntitled(Config.entitlements.mediafusion)) {
-                card.serviceList.push(FusionClusterService.getStatusForService('squared-fusion-media', response.clusterList));
-              }
-              if (response.featureToggles.hasHDSFeatureToggle && Authinfo.isEntitled(Config.entitlements.hds)) {
-                card.serviceList.push(FusionClusterService.getStatusForService('spark-hybrid-datasecurity', response.clusterList));
-              }
-              card.enabled = _.some(card.serviceList, function (service) {
-                return service.setup;
-              });
-              if (card.enabled) {
-                _.each(card.serviceList, function (service) {
-                  service.UIstateLink = getUIStateLink(service.serviceId);
-                  service.healthStatus = service.statusCss;
-                });
-              }
+            })
+          }).then(function (response) {
+            if (response.featureToggles.hasGoogleCalendarFeatureToggle && Authinfo.isEntitled(Config.entitlements.fusion_google_cal)) {
+              card.serviceList.push(response.gcalService);
+            }
+            if (Authinfo.isEntitled(Config.entitlements.fusion_cal)) {
+              card.serviceList.push(FusionClusterService.getStatusForService('squared-fusion-cal', response.clusterList));
+            }
+            if (Authinfo.isEntitled(Config.entitlements.fusion_uc)) {
+              card.serviceList.push(FusionClusterService.getStatusForService('squared-fusion-uc', response.clusterList));
+            }
+            if (response.featureToggles.hasMediaFeatureToggle && Authinfo.isEntitled(Config.entitlements.mediafusion)) {
+              card.serviceList.push(FusionClusterService.getStatusForService('squared-fusion-media', response.clusterList));
+            }
+            if (response.featureToggles.hasHDSFeatureToggle && Authinfo.isEntitled(Config.entitlements.hds)) {
+              card.serviceList.push(FusionClusterService.getStatusForService('spark-hybrid-datasecurity', response.clusterList));
+            }
+            card.enabled = _.some(card.serviceList, function (service) {
+              return service.setup;
             });
+            if (card.enabled) {
+              _.each(card.serviceList, function (service) {
+                service.UIstateLink = getUIStateLink(service.serviceId);
+                service.healthStatus = service.statusCss;
+              });
+            }
           });
         }
         init();
