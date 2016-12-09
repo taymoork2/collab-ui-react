8e3e3fe230adb24f2e90630cfdddd135ba703a14
Hafeez Khader <hpalliko@cisco.com>
2016-12-09 12:06:28 +0530

    Fix Care Reports issue for new Orgs

--- app/modules/sunlight/services/sunlightReportService.spec.js
+++ app/modules/sunlight/services/sunlightReportService.spec.js
@@ -183,7 +183,7 @@ describe(' sunlightReportService', function () {
   xit('should get ReportingData for org for time selected today for mediaType chat', function () {
     sunlightReportService.getReportingData('org_stats', 0, 'chat').then(function (response) {
       var startTimeStamp = moment().startOf('day');
-      var endTimeStamp = moment();
+      var endTimeStamp = moment().add(1, 'hours').startOf('hour');
       var duration = moment.duration(endTimeStamp.diff(startTimeStamp));
       var hours = Math.floor(duration.asHours());
       expect(response.length).toBe(hours);
