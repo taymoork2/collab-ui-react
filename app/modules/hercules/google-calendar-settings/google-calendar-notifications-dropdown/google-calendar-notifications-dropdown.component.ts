fd5f50f93cffd60b78c1ed2a7d95ed60a0242146
Thomas Bassetto <thobasse@cisco.com>
2016-12-16 13:44:02 +0000

    feat(google-calendar-notifications-dropdown): first version

--- app/modules/hercules/google-calendar-settings/google-calendar-notifications-dropdown/google-calendar-notifications-dropdown.component.ts
+++ app/modules/hercules/google-calendar-settings/google-calendar-notifications-dropdown/google-calendar-notifications-dropdown.component.ts
@@ -1,6 +1,60 @@
+interface IAlarms {
+  severity: 'critical' | 'error' | 'warning' | 'alert';
+  title: string;
+  description: string;
+}
+
 class GoogleCalendarNotificationsDropdownCtrl implements ng.IComponentController {
+  private serviceId = 'squared-fusion-gcal';
+
+  public showDropdown = false;
+  public alarms: IAlarms[] = [];
+
   /* @ngInject */
-  constructor() {}
+  constructor(
+    private FusionClusterService,
+  ) {}
+
+  public $onInit() {
+    this.FusionClusterService.getAlarms(this.serviceId)
+      .then(data => {
+        this.alarms = data.items;
+      });
+  }
+
+  public toggleDropdown() {
+    if (!this.showDropdown && this.alarms.length === 0) {
+      return;
+    }
+    this.showDropdown = !this.showDropdown;
+  }
+
+  public removeAlarm(alarm) {
+    this.alarms = _.without(this.alarms, alarm);
+  }
+
+  public getNumberCSSClass() {
+    const allAreWarnings = _.every(this.alarms, { severity: 'warning' });
+    if (this.alarms.length === 0) {
+      return '';
+    } else if (allAreWarnings) {
+      return 'warning';
+    } else {
+      return 'alert';
+    }
+  }
+
+  public getBadgeCSSClass(severity) {
+    if (severity === 'warning') {
+      return 'warning';
+    } else {
+      return 'alert';
+    }
+  }
+
+  public shouldDisplayDropdown() {
+    return this.showDropdown && this.alarms.length > 0;
+  }
 }
 
 export class GoogleCalendarNotificationsDropdownComponent implements ng.IComponentOptions {
