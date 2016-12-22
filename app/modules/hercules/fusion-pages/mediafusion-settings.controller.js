a15c2e766337a528c8c12465078724bbf06ac403
Thomas Bassetto <thobasse@cisco.com>
2016-12-22 12:19:12 +0100

    feat(release-channel-section): use it for hybrid media clusters

--- app/modules/hercules/fusion-pages/mediafusion-settings.controller.js
+++ app/modules/hercules/fusion-pages/mediafusion-settings.controller.js
@@ -6,20 +6,12 @@
     .controller('MediafusionClusterSettingsController', MediafusionClusterSettingsController);
 
   /* @ngInject */
-  function MediafusionClusterSettingsController($stateParams, $translate, FusionClusterService, MediaClusterServiceV2, FusionUtils, Notification, ResourceGroupService) {
+  function MediafusionClusterSettingsController($stateParams, $translate, FusionClusterService, Notification) {
     var vm = this;
     vm.backUrl = 'cluster-list';
     vm.upgradeSchedule = {
       title: 'hercules.expresswayClusterSettings.upgradeScheduleHeader'
     };
-    vm.releaseChannel = {
-      title: 'mediaFusion.clusters.releaseChannel',
-    };
-
-    vm.releaseChannelOptions = [{
-      label: $translate.instant('hercules.fusion.add-resource-group.release-channel.stable'),
-      value: 'stable'
-    }];
 
     vm.deregisterModalOptions = {
       resolve: {
@@ -30,38 +22,6 @@
       controller: 'DeleteClusterSettingControllerV2',
       controllerAs: 'deleteClust',
       templateUrl: 'modules/mediafusion/media-service-v2/delete-cluster/delete-cluster-dialog.html'
-    };
-
-    vm.populateChannels = function () {
-      ResourceGroupService.getAllowedChannels()
-        .then(function (channels) {
-          _.forEach(['beta', 'latest'], function (restrictedChannel) {
-            if (_.includes(channels, restrictedChannel)) {
-              vm.releaseChannelOptions.push({
-                label: $translate.instant('hercules.fusion.add-resource-group.release-channel.' + restrictedChannel),
-                value: restrictedChannel
-              });
-            }
-          });
-        })
-        .catch(function (error) {
-          Notification.errorWithTrackingId(error, 'hercules.genericFailure');
-        });
-    };
-    vm.populateChannels();
-
-    vm.selected = '';
-
-    vm.changeReleaseChannel = function () {
-      if (vm.selected.label != vm.cluster.releaseChannel) {
-        MediaClusterServiceV2.updateV2Cluster(vm.cluster.id, vm.displayName, vm.selected.value)
-          .then(function () {
-            Notification.success('hercules.fusion.add-resource-group.release-channel.saveReleaseChannelSuccess');
-          })
-          .catch(function (error) {
-            Notification.errorWithTrackingId(error, 'hercules.fusion.add-resource-group.release-channel.saveReleaseChannelError');
-          });
-      }
     };
 
     loadCluster($stateParams.id);
@@ -75,7 +35,6 @@
           vm.cluster = cluster;
           vm.clusters = clusters;
           if (vm.cluster) {
-            vm.selectPlaceholder = FusionUtils.getLocalizedReleaseChannel(vm.cluster.releaseChannel);
             vm.localizedTitle = $translate.instant('hercules.expresswayClusterSettings.pageTitle', {
               clusterName: cluster.name
             });
