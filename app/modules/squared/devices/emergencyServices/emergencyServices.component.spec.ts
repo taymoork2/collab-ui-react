6869426df1986407d8b7e2af50b182ae39997f34
zamamoha <zamamoha@cisco.com>
2017-01-03 11:30:48 -0600

    fix(EmergencyServices) Show the address thats validated

--- app/modules/squared/devices/emergencyServices/emergencyServices.component.spec.ts
+++ app/modules/squared/devices/emergencyServices/emergencyServices.component.spec.ts
@@ -42,7 +42,13 @@ describe('component: emergencyServices', () => {
       },
       status: 'PENDING',
     }));
+
     this.compileComponent('ucEmergencyServices');
+    spyOn(this.controller, 'validateAddress').and
+    .returnValue(undefined);
+
+    spyOn(this.controller, 'saveAddress').and
+    .returnValue(undefined);
   });
 
   it('should instantiate emergencyServices', function() {
@@ -56,5 +62,15 @@ describe('component: emergencyServices', () => {
     expect(this.EmergencyServicesService.getAddressForNumber).toHaveBeenCalled();
     this.controller.resetSettings();
     expect(this.controller.emergency).toBe(undefined);
+  });
+
+  it('should validate and saveAddress', function() {
+    this.controller.validateAndSaveAddress();
+    expect(this.controller.validateAddress).toHaveBeenCalled();
+    expect(this.controller.saveAddress).not.toHaveBeenCalled();
+
+    this.controller.addressFound = true;
+    this.controller.validateAndSaveAddress();
+    expect(this.controller.saveAddress).toHaveBeenCalled();
   });
 });
