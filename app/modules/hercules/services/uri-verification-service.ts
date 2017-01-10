dce7ee4994abe1f9f026c7db4b47261fd0ee650b
Alexander Eid <haneid@cisco.com>
2017-01-10 14:01:52 +0100

    refactor (Hercules): Refactor uri-verification-service to TypeScript

--- app/modules/hercules/services/uri-verification-service.ts
+++ app/modules/hercules/services/uri-verification-service.ts
@@ -7,7 +7,7 @@ class UriVerificationService {
   ) {
   }
 
-  public isDomainVerified(domainList, uri): boolean {
+  public isDomainVerified(domainList:string[], uri:string): boolean {
     if (!domainList) {
       return false;
     }
