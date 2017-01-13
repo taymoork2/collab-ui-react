09b8f9a65da061517f8b1017612da0069d14af09
haneid <haneid@cisco.com>
2017-01-13 09:22:13 +0100

    Refactor (Hercules): Rewrite Certificate-formatter-service to TypeScript

--- app/modules/hercules/services/certificate-formatter-service.ts
+++ app/modules/hercules/services/certificate-formatter-service.ts
@@ -1,54 +1,57 @@
+interface ICertificate {
+  url: string;
+  certId: string;
+  orgId: string;
+  decoded: {
+    url: string;
+    subjectDN: string;
+    issuerDN: string;
+    notAfter: string;
+    notBefore: string;
+    serialNumber: number;
+    version: number;
+    sigAlgName: string;
+    sigAlgOID: string;
+    signature: string;
+  };
+  certBytes: string;
+}
+
+interface IformattedCertificate {
+  emailAddress: string;
+  commonName: string;
+  organizationalUnit: string;
+  organization: string;
+  locality: string;
+  stateAndProvince: string;
+  country: string;
+  certId: string;
+  created: string;
+  expires: string;
+}
+
+interface IcertificatesAsMap {
+  EMAILADDRESS?: string;
+  CN?: string;
+  OU?: string;
+  O?: string;
+  L?: string;
+  ST?: string;
+  C?: string;
+}
+
 export class CertificateFormatterService {
 
   /* @ngInject */
-  public formatCerts(certificates): any[] {
+  public formatCerts(certificates): IformattedCertificate[] {
 
     if (!_.isArray(certificates)) {
        return [];
      }
 
-    return _.map(certificates, (certificate: {
-       url?: string,
-      certId?: string,
-      orgId?: string,
-      decoded: {
-         url?: string,
-        subjectDN?: string,
-        issuerDN?: string,
-        notAfter?: string,
-        notBefore?: string,
-        serialNumber?: number,
-        version: number,
-        sigAlgName: string,
-        sigAlgOID: string,
-        signature: string,
-      }
-      certBytes: string,
-    }) => {
-
-      let formattedCertificate: {
-        emailAddress?: string,
-        commonName?: string,
-        organizationalUnit?: string ,
-        organization?: string,
-        locality?: string,
-        stateAndProvince?: string,
-        country?: string,
-        certId?: string,
-        created?: string,
-        expires?: string,
-      } = [];
-
+    return _.map(certificates, (certificate: ICertificate) => {
       let certificateAsArray = _.get(certificate, 'decoded.subjectDN', '').split(/,(?:\s)(?=(?:(?:[^"]*"){2})*[^"]*$)/);
-      let certificatesAsMap: {
-        EMAILADDRESS?: string,
-        CN?: string,
-        OU?: string,
-        O?: string,
-        L?: string,
-        ST?: string,
-        C?: string,
-      } = _.chain(certificateAsArray)
+      let certificatesAsMap: IcertificatesAsMap = _.chain(certificateAsArray)
         .map( (s) => {
           return s.split('=');
         })
@@ -57,22 +60,20 @@ export class CertificateFormatterService {
           return map;
         }, {})
         .value();
-
-      formattedCertificate.emailAddress = certificatesAsMap.EMAILADDRESS || 'N/A';
-      formattedCertificate.commonName = certificatesAsMap.CN || 'N/A';
-      formattedCertificate.organizationalUnit = certificatesAsMap.OU || 'N/A';
-      formattedCertificate.organization = certificatesAsMap.O || 'N/A';
-      formattedCertificate.locality = certificatesAsMap.L || 'N/A';
-      formattedCertificate.stateAndProvince = certificatesAsMap.ST || 'N/A';
-      formattedCertificate.country = certificatesAsMap.C || 'N/A';
-
-      formattedCertificate.certId = certificate.certId || 'N/A';
-      formattedCertificate.created = _.get(certificate, 'decoded.notBefore', 'N/A');
-      formattedCertificate.expires = _.get(certificate, 'decoded.notAfter', 'N/A');
-
+      const formattedCertificate: IformattedCertificate = {
+        emailAddress: certificatesAsMap.EMAILADDRESS || 'N/A',
+        commonName: certificatesAsMap.CN || 'N/A',
+        organizationalUnit: certificatesAsMap.OU || 'N/A',
+        organization: certificatesAsMap.O || 'N/A',
+        locality: certificatesAsMap.L || 'N/A',
+        stateAndProvince: certificatesAsMap.ST || 'N/A',
+        country: certificatesAsMap.C || 'N/A',
+        certId: certificate.certId || 'N/A',
+        created: _.get(certificate, 'decoded.notBefore', 'N/A'),
+        expires: _.get(certificate, 'decoded.notAfter', 'N/A'),
+      };
       return formattedCertificate;
     });
-
   }
 }
 
