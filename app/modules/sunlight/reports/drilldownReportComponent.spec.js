0f3beb5e076a451a3da7913288bf13610df8a26e
Sateesh Kumar Potturu Naga Venkata <satpottu@cisco.com>
2017-01-17 22:35:50 +0530

    feat(sunlight): care drill down reports

---
+++ app/modules/sunlight/reports/drilldownReportComponent.spec.js
@@ -0,0 +1,128 @@
+/**
+ * Created by bijnair on 11/01/17.
+ */
+
+'use strict';
+
+describe('drilldownReports Controller Positive Test cases', function () {
+  var $componentController, $log, ReportConstants, $timeout, CardUtils, $rootScope;
+  var $scope, ddController, callbackSpy;
+  beforeEach(angular.mock.module('Core'));
+
+  beforeEach(inject(function (_$componentController_, _$log_, _ReportConstants_, _$timeout_, _CardUtils_, _$rootScope_) {
+    $componentController = _$componentController_;
+    $log = _$log_;
+    ReportConstants = _ReportConstants_;
+    $timeout = _$timeout_;
+    CardUtils = _CardUtils_;
+    $rootScope = _$rootScope_;
+  }));
+
+  beforeEach(function () {
+    var props = {
+      broadcast: {
+        refresh: "DummyRefreshString",
+        reset: "DummyResetString"
+      },
+      description: "Dummy set Description",
+      display: false,
+      hasData: true,
+      emptyDescription: "Dummy Empty Description",
+      errorDescription: "DUmmy Error Description",
+      show: "Dummy Show",
+      hide: "Dummy Hide",
+      search: true,
+      searchPlaceholder: "Dummy Search Placeholder",
+      state: "SET",
+      table: {
+        columnDefs: [
+          {
+            field: 'userName',
+            id: 'userName',
+            displayName: "Dummy userName",
+            sortable: true
+          }, {
+            field: 'csat',
+            id: 'averageCsat',
+            displayName: "Dummy Csat",
+            sortable: true
+          }
+        ]
+      },
+      title: "Dummy Title"
+    };
+
+    $scope = $rootScope.$new();
+    callbackSpy = jasmine.createSpy('getDataCallback');
+    ddController = $componentController('drilldownReport', {
+      $scope: $scope,
+      $log: $log,
+      ReportConstants: ReportConstants,
+      $timeout: $timeout,
+      CardUtils: CardUtils
+    }, {
+      props: props,
+      callback: callbackSpy,
+      gridData: []
+    });
+    $log.warn($rootScope);
+  });
+
+  it('Binding Props and caallback test', function () {
+
+    $log.warn("Controller......", ddController);
+    //props check
+    expect(ddController.props.broadcast).toBeDefined();
+    expect(ddController.props.broadcast.refresh).toBeDefined();
+    expect(ddController.props.broadcast.reset).toBeDefined();
+    expect(ddController.props.broadcast.nonExistent).toBeUndefined();
+    expect(ddController.props.description).toEqual("Dummy set Description");
+    expect(ddController.toggleDrilldownReport).toBeDefined();
+    expect(ddController.isDataEmpty()).toBe(false);
+
+    //callback check
+    ddController.callback();
+    expect(callbackSpy).toHaveBeenCalled();
+  });
+
+  //it('Refresh Testing via broadcast', function () {
+  //  ddController.toggleDrilldownReport();
+  //  var display = ddController.display();
+  //  $scope.$broadcast("DummyResetString", {});
+  //  $scope.$digest();
+  //  expect(ddController.display()).toBe(!display);
+  //  $log.warn($rootScope);
+  //});
+
+  it('Toggle Drilldown Report test', function () {
+    var display = ddController.display();
+    ddController.toggleDrilldownReport();
+    expect(ddController.display()).toBe(!display);
+  });
+
+  it('State should be set to empty when onGetDataSuccess is called with 0 records', function () {
+    ddController.onGetDataSuccess([]);
+    expect(ddController.isDataEmpty()).toBe(true);
+  });
+
+  it('State should be set to SET when onGetDataSuccess is called with >0 records', function () {
+    ddController.onGetDataSuccess([1, 2, 3]);
+    expect(ddController.isDataAvailable()).toBe(true);
+  });
+
+  it('State should be set to Errored when onGetDataError is called', function () {
+    ddController.onGetDataError();
+    expect(ddController.isDataError()).toBe(true);
+  });
+
+  it('Should trigger the data callback when toggle display is set to true', function () {
+    ddController.toggleDrilldownReport();
+    expect(callbackSpy).toHaveBeenCalled();
+  });
+
+  it('Should not trigger data callback when toggle display while data exists', function () {
+    ddController.onGetDataSuccess([1, 2, 3]);
+    ddController.toggleDrilldownReport();
+    expect(callbackSpy).not.toHaveBeenCalled();
+  });
+});
