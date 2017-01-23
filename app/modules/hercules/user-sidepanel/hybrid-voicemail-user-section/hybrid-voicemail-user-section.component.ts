b0c3709c4a34821db7de79827c5fbbeeb794a8a0
Kjetil Ree <kjeree@cisco.com>
2017-01-23 18:48:52 +0100

    feat(hercules): Adding dummy functions to UCC Service in preparation for Hybrid Voicemail support.

--- app/modules/hercules/user-sidepanel/hybrid-voicemail-user-section/hybrid-voicemail-user-section.component.ts
+++ app/modules/hercules/user-sidepanel/hybrid-voicemail-user-section/hybrid-voicemail-user-section.component.ts
@@ -1,29 +1,49 @@
+import { UCCService } from '../../services/ucc-service';
 class HybridVoicemailUserSectionCtrl implements ng.IComponentController {
 
   public callServiceConnectEnabledForUser: boolean;
+  public pilotNumber: number;
   private isEnabledOrgWide: boolean;
+  private userId: string;
 
   /* @ngInject */
-  constructor( ) {
-    console.log('component: ', this.callServiceConnectEnabledForUser);
-  }
+  constructor(
+    private UCCService: UCCService,
+  ) { }
 
   public $onInit() {
-    this.isEnabledOrgWide = this.checkEnabledOrgWide();
+    this.isEnabledOrgWide = false;
   }
 
-  public checkEnabledOrgWide(): boolean {
-    return true;
+  public $onChanges(changes: { [bindings: string]: ng.IChangesObject }): void {
+    const { callServiceConnectEnabledForUser } = changes;
+    if (callServiceConnectEnabledForUser && callServiceConnectEnabledForUser.currentValue) {
+      this.checkEnabledOrgWide();
+    }
   }
 
+  public checkEnabledOrgWide(): void {
+    this.UCCService.getOrgVoicemailConfiguration()
+      .then((data) => {
+        return this.isEnabledOrgWide = data.voicemailOrgEnableInfo.orgHybridVoicemailEnabled;
+      })
+      .then((isEnabled) => {
+        if (isEnabled) {
+          this.UCCService.getUserVoicemailInfo(this.userId)
+            .then((data) => {
+              this.pilotNumber = data.vmInfo.voicemailPilot;
+            });
+        }
+      });
+  }
 
 }
-
 
 export class HybridVoicemailUserSectionComponent implements ng.IComponentOptions {
   public controller = HybridVoicemailUserSectionCtrl;
   public templateUrl = 'modules/hercules/user-sidepanel/hybrid-voicemail-user-section/hybrid-voicemail-user-section.html';
   public bindings = {
     callServiceConnectEnabledForUser: '<',
+    userId: '<',
   };
 }
