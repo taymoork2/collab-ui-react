e4278570176a14367f9fe53b9c34a2632c611a96
Alexander Eid <haneid@cisco.com>
2017-01-24 12:55:21 +0100

    feat(Hercules): Voicemail on Hybrid Call Settings Page

--- app/modules/hercules/service-settings/voicemail-settings-section/voicemail-settings.component.ts
+++ app/modules/hercules/service-settings/voicemail-settings-section/voicemail-settings.component.ts
@@ -1,20 +1,63 @@
 require('./_voicemail-settings.scss');
 
 class HybridVoicemailCtrl implements ng.IComponentController {
-
+  public voicemailSection = {
+    title: 'common.voicemail',
+  };
   public isCallServiceConnectEnabled: boolean;
+  private voicemail: boolean;
+  private OrgId: string;
 
   /* @ngInject */
   constructor(
+    private Notification,
+    private UCCService,
+    private Authinfo,
   ) {}
 
   public $onInit() {
+    this.OrgId=this.Authinfo.getOrgId()
+    this.UCCService.getOrgVoicemailConfiguration(this.OrgId).then((data)=>{
+      switch(data.orgVoicemailStatus) {
+        case 'ENABLED':
+          this.voicemail=true;
+        case 'DISABLED':
+          this.voicemail=false;
+        default:
+            this.voicemail=true;
+      }
+    }).catch((data)=>{
+      console.log(data)
+    })
 
-  }
+
+    }
 
   public $onChanges() {
 
   }
+
+  public change = () => {
+   console.log('change')
+    this.Notification.success('hercules.settings.voicemail.successDescription');
+  }
+
+  public deactivateVoicemail =  _.debounce(value => {
+      if (value === false) {
+      this.UCCService.setOrgVoicemailConfiguration(true, this.OrgId).then(() => {
+        this.Notification.success('hercules.settings.voicemail.enableDescription');
+      });
+      } else {
+        this.UCCService.setOrgVoicemailConfiguration(false, this.OrgId).then(() => {
+          this.Notification.success('hercules.settings.voicemail.disableDescription');
+        })
+      }
+    }, 2000, {
+      leading: true,
+      trailing: false,
+    });
+
+
 
 }
 
