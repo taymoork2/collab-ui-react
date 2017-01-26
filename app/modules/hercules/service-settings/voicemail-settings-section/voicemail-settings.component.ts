7678b6b84af1ed8c12a3e39757e4e0993b0e6904
Alexander Eid <haneid@cisco.com>
2017-01-26 14:29:32 +0100

    feat(Hercules): Voicemail on Hybrid Call Settings Page

--- app/modules/hercules/service-settings/voicemail-settings-section/voicemail-settings.component.ts
+++ app/modules/hercules/service-settings/voicemail-settings-section/voicemail-settings.component.ts
@@ -16,15 +16,17 @@ class HybridVoicemailCtrl implements ng.IComponentController {
     private Notification: Notification,
     private UCCService: UCCService,
     private Authinfo,
-) {}
+  ) {}
 
   public $onInit() {
     this.orgId = this.Authinfo.getOrgId();
-    this.UCCService.getOrgVoicemailConfiguration(this.orgId).then((data) => {
+    this.UCCService.getOrgVoicemailConfiguration(this.orgId)
+      .then((data) => {
       this.voicemailToggle = data.voicemailOrgEnableInfo.orgHybridVoicemailEnabled;
-        }).catch((response) => {
-      this.Notification.errorWithTrackingId(response, 'hercules.settings.voicemail.voicemailStatusError');
-        });
+      })
+      .catch((response) => {
+        this.Notification.errorWithTrackingId(response, 'hercules.settings.voicemail.voicemailStatusError');
+      });
   }
 
   public $onChanges(changes: {[bindings: string]: ng.IChangesObject}) {
@@ -34,19 +36,22 @@ class HybridVoicemailCtrl implements ng.IComponentController {
     }
   }
 
-public toggleVoicemail = (enableVoiceMail: boolean) => {
-  if (!enableVoiceMail) {
-    this.UCCService.enableHybridVoicemail(this.orgId).then(() => {
-      this.Notification.success('hercules.settings.voicemail.enableDescription');
-    }).catch((response) => {
-      this.Notification.errorWithTrackingId(response, 'hercules.settings.voicemail.voicemailEnableError');
-    });
+  public toggleVoicemail = (enableVoiceMail: boolean) => {
+    if (!enableVoiceMail) {
+      this.UCCService.enableHybridVoicemail(this.orgId)
+        .then(() => {
+          this.Notification.success('hercules.settings.voicemail.enableDescription');
+        })
+        .catch((response) => {
+          this.Notification.errorWithTrackingId(response, 'hercules.settings.voicemail.voicemailEnableError');
+        });
     } else {
-      this.UCCService.disableHybridVoicemail(this.orgId).then(() => {
-        this.Notification.success('hercules.settings.voicemail.disableDescription');
-      }).catch((response) => {
-        this.Notification.errorWithTrackingId(response, 'hercules.settings.voicemail.voicemailDisableError');
-      });
+      this.UCCService.disableHybridVoicemail(this.orgId)
+        .then(() => {
+          this.Notification.success('hercules.settings.voicemail.disableDescription');
+        }).catch((response) => {
+          this.Notification.errorWithTrackingId(response, 'hercules.settings.voicemail.voicemailDisableError');
+        });
     }
   }
 }
