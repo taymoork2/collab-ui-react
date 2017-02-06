3a22bc1655cd1c1482c3168cf88f1e4a73e256bd
Chakravarthy Ravichandran <charavic@cisco.com>
2017-02-06 10:09:38 -0800

    feat(PlacesSetting) - Removed PreferredLanugage field from Places Overview
    - Removed new field logic and handlers from placeOverview.component.ts
    - Removed new field html components from placeOverview.html
    - Removed placeOverview.component.ts reference from index.ts

--- app/modules/squared/places/overview/index.ts
+++ app/modules/squared/places/overview/index.ts
@@ -1,9 +1,7 @@
 import './placeOverview.scss';
 
 import { PlaceOverviewComponent } from './placeOverview.component';
-import { PlaceCallOverviewService } from '../callOverview/placeCallOverviewService.service';
 
 export default angular
   .module('Squared')
-  .component('placeOverview', new PlaceOverviewComponent())
-  .service('PlaceCallOverviewService', PlaceCallOverviewService);
+  .component('placeOverview', new PlaceOverviewComponent());
