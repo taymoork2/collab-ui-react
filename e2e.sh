4a5aee2dbba5e58fbe47040993c52fb42b5b98de
Mike Park <mipark2@cisco.com>
2017-02-17 18:13:16 -0800

    chore(build): allow 'e2e.sh' to run with '--specs=...' arg
    chore(build): shellcheck lint cleanup for 'e2e.sh'

--- e2e.sh
+++ e2e.sh
@@ -22,20 +22,29 @@ kill_wait "lite-server"
 
 npm run serve-dist -- --noopen &
 # poll on web webserver at root context until it comes up
-while [ $( curl_http_status "http://127.0.0.1:8000/" ) -ne 200 ]; do
+while [ "$( curl_http_status "http://127.0.0.1:8000/" )" -ne 200 ]; do
     sleep 2;
 done
 
 # update webdriver
 npm run webdriver
 
-# Run a different test suite if this is a nightly build (and run against integration)
-if [ $NIGHTLY ]; then
-    test_suite="--specs=test/e2e-protractor/nightly/*_spec.js"
-    export E2E_RUN_COUNTER_MAX=0
-else
-    test_suite="--production-backend --suite jenkins"
-fi
+function get_test_suite_arg {
+    # check if a '--specs=...' arg was passed to this script
+    local specs_arg="$1"
+
+    # Run a different test suite if this is a nightly build (and run against integration)
+    if [[ "$NIGHTLY" == "true" ]]; then
+        echo "--specs=test/e2e-protractor/nightly/*_spec.js"
+        export E2E_RUN_COUNTER_MAX=0
+    elif [[ "$specs_arg" == "--specs="* ]]; then
+        echo "--production-backend $specs_arg"
+    else
+        echo "--production-backend --suite jenkins"
+    fi
+}
+set -x
+test_suite="$(get_test_suite_arg "$@")"
 
 # notes:
 # - run e2e directly to acquire exit status (npm wraps any non-zero status and re-exits with 1)
@@ -43,7 +52,9 @@ fi
 #   - 1 => tests failed (sauce link available)
 #   - 100 => protractor terminated abnormally (no sauce link available)
 # - write exit status to temp file (used later to determine whether to set the build 'UNSTABLE')
+# shellcheck disable=SC2086
 node ./protractor/e2e ${test_suite} --sauce
+set +x
 e2e_exit_code=$?
 echo "$e2e_exit_code" > "${WX2_ADMIN_WEB_CLIENT_HOME}/.cache/${E2E_EXIT_CODE_FILE}"
 
