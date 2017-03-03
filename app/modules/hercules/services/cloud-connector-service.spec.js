c4760ebd05b585b05875e03566a6970d7d221c8d
Kjetil Ree <kjeree@cisco.com>
2017-03-03 16:40:29 +0100

    feat(hercules): Support for an ACL admin account for Google Calendar.
    https://jira-eng-chn-sjc1.cisco.com/jira/browse/ATLAS-1774

--- app/modules/hercules/services/cloud-connector-service.spec.js
+++ app/modules/hercules/services/cloud-connector-service.spec.js
@@ -2,19 +2,20 @@
 
 describe('Service: CloudConnectorService', function () {
 
-  var CloudConnectorService;
+  var $httpBackend, CloudConnectorService;
 
   beforeEach(angular.mock.module('Hercules'));
   beforeEach(angular.mock.module(mockDependencies));
   beforeEach(inject(dependencies));
 
-  function dependencies(_CloudConnectorService_) {
+  function dependencies(_CloudConnectorService_, _$httpBackend_) {
     CloudConnectorService = _CloudConnectorService_;
+    $httpBackend = _$httpBackend_;
   }
 
   function mockDependencies($provide) {
     var Authinfo = {
-      getOrgId: sinon.stub().returns('fe5acf7a-6246-484f-8f43-3e8c910fc50d'),
+      getOrgId: sinon.stub().returns('myOrgId'),
       isFusionGoogleCal: sinon.stub().returns(true),
     };
     $provide.value('Authinfo', Authinfo);
@@ -30,5 +31,52 @@ describe('Service: CloudConnectorService', function () {
       expect(CloudConnectorService.getStatusCss({ provisioned: true, status: 'ERROR' })).toBe('danger');
       expect(CloudConnectorService.getStatusCss({ provisioned: true, status: 'WARN' })).toBe('warning');
     });
+  });
+
+  describe('.updateConfig()', function () {
+
+    var serviceId = 'squared';
+
+    afterEach(function () {
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('should post the service account, ACL account, and private key to the correct API', function () {
+      var inputData = {
+        newServiceAccountId: 'test@example.org',
+        newAclAccount: 'acl@example.org',
+        privateKey: 'header,actualKeyData',
+        serviceId: serviceId,
+      };
+      var dataToBeSentToServer = {
+        serviceAccountId: 'test@example.org',
+        aclAdminAccount: 'acl@example.org',
+        privateKeyData: 'actualKeyData',
+      };
+      $httpBackend.expectPOST('https://calendar-cloud-connector-integration.wbx2.com/api/v1/orgs/myOrgId/services/squared', dataToBeSentToServer).respond({});
+      $httpBackend.expectPATCH('https://hercules-integration.wbx2.com/v1/organizations/myOrgId/services/squared').respond({});
+      CloudConnectorService.updateConfig(inputData.newServiceAccountId, inputData.newAclAccount, inputData.privateKey, inputData.serviceId);
+      $httpBackend.flush();
+    });
+
+    it('should post an empty ACL account if an empty string is provided, because doing so is supposed to clear it server-side', function () {
+      var inputData = {
+        newServiceAccountId: 'test@example.org',
+        newAclAccount: '',
+        privateKey: 'header,actualKeyData',
+        serviceId: serviceId,
+      };
+      var dataToBeSentToServer = {
+        serviceAccountId: 'test@example.org',
+        aclAdminAccount: '',
+        privateKeyData: 'actualKeyData',
+      };
+      $httpBackend.expectPOST('https://calendar-cloud-connector-integration.wbx2.com/api/v1/orgs/myOrgId/services/squared', dataToBeSentToServer).respond({});
+      $httpBackend.expectPATCH('https://hercules-integration.wbx2.com/v1/organizations/myOrgId/services/squared').respond({});
+      CloudConnectorService.updateConfig(inputData.newServiceAccountId, inputData.newAclAccount, inputData.privateKey, inputData.serviceId);
+      $httpBackend.flush();
+    });
+
   });
 });
