0d052dea8de49ecb14ae1ba29481bf326a32645c
Paul Clark <aggie@cisco.com>
2017-03-13 16:02:10 -0500

    fix(emerencyService) Show Zip/Postal code label based on 'countryCode'
    TERMINUS-19: UX issues with Zip and Postal Code in Atlas UI

--- app/modules/huron/serviceAddress/serviceAddress.component.ts
+++ app/modules/huron/serviceAddress/serviceAddress.component.ts
@@ -2,7 +2,8 @@ class ServiceAddressCtrl implements ng.IComponentController {
   public stateOptions;
   public countryOptions;
   public countryCode: string;
-  public areaName: string;
+  public stateLabel: string;
+  public zipLabel: string;
   public locationModel;
   public address;
 
@@ -12,7 +13,8 @@ class ServiceAddressCtrl implements ng.IComponentController {
   public $onInit() {
     this.locationModel = undefined;
     this.PstnSetupStatesService.getLocation(this.countryCode).then(location => {
-      this.areaName = location.type;
+      this.zipLabel = location.zip;
+      this.stateLabel = location.type;
       if (this.address.state) {
         this.locationModel = location.areas.filter(state => state.abbreviation === this.address.state)[0];
       }
