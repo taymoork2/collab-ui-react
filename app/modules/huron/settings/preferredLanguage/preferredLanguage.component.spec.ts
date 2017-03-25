096a9e3a028b65053c025cd2bf867d4c57b6cc1b
Jeff Lowery <jlowery@cisco.com>
2017-03-25 08:16:42 -0500

    refactor(huron): Added more unit tests for settings refactor.
    - dateFormat component
    - timeFormat component
    - timeZone component
    - preferredLanguage component
    - defaultCountry component
    - outboundDialDigit component
    - extensionLength component

---
+++ app/modules/huron/settings/preferredLanguage/preferredLanguage.component.spec.ts
@@ -0,0 +1,42 @@
+import preferredLanguageModule from './index';
+
+describe('Component: preferredLanguage', () => {
+  const PREFERRED_LANG_SELECT = '.csSelect-container[name="preferredLanguage"]';
+  const DROPDOWN_OPTIONS = '.dropdown-menu ul li a';
+  const LANG_ENGLISH = 'en_US';
+  const LANG_SVENSKA = 'sv_SE';
+  const preferredLanguageOptions = getJSONFixture('huron/json/settings/languages.json');
+
+  beforeEach(function() {
+    this.initModules(preferredLanguageModule);
+    this.injectDependencies(
+      '$scope',
+    );
+
+    this.$scope.onChangeFn = jasmine.createSpy('onChangeFn');
+
+    this.compileComponent('ucPreferredLanguage', {
+      preferredLanguage: 'preferredLanguage',
+      preferredLanguageOptions: 'preferredLanguageOptions',
+      onChangeFn: 'onChangeFn(preferredLanguage)',
+    });
+
+    this.$scope.preferredLanguage = LANG_ENGLISH;
+    this.$scope.preferredLanguageOptions = preferredLanguageOptions;
+    this.$scope.$apply();
+  });
+
+  it('should have a select box with options', function() {
+    expect(this.view).toContainElement(PREFERRED_LANG_SELECT);
+    expect(this.view.find(PREFERRED_LANG_SELECT).find(DROPDOWN_OPTIONS).get(0)).toHaveText('English (United States)');
+    expect(this.view.find(PREFERRED_LANG_SELECT).find(DROPDOWN_OPTIONS).get(1)).toHaveText('French (Canadian)');
+    expect(this.view.find(PREFERRED_LANG_SELECT).find(DROPDOWN_OPTIONS).get(2)).toHaveText('Español (España)');
+    expect(this.view.find(PREFERRED_LANG_SELECT).find(DROPDOWN_OPTIONS).get(3)).toHaveText('Svenska');
+  });
+
+  it('should invoke onChangeFn when a language is chosen', function() {
+    this.view.find(PREFERRED_LANG_SELECT).find(DROPDOWN_OPTIONS).get(3).click();
+    expect(this.$scope.onChangeFn).toHaveBeenCalledWith(LANG_SVENSKA);
+  });
+
+});
