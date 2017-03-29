ec8a86fd354bfad8d78fb8cd8e0c6dbeeeafe3a7
Alina Gendel <algendel@cisco.com>
2017-03-29 09:09:29 -0700

    feat(trials): Add sorting to webex timezones

--- app/modules/core/trials/trialWebex.controller.spec.js
+++ app/modules/core/trials/trialWebex.controller.spec.js
@@ -5,13 +5,29 @@
 describe('Controller: Trial Webex', function () {
   var controller, scope;
   var trialData = getJSONFixture('core/json/trials/trialData.json');
+  var timeZoneData = [
+    { 'label': '(GMT +07:00) Jakarta' },
+    { 'label': '(GMT +04:00) Yerevan' },
+    { 'label': '(GMT +00:00) Reykjavik' },
+    { 'label': '(GMT -01:00) Azores' },
+    { 'label': '(GMT +02:00) Bucharest' },
+    { 'label': '(GMT -05:00) Toronto' },
+  ];
+  var sortedTimeZoneData = [
+    { 'label': '(GMT -05:00) Toronto' },
+    { 'label': '(GMT -01:00) Azores' },
+    { 'label': '(GMT +00:00) Reykjavik' },
+    { 'label': '(GMT +02:00) Bucharest' },
+    { 'label': '(GMT +04:00) Yerevan' },
+    { 'label': '(GMT +07:00) Jakarta' },
+  ];
 
   afterEach(function () {
     controller = scope = undefined;
   });
 
   afterAll(function () {
-    trialData = undefined;
+    trialData = timeZoneData = sortedTimeZoneData = undefined;
   });
 
   beforeEach(angular.mock.module('core.trial'));
@@ -47,6 +63,7 @@ describe('Controller: Trial Webex', function () {
 
     spyOn(Orgservice, 'getOrg');
     spyOn(Analytics, 'trackTrialSteps');
+    spyOn(TrialTimeZoneService, 'getTimeZones').and.returnValue(timeZoneData);
 
     initController();
   });
@@ -82,4 +99,17 @@ describe('Controller: Trial Webex', function () {
       });
     $rootScope.$apply(); // flush pending promises
   });
+
+  it('should extract timezone offset value and covert it to bool-like string', function () {
+    expect(controller._helpers.getNumericPortion('(GMT -07:20) Jakarta')).toBe(-720);
+    expect(controller._helpers.getNumericPortion('(GMT -07:00) Jakarta')).toBe(-700);
+    expect(controller._helpers.getNumericPortion('(GMT +05:00) Jakarta')).toBe(500);
+    expect(controller._helpers.getNumericPortion('(GMTjibbersh:00) Jakarta')).toBe(0);
+  });
+
+  it('should sort the timezone list correctly', function () {
+    var result = controller.getTimeZones();
+    expect(result).toEqual(sortedTimeZoneData);
+  });
+
 });
