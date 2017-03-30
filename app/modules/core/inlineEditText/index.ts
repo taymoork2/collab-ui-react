c603ed398e678e582b5fa2e2db8d295f92b1db56
Brian Spence <brspence@cisco.com>
2017-03-30 11:49:13 -0500

    refactor(inlineEditText): import modular toolkit dependency

--- app/modules/core/inlineEditText/index.ts
+++ app/modules/core/inlineEditText/index.ts
@@ -1,12 +1,13 @@
 import './inlineEditText.scss';
 import focusModule from 'modules/core/focus';
+import csInput from 'collab-ui-ng/dist/directives/input';
 
 import { InlineEditTextComponent } from './inlineEditText.component';
 
 export default angular
   .module('core.inline-edit-text', [
     require('scripts/app.templates'),
-    require('collab-ui-ng').default,
+    csInput,
     focusModule,
   ])
   .component('crInlineEditText', new InlineEditTextComponent())
