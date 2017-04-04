993298e326f446fac33d523a3c1a0ede813fa125
Alina Gendel <algendel@cisco.com>
2017-04-04 15:18:59 -0700

    feat(propack): Create Feature Toggles and a service for the ITPropack work

--- app/modules/core/featureToggle/features.config.js
+++ app/modules/core/featureToggle/features.config.js
@@ -15,6 +15,8 @@ module.exports = {
   atlasHelpDeskExt: 'atlas-helpdesk-extended-information',
   atlasHelpDeskOrderSearch: 'atlas-helpdesk-order-search',
   atlasHybridServicesResourceList: 'atlas-hybrid-services-resource-list',
+  atlasITProPack: 'atlas-it-pro-pack',
+  atlasITProPackPurchased: 'atlas-it-pro-pack-purchased',
   atlasIdleLogout: 'atlas-idle-logout',
   atlasMediaServiceMetrics: 'atlas-media-service-metrics',
   atlasMediaServiceMetricsMilestoneOne: 'atlas-media-service-metrics-milestone-one',
