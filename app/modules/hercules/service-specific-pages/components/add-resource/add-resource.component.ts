30b536cdb3de0801adc8e42a3353705685c41be7
Tom Vasset <tvasset@cisco.com>
2017-05-19 12:56:30 +0200

    feat(hercules): Tweaks to the partner setup of Hybrid Services

--- app/modules/hercules/service-specific-pages/components/add-resource/add-resource.component.ts
+++ app/modules/hercules/service-specific-pages/components/add-resource/add-resource.component.ts
@@ -4,7 +4,7 @@ class AddResourceComponentCtrl implements ng.IComponentController {
 
   private modalWindowOptions: any;
   private isPartnerAdmin = false;
-  private hasPartnerRegistrationFeatureToggle = false;
+  private allowPartnerRegistration: boolean;
 
   /* @ngInject */
   constructor(
@@ -18,14 +18,17 @@ class AddResourceComponentCtrl implements ng.IComponentController {
     if (this.Authinfo.isCustomerLaunchedFromPartner()) {
       this.isPartnerAdmin = true;
     }
-    this.FeatureToggleService.supports(this.FeatureToggleService.features.atlasHybridPartnerRegistration)
-      .then(enabled => {
-        this.hasPartnerRegistrationFeatureToggle = enabled;
-      });
+    if (this.allowPartnerRegistration) {
+      // Also check the feature toggle
+      this.FeatureToggleService.supports(this.FeatureToggleService.features.atlasHybridPartnerRegistration)
+        .then(enabled => {
+          this.allowPartnerRegistration = enabled;
+        });
+    }
   }
 
   public openAddResourceModal = () => {
-    if (this.isPartnerAdmin && !this.hasPartnerRegistrationFeatureToggle) {
+    if (this.isPartnerAdmin && !this.allowPartnerRegistration) {
       this.$modal.open({
         templateUrl: 'modules/hercules/service-specific-pages/components/add-resource/partnerAdminWarning.html',
         type: 'dialog',
@@ -47,6 +50,7 @@ class AddResourceComponent implements ng.IComponentOptions {
   public templateUrl = 'modules/hercules/service-specific-pages/components/add-resource/add-resource-button.html';
   public bindings = {
     modalWindowOptions: '<',
+    allowPartnerRegistration: '<',
   };
 }
 
