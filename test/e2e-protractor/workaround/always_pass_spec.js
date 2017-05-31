96099f5016bc399f5e6661fe5cab80ac368f0287
Brian Spence <brspence@cisco.com>
2017-05-31 14:27:41 -0500

    Revert "test(sauce): workaround protractor error with sauce"
    This reverts commit 217376572db59b32719126eb54c9a7da3da4bb5c.

--- test/e2e-protractor/workaround/always_pass_spec.js
+++
@@ -1,5 +0,0 @@
-// Sauce Labs Ticket 39647 - individual spec exits with error code 100
-// TODO: temporary - need to run more than one spec at a time
-describe('protractor sauce workaround', function () {
-  it('always pass', _.noop);
-});
