e23a5ebb7d50f374c9530284c7ec24267d4d4593
Brian Spence <brspence@cisco.com>
2017-06-19 11:10:57 -0500

    test(e2e): remove verbose logging from manageusers-csv_spec.js

--- test/e2e-protractor/squared/manageusers-csv_spec.js
+++ test/e2e-protractor/squared/manageusers-csv_spec.js
@@ -8,23 +8,13 @@ describe('Manage Users - CSV File -', function () {
   var CSV_FILE_NAME = 'DELETE_DO_NOT_CHECKIN_onboard_csv_test_file.csv';
   var CSV_FILE_PATH = utils.resolvePath('./../data/' + CSV_FILE_NAME);
   var userList;
-  var originalLogVerbose;
 
   beforeAll(function (done) {
-    originalLogVerbose = log.verbose;
-    // TODO: remove verbose logging after monitoring sauce runs through CI job
-    log.verbose = true;
-
     users.createCsvAndReturnUsers(CSV_FILE_PATH).then(function (_userList) {
       userList = _userList;
     }).catch(function () {
       fail('failed to create csv file');
     }).finally(done);
-  });
-
-  afterAll(function () {
-    log.verbose = originalLogVerbose;
-    originalLogVerbose = undefined;
   });
 
   // Given an email alias, find user and confirm entitlements set
