78c2d61069585e36cdd319d8d3e0e7ab75daf70a
Helge Langehaug <hellange@cisco.com>
2017-06-19 11:22:32 +0200

    WIP(cmc): FIx problem with scrollbar in user list
    F7591

--- app/modules/cmc/status/cmc-details-status.component.ts
+++ app/modules/cmc/status/cmc-details-status.component.ts
@@ -14,6 +14,7 @@ class CmcDetailsStatusComponentCtrl implements ng.IComponentController {
   public filters: any[];
   private searchStr: string;
   private nextUrl: string;
+  public gridApi;
 
   /* @ngInject */
   constructor(
@@ -26,11 +27,10 @@ class CmcDetailsStatusComponentCtrl implements ng.IComponentController {
     private $templateCache,
     private $filter: ng.IFilterService,
     private $scope: ng.IScope,
+    private $window,
   ) {
     this.orgId = this.Authinfo.getOrgId();
     this.orgName = this.Authinfo.getOrgName();
-
-
     this.statTemplate = this.$templateCache.get('modules/cmc/status/statColumn.tpl.html');
 
     // TODO Add this and also  errors when CMC provides more info
@@ -48,9 +48,20 @@ class CmcDetailsStatusComponentCtrl implements ng.IComponentController {
     this.userStatuses = this.$filter('filter')(this.fetchedUserStatuses, { displayName: this.searchStr });
   }
 
+  public updateGridAtResize = () => {
+    this.gridApi.core.handleWindowResize();
+  }
+
+  public $onDestroy() {
+    angular.element(this.$window).off('resize');
+  }
+
   public $onInit() {
 
     this.initGrid();
+
+    angular.element(this.$window).on('resize', _.debounce(this.updateGridAtResize, 100));
+
     this.CmcService.preCheckOrg(this.Authinfo.getOrgId())
       .then((res: ICmcOrgStatusResponse) => {
         this.$log.info('Result from preCheckOrg:', res);
@@ -67,6 +78,7 @@ class CmcDetailsStatusComponentCtrl implements ng.IComponentController {
 
     this.fetchUserStatuses(100).then( () => {
       this.CmcUserService.insertUserDisplayNames(this.userStatuses);
+      this.updateGridAtResize();
     });
   }
 
@@ -98,7 +110,7 @@ class CmcDetailsStatusComponentCtrl implements ng.IComponentController {
       width: '35%',
       sortable: true,
       field: 'displayName',
-      displayName: 'User Name',
+      displayName: this.$translate.instant('cmc.statusPage.table.username'),
     }, {
       width: '30%',
       sortable: true,
@@ -111,7 +123,7 @@ class CmcDetailsStatusComponentCtrl implements ng.IComponentController {
       width: '35%',
       sortable: true,
       field: 'lastStatusUpdate',
-      displayName: 'Last Updated',
+      displayName: this.$translate.instant('cmc.statusPage.table.lastUpdated'),
     }];
 
     this.gridOptions = {
@@ -138,6 +150,7 @@ class CmcDetailsStatusComponentCtrl implements ng.IComponentController {
     gridApi.infiniteScroll.on.needLoadMoreDataTop(this.$scope, () => {
       this.$log.debug('needLoadMoreDataTop');
     });
+    this.gridApi = gridApi;
     this.$log.debug('gridApi', gridApi);
   }
 }
