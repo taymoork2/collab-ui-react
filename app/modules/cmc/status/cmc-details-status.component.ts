83865d99244122eece40999f914055bdaf2b6e05
Helge Langehaug <hellange@cisco.com>
2017-06-20 10:44:44 +0200

    WIP(cmc): search for username instead of displayname
    F7591

--- app/modules/cmc/status/cmc-details-status.component.ts
+++ app/modules/cmc/status/cmc-details-status.component.ts
@@ -46,7 +46,7 @@ class CmcDetailsStatusComponentCtrl implements ng.IComponentController {
     this.$log.debug('searchStr', searchStr );
     this.searchStr = searchStr;
     this.$log.debug('user statuses', this.fetchedUserStatuses);
-    this.userStatuses = this.$filter('filter')(this.fetchedUserStatuses, { displayName: this.searchStr });
+    this.userStatuses = this.$filter('filter')(this.fetchedUserStatuses, { userName: this.searchStr });
   }
 
   public updateGridAtResize = () => {
@@ -164,6 +164,11 @@ class CmcDetailsStatusComponentCtrl implements ng.IComponentController {
     gridApi.infiniteScroll.on.needLoadMoreDataTop(this.$scope, () => {
       this.$log.debug('needLoadMoreDataTop');
     });
+
+    // gridApi.selection.on.rowSelectionChanged(this.$scope, (row) => {
+    //   this.$log.warn('click XXXXXX click ', row)
+    // });
+
     this.gridApi = gridApi;
     this.$log.debug('gridApi', gridApi);
   }
