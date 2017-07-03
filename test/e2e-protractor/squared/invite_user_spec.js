c1e4fbdd129b3c58876ab5db8865d177f02cb54f
Brian Spence <brspence@cisco.com>
2017-07-03 13:48:22 -0500

    Revert "test(e2e): disable invite_user_spec.js"
    This reverts commit 06f849b7c39b86ed3f62ce1b4b8d1b83cc810ef8.

--- test/e2e-protractor/squared/invite_user_spec.js
+++ test/e2e-protractor/squared/invite_user_spec.js
@@ -1,8 +1,8 @@
 'use strict';
 
 /* globals manageUsersPage */
-// TODO re-enable after backend is fixed in prod
-xdescribe('Squared Add User Flow', function () {
+
+describe('Squared Add User Flow', function () {
   var token;
   var inviteEmail, inviteEmail2;
 
