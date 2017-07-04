739104aee138aa69fed1543ea062333609c3d039
Brian Spence <brspence@cisco.com>
2017-07-03 23:14:18 -0500

    chore(types): convert typings to types

---
+++ typings/custom/global-shims.d.ts
@@ -0,0 +1,9 @@
+import * as __ from 'lodash';
+import * as _angular from 'angular';
+import * as _moment from 'moment';
+
+declare global {
+  const _: typeof __;
+  const angular: typeof _angular;
+  const moment: typeof _moment;
+}
