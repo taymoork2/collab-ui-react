7c8d4d063f52b158f2e0c4cd598de10cf31e4914
Brian Spence <brspence@cisco.com>
2017-07-10 11:36:54 -0500

    fix(angular): disable reporting of unhandled rejections
    - https://github.com/christopherthielen/ui-router-extras/issues/356

--- app/bootstrap.dependencies.js
+++ app/bootstrap.dependencies.js
@@ -2,8 +2,12 @@ require('jquery');
 require('angular');
 
 angular.module('ng')
-  .config(/* @ngInject */ function ($locationProvider) {
+  .config(/* @ngInject */ function ($locationProvider, $qProvider) {
     // TODO: migrate towards html5 mode. Currently maintaining backwards compatibility
     // https://rally1.rallydev.com/#/detail/userstory/133819728820
     $locationProvider.hashPrefix('');
+    // TODO: Revisit temporarily disabling 1.6 feature of reporting unhandled rejections
+    // https://github.com/christopherthielen/ui-router-extras/issues/356
+    // https://rally1.rallydev.com/#/detail/userstory/133821369776
+    $qProvider.errorOnUnhandledRejections(false);
   });
