b771dcec3990a87616ba29389fc9e77be41b8e9a
Brian Spence <brspence@cisco.com>
2017-07-10 11:36:00 -0500

    fix(angular): set default hash-prefix
    - https://docs.angularjs.org/guide/migration#-location-

--- app/bootstrap.dependencies.js
+++ app/bootstrap.dependencies.js
@@ -1,2 +1,9 @@
 require('jquery');
 require('angular');
+
+angular.module('ng')
+  .config(/* @ngInject */ function ($locationProvider) {
+    // TODO: migrate towards html5 mode. Currently maintaining backwards compatibility
+    // https://rally1.rallydev.com/#/detail/userstory/133819728820
+    $locationProvider.hashPrefix('');
+  });
