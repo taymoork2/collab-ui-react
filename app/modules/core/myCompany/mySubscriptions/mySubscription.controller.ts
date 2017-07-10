taymoork2/collab-ui-react

commit e05519aae0d6da59cd6d894cec3beecca48aec54

feat(core): US17605 - Pro Pack updates for Subscriptions page

app/modules/core/myCompany/mySubscriptions/mySubscription.controller.ts
@@ -1,14 +1,14 @@
 import { DigitalRiverService } from 'modules/online/digitalRiver/digitalRiver.service';
 import { Notification } from 'modules/core/notifications';
 import { OnlineUpgradeService, IBmmpAttr, IProdInst } from 'modules/online/upgrade/upgrade.service';
-import { SharedMeetingsReportService } from './sharedMeetings/sharedMeetingsReport.service';
 import { IOfferData, IOfferWrapper, ISubscription, ISubscriptionCategory } from './subscriptionsInterfaces';
 import * as moment from 'moment';
 import { HybridServicesUtilsService } from 'modules/hercules/services/hybrid-services-utils.service';
 import { ServiceDescriptorService } from 'modules/hercules/services/service-descriptor.service';
+import { ProPackService } from 'modules/core/proPack/proPack.service';
 
 export class MySubscriptionCtrl {
-  public hybridServices: any[] = [];
+  public hybridServices: string[] = [];
   public licenseCategory: ISubscriptionCategory[] = [];
   public subscriptionDetails: ISubscription[] = [];
   public visibleSubscriptions: boolean = false;
@@ -17,13 +17,17 @@ export class MySubscriptionCtrl {
   public productInstanceFailed: boolean = false;
   public loading: boolean = false;
   public digitalRiverSubscriptionsUrl: string;
-  public isSharedMeetingsReportsEnabled: boolean;
-  public temporarilyOverrideSharedMeetingsReportsFeatureToggle = { default: false, defaultValue: true };
   public bmmpAttr: IBmmpAttr;
   public licenseSummary: string;
   public subExpiration: string;
   public oneOnlineSub: boolean = false;
   public showSingleSub: boolean = false;
+  public isSharedMeetingsLicense: boolean;
+  public isProPackPurchased: boolean;
+
+  public proPackData: IOfferData;
+  public proPackList: string[] = ['subscriptions.hybridDataSecurity', 'subscriptions.advancedReporting', 'subscriptions.complianceFunctionality'];
+  public premiumTooltip: string = this.$translate.instant('subscriptions.premiumTooltip');
 
   private readonly BASE_CATEGORY: ISubscriptionCategory = {
     offers: [],
@@ -61,20 +65,22 @@ export class MySubscriptionCtrl {
     private $http: ng.IHttpService,
     private $rootScope: ng.IRootScopeService,
     private $translate: ng.translate.ITranslateService,
-    private $window: ng.IWindowService,
     private Authinfo,
     private Config,
     private DigitalRiverService: DigitalRiverService,
-    private FeatureToggleService,
     private HybridServicesUtilsService: HybridServicesUtilsService,
     private Notification: Notification,
     private OnlineUpgradeService: OnlineUpgradeService,
     private Orgservice,
+    private ProPackService: ProPackService,
     private ServiceDescriptorService: ServiceDescriptorService,
-    private SharedMeetingsReportService: SharedMeetingsReportService,
     private UrlConfig,
     private WebExUtilsFact,
   ) {
+    this.ProPackService.hasProPackPurchased().then((isProPackPurchased: boolean): void => {
+      this.isProPackPurchased = isProPackPurchased;
+    });
+
     _.forEach(this.SUBSCRIPTION_TYPES, (_value, key: string): void => {
       const category: ISubscriptionCategory = _.cloneDeep(this.BASE_CATEGORY);
       category.label = $translate.instant('subscriptions.' + key);
@@ -84,17 +90,6 @@ export class MySubscriptionCtrl {
 
     this.hybridServicesRetrieval();
     this.subscriptionRetrieval();
-    this.initFeatures();
-  }
-
-  public hideUsage(offer: IOfferData): boolean {
-    return !_.isUndefined(offer.isCI) && !offer.isCI;
-  }
-
-  public nonCISignIn(offer: IOfferData): void {
-    if (offer.siteUrl) {
-      this.$window.open(this.WebExUtilsFact.getSiteAdminUrl(offer.siteUrl), '_blank');
-    }
   }
 
   public showCategory(category: ISubscriptionCategory): boolean {
@@ -107,28 +102,6 @@ export class MySubscriptionCtrl {
 
   public isUsageDefined(usage?: number): boolean {
     return _.isNumber(usage);
-  }
-
-  public isSharedMeetingsLicense(offer: IOfferData): boolean {
-    return _.toLower(_.get(offer, 'licenseModel', '')) === this.Config.licenseModel.cloudSharedMeeting;
-  }
-
-  public determineLicenseType(offer: IOfferData): string {
-    return this.isSharedMeetingsLicense(offer) ? this.$translate.instant('firstTimeWizard.sharedLicenses') : this.$translate.instant('firstTimeWizard.namedLicenses');
-  }
-
-  public launchSharedMeetingsLicenseUsageReport(siteUrl: string): void {
-    this.SharedMeetingsReportService.openModal(siteUrl);
-  }
-
-  private initFeatures(): void {
-    if (this.temporarilyOverrideSharedMeetingsReportsFeatureToggle.default) {
-      this.isSharedMeetingsReportsEnabled = this.temporarilyOverrideSharedMeetingsReportsFeatureToggle.defaultValue;
-    } else {
-      this.FeatureToggleService.atlasSharedMeetingsReportsGetStatus().then((sharedMeetingReportsStatus) => {
-        this.isSharedMeetingsReportsEnabled = sharedMeetingReportsStatus;
-      });
-    }
   }
 
   private upgradeTrialUrl(subId: string): ng.IPromise<any> {
@@ -170,10 +143,10 @@ export class MySubscriptionCtrl {
   }
 
   // generating the subscription view tooltips
-  private generateTooltip(offerName: string, usage?: number, volume?: number): string | undefined {
+  private generateTooltip(offer: IOfferData, usage?: number, volume?: number): string | undefined {
     if (_.isNumber(volume)) {
-      let tooltip = this.$translate.instant('subscriptions.licenseTypes.' + offerName) + '<br>';
-      if (this.useTotal(offerName) || !_.isNumber(usage)) {
+      let tooltip = this.$translate.instant('subscriptions.licenseTypes.' + offer.offerName) + '<br>';
+      if (this.useTotal(offer) || !_.isNumber(usage)) {
         tooltip += this.$translate.instant('subscriptions.licenses') + volume;
       } else if (usage > volume) {
         tooltip += this.$translate.instant('subscriptions.usage') + `<span class="warning">${usage}/${volume}</span>`;
@@ -230,7 +203,6 @@ export class MySubscriptionCtrl {
           licenses: [],
           isTrial: false,
           isOnline: false,
-          viewAll: false,
           numSubscriptions: subscriptions.length,
           endDate: '',
           badge: '',
@@ -264,38 +236,19 @@ export class MySubscriptionCtrl {
         }
 
         _.forEach(subscription.licenses, (license: any, licenseIndex: number): void => {
-          if (license.offerName in this.Config.offerCodes) {
-            const offer: IOfferData = {
-              licenseId: license.licenseId,
-              licenseType: license.licenseType,
-              licenseModel: _.get(license, 'licenseModel', ''),
-              offerName: license.offerName,
-              volume: license.volume,
-              id: 'donutId' + subIndex + licenseIndex,
-              tooltip: this.generateTooltip(license.offerName, license.usage, license.volume),
-            };
+          if (license.offerName in this.Config.offerCodes && !this.isOfferType(license, 'MGMTPRO')) {
+            const offer: IOfferData = this.generateOffer(license, subIndex, licenseIndex);
 
-            if (this.useTotal(license.offerName)) {
-              offer.totalUsage = license.usage || 0;
-            } else {
-              offer.usage = license.usage || 0;
-            }
-
-            if (license.siteUrl) {
-              offer.siteUrl = license.siteUrl;
-              offer.isCI = this.WebExUtilsFact.isCIEnabledSite(license.siteUrl);
-            }
-
-            if (license.offerName === this.Config.offerCodes.MS) {
+            if (this.isOfferType(offer, 'MS')) {
               offer.class = this.MESSAGE_CLASS;
               this.addSubscription(this.SUBSCRIPTION_TYPES.message, _.cloneDeep(offer));
-            } else if (license.offerName === this.Config.offerCodes.CO) {
+            } else if (this.isOfferType(offer, 'CO')) {
               offer.class = this.CALL_CLASS;
               this.addSubscription(this.SUBSCRIPTION_TYPES.call, _.cloneDeep(offer));
-            } else if (license.offerName === this.Config.offerCodes.SD || license.offerName === this.Config.offerCodes.SB) {
+            } else if (this.useTotal(offer)) {
               offer.class = this.MEETING_CLASS;
               this.addSubscription(this.SUBSCRIPTION_TYPES.room, _.cloneDeep(offer));
-            } else if (license.offerName === this.Config.offerCodes.CDC || license.offerName === this.Config.offerCodes.CVC) {
+            } else if (this.isOfferType(offer, 'CDC') || this.isOfferType(offer, 'CVC')) {
               offer.class = this.CARE_CLASS;
               const existingIndex = _.findIndex(this.licenseCategory[this.SUBSCRIPTION_TYPES.care].offerWrapper, (sub: IOfferWrapper): boolean => {
                 return sub.type === this.CARE;
@@ -310,8 +263,8 @@ export class MySubscriptionCtrl {
                   type: this.CARE,
                 });
               }
-            } else if (license.offerName !== this.Config.offerCodes.MGMTPRO) {
-              if (license.offerName === this.Config.offerCodes.CF) {
+            } else {
+              if (this.isOfferType(offer, 'CF')) {
                 offer.class = this.MEETING_CLASS;
               } else {
                 offer.class = this.WEBEX_CLASS;
@@ -335,10 +288,24 @@ export class MySubscriptionCtrl {
 
             this.visibleSubscriptions = true;
             newSubscription.licenses.push(offer);
-            // if the subscription is a trial, all licenses will have isTrial set to true
-            newSubscription.isTrial = license.isTrial;
+          } else if (this.isOfferType(license, 'MGMTPRO')) {
+            const offer: IOfferData = this.generateOffer(license, subIndex, licenseIndex);
+
+            newSubscription.proPack = offer;
+            if (this.proPackData && this.proPackData.usage && offer.usage) {
+              this.proPackData.usage += offer.usage;
+              this.proPackData.volume += offer.volume;
+            } else if (this.proPackData && offer.usage) {
+              this.proPackData.usage = offer.usage;
+              this.proPackData.volume += offer.volume;
+            } else {
+              this.proPackData = _.cloneDeep(offer);
+            }
           }
         });
+
+        // if the subscription is a trial, all licenses will have isTrial set to true
+        newSubscription.isTrial = _.get(subscription, 'licenses[0].isTrial', false);
 
         if (newSubscription.licenses.length > 0) {
           if (newSubscription.isOnline) {
@@ -346,7 +313,7 @@ export class MySubscriptionCtrl {
           }
           // sort licenses into display order/order for determining subscription name
           const licenseTypes: any[] = _.toArray(this.Config.offerCodes);
-          newSubscription.licenses.sort((a, b) => {
+          newSubscription.licenses.sort((a, b): number => {
             return licenseTypes.indexOf(a.offerName) - licenseTypes.indexOf(b.offerName);
           });
           if (newSubscription.isOnline) {
@@ -460,16 +427,52 @@ export class MySubscriptionCtrl {
       enabledServices.sort((s1, s2) => this.HybridServicesUtilsService.hybridServicesComparator(s1.id, s2.id));
       return _.map(enabledServices, (service: any) => {
         if (service.id === 'squared-fusion-uc' || service.id === 'squared-fusion-ec') {
-          return this.$translate.instant(`hercules.serviceNames.${service.id}.full`);
+          return `hercules.serviceNames.${service.id}.full`;
+        } else if (service.id === 'spark-hybrid-datasecurity') {
+          return `hercules.hybridServiceNames.${service.id}`;
+        } else {
+          return `hercules.serviceNames.${service.id}`;
         }
-        return this.$translate.instant(`hercules.serviceNames.${service.id}`);
       });
     }).then((humanReadableServices) => {
       this.hybridServices = humanReadableServices;
     });
   }
 
-  private useTotal(offerName: string): boolean {
-    return offerName === this.Config.offerCodes.SB || offerName === this.Config.offerCodes.SD;
+  private useTotal(offer: IOfferData): boolean {
+    return this.isOfferType(offer, 'SD') || this.isOfferType(offer, 'SB');
+  }
+
+  private isOfferType(offer: IOfferData, offerName: string): boolean {
+    return offer.offerName === this.Config.offerCodes[offerName];
+  }
+
+  private generateOffer(license: any, subIndex: number, licenseIndex: number) {
+    const offer: IOfferData = {
+      licenseId: license.licenseId,
+      licenseType: license.licenseType,
+      licenseModel: _.get(license, 'licenseModel', ''),
+      offerName: license.offerName,
+      volume: license.volume,
+      id: 'donutId' + subIndex + licenseIndex,
+      tooltip: this.generateTooltip(license, license.usage, license.volume),
+    };
+
+    if (this.useTotal(offer)) {
+      offer.totalUsage = license.usage || 0;
+    } else {
+      offer.usage = license.usage || 0;
+    }
+
+    if (!this.isSharedMeetingsLicense) {
+      this.isSharedMeetingsLicense = _.toLower(_.get(offer, 'licenseModel', '')) === this.Config.licenseModel.cloudSharedMeeting;
+    }
+
+    if (license.siteUrl) {
+      offer.siteUrl = license.siteUrl;
+      offer.isCI = this.WebExUtilsFact.isCIEnabledSite(license.siteUrl);
+    }
+
+    return offer;
   }
 }

commit 12747101c0d3d630cb0d19d12dad44673d005a3f

fix(core): Fix merge damage to Subscriptions page
Commit
https://sqbu-github.cisco.com/WebExSquared/wx2-admin-web-client/commit/e05519aae0d6da59cd6d894cec3beecca48aec54
caused some damage to the Subscriptions page for Online subscriptions.

app/modules/core/myCompany/mySubscriptions/mySubscription.controller.ts
@@ -355,16 +355,18 @@ export class MySubscriptionCtrl {
             }
           } else {
             const prodResponse: IProdInst = _.find(instances, ['subscriptionId', subscription.internalSubscriptionId]);
-            if (prodResponse.autoBilling) {
-              this.subscriptionDetails[index].endDate = '';
-            }
-            if (subscription.isTrial) {
-              this.setBMMPTrial(subscription, prodResponse);
-            } else {
-              this.setBMMP(subscription, prodResponse);
-            }
-            if (this.subscriptionDetails.length === 1) {
-              this.licenseSummary = this.$translate.instant('subscriptions.licenseSummaryOnline', { name: prodResponse.name });
+            if (prodResponse) {
+              if (prodResponse.autoBilling) {
+                this.subscriptionDetails[index].endDate = '';
+              }
+              if (subscription.isTrial) {
+                this.setBMMPTrial(subscription, prodResponse);
+              } else {
+                this.setBMMP(subscription, prodResponse);
+              }
+              if (this.subscriptionDetails.length === 1) {
+                this.licenseSummary = this.$translate.instant('subscriptions.licenseSummaryOnline', { name: prodResponse.name });
+              }
             }
           }
         });

app/modules/core/myCompany/mySubscriptions/mySubscription.tpl.html
@@ -10,9 +10,11 @@
     <cs-card class="full subscriptions" ng-if="mcpSub.hybridServices.length > 0 || mcpSub.visibleSubscriptions">
       <article>
         <header ng-if="mcpSub.licenseSummary">
-          <span class="column medium-2 card-title" ng-class="mcpSub.oneOnlineSub ? 'h5' : 'h3'">{{mcpSub.licenseSummary}}</span>
-          <div class="subscription-badge" ng-if="mcpSub.showSingleSub && mcpSub.subscriptionDetails[0].isOnline">
-            <span class="badge ng-scope" ng-class="mcpSub.subscriptionDetails[0].badge">{{::mcpSub.subscriptionDetails[0].endDate}}</span>
+          <div class="column medium-2 card-title">
+            <span ng-class="mcpSub.oneOnlineSub ? 'h5' : 'h3'">{{mcpSub.licenseSummary}}</span>
+            <div class="subscription-badge" ng-if="mcpSub.showSingleSub && mcpSub.subscriptionDetails[0].isOnline">
+              <span class="badge ng-scope" ng-class="mcpSub.subscriptionDetails[0].badge">{{::mcpSub.subscriptionDetails[0].endDate}}</span>
+            </div>
           </div>
           <div class="column medium-5 sub-name" ng-if="mcpSub.showSingleSub">
             <div class="container text-right">

app/modules/core/myCompany/mySubscriptions/subscription-details/subscriptionDetails.tpl.html
@@ -4,8 +4,8 @@
   </div>
   <h5 class="sub-name medium-2">
     {{::$ctrl.subscription.name}}
-    <div class="subscription-badge" ng-if="subscription.isOnline">
-      <span class="badge ng-scope" ng-class="subscription.badge">{{::subscription.endDate}}</span>
+    <div class="subscription-badge" ng-if="$ctrl.subscription.isOnline">
+      <span class="badge ng-scope" ng-class="$ctrl.subscription.badge">{{::$ctrl.subscription.endDate}}</span>
     </div>
   </h5>
 
@@ -41,12 +41,13 @@
       bmmp-attr="$ctrl.bmmpAttr"
       widget-location="navigation-bar">
     </bmmp-subscription-upgrade-button>
-    <span translate="common.quantity"></span>
-    <span>: {{$ctrl.getQuantity()}}</span>
+    <div ng-if="!$ctrl.subscription.isOnline">
+      <span translate="common.quantity"></span>
+      <span>: {{$ctrl.getQuantity()}}</span>
+    </div>
   </div>
   <div ng-class="{'online-subscription-id': $ctrl.subscription.isOnline}" class="subscription-id h6" ng-if="$ctrl.getView()">
     <span translate="subscriptions.subscription"></span>
     <span>{{::$ctrl.subscription.subscriptionId}}</span>
-    <div>{{::$ctrl.subscription.endDate}}</div>
   </div>
 </div>