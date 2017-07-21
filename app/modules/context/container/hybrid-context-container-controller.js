e50c962ca2a88d475ac8fd693a03d13e20c3d1b0
Shravan Shetty <shravash@cisco.com>
2017-07-21 16:40:23 -0500

    fix(hybrid-context-container): changed input values to cs-page-header in hybrid-context-container to match the new translate indipendent changes in the cs-page-header component.

--- app/modules/context/container/hybrid-context-container-controller.js
+++ app/modules/context/container/hybrid-context-container-controller.js
@@ -8,21 +8,22 @@ require('../resources/_resources.scss');
     .controller('HybridContextContainerController', HybridContextContainerController);
 
   /* @ngInject */
-  function HybridContextContainerController(backState) {
+  function HybridContextContainerController($translate, backState) {
     var vm = this;
     // default backState to the services-overview page unless otherwise specified
     vm.backState = backState || 'services-overview';
+    vm.header = $translate.instant('hercules.hybridServiceNames.hybrid-context');
     vm.tabs = [
       {
-        title: 'servicesOverview.cards.hybridContext.buttons.resources',
+        title: this.$translate.instant('servicesOverview.cards.hybridContext.buttons.resources'),
         state: 'context-resources',
       },
       {
-        title: 'servicesOverview.cards.hybridContext.buttons.fields',
+        title: this.$translate.instant('servicesOverview.cards.hybridContext.buttons.fields'),
         state: 'context-fields',
       },
       {
-        title: 'servicesOverview.cards.hybridContext.buttons.fieldsets',
+        title: this.$translate.instant('servicesOverview.cards.hybridContext.buttons.fieldsets'),
         state: 'context-fieldsets',
       },
     ];
