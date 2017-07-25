b593dbd1f7c775b3d3ddd609e8697f4ebb068185
Chakravarthy Ravichandran <charavic@cisco.com>
2017-07-25 11:52:56 -0700

    feat(huron): Primary Line Support Feature
    - Created new feature primary line in user/places
    - Added new feature toggle in features.config.js
    - Included new primaryLine module in User Overview  index.ts
    - Handled logic to pass on userPrimaryNumber to user-overview
      primary line modal
    - Updated index.ts with new interfaces, class and service files
    - Updated primaryLine.component.ts to handle GET, PUT logics
    - Also added ft and multiline check
    - Updated primaryLine.html with new ng-model
    - Updated primaryLine.interfaces.ts with interface structure
    - Added new GET, PUT action fuctions in primaryLine.service.ts
    - Added new PrimaryNumber class in primaryLine.ts
    - Updated user.service.ts with new V2 primary line GET method
    - Updated user.ts with Primary Number class reference
    - Included new primaryLine module in Place Call overview index.ts
    - Updated placeCallOverview.component.ts to handle logics to
      pass on places primary number info to primaryLine modal
    - Updated placeCallOverview.service.ts to handle new query param
    - Updated appconfig.js with new states and handled stateparams
    - Fixed review comments from jlowery and lsoell
    - Removed block of new code form $onInit to a new fn initServices in
      userCallOverview.component.ts and placeCallOverview.component.ts
    - Removed finally block in the promise array used in userCallOverview.component.ts and
      placeCallOverview.component.ts
    - Changed initNumber fn order of execution since based on the multi-line or
      shared lines the new feature will be displayed.
    - Fixed @jlowery  review comment.
    - Changed user.service.ts updateV2 argument from any to UserV2
    - Created necessary UserV2 obj and updated primaryLine.service.ts
    - Made all class member as optional in UserV2 user.ts
    
    https://rally1.rallydev.com/#/16735519999d/detail/userstory/123658934204

--- app/modules/core/featureToggle/features.config.js
+++ app/modules/core/featureToggle/features.config.js
@@ -206,4 +206,5 @@ module.exports = {
   atlasRolesAndSecurity: 'atlas-roles-and-security',
   csdmMultipleDevicesPerPlace: 'csdm-multiple-devices-per-place',
   avrilI1558: 'avril-i1558',
+  hI1474: 'h-i1474',
 };
