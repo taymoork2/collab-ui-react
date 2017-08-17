taymoork2/collab-ui-react

commit f1eaf3cb40dbfa9cac22164f4f0ed0ab98530c81

fix(places): Place cleanup
Stopping unneeded calls to cmi and uss, doing checks on the type of
place before loading resources only used by certain place types. Also
found a bug in the language display code where it would try to translate
an already translated field, while the fallback would stay untranslated.

app/modules/squared/places/overview/placeOverview.component.ts
@@ -68,11 +68,17 @@ class PlaceOverview implements ng.IComponentController {
     if (this.hasSparkCall) {
       this.initPlaceCallOverviewData();
       this.initActions();
+      this.getPlaceLocation();
     }
-    this.getPlaceLocation();
     this.fetchAsyncSettings();
     this.setDisplayDescription();
-    this.initPreferredLanguage();
+    if (this.showLanguage()) {
+      this.initPreferredLanguage();
+    }
+  }
+
+  public hasSquaredFusionUc(): boolean {
+    return this.hasEntitlement('squared-fusion-uc');
   }
 
   public initPlaceCallOverviewData(): void {

app/modules/squared/places/overview/placeOverview.html
@@ -19,7 +19,7 @@
         features="$ctrl.services"
         on-feature-click="$ctrl.clickService(feature)">
       </feature-list>
-      <hercules-cloud-extensions place="$ctrl.getCurrentPlace" eservice="$ctrl.editCloudberryServices" ng-if="$ctrl.anyHybridServiceToggle()"></hercules-cloud-extensions>
+      <hercules-cloud-extensions place="$ctrl.getCurrentPlace" eservice="$ctrl.editCloudberryServices" ng-if="$ctrl.anyHybridServiceToggle() && $ctrl.hasSquaredFusionUc()"></hercules-cloud-extensions>
     </cs-sp-section>
 
     <cs-sp-section>

app/modules/squared/places/callOverview/placeCallOverview.service.ts
@@ -107,8 +107,8 @@ export class PlaceCallOverviewService {
   private getDefaultPreferredLanguage(organizationLevelLanguage): any {
     const defaultPrefix: string = this.$translate.instant('preferredLanguage.organizationSettingLabel');
     const translatedLanguageLabel: string = organizationLevelLanguage ?
-                                        this.$translate.instant(organizationLevelLanguage) :
-                                        'languages.englishAmerican';
+                                        organizationLevelLanguage :
+      this.$translate.instant('languages.englishAmerican');
     const defaultLanguage = {
       label: defaultPrefix + translatedLanguageLabel,
       value: '',

commit f5354e53e7aaf3f63bf2394246111f55f63ee500

fix(devices): enabling hybrid services section if calendar entitlements are set
this is fixing a regression in f1eaf3cb40dbfa9cac22164f4f0ed0ab98530c81

app/modules/squared/places/overview/placeOverview.component.ts
@@ -77,8 +77,8 @@ class PlaceOverview implements ng.IComponentController {
     }
   }
 
-  public hasSquaredFusionUc(): boolean {
-    return this.hasEntitlement('squared-fusion-uc');
+  public hasHybridEntitlements(): boolean {
+    return this.hasEntitlement('squared-fusion-uc') || this.hasEntitlement('squared-fusion-cal') || this.hasEntitlement('squared-fusion-gcal');
   }
 
   public initPlaceCallOverviewData(): void {

app/modules/squared/places/overview/placeOverview.html
@@ -19,7 +19,7 @@
         features="$ctrl.services"
         on-feature-click="$ctrl.clickService(feature)">
       </feature-list>
-      <hercules-cloud-extensions place="$ctrl.getCurrentPlace" eservice="$ctrl.editCloudberryServices" ng-if="$ctrl.anyHybridServiceToggle() && $ctrl.hasSquaredFusionUc()"></hercules-cloud-extensions>
+      <hercules-cloud-extensions place="$ctrl.getCurrentPlace" eservice="$ctrl.editCloudberryServices" ng-if="$ctrl.anyHybridServiceToggle() && $ctrl.hasHybridEntitlements()"></hercules-cloud-extensions>
     </cs-sp-section>
 
     <cs-sp-section>