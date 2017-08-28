2a2a37119f4ee98f5ae18bc5806773bebda877e8
Shafi Razzack <shrazzac@cisco.com>
2017-08-28 16:20:12 -0700

    fix(core): US2534 - Unable to add meeting service after reload
    https://rally1.rallydev.com/#/76458540756d/detail/defect/147611333032?fdp=true

--- app/modules/core/setupWizard/setupWizardCtrl.js
+++ app/modules/core/setupWizard/setupWizardCtrl.js
@@ -388,6 +388,13 @@ require('./_setup-wizard.scss');
           tab.steps = _.slice(tab.steps, index, index + $stateParams.numberOfSteps);
         }
       }
+
+      // Show Subscription selection step if user is setting up WebEx meetings
+      if ($stateParams.currentTab === 'meetingSettings') {
+        var planReviewTab = _.find(tabs, { name: 'planReview' });
+        filteredTabs.unshift(planReviewTab);
+      }
+
       return filteredTabs;
     }
   }
