050df66d97ac08b2ebc7e3c455b1baf097185447
Kriti Rawat <kritrawa@cisco.com>
2017-08-30 17:42:28 +0530

    fix(huron): AUTOATTN-1272 : GT : Warning for dynamic variable is not removed

--- app/modules/huron/features/autoAttendant/dynamicAnnouncements/aaInsertionElementCtrl.js
+++ app/modules/huron/features/autoAttendant/dynamicAnnouncements/aaInsertionElementCtrl.js
@@ -52,6 +52,7 @@
                 var ele = '<aa-insertion-element element-text="' + node.say.value + '" read-as="' + node.say.as + '" element-id="' + id + '"></aa-insertion-element>';
                 node.htmlModel = encodeURIComponent(ele);
                 AACommonService.setSayMessageStatus(true);
+                $rootScope.$broadcast('CE Updated');
               }
             });
         }
