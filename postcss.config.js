9f98387cca4f06a54dd21957f24da18bb2e83b84
Brian Spence <brspence@cisco.com>
2017-08-30 15:42:04 -0500

    chore(css): fix the css output of dist build
    - https://jira-eng-chn-sjc1.cisco.com/jira/browse/ATLAS-2369
    - order of css similar to dev serve
      - styles.css (monolithic scss entry)
      - bootstrap.css (extracted async modules and bootstrap)
    - cssnano
      - minifify css
      - remove comments

--- postcss.config.js
+++ postcss.config.js
@@ -3,5 +3,8 @@ module.exports = {
     require('autoprefixer')({
       browsers: ['last 2 version'],
     }),
+    require('cssnano')({
+      reduceIdents: false, // http://cssnano.co/optimisations/reduceidents/
+    }),
   ],
 };
