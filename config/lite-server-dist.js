e5d3639c9953bf1fd4db1fdc9e24973f6cf1c6d7
Brian Spence <brspence@cisco.com>
2017-08-31 15:02:52 -0500

    chore(lite-server-dist): add asset caching for serve-dist
    - cache durations should match production config
    - https://sqbu-github.cisco.com/Platform-Common/static-nginx-configs/blob/master/admin-client.conf

--- config/lite-server-dist.js
+++ config/lite-server-dist.js
@@ -2,4 +2,14 @@ var liteServerConfig = require('./lite-server');
 liteServerConfig.server.baseDir = './dist';
 liteServerConfig.codeSync = false;
 
+liteServerConfig.server.middleware.push(function (req, res, next) {
+  if (/\.(js|css)$/.test(req.url)) {
+    res.setHeader('Cache-Control', 'max-age=604800'); // 1 week
+  }
+  if (/\.(ico|png|jpg|jpeg|gif|svg|eot|woff|woff2|ttf)(\?.*)?$/.test(req.url)) {
+    res.setHeader('Cache-Control', 'max-age=86400'); // 1 day
+  }
+  next();
+});
+
 module.exports = liteServerConfig;
