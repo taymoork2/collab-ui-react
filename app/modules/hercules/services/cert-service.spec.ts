ecf851493b84719750cfb53b345b5923b9d86607
Kjetil Ree <kjeree@cisco.com>
2017-08-31 10:56:09 +0200

    fix(hercules): Partner admins can now delete customers' certificates.
    cert-service.ts did not provide an orgId to the certs backend, which meant that partner admins could not delete certificates in their managed orgs. Also adding missing translation key, and showing a green toast when deletion succeeded.
    
    https://sqbu-github.cisco.com/WebExSquared/fusion-management-service/issues/512

--- app/modules/hercules/services/cert-service.spec.ts
+++ app/modules/hercules/services/cert-service.spec.ts
@@ -1,6 +1,8 @@
 describe('Service: CertService', () => {
   let  CertService, $httpBackend: any, UrlConfig: any, $q: ng.IQService;
+  const fakeOrgId = '555-6677';
   beforeEach(angular.mock.module('Hercules'));
+  beforeEach(angular.mock.module(mockDependencies));
   beforeEach(inject(dependencies));
   beforeEach(initSpies);
 
@@ -15,32 +17,54 @@ describe('Service: CertService', () => {
     spyOn(CertService, 'uploadCert').and.returnValue($q.resolve(true));
 
   }
+  function mockDependencies($provide) {
+    const Authinfo = {
+      getOrgId: jasmine.createSpy('getOrgId').and.returnValue(fakeOrgId),
+    };
+    $provide.value('Authinfo', Authinfo);
+  }
 
   afterEach( () => {
     $httpBackend.verifyNoOutstandingExpectation();
     $httpBackend.verifyNoOutstandingRequest();
   });
 
-  it('getCerts should call the right backend', () => {
-    const CertsUrl = UrlConfig.getCertsUrl() + 'certificate/api/v1';
-    const orgId = '0FF1C3';
-    $httpBackend.expectGET(`${CertsUrl}/certificates?expand=decoded&orgId=0FF1C3`).respond([]);
-    CertService.getCerts(orgId);
-    $httpBackend.flush();
+  describe('getCerts', () => {
+    it('should call the right backend', () => {
+      const CertsUrl = UrlConfig.getCertsUrl() + 'certificate/api/v1';
+      const orgId = '0FF1C3';
+      $httpBackend.expectGET(`${CertsUrl}/certificates?expand=decoded&orgId=0FF1C3`).respond([]);
+      CertService.getCerts(orgId);
+      $httpBackend.flush();
+    });
   });
 
-  it('deleteCert should call the right backend', () => {
-    const CertsUrl = UrlConfig.getCertsUrl() + 'certificate/api/v1';
-    const certId = '0-1-2-3-4-5';
-    $httpBackend.expectDELETE(`${CertsUrl}/certificates/0-1-2-3-4-5`).respond([]);
-    CertService.deleteCert(certId);
-    $httpBackend.flush();
+  describe ('deleteCert', () => {
+    it('should call the right backend using the provided orgIg', () => {
+      const CertsUrl = UrlConfig.getCertsUrl() + 'certificate/api/v1';
+      const certId = '0-1-2-3-4-5';
+      const orgId = '1983';
+      $httpBackend.expectDELETE(`${CertsUrl}/certificates/${certId}?orgId=${orgId}`).respond([]);
+      CertService.deleteCert(certId, orgId);
+      $httpBackend.flush();
+    });
+
+    it("should call the right backend and fall back to the customer's orgId if none is provided", () => {
+      const CertsUrl = UrlConfig.getCertsUrl() + 'certificate/api/v1';
+      const certId = '0-1-2-3-4-5';
+      $httpBackend.expectDELETE(`${CertsUrl}/certificates/${certId}?orgId=${fakeOrgId}`).respond([]);
+      CertService.deleteCert(certId);
+      $httpBackend.flush();
+    });
   });
-  it('uploadCert should call the right backend', () => {
-    const file = '0-1-2-3-4-5';
-    const orgId = '0FF1C3';
-    CertService.uploadCert(orgId, file);
-    expect(CertService.uploadCert).toHaveBeenCalledWith(orgId, file);
+
+  describe('uploadCert', () => {
+    it('uploadCert should call the right backend', () => {
+      const file = '0-1-2-3-4-5';
+      const orgId = '0FF1C3';
+      CertService.uploadCert(orgId, file);
+      expect(CertService.uploadCert).toHaveBeenCalledWith(orgId, file);
+    });
   });
 
 });
