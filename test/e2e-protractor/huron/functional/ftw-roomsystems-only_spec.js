3428a41f8f7deb0edbc8f8cd3a7a5437720f5f38
Shawn Casey <shacasey@cisco.com>
2017-09-07 15:19:55 -0500

    fix(huron): fixed voicemail failures in test cases
    There was an issue of sauce labs not having an element come into view in test runs, causing the test
    to fail. This should force the page to scroll these elements into view to prevent further failures.

--- test/e2e-protractor/huron/functional/ftw-roomsystems-only_spec.js
+++ test/e2e-protractor/huron/functional/ftw-roomsystems-only_spec.js
@@ -145,21 +145,23 @@ describe('Huron Functional: first-time-setup', () => {
     });
   });
 
-  xdescribe('Voicemail settings', () => {
+  describe('Voicemail settings', () => {
     it('should default to not having External Voicemail Access', () => {
-      utils.scrollIntoView(wizard.companyVoicemailToggle);
+      utils.click(wizard.scrollToBottomButton);
       expect(utils.getCheckboxVal(wizard.companyVoicemailToggle)).toBeFalsy();
     });
     it('should enable voicemail toggle', () => {
+      utils.scrollBottom('.modal');
       utils.setCheckboxIfDisplayed(wizard.companyVoicemailToggle, true, 1000);
     });
     it('should have two blank checkbox options', () => {
+      utils.scrollIntoView(callSettings.voicemailToEmailCheckBox);
       expect(utils.getCheckboxVal(callSettings.externalVoicemailCheckBox)).toBeFalsy();
       expect(utils.getCheckboxVal(callSettings.voicemailToEmailCheckBox)).toBeFalsy();
     });
     it('should check External Voicemail Access box', () => {
-      utils.click(wizard.scrollToBottomButton);
       utils.setCheckboxIfDisplayed(callSettings.externalVoicemailCheckBox, true, 1000);
+      utils.scrollBottom('.modal');
     });
     it('should display a dropdown to select a phone number for external voicemail access when activated', () => {
       utils.click(wizard.voicemailDropdown);
@@ -174,13 +176,12 @@ describe('Huron Functional: first-time-setup', () => {
     });
     it('should allow Voicemail to Email when box is checked', () => {
       utils.setCheckboxIfDisplayed(callSettings.voicemailToEmailCheckBox, true, 1000);
+      utils.scrollIntoView(wizard.voicemailEmailWithoutAttachment);
     });
     it('should default to Email notification with Attachment', () => {
-      utils.scrollIntoView(wizard.voicemailEmailWithAttachment);
       utils.click(wizard.voicemailEmailWithAttachment);
     });
     it('should click Email notification without attachment', () => {
-      utils.scrollIntoView(wizard.voicemailEmailWithoutAttachment);
       utils.click(wizard.voicemailEmailWithoutAttachment);
     });
     it('should enable save button', () => {
