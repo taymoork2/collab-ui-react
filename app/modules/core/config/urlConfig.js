46dab3a12ecbed56eb784e139bad0ae9d41b433e
Shafi Razzack <shrazzac@cisco.com>
2017-10-11 15:52:29 -0700

    fix(core): Send Mixpanel calls over https
    Currently, we are making event calls to mixpanel over http which is not secure.
    
    https://mixpanel.com/help/questions/articles/how-can-i-send-data-to-mixpanel-over-https-instead-of-http-using-the-javascript-library

--- app/modules/core/config/urlConfig.js
+++ app/modules/core/config/urlConfig.js
@@ -347,6 +347,7 @@
     ItunesStoreUrl: 'http://cs.co/sqios',
     SquaredAppUrl: 'squared://',
     StatusPageUrl: 'http://status.ciscospark.com/',
+    MixpanelUrl: 'https://api.mixpanel.com',
     CallflowServiceUrl: 'https://admin-portal-test-public.wbx2.com/atlas-server/admin/api/v1/',
     LogMetricsUrl: 'https://metrics-a.wbx2.com/metrics/api/v1/metrics',
     SSOTestUrl: 'https://idbroker.webex.com/idb/saml2/jsp/spSSOInit.jsp',
