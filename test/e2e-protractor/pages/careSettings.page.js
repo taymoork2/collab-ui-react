6525ee047a5ab6e269f221d441aaccedc191b81f
Abhishek Jaiswal <jajaiswa@cisco.com>
2017-10-13 18:23:25 +0530

    fix(sunlight): fix for caresttings e2e

--- test/e2e-protractor/pages/careSettings.page.js
+++ test/e2e-protractor/pages/careSettings.page.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var CareSettingsPage = function () {
-  this.pickRouting = element.all(by.model('localCareSettings.orgChatConfig.selectedRouting')).first();
-  this.pushRouting = element.all(by.model('localCareSettings.orgChatConfig.selectedRouting')).last();
+  this.pickRouting = element.all(by.model('localCareSettings.queueConfig.selectedRouting')).first();
+  this.pushRouting = element.all(by.model('localCareSettings.queueConfig.selectedRouting')).last();
   this.chatCount = element(by.model('localCareSettings.orgChatConfig.selectedChatCount'));
   this.chatToVideoToggle = element(by.model('localCareSettings.orgChatConfig.selectedVideoInChatToggle'));
   this.careSettingsSaveButton = element(by.css('.save-care-section .btn.btn--primary'));
