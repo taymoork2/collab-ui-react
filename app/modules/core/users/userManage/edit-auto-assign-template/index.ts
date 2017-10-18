416d080c6b99d0c38faf0683b8e2706e4288d2a4
Mike Park <mipark2@cisco.com>
2017-10-17 19:50:28 -0700

    refactor(core): add mixin 'keep-flex()', and use for 'multi-step-modal' and 'edit-auto-assign-template' components

--- app/modules/core/users/userManage/edit-auto-assign-template/index.ts
+++ app/modules/core/users/userManage/edit-auto-assign-template/index.ts
@@ -1,3 +1,5 @@
+import './edit-auto-assign-template.scss';
+
 import { EditAutoAssignTemplateComponent } from './edit-auto-assign-template.component';
 import multiStepModalModuleName from 'modules/core/shared/multi-step-modal';
 
