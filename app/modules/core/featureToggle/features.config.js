77a9ab628229ed7105dc01df40bd9b6787a3db58
Christine Pham <chrispha@cisco.com>
2017-10-19 16:21:57 -0700

    refactor(core): remove atlasRolesAndSecurity feature toggle from codebase
    https://rally1.rallydev.com/#/104328613908ud/detail/userstory/153705714916
    
    refactor(core): refactor displayName line

--- app/modules/core/featureToggle/features.config.js
+++ app/modules/core/featureToggle/features.config.js
@@ -60,7 +60,6 @@ module.exports = {
   atlasPMRonM2: 'atlas-pmr-on-m2',
   atlasReadOnlyAdmin: 'atlas-read-only-admin',
   atlasReportsUpdate: 'atlas-reports-update',
-  atlasRolesAndSecurity: 'atlas-roles-and-security',
   atlasSendEmailViaBMMP: 'atlas-send-email-via-bmmp',
   atlasServicesOverviewRefresh: 'atlas-services-overview-refresh',
   atlasSetupSiteUserManagement: 'atlas-setup-site-user-management',
