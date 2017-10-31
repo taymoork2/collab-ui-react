7fdf1a42cda09525cdfb7b5a275c1a6735ff367d
Mike Park <mipark2@cisco.com>
2017-10-30 19:14:50 -0700

    refactor(users): mv 'onboard.module.js' -> 'index.ts' + convert to TS

--- app/modules/core/users/userOverview/index.ts
+++ app/modules/core/users/userOverview/index.ts
@@ -6,10 +6,10 @@ import featureToggleServiceModule from 'modules/core/featureToggle';
 import notifictionModule from 'modules/core/notifications';
 import sunlightServiceModule from 'modules/sunlight/services';
 import webExUtilsModule from 'modules/webex/utils';
+import onboardModule from 'modules/core/users/userAdd';
 
 const coreAuthModule = require('modules/core/auth/auth');
 const ngResourceModule = require('angular-resource');
-const onboardModule = require('modules/core/users/userAdd/onboard.module');
 
 import './_user-overview.scss';
 export * from './userOverview.service';
