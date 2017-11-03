2b06f0c988e6222702f92d8eed9f8f1355074eb9
Yvette Ren <shiren@cisco.com>
2017-11-03 20:14:09 +0800

    fix(customer-report): fix report tab issues

--- app/modules/core/customerReports/customerReportsHeader.controller.spec.ts
+++ app/modules/core/customerReports/customerReportsHeader.controller.spec.ts
@@ -65,7 +65,7 @@ describe('Controller: Customer Reports Ctrl', function () {
       spyOn(this.MediaServiceActivationV2, 'getMediaServiceState').and.returnValue(this.$q.resolve(false));
       spyOn(this.FeatureToggleService, 'webexMetricsGetStatus').and.returnValue(this.$q.resolve(false));
       spyOn(this.ProPackService, 'hasProPackEnabled').and.returnValue(this.$q.resolve(false));
-      spyOn(this.WebexMetricsService, 'hasClassicEnabled').and.returnValue(this.$q.resolve([]));
+      spyOn(this.WebexMetricsService, 'hasClassicEnabled').and.returnValue(this.$q.resolve([false]));
       spyOn(this.WebexMetricsService, 'checkWebexAccessiblity').and.returnValue(this.$q.resolve([true]));
 
       this.controller = this.$controller('CustomerReportsHeaderCtrl', {
@@ -80,7 +80,8 @@ describe('Controller: Customer Reports Ctrl', function () {
     });
 
     it('should only display spark and care reports tab', function () {
-      expect(this.controller.headerTabs).toEqual([headerTabs[0], headerTabs[1], headerTabs[2], headerTabs[4]]);
+      expect(this.controller.headerTabs).toContain(headerTabs[0], headerTabs[1]);
+      expect(this.controller.headerTabs).toContain(headerTabs[2], headerTabs[4]);
     });
 
   });
@@ -92,7 +93,7 @@ describe('Controller: Customer Reports Ctrl', function () {
       spyOn(this.MediaServiceActivationV2, 'getMediaServiceState').and.returnValue(this.$q.resolve(true));
       spyOn(this.FeatureToggleService, 'webexMetricsGetStatus').and.returnValue(this.$q.resolve(true));
       spyOn(this.ProPackService, 'hasProPackEnabled').and.returnValue(this.$q.resolve(true));
-      spyOn(this.WebexMetricsService, 'hasClassicEnabled').and.returnValue(this.$q.resolve([false, true]));
+      spyOn(this.WebexMetricsService, 'hasClassicEnabled').and.returnValue(this.$q.resolve([true]));
       spyOn(this.WebexMetricsService, 'checkWebexAccessiblity').and.returnValue(this.$q.resolve([true]));
 
       this.controller = this.$controller('CustomerReportsHeaderCtrl', {
@@ -107,7 +108,9 @@ describe('Controller: Customer Reports Ctrl', function () {
     });
 
     it('should display all reports tabs', function () {
-      expect(this.controller.headerTabs).toEqual([headerTabs[0], propackTabs[0], headerTabs[3], headerTabs[4], propackTabs[1]]);
+      expect(this.controller.headerTabs).toContain(headerTabs[0]);
+      expect(this.controller.headerTabs).toContain(headerTabs[3], headerTabs[4]);
+      expect(this.controller.headerTabs).toContain(propackTabs[0], propackTabs[1]);
     });
   });
 });
