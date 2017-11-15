d655a41a31216ede2605c76483411184042eddf9
Mike Park <mipark2@cisco.com>
2017-11-14 18:22:06 -0800

    test(users): add unit-tests for 'assignable-services'

--- app/modules/core/users/userAdd/assignable-services/assignable-services.component.spec.ts
+++ app/modules/core/users/userAdd/assignable-services/assignable-services.component.spec.ts
@@ -4,25 +4,75 @@ describe('Component: assignableServices:', () => {
   beforeEach(function() {
     this.initModules(moduleName);
     this.injectDependencies(
-      // TODO: add dependencies here
+      '$httpBackend',
+      '$q',
+      'Authinfo',
+      'Orgservice',
     );
+    this.fixtures = {};
+    this.fixtures.fakeLicenseUsage = [{
+      subscriptionId: 'fake-subscriptionId-2',
+    }, {
+      subscriptionId: 'fake-subscriptionId-3',
+    }, {
+      subscriptionId: 'fake-subscriptionId-1',
+    }];
   });
 
-  // TODO: use as-appropriate
-  beforeEach(function () {
-    // this.compileTemplate('<assignable-services></assignable-services>');
-    // this.compileComponent('assignableServices', { ... });
+  afterEach(function () {
+    this.$httpBackend.verifyNoOutstandingExpectation();
+    this.$httpBackend.verifyNoOutstandingRequest();
   });
 
   describe('primary behaviors (view):', () => {
-    it('...', function () {
-      // TODO: implement
+    it('should have three or four columns depending on "isCareEnabled"', function () {
+      spyOn(this.Authinfo, 'isCareAndCDC').and.returnValue(false);
+      spyOn(this.Authinfo, 'isCareVoiceAndCVC').and.returnValue(false);
+      this.compileComponent('assignableServices');
+      expect(this.view.find('.license-wrapper .column-title').length).toBe(3);
+
+      this.Authinfo.isCareAndCDC.and.returnValue(true);
+      this.compileComponent('assignableServices');
+      expect(this.view.find('.license-wrapper .column-title').length).toBe(4);
+
+      this.Authinfo.isCareAndCDC.and.returnValue(false);
+      this.Authinfo.isCareVoiceAndCVC.and.returnValue(true);
+      this.compileComponent('assignableServices');
+      expect(this.view.find('.license-wrapper .column-title').length).toBe(4);
     });
   });
 
   describe('primary behaviors (controller):', () => {
-    it('...', function () {
-      // TODO: implement
+    beforeEach(function () {
+      spyOn(this.Orgservice, 'getLicensesUsage').and.returnValue(this.$q.resolve(this.fixtures.fakeLicenseUsage));
+    });
+
+    it('should initialize "sortedSubscription" property', function () {
+      this.compileComponent('assignableServices');
+      expect(this.controller.sortedSubscriptions.length).toBe(3);
+      expect(_.get(this.controller, 'sortedSubscriptions[0].subscriptionId')).toBe('fake-subscriptionId-1');
+      expect(_.get(this.controller, 'sortedSubscriptions[1].subscriptionId')).toBe('fake-subscriptionId-2');
+      expect(_.get(this.controller, 'sortedSubscriptions[2].subscriptionId')).toBe('fake-subscriptionId-3');
+    });
+
+    it('should initialize "isCare*" properties', function () {
+      spyOn(this.Authinfo, 'isCareAndCDC').and.returnValue(false);
+      spyOn(this.Authinfo, 'isCareVoiceAndCVC').and.returnValue(false);
+      this.compileComponent('assignableServices');
+      expect(this.controller.isCareAndCDCEnabled).toBe(false);
+      expect(this.controller.isCareAndCVCEnabled).toBe(false);
+      expect(this.controller.isCareEnabled).toBe(false);
+
+      this.Authinfo.isCareAndCDC.and.returnValue(true);
+      this.compileComponent('assignableServices');
+      expect(this.controller.isCareAndCDCEnabled).toBe(true);
+      expect(this.controller.isCareEnabled).toBe(true);
+
+      this.Authinfo.isCareAndCDC.and.returnValue(false);
+      this.Authinfo.isCareVoiceAndCVC.and.returnValue(true);
+      this.compileComponent('assignableServices');
+      expect(this.controller.isCareAndCVCEnabled).toBe(true);
+      expect(this.controller.isCareEnabled).toBe(true);
     });
   });
 });
--- app/modules/core/users/userAdd/assignable-services/index.ts
+++ app/modules/core/users/userAdd/assignable-services/index.ts
@@ -1,10 +1,12 @@
 import { AssignableServicesComponent } from './assignable-services.component';
+import * as orgServiceModuleName from 'modules/core/scripts/services/org.service';
 import sharedModuleName from './shared';
 import assignableServicesRowModuleName from './assignable-services-row';
 
 export default angular.module('core.users.userAdd.assignable-services', [
   require('angular-translate'),
   require('collab-ui-ng').default,
+  orgServiceModuleName,
   sharedModuleName,
   assignableServicesRowModuleName,
 ])
