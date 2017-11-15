716638a897f532cbaa789d64f574fd2429d64a2b
Mike Park <mipark2@cisco.com>
2017-11-14 19:47:29 -0800

    test(users): add unit-tests for 'LicenseUsageUtilService'

--- app/modules/core/users/userAdd/assignable-services/shared/index.ts
+++ app/modules/core/users/userAdd/assignable-services/shared/index.ts
@@ -1,8 +1,10 @@
+import configModuleName from 'modules/core/config/config';
 import { LicenseUsageUtilService } from './license-usage-util.service';
 export { LicenseUsageUtilService } from './license-usage-util.service';
 export * from './license-usage-util.interfaces';
 
 export default angular.module('core.users.userAdd.assignable-services.shared', [
+  configModuleName,
 ])
   .service('LicenseUsageUtilService', LicenseUsageUtilService)
   .name;
