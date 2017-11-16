be86ec03339e3ae6375fc8656b087bf701de837a
Mike Park <mipark2@cisco.com>
2017-11-15 23:57:02 -0800

    test(users): add unit-tests for 'advanced-meeting-site-url'

--- app/modules/core/users/userAdd/assignable-services/assignable-services-row/assignable-advanced-meetings/advanced-meeting-site-url/advanced-meeting-site-url.component.spec.ts
+++ app/modules/core/users/userAdd/assignable-services/assignable-services-row/assignable-advanced-meetings/advanced-meeting-site-url/advanced-meeting-site-url.component.spec.ts
@@ -4,25 +4,43 @@ describe('Component: advancedMeetingSiteUrl:', () => {
   beforeEach(function() {
     this.initModules(moduleName);
     this.injectDependencies(
-      // TODO: add dependencies here
+      '$scope',
+      'WebExUtilsFact',
     );
   });
 
-  // TODO: use as-appropriate
-  beforeEach(function () {
-    // this.compileTemplate('<advanced-meeting-site-url></advanced-meeting-site-url>');
-    // this.compileComponent('advancedMeetingSiteUrl', { ... });
-  });
-
   describe('primary behaviors (view):', () => {
-    it('...', function () {
-      // TODO: implement
+    it('should render a site url as plain text if it is a CI site, or as a link to a respective site admin url otherwise', function () {
+      this.compileTemplate('<advanced-meeting-site-url site-url="fake-site-url"></advanced-meeting-site-url>');
+      this.controller = this.view.controller('advanced-meeting-site-url');
+      expect(this.view.find('.license__siteUrl').length).toBe(1);
+      expect(this.view.find('.license__siteUrl span[ng-if="$ctrl.isCISite"]').length).toBe(1);
+
+      this.controller.isCISite = false;
+      this.controller.siteAdminUrl = 'fake-site-admin-url';
+      this.$scope.$apply();
+      expect(this.view.find('.license__siteUrl span[ng-if="!$ctrl.isCISite"]').length).toBe(1);
+      expect(this.view.find('.license__siteUrl a[id="fake-site-url_siteAdminUrl"]').length).toBe(1);
+      expect(this.view.find('.license__siteUrl a[href="fake-site-admin-url"]').length).toBe(1);
     });
   });
 
   describe('primary behaviors (controller):', () => {
-    it('...', function () {
-      // TODO: implement
+    it('should set "isCISite" and "siteAdminUrl" properties according to "WebExUtilsFact.isCIEnabledSite()" and "WebExUtilsFact.getSiteAdminUrl()"', function () {
+      spyOn(this.WebExUtilsFact, 'isCIEnabledSite').and.returnValue(true);
+      spyOn(this.WebExUtilsFact, 'getSiteAdminUrl').and.returnValue('fake-site-admin-url');
+      this.compileComponent('advancedMeetingSiteUrl', {
+        siteUrl: 'fake-site-url',
+      });
+      expect(this.controller.isCISite).toBe(true);
+      expect(this.controller.siteAdminUrl).toBe('');
+
+      this.WebExUtilsFact.isCIEnabledSite.and.returnValue(false);
+      this.compileComponent('advancedMeetingSiteUrl', {
+        siteUrl: 'fake-site-url',
+      });
+      expect(this.controller.isCISite).toBe(false);
+      expect(this.controller.siteAdminUrl).toBe('fake-site-admin-url');
     });
   });
 });
