edcea4d603c1f90cae32853ae54f7ccb059b6bfe
Mike Park <mipark2@cisco.com>
2017-11-16 18:04:17 -0800

    refactor(users): cleanup 'export { ... }' statement

--- app/modules/core/users/userAdd/assignable-services/shared/index.ts
+++ app/modules/core/users/userAdd/assignable-services/shared/index.ts
@@ -1,6 +1,6 @@
 import configModuleName from 'modules/core/config/config';
 import { LicenseUsageUtilService } from './license-usage-util.service';
-export { LicenseUsageUtilService } from './license-usage-util.service';
+export { LicenseUsageUtilService };
 export * from './license-usage-util.interfaces';
 
 export default angular.module('core.users.userAdd.assignable-services.shared', [
