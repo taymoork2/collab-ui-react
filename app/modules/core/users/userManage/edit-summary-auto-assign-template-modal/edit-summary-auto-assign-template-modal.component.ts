fb01fe688c2271f5a757e619bcf276b2a650f8c6
Mike Park <mipark2@cisco.com>
2017-11-17 16:32:19 -0800

    WIP(users): add stub logic for 'back' and 'save' in 'edit-summary-auto-assign-template-modal'

--- app/modules/core/users/userManage/edit-summary-auto-assign-template-modal/edit-summary-auto-assign-template-modal.component.ts
+++ app/modules/core/users/userManage/edit-summary-auto-assign-template-modal/edit-summary-auto-assign-template-modal.component.ts
@@ -1,4 +1,21 @@
 class EditSummaryAutoAssignTemplateModalController implements ng.IComponentController {
+
+  /* @ngInject */
+  constructor(
+    private $state: ng.ui.IStateService,
+  ) {
+  }
+
+  public back(): void {
+    this.$state.go('users.manage.edit-auto-assign-template-modal', {
+      // TODO: pass back subscription assignments data
+    });
+  }
+
+  public save(): void {
+    alert('TODO: post to API endpoint');
+    this.$state.go('users.list');
+  }
 }
 
 export class EditSummaryAutoAssignTemplateModalComponent implements ng.IComponentOptions {
