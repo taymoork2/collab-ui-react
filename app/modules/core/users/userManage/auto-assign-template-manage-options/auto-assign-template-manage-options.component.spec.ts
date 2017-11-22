e2a2b59ad4b7cdd06cfe7addaa2b644be811410a
Mike Park <mipark2@cisco.com>
2017-11-21 23:24:04 -0800

    test(users): add unit-tests for 'auto-assign-template-manage-options'

--- app/modules/core/users/userManage/auto-assign-template-manage-options/auto-assign-template-manage-options.component.spec.ts
+++ app/modules/core/users/userManage/auto-assign-template-manage-options/auto-assign-template-manage-options.component.spec.ts
@@ -4,25 +4,52 @@ describe('Component: autoAssignTemplateManageOptions:', () => {
   beforeEach(function() {
     this.initModules(moduleName);
     this.injectDependencies(
-      // TODO: add dependencies here
+      '$q',
+      '$scope',
+      'AutoAssignTemplateService',
+      'ModalService',
+      'Notification',
     );
+    this.$scope.autoAssignTemplates = {
+      Default: {
+        templateId: 'fake-template-id',
+      },
+    };
+    this.$scope.onDelete = _.noop;
   });
 
   // TODO: use as-appropriate
   beforeEach(function () {
-    // this.compileTemplate('<auto-assign-template-manage-options></auto-assign-template-manage-options>');
-    // this.compileComponent('autoAssignTemplateManageOptions', { ... });
+    this.compileComponent('autoAssignTemplateManageOptions', {
+      autoAssignTemplates: 'autoAssignTemplates',
+      onDelete: 'onDelete',
+    });
   });
 
   describe('primary behaviors (view):', () => {
-    it('...', function () {
-      // TODO: implement
+    it('should render a toggle button, a drop-down menu, and three menu items', function () {
+      expect(this.view.find('button[cs-dropdown-toggle]').length).toBe(1);
+      expect(this.view.find('ul[cs-dropdown-menu]').length).toBe(1);
+      expect(this.view.find('li.menu-item').length).toBe(3);
+      expect(this.view.find('a[translate="userManage.org.moreOptions.modifyAutoAssign"]').length).toBe(1);
+      expect(this.view.find('a[translate="userManage.org.moreOptions.deactivateAutoAssign"]').length).toBe(1);
+      expect(this.view.find('a[translate="userManage.org.moreOptions.deleteAutoAssign"]').length).toBe(1);
     });
   });
 
   describe('primary behaviors (controller):', () => {
     it('...', function () {
-      // TODO: implement
+      spyOn(this.ModalService, 'open').and.returnValue({
+        result: this.$q.resolve(true),
+      });
+      spyOn(this.AutoAssignTemplateService, 'deleteTemplate').and.returnValue(this.$q.resolve());
+      spyOn(this.Notification, 'success');
+      spyOn(this.controller, 'onDelete');
+      this.controller.deleteAutoAssignTemplate();
+      this.$scope.$apply();
+      expect(this.AutoAssignTemplateService.deleteTemplate).toHaveBeenCalledWith('fake-template-id');
+      expect(this.Notification.success).toHaveBeenCalledWith('userManage.org.deleteAutoAssignModal.deleteSuccess');
+      expect(this.controller.onDelete).toHaveBeenCalled();
     });
   });
 });
