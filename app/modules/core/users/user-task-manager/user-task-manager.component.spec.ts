9bd21a6b2f3fd58a5accb6b0cf54b8a8eeb72eff
Brian Spence <brspence@cisco.com>
2017-11-29 11:18:08 -0600

    test(user-task-manager): test component interactions and initialization

--- app/modules/core/users/user-task-manager/user-task-manager.component.spec.ts
+++ app/modules/core/users/user-task-manager/user-task-manager.component.spec.ts
@@ -1,133 +1,124 @@
 import userTaskManagerModalModule from './index';
 import { TaskListFilterType } from './user-task-manager.constants';
 
-describe('Component: userTaskManagerModalModule', () => {
-
-  const MODAL_TITLE = '.modal-header';
-  const MODAL_BODY = '.modal-body';
-  const TASK_CONTAINER = '.task-container';
-  const TASK_CONTAINER_PANEL = '.task-container__panel';
-  const TASK_CONTAINER_PANEL_HEADER = '.task-container__panel-header';
-  const TASK_CONTAINER_DETAILS = '.task-container__details';
-  const BUTTONS = 'button.btn.btn--white';
-  const TASK_LIST_ITEM = 'user-task-list-item';
-  const TASK = '.task';
-  const ACTIVE_FILTER = 'task-list-filter--active';
-  const ACTIVE_TASK = 'task--active';
-
-  beforeEach(angular.mock.module(mockDependencies));
-
-  function mockDependencies($provide) {
-    const Userservice = {
-      getUserAsPromise: () => {
-        return {
-          then: (success) => {
-            return success({
-              data: {
-                displayName: 'User Me',
-                userName: 'user.me@gmail.com',
-              },
-            });
-          },
-        };
-      },
-    };
-    $provide.value('Userservice', Userservice);
-  }
+describe('Component: userTaskManager', () => {
+  const LOADING_SPINNER = '.loading-wrapper';
+  const TASK_CONTAINER = 'task-container';
 
   beforeEach(function() {
-    this.initModules(userTaskManagerModalModule);
+    this.components = {
+      multiStepModal: this.spyOnComponent('multiStepModal'),
+      taskContainer: this.spyOnComponent('taskContainer'),
+      userTaskListFilter: this.spyOnComponent('userTaskListFilter'),
+      userTaskList: this.spyOnComponent('userTaskList'),
+      csvUploadResults: this.spyOnComponent('csvUploadResults'),
+    };
+    this.initModules(
+      userTaskManagerModalModule,
+      this.components.multiStepModal,
+      this.components.taskContainer,
+      this.components.userTaskListFilter,
+      this.components.userTaskList,
+      this.components.csvUploadResults,
+    );
     this.injectDependencies(
       '$scope',
       '$stateParams',
       '$q',
       'UserTaskManagerService',
     );
-    this.taskList = require('./test-tasks.json').taskManagerTasks;
+    this.taskList = _.cloneDeep(require('./test-tasks.json').taskManagerTasks);
 
-    spyOn(this.UserTaskManagerService, 'getTasks').and.returnValue(this.$q.resolve(this.taskList));
     spyOn(this.UserTaskManagerService, 'submitCsvImportTask').and.returnValue(this.$q.resolve(this.taskList[1]));
+    spyOn(this.UserTaskManagerService, 'initAllTaskListPolling');
   });
 
   function initComponent() {
     this.compileComponent('userTaskManagerModal', {});
-    spyOn(this.controller, 'setActiveFilter').and.callThrough();
-    spyOn(this.controller, 'setActiveTask').and.callThrough();
   }
 
-  describe('UserTaskManagerModal at init', () => {
+  describe('initial state', () => {
     beforeEach(initComponent);
-    it('should have modal title, body, left panel, right panel and options', function() {
-      expect(this.view.find(MODAL_TITLE)).toExist();
-      expect(this.view.find(MODAL_BODY)).toExist();
+    it('should have title and dismiss functionality', function () {
+      expect(this.components.multiStepModal.bindings[0].l10nTitle).toBe('userTaskManagerModal.title');
+
+      spyOn(this.controller, 'dismiss');
+      this.components.multiStepModal.bindings[0].dismiss();
+      expect(this.controller.dismiss).toHaveBeenCalled();
+    });
+
+    it('should have started polling', function () {
+      expect(this.UserTaskManagerService.initAllTaskListPolling).toHaveBeenCalled();
+    });
+
+    it('should show loading spinner until the first interval callback', function () {
+      expect(this.view.find(LOADING_SPINNER)).toExist();
+      expect(this.view.find(TASK_CONTAINER)).not.toExist();
+
+      this.controller.intervalCallback();
+      this.$scope.$apply();
+
+      expect(this.view.find(LOADING_SPINNER)).not.toExist();
       expect(this.view.find(TASK_CONTAINER)).toExist();
-      expect(this.view.find(TASK_CONTAINER).find(TASK_CONTAINER_PANEL)).toExist();
-      expect(this.view.find(TASK_CONTAINER).find(TASK_CONTAINER_DETAILS)).toExist();
-      expect(this.view.find(TASK_CONTAINER_PANEL_HEADER).find(BUTTONS).length).toBe(2);
-      expect(this.view.find(TASK_CONTAINER_PANEL_HEADER).find(BUTTONS).get(0)).toHaveClass(ACTIVE_FILTER);
-      expect(this.view.find(TASK_CONTAINER_PANEL_HEADER).find(BUTTONS).get(1)).not.toHaveClass(ACTIVE_FILTER);
-      expect(this.view.find(TASK_LIST_ITEM).length).toBe(3);
-      expect(this.UserTaskManagerService.getTasks).toHaveBeenCalled();
-      expect(this.controller.allTaskList).toHaveLength(3);
-      expect(this.controller.inProcessTaskList).toHaveLength(1);
-      expect(this.controller.activeTask.jobTypeTranslate).toEqual('userTaskManagerModal.csvImport');
-      expect(this.controller.activeTask.statusTranslate).toEqual('userTaskManagerModal.taskStatus.completed');
-      expect(this.controller.activeTask.jobInstanceId).toEqual('CSV Import 1');
-      expect(this.controller.activeTask.createdDate).toEqual('Oct 6, 2017');
-      expect(this.controller.activeTask.createdTime).toEqual('3:54 PM');
     });
   });
 
-  describe('UserTaskManagerModal filter type', () => {
+  describe('component behavior', () => {
     beforeEach(initComponent);
-    it('when all-task selected', function() {
-      const allButton = this.view.find(BUTTONS).get(0);
-      allButton.click();
-      this.$scope.$apply();
-      expect(this.view.find(BUTTONS).get(0)).toHaveClass(ACTIVE_FILTER);
-      expect(this.view.find(BUTTONS).get(1)).not.toHaveClass(ACTIVE_FILTER);
-      expect(this.controller.setActiveFilter).toHaveBeenCalledWith(TaskListFilterType.ALL);
-      expect(this.controller.activeTask).toBe(this.taskList[0]);
-      expect(this.controller.activeTask.jobInstanceId).toEqual('CSV Import 1');
-    });
-    it('when active-task selected', function() {
-      const activeButton = this.view.find(BUTTONS).get(1);
-      activeButton.click();
-      this.$scope.$apply();
-      expect(this.view.find(BUTTONS).get(0)).not.toHaveClass(ACTIVE_FILTER);
-      expect(this.view.find(BUTTONS).get(1)).toHaveClass(ACTIVE_FILTER);
-      expect(this.controller.setActiveFilter).toHaveBeenCalledWith(TaskListFilterType.ACTIVE);
-      expect(this.controller.activeTask).toBe(this.taskList[1]);
-      expect(this.controller.activeTask.jobInstanceId).toEqual('CSV Import 2');
-    });
-  });
-
-  describe('UserTaskManagerModal picks a new task', () => {
-    beforeEach(initComponent);
-    it('when select 2nd task', function() {
-      const secondTask = this.view.find(TASK_LIST_ITEM).get(1);
-      secondTask.click();
-      expect(this.controller.setActiveTask).toHaveBeenCalledWith(this.taskList[1]);
-      expect(this.view.find(TASK_LIST_ITEM).eq(0).find(TASK)).not.toHaveClass(ACTIVE_TASK);
-      expect(this.view.find(TASK_LIST_ITEM).eq(1).find(TASK)).toHaveClass(ACTIVE_TASK);
-      expect(this.controller.activeTask.jobInstanceId).toEqual('CSV Import 2');
-    });
-  });
-
-  describe('UserTaskManagerModal display a specific task', () => {
     beforeEach(function () {
-      this.$stateParams.task = this.taskList[1];
+      this.controller.intervalCallback(this.taskList);
+      this.$scope.$apply();
     });
-    beforeEach(initComponent);
-    it('when specify 2nd task', function() {
-      expect(this.controller.activeTask).toBe(this.taskList[1]);
-      expect(this.controller.activeFilter).toBe(TaskListFilterType.ACTIVE);
-      expect(this.controller.setActiveTask).not.toHaveBeenCalled();
-      expect(this.controller.activeTask.jobInstanceId).toEqual('CSV Import 2');
+
+    it('should initialize filter, task list, and results', function () {
+      expect(this.components.userTaskListFilter.bindings[0].filter).toBe(TaskListFilterType.ALL);
+      expect(this.components.userTaskList.bindings[0].task).toEqual(this.taskList[0]);
+      expect(this.components.userTaskList.bindings[0].taskList).toEqual(this.taskList);
+
+      const activeTask = this.components.csvUploadResults.bindings[0].inputActiveTask;
+      expect(activeTask).toEqual(this.taskList[0]);
+      expect(activeTask.jobTypeTranslate).toBe('userTaskManagerModal.csvImport');
+      expect(activeTask.statusTranslate).toBe('userTaskManagerModal.taskStatus.completed');
+      expect(activeTask.jobInstanceId).toBe('CSV Import 1');
+      expect(activeTask.createdDate).toBe('Oct 6, 2017');
+      expect(activeTask.createdTime).toBe('3:54 PM');
+    });
+
+    it('should change result task on task list change', function () {
+      this.components.userTaskList.bindings[0].onActiveTaskChange({
+        task: this.taskList[2],
+      });
+      this.$scope.$apply();
+
+      expect(this.components.userTaskList.bindings[0].task).toEqual(this.taskList[2]);
+      expect(this.components.csvUploadResults.bindings[0].inputActiveTask).toEqual(this.taskList[2]);
+    });
+
+    it('should change active filter and task on filter change', function () {
+      this.components.userTaskListFilter.bindings[0].onFilterChange({
+        filter: TaskListFilterType.ACTIVE,
+      });
+      this.$scope.$apply();
+
+      expect(this.components.userTaskListFilter.bindings[0].filter).toBe(TaskListFilterType.ACTIVE);
+      expect(this.components.userTaskList.bindings[0].task).toEqual(this.taskList[1]);
+      expect(this.components.userTaskList.bindings[0].taskList).toEqual([this.taskList[1]]);
+      expect(this.components.csvUploadResults.bindings[0].inputActiveTask).toEqual(this.taskList[1]);
+    });
+
+    it('should update the active task status', function () {
+      expect(this.components.userTaskList.bindings[0].taskList[0].status).toBe('COMPLETED');
+
+      this.components.csvUploadResults.bindings[0].onStatusUpdate({
+        status: 'STARTED',
+      });
+      this.$scope.$apply();
+
+      expect(this.components.userTaskList.bindings[0].taskList[0].status).toBe('STARTED');
     });
   });
 
-  describe('UserTaskManagerModal submit a new task', () => {
+  describe('submit a new task', () => {
     beforeEach(function () {
       this.$stateParams.job = {
         fileName: 'AllSparkCall.csv',
@@ -136,13 +127,19 @@ describe('Component: userTaskManagerModalModule', () => {
       };
     });
     beforeEach(initComponent);
+    beforeEach(function () {
+      this.controller.intervalCallback(this.taskList);
+      this.$scope.$apply();
+    });
+
     it('the task should be added and set as active', function() {
       expect(this.UserTaskManagerService.submitCsvImportTask)
         .toHaveBeenCalledWith('AllSparkCall.csv', 'CSV content', true);
-      expect(this.controller.setActiveTask).not.toHaveBeenCalled();
-      expect(this.controller.activeTask).toBe(this.taskList[1]);
-      expect(this.controller.activeFilter).toBe(TaskListFilterType.ACTIVE);
-      expect(this.controller.activeTask.jobInstanceId).toEqual('CSV Import 2');
+
+      expect(this.components.userTaskListFilter.bindings[0].filter).toBe(TaskListFilterType.ACTIVE);
+      expect(this.components.userTaskList.bindings[0].task).toEqual(this.taskList[1]);
+      expect(this.components.userTaskList.bindings[0].taskList).toEqual([this.taskList[1]]);
+      expect(this.components.csvUploadResults.bindings[0].inputActiveTask).toEqual(this.taskList[1]);
     });
   });
 });
