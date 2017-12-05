844e5cd5bfef4d1fdff34cbf80b771cea936c08e
Mike Park <mipark2@cisco.com>
2017-12-04 16:25:12 -0800

    refactor(users): add 'assignable-item-checkbox' component
    - 'assignable-license-checkbox' now a wrapper around the more primitive 'assignable-item-checkbox'
    
    test(users): add unit-tests for 'assignable-license-checkbox'

---
+++ app/modules/core/users/userAdd/assignable-services/assignable-services-row/assignable-item-checkbox/assignable-item-checkbox.component.ts
@@ -0,0 +1,57 @@
+import { ILicenseUsage, AssignableServicesItemCategory } from 'modules/core/users/userAdd/assignable-services/shared';
+
+class AssignableItemCheckboxController implements ng.IComponentController {
+  private static readonly itemCategory = AssignableServicesItemCategory.LICENSE;
+  private itemId: string;
+  private license: ILicenseUsage;
+  private stateData: any;  // TODO: better type
+  private onUpdate: Function;
+  public formItemId: string;
+  public isSelected = false;
+  public isDisabled = false;
+
+  /* @ngInject */
+  constructor(
+    private LicenseUsageUtilService,
+  ) {}
+
+  public $onInit(): void {
+    const licenseId = this.license.licenseId;
+    this.itemId = licenseId;
+    this.formItemId = this.LicenseUsageUtilService.sanitizeIdForJs(licenseId);
+
+    // notes:
+    // - 'licenseId' can contain period chars ('.')
+    // - so we wrap interpolated value in double-quotes to prevent unintended deep property creation
+    const stateDataKey = `${AssignableItemCheckboxController.itemCategory}["${licenseId}"]`;
+    this.isSelected = _.get(this.stateData, `${stateDataKey}.isSelected`);
+    this.isDisabled = _.get(this.stateData, `${stateDataKey}.isDisabled`);
+  }
+
+  public recvChange(): void {
+    this.onUpdate({
+      $event: {
+        itemId: this.itemId,
+        itemCategory: AssignableItemCheckboxController.itemCategory,
+
+        // notes:
+        // - for convenience, we include the 'license' property in the callback as well
+        // - this allows for observers looking only at the top-level 'stateData' property to know
+        //   which licenses were selected
+        item: _.pick(this, ['isSelected', 'isDisabled', 'license']),
+      },
+    });
+  }
+}
+
+export class AssignableItemCheckboxComponent implements ng.IComponentOptions {
+  public controller = AssignableItemCheckboxController;
+  public template = require('./assignable-item-checkbox.html');
+  public transclude = true;
+  public bindings = {
+    license: '<',
+    l10nLabel: '@',
+    onUpdate: '&',
+    stateData: '<',
+  };
+}
--- app/modules/core/users/userAdd/assignable-services/assignable-services-row/assignable-license-checkbox/index.ts
+++ app/modules/core/users/userAdd/assignable-services/assignable-services-row/assignable-license-checkbox/index.ts
@@ -1,12 +1,10 @@
 import './assignable-license-checkbox.scss';
 
 import { AssignableLicenseCheckboxComponent } from './assignable-license-checkbox.component';
-import sharedModuleName from 'modules/core/users/userAdd/assignable-services/shared';
 
 export default angular.module('core.users.userAdd.assignable-services.assignable-services-row.assignable-license-checkbox', [
   require('angular-translate'),
   require('collab-ui-ng').default,
-  sharedModuleName,
 ])
   .component('assignableLicenseCheckbox', new AssignableLicenseCheckboxComponent())
   .name;
