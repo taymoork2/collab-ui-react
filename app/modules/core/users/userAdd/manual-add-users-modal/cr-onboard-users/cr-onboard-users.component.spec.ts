496e591debfaaca2934514a1ff5edecd2d28292d
Mike Park <mipark2@cisco.com>
2018-01-09 00:33:03 -0800

    test(users): (18/19) add unit-tests for 'cr-onboard-users'
    - add missing deps to make spec KTEST__MODULAR-friendly

--- app/modules/core/users/userAdd/manual-add-users-modal/cr-onboard-users/cr-onboard-users.component.spec.ts
+++ app/modules/core/users/userAdd/manual-add-users-modal/cr-onboard-users/cr-onboard-users.component.spec.ts
@@ -7,25 +7,35 @@ describe('Component: crOnboardUsers:', () => {
   beforeEach(function (this: Test) {
     this.initModules(moduleName);
     this.injectDependencies(
-      // TODO: add dependencies here
     );
   });
 
-  // TODO: use as-appropriate
   beforeEach(function (this: Test) {
-    // this.compileTemplate('<cr-onboard-users></cr-onboard-users>');
-    // this.compileComponent('crOnboardUsers', {});
+    this.compileComponent('crOnboardUsers', {});
   });
 
   describe('primary behaviors (view):', () => {
-    it('...', function (this: Test) {
-      // TODO: implement
+    it('should render a form', function (this: Test) {
+      expect(this.view.find('form.onboard-users').length).toBe(1);
+    });
+
+    it('should render a radio button list', function (this: Test) {
+      expect(this.view.find('cs-radiolist[options="$ctrl.userInputOptions"]').length).toBe(1);
+    });
+
+    it('should render a token field widget regardless of which radio button is selected', function (this: Test) {
+      const CS_RADIO_LIST = 'cs-radiolist[options="$ctrl.userInputOptions"]';
+      const OPTION_0 = `${CS_RADIO_LIST} input[type="radio"][value="0"]`;
+      const OPTION_1 = `${CS_RADIO_LIST} input[type="radio"][value="1"]`;
+      this.view.find(`${OPTION_0}`).click();
+      expect(this.view.find('input.token-input[tokens="$ctrl.model.userList"]').length).toBe(1);
+      this.view.find(`${OPTION_1}`).click();
+      expect(this.view.find('input.token-input[tokens="$ctrl.model.userList"]').length).toBe(1);
     });
   });
 
-  describe('primary behaviors (controller):', () => {
-    it('...', function (this: Test) {
-      // TODO: implement
-    });
-  });
+  // TODO (mipark2): add unit-tests for controller methods
+  // describe('primary behaviors (controller):', () => {
+  //   ...
+  // });
 });
