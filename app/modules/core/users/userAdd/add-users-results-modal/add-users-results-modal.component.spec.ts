2de97607b8a71083a501b4ab45f33fe18feb6061
Mike Park <mipark2@cisco.com>
2018-01-23 22:55:08 -0800

    test(users): (13/14) add unit-tests
    - 'add-users-results-modal' component

--- app/modules/core/users/userAdd/add-users-results-modal/add-users-results-modal.component.spec.ts
+++ app/modules/core/users/userAdd/add-users-results-modal/add-users-results-modal.component.spec.ts
@@ -1,31 +1,141 @@
 import moduleName from './index';
 import { AddUsersResultsModalController } from './add-users-results-modal.component';
+import { CrAddUsersResultsComponent } from './cr-add-users-results/cr-add-users-results.component';
+import { MultiStepModalComponent } from 'modules/core/shared/multi-step-modal/multi-step-modal.component';
 
-type Test = atlas.test.IComponentTest<AddUsersResultsModalController, {}, {}>;
+type Test = atlas.test.IComponentTest<AddUsersResultsModalController, {
+  $previousState;
+  $state;
+  Analytics;
+  OnboardService;
+  OnboardStore;
+}, {
+  components: {
+    multiStepModal: atlas.test.IComponentSpy<MultiStepModalComponent>;
+    crAddUsersResults: atlas.test.IComponentSpy<CrAddUsersResultsComponent>;
+  },
+}>;
 
 describe('Component: addUsersResultsModal:', () => {
   beforeEach(function (this: Test) {
-    this.initModules(moduleName);
-    this.injectDependencies(
-      // TODO: add dependencies here
+    this.components = {
+      multiStepModal: this.spyOnComponent('multiStepModal'),
+      crAddUsersResults: this.spyOnComponent('crAddUsersResults'),
+    };
+    this.initModules(
+      moduleName,
+      this.components.multiStepModal,
+      this.components.crAddUsersResults,
     );
+    this.injectDependencies(
+      '$previousState',
+      '$scope',
+      '$state',
+      'Analytics',
+      'OnboardService',
+      'OnboardStore',
+    );
+    this.$scope.fakeConvertUsersFlow = false;
+    this.$scope.fakeConvertPending = false;
+    this.$scope.fakeNumAddedUsers = 0;
+    this.$scope.fakeNumUpdatedUsers = 0;
+    this.$scope.fakeResults = {
+      resultList: [],
+      errors: [],
+      warnings: [],
+    };
   });
 
-  // TODO: use as-appropriate
-  beforeEach(function (this: Test) {
-    // this.compileTemplate('<add-users-results-modal></add-users-results-modal>');
-    // this.compileComponent('addUsersResultsModal', {});
-  });
+  function initComponent(this: Test) {
+    this.compileComponent('addUsersResultsModal', {
+      convertPending: 'fakeConvertPending',
+      convertUsersFlow: 'fakeConvertUsersFlow',
+      numUpdatedUsers: 'fakeNumUpdatedUsers',
+      numAddedUsers: 'fakeNumAddedUsers',
+      results: 'fakeResults',
+    });
+  }
 
-  describe('primary behaviors (view):', () => {
-    it('...', function (this: Test) {
-      // TODO: implement
+  describe('initial state:', () => {
+    describe('convert flow:', () => {
+      it('should have a title for converting users', function (this: Test) {
+        this.$scope.fakeConvertUsersFlow = true;
+        initComponent.call(this);
+        expect(this.components.multiStepModal.bindings[0].l10nTitle).toBe('usersPage.convertUsers');
+      });
+
+      it('should have finish button if convert flow', function (this: Test) {
+        this.$scope.fakeConvertUsersFlow = true;
+        initComponent.call(this);
+        expect(this.components.multiStepModal.bindings[0].customFinishL10nLabel).toBe('common.finish');
+      });
+    });
+
+    describe('add users flow:', () => {
+      it('should have a title for adding users', function (this: Test) {
+        this.$scope.fakeConvertUsersFlow = false;
+        initComponent.call(this);
+        expect(this.components.multiStepModal.bindings[0].l10nTitle).toBe('usersPage.addUsers');
+      });
+
+      it('should have finish button if no errors', function (this: Test) {
+        this.$scope.fakeConvertUsersFlow = false;
+        this.$scope.fakeResults.errors = [];
+        initComponent.call(this);
+        expect(this.components.multiStepModal.bindings[0].customFinishL10nLabel).toBe('common.finish');
+      });
+
+      it('should have a skip errors and finish button if errors are present', function (this: Test) {
+        this.$scope.fakeConvertUsersFlow = false;
+        this.$scope.fakeResults.errors = ['fake-errorMsg-1'];
+        initComponent.call(this);
+        expect(this.components.multiStepModal.bindings[0].customFinishL10nLabel).toBe('usersPage.skipErrorsAndFinish');
+      });
+    });
+
+    it('should have dismiss functionality', function (this: Test) {
+      initComponent.call(this);
+      spyOn(this.controller, 'dismissModal');
+      this.components.multiStepModal.bindings[0].dismiss();
+      expect(this.controller.dismissModal).toHaveBeenCalled();
     });
   });
 
   describe('primary behaviors (controller):', () => {
-    it('...', function (this: Test) {
-      // TODO: implement
+    describe('dismissModal():', () => {
+      it('should track the event', function (this: Test) {
+        spyOn(this.Analytics, 'trackAddUsers');
+        this.$scope.fakeConvertUsersFlow = false;
+        initComponent.call(this);
+        this.controller.dismissModal();
+        expect(this.Analytics.trackAddUsers).toHaveBeenCalledWith(this.Analytics.eventNames.CANCEL_MODAL);
+      });
+
+      it('should set "convertCancelled" property in OnboardStore to true if a convert was pending', function (this: Test) {
+        this.$scope.fakeConvertUsersFlow = true;
+        this.$scope.fakeConvertPending = true;
+        initComponent.call(this);
+        this.controller.dismissModal();
+        expect(this.OnboardStore['users.convert'].convertCancelled).toBe(true);
+      });
+    });
+
+    describe('goToUsersPage():', () => {
+      it('should track the event and jump back to users list', function (this: Test) {
+        spyOn(this.$previousState, 'forget');
+        spyOn(this.OnboardService, 'createPropertiesForAnalytics').and.returnValue('fake-createPropertiesForAnalytics-result');
+        spyOn(this.Analytics, 'trackAddUsers');
+        spyOn(this.$state, 'go');
+        this.$scope.fakeNumAddedUsers = 1;
+        this.$scope.fakeNumUpdatedUsers = 2;
+        this.$scope.fakeResults.errors = ['fake-errorMsg-1', 'fake-errorMsg-2', 'fake-errorMsg-3'];
+        initComponent.call(this);
+        this.controller.goToUsersPage();
+        expect(this.$previousState.forget).toHaveBeenCalledWith('modalMemo');
+        expect(this.OnboardService.createPropertiesForAnalytics).toHaveBeenCalledWith(2, 1, 3, {});
+        expect(this.Analytics.trackAddUsers).toHaveBeenCalledWith(this.Analytics.sections.ADD_USERS.eventNames.FINISH, null, 'fake-createPropertiesForAnalytics-result');
+        expect(this.$state.go).toHaveBeenCalledWith('users.list');
+      });
     });
   });
 });
