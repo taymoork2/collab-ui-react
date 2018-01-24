e2722c4ce3873121306ed6069a3620cecb31ff5c
Mike Park <mipark2@cisco.com>
2018-01-23 22:55:04 -0800

    test(users): (12/14) add unit-tests
    - 'cr-add-users-results' component

--- app/modules/core/users/userAdd/add-users-results-modal/cr-add-users-results/index.ts
+++ app/modules/core/users/userAdd/add-users-results-modal/cr-add-users-results/index.ts
@@ -1,8 +1,12 @@
 import { CrAddUsersResultsComponent } from './cr-add-users-results.component';
+import * as analyticsModuleName from 'modules/core/analytics';
+import userAddSharedModuleName from 'modules/core/users/userAdd/shared';
 
 export default angular.module('core.users.userAdd.add-users-results-modal.cr-add-users-results', [
   require('angular-translate'),
   require('@collabui/collab-ui-ng').default,
+  analyticsModuleName,
+  userAddSharedModuleName,
 ])
   .component('crAddUsersResults', new CrAddUsersResultsComponent())
   .name;
