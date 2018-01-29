4d00f6f1191739053f4ecb915ca3085777f5483d
Brian Spence <brspence@cisco.com>
2018-01-29 13:07:33 -0600

    WIP(user-manage): fix cr-user-csv-export styling
    - move spinner and cancel styling into component scss
    - put component anchors in paragraphs for typography
    - refactor .csv-export-link with user-manage__manage-type-list-item-indent
      - only apply to two implementations used in manage type radio options
      - remove unnecessary bottom padding
    - remove trailing period in translation

--- app/modules/core/users/userCsv/userCsvExport.component.ts
+++ app/modules/core/users/userCsv/userCsvExport.component.ts
@@ -2,6 +2,8 @@ import { CsvDownloadTypes, CsvDownloadService } from 'modules/core/csvDownload';
 import { IToolkitModalService } from 'modules/core/modal';
 import { AutoAssignTemplateModel } from 'modules/core/users/shared/auto-assign-template';
 
+import './userCsvExport.scss';
+
 /* UI for allowing user to export the Csv and Template. Does not do downloading on its own! See csvDownload.component */
 export class UserCsvExportComponent {
   public controller = UserCsvExportController;
