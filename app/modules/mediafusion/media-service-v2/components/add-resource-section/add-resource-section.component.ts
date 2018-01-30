74104ad8c7eb600ae4187a8b0b62251ddcb0de36
Naveen amalraj <namalraj@cisco.com>
2018-01-30 10:05:03 +0530

    fix(mediafusion): reverting a commit

--- app/modules/mediafusion/media-service-v2/components/add-resource-section/add-resource-section.component.ts
+++
@@ -1,8 +0,0 @@
-export class AddResourceSectionController implements ng.IComponentController {
-}
-
-export class AddResourceSectionComponent implements ng.IComponentOptions {
-  public controller = AddResourceSectionController;
-  public template = require('./add-resource-section.html');
-  public bindings = {};
-}
