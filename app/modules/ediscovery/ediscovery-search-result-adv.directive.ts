d57c49566a5bba78a32337c23f1bacee0686f6e1
Alina Gendel <algendel@cisco.com>
2018-01-31 15:51:12 -0800

    chore(ediscovery): rename ediscovery-search-result-adv directive

--- app/modules/ediscovery/ediscovery-search-result-adv.directive.ts
+++
@@ -1,7 +0,0 @@
-class EdiscoverySearchResultAdvDirective implements ng.IDirective {
-  public template = require('./ediscovery-search-result-adv.html');
-  public scope = true;
-  public restrict = 'E';
-}
-
-export const EdiscoverySearchResultAdvDirectiveFactory: ng.IDirectiveFactory = () => new EdiscoverySearchResultAdvDirective();
