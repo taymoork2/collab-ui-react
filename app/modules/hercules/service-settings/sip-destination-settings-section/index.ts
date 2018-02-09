fc7f70d47667a2d131d160cb4b5d1d0138bf73f0
Kjetil Ree <kjeree@cisco.com>
2018-02-09 09:28:58 +0100

    chore(hercules): Removing atlasHybridCallDiagnosticTool feature toggle
    GA for 8 weeks now, time to remove the feature toggle!
    
    https://sqbu-github.cisco.com/WebExSquared/fusion-management-service/issues/701

--- app/modules/hercules/service-settings/sip-destination-settings-section/index.ts
+++ app/modules/hercules/service-settings/sip-destination-settings-section/index.ts
@@ -1,19 +1,17 @@
 import { SipDestinationSettingsSectionComponent } from './sip-destination-settings-section.component';
-import USSServiceModuleName from 'modules/hercules/services/uss.service';
-import FeatureToggleServiceModuleName from 'modules/core/featureToggle';
-import * as AuthInfoModuleName from 'modules/core/scripts/services/authinfo';
-import ModalServiceModuleName from 'modules/core/modal';
-import NotificationServiceModuleName from 'modules/core/notifications';
+import ussServiceModuleName from 'modules/hercules/services/uss.service';
+import * as authInfoModuleName from 'modules/core/scripts/services/authinfo';
+import modalServiceModuleName from 'modules/core/modal';
+import notificationServiceModuleName from 'modules/core/notifications';
 
 import './_sip-destination-settings-section.scss';
 
 export default angular
   .module('hercules.sip-destination-test-tool', [
-    ModalServiceModuleName,
-    AuthInfoModuleName,
-    FeatureToggleServiceModuleName,
-    NotificationServiceModuleName,
-    USSServiceModuleName,
+    modalServiceModuleName,
+    authInfoModuleName,
+    notificationServiceModuleName,
+    ussServiceModuleName,
   ])
   .component('sipDestinationSettingsSection', new SipDestinationSettingsSectionComponent())
   .name;
