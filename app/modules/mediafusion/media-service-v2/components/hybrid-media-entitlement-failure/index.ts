92a84b8a89ab65c1806a80fa31a449799a763a06
Naveen amalraj <namalraj@cisco.com>
2018-03-19 12:20:11 +0530

    WIP(mediafusion): review comments - Part 2

--- app/modules/mediafusion/media-service-v2/components/hybrid-media-entitlement-failure/index.ts
+++ app/modules/mediafusion/media-service-v2/components/hybrid-media-entitlement-failure/index.ts
@@ -1,8 +1,6 @@
 import { HybridMediaEntitlementFailureComponent } from './hybrid-media-entitlement-failure.component';
 
 export default angular.module('mediafusion.media-service-v2.components.hybrid-media-entitlement-failure', [
-  require('angular-translate'),
-  require('@collabui/collab-ui-ng').default,
 ])
   .component('hybridMediaEntitlementFailure', new HybridMediaEntitlementFailureComponent())
   .name;
