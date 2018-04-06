e14d7eaeba8e8de3ffda5c8c3fb070f83e6009dd
amit <kumara4@cisco.com>
2018-04-06 11:48:10 +0530

    refactor(huron): [CIR-218]
    [CIR-218]
    
    [CIR-218]

--- app/modules/huron/features/autoAttendant/builder/aaBuilderMainCtrl.spec.js
+++ app/modules/huron/features/autoAttendant/builder/aaBuilderMainCtrl.spec.js
@@ -935,10 +935,10 @@ describe('Controller: AABuilderMainCtrl', function () {
     it('should return an error when uuid of a number is different on CMI and CES', function () {
       this.$scope.vm.aaModel = {};
       this.$scope.vm.aaModel.aaRecords = this.ces;
-      this.$scope.vm.aaModel.aaRecords[1].assignedResources.uuid="00097a86-45ef-44a7-aa78-6d32a0ca1d3c";
-      this.controller.selectAA("AAA2");
+      this.$scope.vm.aaModel.aaRecords[1].assignedResources.uuid = '00097a86-45ef-44a7-aa78-6d32a0ca1d3c';
+      this.controller.selectAA('AAA2');
       this.$scope.$apply();
-      this.$scope.vm.aaModel.aaRecords[1].assignedResources.uuid="00097a86-45ef-44a7-aa78-6d32a0ca1d3b";
+      this.$scope.vm.aaModel.aaRecords[1].assignedResources.uuid = '00097a86-45ef-44a7-aa78-6d32a0ca1d3b';
       expect(this.AANotificationService.error).toHaveBeenCalled();
     });
   });
