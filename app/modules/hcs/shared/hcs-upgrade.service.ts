10744527c213d5583da60a15f7452a98f013116d
Sani Tripathy <sani@cisco.com>
2018-04-12 16:38:36 -0500

    WIP(huron): I1735: Add Upgrade cluster service apis

--- app/modules/hcs/shared/hcs-upgrade.service.ts
+++ app/modules/hcs/shared/hcs-upgrade.service.ts
@@ -1,12 +1,18 @@
 import { ISftpServer } from '../setup/hcs-setup-sftp';
+import { IHcsCluster } from './hcs-upgrade';
 
 interface ISftpServerResource extends ng.resource.IResourceClass<ng.resource.IResource<ISftpServer>> {
   update: ng.resource.IResourceMethod<ng.resource.IResource<ISftpServer>>;
 }
 
+interface IClusterResource extends ng.resource.IResourceClass<ng.resource.IResource<IHcsCluster>> {
+  update: ng.resource.IResourceMethod<ng.resource.IResource<IHcsCluster>>;
+}
+
 export class HcsUpgradeService {
   private sftpServerResource: ISftpServerResource;
-
+  private clusterResource: IClusterResource;
+  private clusterCustomerResource: IClusterResource;
   /* @ngInject */
   constructor(
     private $resource: ng.resource.IResourceService,
@@ -31,8 +37,20 @@ export class HcsUpgradeService {
         Authorization: BASIC_AUTH_VAL,
       },
     };
-
     this.sftpServerResource = <ISftpServerResource>this.$resource(BASE_URL + 'partners/:partnerId/sftpServers/:sftpServerId', {},
+      {
+        update: updateAction,
+        save: saveAction,
+        query: queryAction,
+      });
+    this.clusterResource = <IClusterResource>this.$resource(BASE_URL + 'partners/:partnerId/clusters/:clusterid', {},
+      {
+        update: updateAction,
+        save: saveAction,
+        query: queryAction,
+      });
+
+    this.clusterCustomerResource = <IClusterResource>this.$resource(BASE_URL + 'partners/:partnerId/clusters?customer=:customerId', {},
       {
         update: updateAction,
         save: saveAction,
@@ -63,6 +81,33 @@ export class HcsUpgradeService {
   public listSftpServers(): ng.IPromise <any[]> {
     return this.sftpServerResource.query({
       partnerId: this.Authinfo.getOrgId(),
+    }).$promise;
+  }
+
+  public getCluster(_clusterId: string): ng.IPromise<IHcsCluster> {
+    return this.clusterResource.get({
+      partnerId: this.Authinfo.getOrgId(),
+      clusterId: _clusterId,
+    }).$promise;
+  }
+
+  public updateCluster(_clusterId: string, cluster: IHcsCluster) {
+    return this.clusterResource.update({
+      partnerId: this.Authinfo.getOrgId(),
+      clusterId: _clusterId,
+    }, cluster).$promise;
+  }
+
+  public listAllClusters(): ng.IPromise <any[]> {
+    return this.clusterResource.query({
+      partnerId: this.Authinfo.getOrgId(),
+    }).$promise;
+  }
+
+  public listClusters(customerId?: string): ng.IPromise <any[]> {
+    return this.clusterCustomerResource.query({
+      partnerId: this.Authinfo.getOrgId(),
+      customerId: customerId,
     }).$promise;
   }
 }
