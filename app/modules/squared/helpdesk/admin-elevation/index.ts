725b1c673de4d650c85b0934ae3e089f946ea91a
Anders Froyhaug <afroyhau@cisco.com>
2018-04-20 14:23:51 +0200

    WIP: Added unit test for elevation

--- app/modules/squared/helpdesk/admin-elevation/index.ts
+++ app/modules/squared/helpdesk/admin-elevation/index.ts
@@ -1,12 +1,13 @@
 require('./admin-elevation.scss');
 import { AdminElevationService } from './admin-elevation.service';
-
 import { HelpdeskAdminElevationComponent } from './admin-elevation.component';
+import * as coreUrlConfigModule from 'modules/core/config/urlConfig';
 
 export default angular
   .module('helpdesk.admin-elevation', [
     require('@collabui/collab-ui-ng').default,
     require('angular-translate'),
+    coreUrlConfigModule,
   ])
   .component('helpdeskAdminElevation', new HelpdeskAdminElevationComponent())
   .service('AdminElevationService', AdminElevationService)
