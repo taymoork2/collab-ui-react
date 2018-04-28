c578359e21d96725349936b155e105ae5f45cd04
Xiaopeng Liu <xiaopeli@cisco.com>
2018-04-28 17:08:41 +0800

    feat(core): add partner component template

--- app/modules/core/partnerReports/webexReports/diagnostic/index.ts
+++ app/modules/core/partnerReports/webexReports/diagnostic/index.ts
@@ -1,5 +1,10 @@
-import { DgcPartnerTabComponent } from './dgc-partner-tab.component';
 import notifications from 'modules/core/notifications';
+import { DgcPartnerTabComponent } from './dgc-partner-tab.component';
+import { DgcTimeLineComponent } from './time-line.component';
+import { DgcTimeZoneComponent } from './time-zone.component';
+import { DgcWebexReportsSearchComponent } from './webex-reports-search.component';
+import { MeetingDetailsComponent } from './tab-meeting-details.component';
+import { ParticipantsComponent } from './tab-participants.component';
 import { PartnerSearchService } from './partner-search.service';
 
 export default angular
@@ -12,4 +17,9 @@ export default angular
   ])
   .service('PartnerSearchService', PartnerSearchService)
   .component('dgcPartnerTab', new DgcPartnerTabComponent())
+  .component('dgcPartnerTimeLine', new DgcTimeLineComponent())
+  .component('dgcPartnerTimeZone', new DgcTimeZoneComponent())
+  .component('dgcPartnerWebexReportsSearch', new DgcWebexReportsSearchComponent())
+  .component('dgcPartnerTabMeetingDetail', new MeetingDetailsComponent())
+  .component('dgcPartnerTabParticipants', new ParticipantsComponent())
   .name;
