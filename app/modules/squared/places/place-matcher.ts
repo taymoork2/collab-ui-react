82cc1bdf9395ab8ad0a0e20c9aca5896d8a593a2
Wouter Lammers <woulamme@cisco.com>
2018-05-03 08:54:07 +0200

    fix(places): Validate external linked account
    Check if the external linked account an admin configures is not already
    used either as the email address for a user, or as an external linked
    account of the same type on another place.
    
    SPARK-10887

--- app/modules/squared/places/place-matcher.ts
+++ app/modules/squared/places/place-matcher.ts
@@ -19,9 +19,15 @@ export class PlaceMatcher implements IMatcher<IPlace> {
   }
 
   private termMatchesAnyFieldOfItem(term: string, item: IPlace): boolean {
-    return item && ['displayName', 'readableType', 'sipUrl']
+    return item && (['displayName', 'readableType', 'sipUrl']
         .some((field) => {
           return _.includes(_.toLower(item[field]), term);
-        });
+        })
+      || _.some(item['entitlements'] || [], (entitlement) => {
+        return _.includes(_.toLower(entitlement), term);
+      })
+      || _.some(item['externalLinkedAccounts'] || [], (extLinkedAcct) => {
+        return _.includes(_.toLower(extLinkedAcct.accountGUID), term);
+      }));
   }
 }
