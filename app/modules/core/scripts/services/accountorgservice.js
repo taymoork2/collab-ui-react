66ee25b3643b6f3c349cf1e5a4c09e891a5cd436
Brian Spence <brspence@cisco.com>
2018-05-10 13:41:19 -0500

    WIP(core): remove unused AccountOrgService methods

--- app/modules/core/scripts/services/accountorgservice.js
+++ app/modules/core/scripts/services/accountorgservice.js
@@ -23,11 +23,6 @@
       getServices: getServices,
       addMessengerInterop: addMessengerInterop,
       deleteMessengerInterop: deleteMessengerInterop,
-      addOrgCloudSipUri: addOrgCloudSipUri,
-      addOrgDataRetentionPeriodDays: addOrgDataRetentionPeriodDays,
-      modifyOrgDataRetentionPeriodDays: modifyOrgDataRetentionPeriodDays,
-      deleteOrgSettings: deleteOrgSettings,
-      getOrgSettings: getOrgSettings,
       getAppSecurity: getAppSecurity,
       setAppSecurity: setAppSecurity,
       getBlockExternalCommunication: getBlockExternalCommunication,
@@ -47,12 +42,6 @@
 
     function getServicesUrl(org) {
       var url = accountUrl + 'organizations/' + org + '/services';
-
-      return url;
-    }
-
-    function getAccountSettingsUrl(org) {
-      var url = accountUrl + 'organization/' + org + '/settings';
 
       return url;
     }
@@ -83,57 +72,6 @@
       var url = getServicesUrl(org) + '/messengerInterop';
 
       return $http.delete(url);
-    }
-
-    function addOrgCloudSipUri(org, cloudSipUri) {
-      var url = getAccountSettingsUrl(org);
-      var request = {
-        id: org,
-        settings: [{
-          key: 'orgCloudSipUri',
-          value: cloudSipUri + '.ciscospark.com',
-        }],
-      };
-
-      return $http.put(url, request);
-    }
-
-    function addOrgDataRetentionPeriodDays(org, dataRetentionPeriodDays) {
-      var url = getAccountSettingsUrl(org);
-      var request = {
-        id: org,
-        settings: [{
-          key: 'dataRetentionPeriodDays',
-          value: dataRetentionPeriodDays,
-        }],
-      };
-
-      return $http.put(url, request);
-    }
-
-    function modifyOrgDataRetentionPeriodDays(org, dataRetentionPeriodDays) {
-      var url = getAccountSettingsUrl(org);
-      var request = {
-        id: org,
-        settings: [{
-          key: 'dataRetentionPeriodDays',
-          value: dataRetentionPeriodDays,
-        }],
-      };
-
-      return $http.patch(url, request);
-    }
-
-    function deleteOrgSettings(org) {
-      var url = getAccountSettingsUrl(org) + '/' + org;
-
-      return $http.delete(url);
-    }
-
-    function getOrgSettings(org) {
-      var url = getAccountSettingsUrl(org) + '/' + org;
-
-      return $http.get(url);
     }
 
     // Get the account App Security Status from the clientSecurityPolicy API(boolean)
