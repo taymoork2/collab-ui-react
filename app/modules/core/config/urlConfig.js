27f64610ce7ba27fc48f7bd4b70e84c442ccb304
tarun joshi <tarujosh@cisco.com>
2018-05-17 10:58:10 -0700

    fix(*): pointing back to integration through local
    pointing back to integration through local

--- app/modules/core/config/urlConfig.js
+++ app/modules/core/config/urlConfig.js
@@ -3,7 +3,7 @@
 
   var serviceUrlMapping = {
     AdminServiceUrl: {
-      dev: 'http://localhost:8080/atlas-server/admin/api/v1/',
+      dev: 'https://atlas-intb.ciscospark.com/admin/api/v1/',
       cfe: 'https://atlas-loada.ciscospark.com/admin/api/v1/',
       integration: 'https://atlas-intb.ciscospark.com/admin/api/v1/',
       prod: 'https://atlas-a.wbx2.com/admin/api/v1/',
