f439e33c312decb8ad4cb989320ed11488887e3a
Paul Jeter <pajeter@cisco.com>
2018-05-18 09:58:14 -0500

    feat(core): remove directive and fix css

--- app/modules/core/shared/cui-panel/index.ts
+++
@@ -1,6 +0,0 @@
-require('./cui-panel.scss');
-import { CuiPanelDirectiveFactory } from './cui-panel.directive';
-
-export default angular.module('core.shared.cui-panel', [])
-  .directive('cuiPanel', CuiPanelDirectiveFactory)
-  .name;
