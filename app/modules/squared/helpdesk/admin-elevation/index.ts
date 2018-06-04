50aee4fe57ccb7d8d4ac5012637bb88e1751a1b6
Helge Langehaug <hellange@cisco.com>
2018-06-04 13:58:34 +0200

    WIP: Added some splunk logging of helpdesk admin elevation.

--- app/modules/squared/helpdesk/admin-elevation/index.ts
+++ app/modules/squared/helpdesk/admin-elevation/index.ts
@@ -2,12 +2,14 @@ require('./admin-elevation.scss');
 import { AdminElevationService } from './admin-elevation.service';
 import { HelpdeskAdminElevationComponent } from './admin-elevation.component';
 import * as coreUrlConfigModule from 'modules/core/config/urlConfig';
+import metricsModule from 'modules/core/metrics';
 
 export default angular
   .module('helpdesk.admin-elevation', [
     require('@collabui/collab-ui-ng').default,
     require('angular-translate'),
     coreUrlConfigModule,
+    metricsModule,
   ])
   .component('helpdeskAdminElevation', new HelpdeskAdminElevationComponent())
   .service('AdminElevationService', AdminElevationService)
