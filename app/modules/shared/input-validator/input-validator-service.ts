bbe6200d06a4a53d245c99da5a32e6ce104afe82
Brad DeGrazia <degrazia@cisco.com>
2018-06-06 11:56:44 -0700

    fix(*): clear validator messages on input change
    If the user enters a value into an input and gets a validation error like 'invalid email' this
    messages continues to be displayed even after the user changes the value of the input.  This error
    message persists until the user stops typing and the debounce timer fires, or until the user moves
    off the control causing the blur event.  With this change, the validation message is reset
    as soon as the user changes the value of the input so that the control is more responsive to the
    user's actions, and gives an overall better user experience.

--- app/modules/shared/input-validator/input-validator-service.ts
+++ app/modules/shared/input-validator/input-validator-service.ts
@@ -5,6 +5,9 @@ interface IFormField {
   $touched: boolean;
   $invalid?: boolean;
   $valid?: boolean;
+  $setPristine: Function;
+  $setUntouched: Function;
+  $setValidity: Function;
 }
 
 interface IResult {
@@ -17,6 +20,8 @@ export class InputValidatorService {
   constructor(
   ) {}
 
+  // returns an object in the form {error: boolean} for use with an input field
+  // usage:  ng-class="$ctrl.validatorSvc.makeErrorClass(myForm.myField))"
   public makeErrorClass(name: IFormField): IResult {
     if (_.isUndefined(name)) {
       name = this.createEmptyFormField();
@@ -26,6 +31,9 @@ export class InputValidatorService {
     };
   }
 
+  // creates an obect in the form {error: boolean, warning: boolean} for use with an input field
+  // usage: ng-class="$ctrl.validatorSvc.makeErrorAndWarnClass(myForm.myField, warningFlag)"
+  // (typically warning flag will be set by the validator as something like '$ctrl.messages.myField.warning')
   public makeErrorAndWarnClass(name: IFormField, warnFlag: boolean): IResult {
     if (_.isUndefined(name)) {
       name = this.createEmptyFormField();
@@ -36,11 +44,24 @@ export class InputValidatorService {
     };
   }
 
+  // clears field conditions that would show an error or warning (to be used with ng-keyup on input)
+  // usage: ng-keyup="$ctrl.validatorSvc.resetField(myForm.myField, $ctrl, 'myValidatorInCamelCase')"
+  public resetField(field: IFormField, controller: ng.IController, validator: string): void {
+    field.$setPristine();
+    field.$setUntouched();
+    if (controller && !_.isEmpty(validator)) {
+      field.$setValidity(validator, false, controller);
+    }
+  }
+
   private createEmptyFormField(): IFormField {
     return {
       $dirty: false,
       $invalid: false,
       $touched: false,
+      $setPristine: _.noop,
+      $setUntouched: _.noop,
+      $setValidity: _.noop,
     };
   }
 }
