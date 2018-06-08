745c612e45f45815c196d4834c599c68225d5be9
Brian Spence <brspence@cisco.com>
2018-06-08 17:10:18 -0500

    WIP(integrations): remove module-level placeholder scss

--- app/modules/integrations-management/index.ts
+++ app/modules/integrations-management/index.ts
@@ -2,7 +2,6 @@ import collabUiModuleName from '@collabui/collab-ui-ng';
 import * as ngTranslateModuleName from 'angular-translate';
 import { IntegrationsManagementListComponent } from './integrations-management-list.component';
 import { IntegrationsManagementFakeService } from './integrations-management.fake-service';
-import './integrations-management.scss';
 
 export default angular.module('integrations-management', [
   ngTranslateModuleName,
