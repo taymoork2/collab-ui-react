e1db24a11fc6f2abbd98199099cfb649bdaf5269
Brian Spence <brspence@cisco.com>
2018-06-08 17:08:29 -0500

    WIP(integrations): create fake service for development purposes
    - https://jira-eng-gpk2.cisco.com/jira/browse/SPARK-18919

--- app/modules/integrations-management/index.ts
+++ app/modules/integrations-management/index.ts
@@ -1,12 +1,13 @@
-import './integrations-management.scss';
-
-import { IntegrationsManagementListComponent } from './integrations-management-list.component';
-import * as ngTranslateModuleName from 'angular-translate';
 import collabUiModuleName from '@collabui/collab-ui-ng';
+import * as ngTranslateModuleName from 'angular-translate';
+import { IntegrationsManagementListComponent } from './integrations-management-list.component';
+import { IntegrationsManagementFakeService } from './integrations-management.fake-service';
+import './integrations-management.scss';
 
 export default angular.module('integrations-management', [
   ngTranslateModuleName,
   collabUiModuleName,
 ])
   .component('integrationsManagementList', new IntegrationsManagementListComponent())
+  .service('IntegrationsManagementFakeService', IntegrationsManagementFakeService)
   .name;
