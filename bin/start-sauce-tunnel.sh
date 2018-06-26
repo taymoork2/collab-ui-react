84d8d1ce64d8254c3ed10b935de05932324db1a6
Mike Park <mipark2@cisco.com>
2018-06-26 14:09:46 -0700

    chore(sauce): tidy up old bash code
    - add log-level-like prefixes to 'echo' statements (e.g. '[INFO]',
      '[WARN]', '[ERROR]')
    - update 2-space indent levels -> 4-space
    - shellcheck-driven cleanups

--- bin/start-sauce-tunnel.sh
+++ bin/start-sauce-tunnel.sh
@@ -4,10 +4,11 @@ set -e
 
 this_pwd="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# shellcheck disable=SC1090
 source "${this_pwd}/include/env-var-helpers"
 
 if ! is_ci; then
-    echo "Warning: using limited-privilege Sauce creds."
+    echo "[WARN] Using limited-privilege Sauce creds."
     inj_build_env_vars_for "dev" >/dev/null
 fi
 
@@ -19,79 +20,79 @@ export SAUCE_USERNAME="${SAUCE__USERNAME}"
 # shellcheck disable=SC2153
 export SAUCE_ACCESS_KEY="${SAUCE__ACCESS_KEY}"
 
-PLATFORM=$(uname)
-SC_VERSION="${SAUCE__SC_VERSION}"
-WORK_DIR="${this_pwd}/../.sauce"
+platform=$(uname)
+sc_version="${SAUCE__SC_VERSION}"
+work_dir="${this_pwd}/../.sauce"
 
-SC_DIR="${WORK_DIR}/sc"
-READY_FILE="${WORK_DIR}/sc.ready"
-PID_FILE="${WORK_DIR}/sc.pid"
+sc_dir="${work_dir}/sc"
+sc_ready_file="${work_dir}/sc.ready"
+sc_pid_file="${work_dir}/sc.pid"
 
-if [[ $PLATFORM == 'Darwin' ]]; then
-  SC_PACKAGE="sc-${SC_VERSION}-osx.zip"
+if [[ $platform == 'Darwin' ]]; then
+    sc_package="sc-${sc_version}-osx.zip"
 else
-  SC_PACKAGE="sc-${SC_VERSION}-linux.tar.gz"
+    sc_package="sc-${sc_version}-linux.tar.gz"
 fi
 
-SC_URL="https://saucelabs.com/downloads/${SC_PACKAGE}"
-SC_PACKAGE_PATH="${WORK_DIR}/${SC_PACKAGE}"
+sc_url="https://saucelabs.com/downloads/${sc_package}"
+sc_package_path="${work_dir}/${sc_package}"
 
 # 'SAUCE__RM_WORK_DIR' => jenkins job config env var
 # - see: https://sqbu-jenkins.cisco.com:8443/job/team/job/atlas/job/atlas-web/configure
 if [ "$SAUCE__RM_WORK_DIR" = "true" ]; then
-  rm -rf "$WORK_DIR"
+    rm -rf "$work_dir"
 fi
 
-mkdir -p "$WORK_DIR"
+mkdir -p "$work_dir"
 
-if [ ! -d "$SC_DIR" ]; then
-  echo "Downloading Sauce Connect binary (v${SC_VERSION})"
-  curl -o "$SC_PACKAGE_PATH" "$SC_URL"
-  if [[ $PLATFORM == 'Darwin' ]]; then
-    unzip "$SC_PACKAGE_PATH" -d "$WORK_DIR"
-    mv "${WORK_DIR}/sc-${SC_VERSION}-osx" "$SC_DIR"
-  else
-    tar -xf "$SC_PACKAGE_PATH" -C "$WORK_DIR"
-    mv "${WORK_DIR}/sc-${SC_VERSION}-linux" "$SC_DIR"
-  fi
+if [ ! -d "$sc_dir" ]; then
+    echo "[INFO] Downloading Sauce Connect binary (v${sc_version})"
+    curl -o "$sc_package_path" "$sc_url"
+    if [[ $platform == 'Darwin' ]]; then
+        unzip "$sc_package_path" -d "$work_dir"
+        mv "${work_dir}/sc-${sc_version}-osx" "$sc_dir"
+    else
+        tar -xf "$sc_package_path" -C "$work_dir"
+        mv "${work_dir}/sc-${sc_version}-linux" "$sc_dir"
+    fi
 fi
 
-cd "$SC_DIR"
+cd "$sc_dir"
 rm -f sauce_connect.log
 touch sauce_connect.log
 
 i=0
 while [ "$i" -lt 3 ]; do
-  echo "Sauce Connect startup attempt ${i}"
-  i=$((i+1))
-  bin/sc \
-    --direct-domains "*.wbx2.com,*.webex.com,*.sc-tx2.huron-dev.com,*.huron-int.com" \
-    --no-ssl-bump-domains "*.atlasad.koalabait.com" \
-    -vv \
-    --logfile "sauce_connect.log" \
-    --pidfile "$PID_FILE" \
-    --readyfile "$READY_FILE" \
-    --tunnel-identifier "$SAUCE__TUNNEL_ID" \
-    > /dev/null 2>&1 \
-    &
+    echo "[INFO] Sauce Connect startup attempt ${i}"
+    i=$((i+1))
+    bin/sc \
+        --direct-domains "*.wbx2.com,*.webex.com,*.sc-tx2.huron-dev.com,*.huron-int.com" \
+        --no-ssl-bump-domains "*.atlasad.koalabait.com" \
+        -vv \
+        --logfile "sauce_connect.log" \
+        --pidfile "$sc_pid_file" \
+        --readyfile "$sc_ready_file" \
+        --tunnel-identifier "$SAUCE__TUNNEL_ID" \
+        > /dev/null 2>&1 \
+        &
 
-  echo 'Connecting to Sauce Labs - waiting a maximum 120s for ready file . . .'
-  x=0
-  while [ "$x" -lt 120 -a ! -e "$READY_FILE" ]; do
-    x=$((x+1))
-    sleep 1
-  done
+    echo '[INFO] Connecting to Sauce Labs - waiting a maximum 120s for ready file...'
+    x=0
+    while [ "$x" -lt 120 ] && [ ! -e "$sc_ready_file" ]; do
+        x=$((x+1))
+        sleep 1
+    done
 
-  if [ -e "$READY_FILE" ]; then
-    echo 'Done'
-    exit 0
-  fi
+    if [ -e "$sc_ready_file" ]; then
+        echo '[INFO] Done'
+        exit 0
+    fi
 
-  echo 'No ready file, killing sc process and trying again . . .'
-  if [ -e "$PID_FILE" ]; then
-    xargs kill -9 < "$PID_FILE"
-  fi
+    echo '[WARN] No ready file, killing sc process and trying again...'
+    if [ -e "$sc_pid_file" ]; then
+        xargs kill -9 < "$sc_pid_file"
+    fi
 done
 
-echo 'Failed to connect to Sauce Labs after 3 tries!'
+echo '[ERROR] Failed to connect to Sauce Labs after 3 tries!'
 exit 1
--- bin/stop-sauce-tunnel.sh
+++ bin/stop-sauce-tunnel.sh
@@ -3,14 +3,14 @@
 set -e
 this_pwd="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-WORK_DIR="${this_pwd}/../.sauce"
-READY_FILE="${WORK_DIR}/sc.ready"
-PID_FILE="${WORK_DIR}/sc.pid"
+work_dir="${this_pwd}/../.sauce"
+sc_ready_file="${work_dir}/sc.ready"
+sc_pid_file="${work_dir}/sc.pid"
 
 echo "[INFO] Disconnecting from Sauce Labs"
-xargs kill -TERM < "$PID_FILE"
-while [ -f "$READY_FILE" ] ;
+xargs kill -TERM < "$sc_pid_file"
+while [ -f "$sc_ready_file" ] ;
 do
   sleep 2
 done
-echo 'Done'
+echo "[INFO] Done"
