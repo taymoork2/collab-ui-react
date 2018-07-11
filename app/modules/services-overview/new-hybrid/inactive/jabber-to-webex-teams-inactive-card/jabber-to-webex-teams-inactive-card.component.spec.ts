cbdb50c7b2b093ff31de6ef6ebde884c8bf6b920
Mike Park <mipark2@cisco.com>
2018-07-11 15:08:55 -0700

    test(jabber-to-webex-teams): add unit-tests for 'jabber-to-webex-teams-inactive-card'

--- app/modules/services-overview/new-hybrid/inactive/jabber-to-webex-teams-inactive-card/jabber-to-webex-teams-inactive-card.component.spec.ts
+++ app/modules/services-overview/new-hybrid/inactive/jabber-to-webex-teams-inactive-card/jabber-to-webex-teams-inactive-card.component.spec.ts
@@ -1,30 +1,39 @@
 import moduleName from './index';
 import { JabberToWebexTeamsInactiveCardController } from './jabber-to-webex-teams-inactive-card.component';
 
-type Test = atlas.test.IComponentTest<JabberToWebexTeamsInactiveCardController, {}, {}>;
+type Test = atlas.test.IComponentTest<JabberToWebexTeamsInactiveCardController, {
+  $state: ng.ui.IStateService,
+}, {}>;
 
-// TODO (mipark2): add unit-tests
 describe('Component: jabberToWebexTeamsInactiveCard:', () => {
   beforeEach(function (this: Test) {
     this.initModules(moduleName);
     this.injectDependencies(
-      // TODO: add dependencies here
+      '$state',
     );
+    spyOn(this.$state, 'go');
   });
 
   beforeEach(function (this: Test) {
-    // this.compileComponent('jabberToWebexTeamsInactiveCard', {});
+    this.compileComponent('jabberToWebexTeamsInactiveCard', {});
   });
 
   describe('primary behaviors (view):', () => {
-    it('...', function (this: Test) {
-      // TODO: implement
+    enum View {
+      SETUP_BUTTON = '.inactive-card_footer button[translate="common.setUp"]',
+    }
+
+    it('should render a set up button', function (this: Test) {
+      expect(this.view.find(View.SETUP_BUTTON)).toExist();
     });
   });
 
   describe('primary behaviors (controller):', () => {
-    it('...', function (this: Test) {
-      // TODO: implement
+    describe('openSetUp():', () => {
+      it('should transition to the first step of the set up wizard', function (this: Test) {
+        this.controller.openSetUp();
+        expect(this.$state.go).toHaveBeenCalledWith('jabber-to-webex-teams.modal.confirm-prerequisites');
+      });
     });
   });
 });
