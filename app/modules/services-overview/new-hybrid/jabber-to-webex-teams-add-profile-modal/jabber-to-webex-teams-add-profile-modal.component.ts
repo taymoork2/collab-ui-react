295ee5a4c31eabc0fde4b3c515764bd6220bc179
Changlong Liu <changlol@cisco.com>
2018-07-20 05:27:04 +0800

    WIP(core): fix mis-used invalidation codes
    1. fix bugs and redundent invalidation codes.
    2. use enum item instead of string for radio values
    in .html

--- app/modules/services-overview/new-hybrid/jabber-to-webex-teams-add-profile-modal/jabber-to-webex-teams-add-profile-modal.component.ts
+++ app/modules/services-overview/new-hybrid/jabber-to-webex-teams-add-profile-modal/jabber-to-webex-teams-add-profile-modal.component.ts
@@ -16,13 +16,44 @@ interface IUCManagerProfileData {
 }
 
 export class JabberToWebexTeamsAddProfileModalController implements ng.IComponentController {
+  public readonly MIN_LENGTH: number = 4;
+  public readonly MAX_LENGTH: number = 128;
   public dismiss: Function;
   public errorMessages = {
     profileName: {
       required: this.$translate.instant('common.invalidRequired'),
+      minlength: this.$translate.instant('common.invalidMinLength', {
+        min: this.MIN_LENGTH,
+      }),
+      maxlength: this.$translate.instant('common.invalidMaxLength', {
+        max: this.MAX_LENGTH,
+      }),
     },
     voiceServerDomainName: {
       required: this.$translate.instant('common.invalidRequired'),
+      minlength: this.$translate.instant('common.invalidMinLength', {
+        min: this.MIN_LENGTH,
+      }),
+      maxlength: this.$translate.instant('common.invalidMaxLength', {
+        max: this.MAX_LENGTH,
+      }),
+    },
+    udsServerAddress: {
+      required: this.$translate.instant('common.invalidRequired'),
+      minlength: this.$translate.instant('common.invalidMinLength', {
+        min: this.MIN_LENGTH,
+      }),
+      maxlength: this.$translate.instant('common.invalidMaxLength', {
+        max: this.MAX_LENGTH,
+      }),
+    },
+    udsBackupServerAddress: {
+      minlength: this.$translate.instant('common.invalidMinLength', {
+        min: this.MIN_LENGTH,
+      }),
+      maxlength: this.$translate.instant('common.invalidMaxLength', {
+        max: this.MAX_LENGTH,
+      }),
     },
   };
   private profileData: IUCManagerProfileData = {
@@ -35,6 +66,7 @@ export class JabberToWebexTeamsAddProfileModalController implements ng.IComponen
   public backendType: BackendTypes = BackendTypes.NONE;
   public finishDisable: boolean = true;
   public BackendTypes = BackendTypes;
+  public addProfileForm: ng.IFormController;
 
   /* @ngInject */
   constructor(
@@ -59,6 +91,7 @@ export class JabberToWebexTeamsAddProfileModalController implements ng.IComponen
 
   private finish(): void {
     this.JabberProfileService.create(this.profileData).then(() => {
+      // TODO : 'spark14176.ucManagerProfiles' logic should be removed after the CI service is fullly coded
       const ucManagerProfiles = _.toArray(JSON.parse(this.$window.sessionStorage.getItem('spark14176.ucManagerProfiles') || '[]'));
       ucManagerProfiles.push(this.profileData);
       this.$window.sessionStorage.setItem('spark14176.ucManagerProfiles', JSON.stringify(ucManagerProfiles));
