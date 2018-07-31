53ed3ea954640bae5c2197cbade463145dab538a
Changlong Liu <changlol@cisco.com>
2018-07-31 10:18:23 +0800

    WIP(core): fix dependency changes after rebased to the main stream

--- app/modules/services-overview/new-hybrid/active/jabber-to-webex-teams-active-card/index.ts
+++ app/modules/services-overview/new-hybrid/active/jabber-to-webex-teams-active-card/index.ts
@@ -5,11 +5,13 @@ import { JabberToWebexTeamsService } from 'modules/services-overview/new-hybrid/
 import * as authInfoModuleName from 'modules/core/scripts/services/authinfo';
 import * as ngTranslateModuleName from 'angular-translate';
 import collabUiModuleName from '@collabui/collab-ui-ng';
+import * as urlConfigModuleName from 'modules/core/config/urlConfig';
 
 export default angular.module('services-overview.new-hybrid.active.jabber-to-webex-teams-active-card', [
   ngTranslateModuleName,
   collabUiModuleName,
   authInfoModuleName,
+  urlConfigModuleName,
 ])
   .component('jabberToWebexTeamsActiveCard', new JabberToWebexTeamsActiveCardComponent())
   .service('JabberToWebexTeamsService', JabberToWebexTeamsService)
