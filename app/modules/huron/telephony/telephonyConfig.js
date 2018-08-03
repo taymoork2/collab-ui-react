1147c15311c8f5aac19a9fd30fa5e24eb66615bb
Sam Williams <samwi@cisco.com>
2018-08-03 13:33:28 -0500

    fix(bsft): new https server

--- app/modules/huron/telephony/telephonyConfig.js
+++ app/modules/huron/telephony/telephonyConfig.js
@@ -70,7 +70,7 @@
       },
 
       getRialtoUrl: function () {
-        return 'http://172.24.77.211:8000/enterpriseportalapi/v1.0';
+        return 'https://flexdev-rep.ums.broadsoft.net/enterpriseportalapi/v1.0';
       },
 
       getUpgradeUrl: function () { //TBD-- add the devhost:port for local testing
